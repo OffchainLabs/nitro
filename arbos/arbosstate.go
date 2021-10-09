99fd535ef2fb437824a0b159b30f1adfaab9cf52
Harry Kalodner <harry@offchainlabs.com>
2021-10-09 00:17:36 -0400

    Split out storage segment

--- arbos/arbosstate.go
+++ arbos/arbosstate.go
@@ -211,15 +211,6 @@ func (state *ArbosState) SetLastTimestampSeen(val *big.Int) {
 	state.backingStorage.Set(lastTimestampKey, common.BigToHash(val))
 }
 
-
-type StorageSegment struct {
-	offset      common.Hash
-	size        uint64
-	storage     *ArbosState
-}
-
-const MaxSizedSegmentSize = 1<<48
-
 func (state *ArbosState) AllocateSegment(size uint64) (*StorageSegment, error) {
 	if size > MaxSizedSegmentSize {
 		return nil, errors.New("requested segment size too large")
@@ -232,7 +223,7 @@ func (state *ArbosState) AllocateSegment(size uint64) (*StorageSegment, error) {
 	return &StorageSegment{
 		*offset,
 		size,
-		state,
+		state.backingStorage,
 	}, nil
 }
 
@@ -252,38 +243,8 @@ func (state *ArbosState) OpenSegment(offset common.Hash) *StorageSegment {
 	return &StorageSegment{
 		offset,
 		size,
-		state,
+		state.backingStorage,
 	}
-}
-
-func (seg *StorageSegment) Get(offset uint64) common.Hash {
-	if offset >= seg.size {
-		panic("out of bounds access to storage segment")
-	}
-	return seg.storage.backingStorage.Get(hashPlusInt(seg.offset, int64(1+offset)))
-}
-
-func (seg *StorageSegment) GetAsInt64(offset uint64) int64 {
-	raw := seg.Get(offset).Big()
-	if ! raw.IsInt64() {
-		panic("out of range")
-	}
-	return raw.Int64()
-}
-
-func (seg *StorageSegment) GetAsUint64(offset uint64) uint64 {
-	raw := seg.Get(offset).Big()
-	if ! raw.IsUint64() {
-		panic("out of range")
-	}
-	return raw.Uint64()
-}
-
-func (seg *StorageSegment) Set(offset uint64, value common.Hash) {
-	if offset >= seg.size {
-		panic("offset too large in StorageSegment::Set")
-	}
-	seg.storage.backingStorage.Set(hashPlusInt(seg.offset, int64(offset+1)), value)
 }
 
 func (state *ArbosState) AllocateSegmentForBytes(buf []byte) *StorageSegment {
@@ -315,27 +276,4 @@ func (state *ArbosState) AdvanceTimestampToAtLeast(newTimestamp *big.Int) {
 		state.SetLastTimestampSeen(newTimestamp)
 	}
 }
-
-func (seg *StorageSegment) GetBytes() []byte {
-	rawSize := seg.Get(0)
-
-	if ! rawSize.Big().IsUint64() {
-		panic("invalid segment size")
-	}
-	size := rawSize.Big().Uint64()
-	sizeWords := (size+31) / 32
-	buf := make([]byte, 32*sizeWords)
-	for i := uint64(0); i < sizeWords; i++ {
-		iterBuf := seg.Get(i+1).Bytes()
-		for j, b := range iterBuf {
-			buf[32*i+uint64(j)] = b
-		}
-	}
-	return buf[:size]
-}
-
-func (seg *StorageSegment) Equals(other *StorageSegment) bool {
-	return seg.offset == other.offset
-}
-
 
---
+++ arbos/storageSegment.go
@@ -0,0 +1,63 @@
+package arbos
+
+import "github.com/ethereum/go-ethereum/common"
+
+type StorageSegment struct {
+	offset      common.Hash
+	size        uint64
+	storage     EvmStorage
+}
+
+const MaxSizedSegmentSize = 1<<48
+
+func (seg *StorageSegment) Get(offset uint64) common.Hash {
+	if offset >= seg.size {
+		panic("out of bounds access to storage segment")
+	}
+	return seg.storage.Get(hashPlusInt(seg.offset, int64(1+offset)))
+}
+
+func (seg *StorageSegment) GetAsInt64(offset uint64) int64 {
+	raw := seg.Get(offset).Big()
+	if ! raw.IsInt64() {
+		panic("out of range")
+	}
+	return raw.Int64()
+}
+
+func (seg *StorageSegment) GetAsUint64(offset uint64) uint64 {
+	raw := seg.Get(offset).Big()
+	if ! raw.IsUint64() {
+		panic("out of range")
+	}
+	return raw.Uint64()
+}
+
+func (seg *StorageSegment) Set(offset uint64, value common.Hash) {
+	if offset >= seg.size {
+		panic("offset too large in StorageSegment::Set")
+	}
+	seg.storage.Set(hashPlusInt(seg.offset, int64(offset+1)), value)
+}
+
+func (seg *StorageSegment) GetBytes() []byte {
+	rawSize := seg.Get(0)
+
+	if ! rawSize.Big().IsUint64() {
+		panic("invalid segment size")
+	}
+	size := rawSize.Big().Uint64()
+	sizeWords := (size+31) / 32
+	buf := make([]byte, 32*sizeWords)
+	for i := uint64(0); i < sizeWords; i++ {
+		iterBuf := seg.Get(i+1).Bytes()
+		for j, b := range iterBuf {
+			buf[32*i+uint64(j)] = b
+		}
+	}
+	return buf[:size]
+}
+
+func (seg *StorageSegment) Equals(other *StorageSegment) bool {
+	return seg.offset == other.offset
+}
