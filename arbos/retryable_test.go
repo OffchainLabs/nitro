98707f8162122de44362a4e86c2179291ccc0282
Ed Felten <ed@felten.com>
2021-10-14 15:49:30 -0400

    Add test

--- arbos/retryable_test.go
+++ arbos/retryable_test.go
@@ -70,3 +70,67 @@ func TestRetryableCreate(t *testing.T) {
 		t.Fatal()
 	}
 }
+
+func TestPlanOneRedeem(t *testing.T) {
+	state := OpenArbosStateForTest()
+	id := common.BigToHash(big.NewInt(978645611142))
+	timeout := new(big.Int).Add(state.LastTimestampSeen(), big.NewInt(10000000))
+	from := common.BytesToAddress([]byte{3, 4, 5})
+	to := common.BytesToAddress([]byte{6, 7, 8, 9})
+	callvalue := big.NewInt(0)
+	calldata := []byte{42}
+	_ = CreateRetryable(state, id, timeout, from, to, callvalue, calldata)
+
+	refundAddr := common.Address{ 3, 4 }
+	gas := uint64(100983)
+	gasPrice := big.NewInt(486687768)
+	redeem := NewPlannedRedeem(state, id, refundAddr, gas, gasPrice)
+
+	readBack := PeekNextPlannedRedeem(state)
+	if readBack == nil {
+		t.Fatal()
+	}
+	if redeem.retryableId != readBack.retryableId {
+		t.Fatal()
+	}
+	if redeem.gasRefundAddr != readBack.gasRefundAddr {
+		t.Fatal()
+	}
+	if redeem.gas != readBack.gas {
+		t.Fatal()
+	}
+	if redeem.gasPrice.Cmp(readBack.gasPrice) != 0 {
+		t.Fatal()
+	}
+
+	// read back again, verify we got the same thing
+	readBack = PeekNextPlannedRedeem(state)
+	if readBack == nil {
+		t.Fatal()
+	}
+	if redeem.retryableId != readBack.retryableId {
+		t.Fatal()
+	}
+	if redeem.gasRefundAddr != readBack.gasRefundAddr {
+		t.Fatal()
+	}
+	if redeem.gas != readBack.gas {
+		t.Fatal()
+	}
+	if redeem.gasPrice.Cmp(readBack.gasPrice) != 0 {
+		t.Fatal()
+	}
+
+	// discard the redeem without deleting the retryable, then make sure retryable is still there
+	DiscardNextPlannedRedeem(state, false)
+	if OpenRetryable(state, id) == nil {
+		t.Fatal()
+	}
+
+	// make a new redeem, discard it with delete, then make sure the retryable is gone
+	_ = NewPlannedRedeem(state, id, refundAddr, gas, gasPrice)
+	DiscardNextPlannedRedeem(state, true)
+	if OpenRetryable(state, id) != nil {
+		t.Fatal()
+	}
+}
