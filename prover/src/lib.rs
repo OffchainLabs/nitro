6051a0790104c1d6685ac6592c0d66487d784ba8
Tristan Wilson <twilson@offchainlabs.com>
2021-10-22 22:09:24 -0700

    Formatting and whitespace

--- prover/src/lib.rs
+++ prover/src/lib.rs
@@ -9,8 +9,14 @@ mod value;
 pub mod wavm;
 
 use crate::{binary::WasmBinary, machine::Machine};
-use eyre::{Result, bail};
-use std::{ffi::{CStr, CString}, fs::File, io::Read, os::raw::c_char, path::Path};
+use eyre::{bail, Result};
+use std::{
+    ffi::{CStr, CString},
+    fs::File,
+    io::Read,
+    os::raw::c_char,
+    path::Path,
+};
 
 pub fn parse_binary(path: &Path) -> Result<WasmBinary> {
     let mut f = File::open(path)?;
@@ -32,38 +38,53 @@ pub fn parse_binary(path: &Path) -> Result<WasmBinary> {
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn arbitrator_load_machine(binary_path: *const c_char, library_paths: *const *const c_char, library_paths_size: isize, always_merkelize: bool) -> *mut Machine {
-	match arbitrator_load_machine_impl(binary_path, library_paths, library_paths_size, always_merkelize) {
-		Ok(mach) => {mach}
-		Err(err) => {
-			eprintln!("Error loading binary: {}", err);
-			std::ptr::null_mut()
-		}
-	}
+pub unsafe extern "C" fn arbitrator_load_machine(
+    binary_path: *const c_char,
+    library_paths: *const *const c_char,
+    library_paths_size: isize,
+    always_merkelize: bool,
+) -> *mut Machine {
+    match arbitrator_load_machine_impl(
+        binary_path,
+        library_paths,
+        library_paths_size,
+        always_merkelize,
+    ) {
+        Ok(mach) => mach,
+        Err(err) => {
+            eprintln!("Error loading binary: {}", err);
+            std::ptr::null_mut()
+        }
+    }
 }
 
-unsafe fn arbitrator_load_machine_impl(binary_path: *const c_char, library_paths: *const *const c_char, library_paths_size: isize, always_merkelize: bool) -> Result<*mut Machine> {
-	let main_mod = {
-		let binary_path = cstr_to_string(binary_path);
-		let binary_path = Path::new(&binary_path);
-		parse_binary(binary_path)?
-	};
+unsafe fn arbitrator_load_machine_impl(
+    binary_path: *const c_char,
+    library_paths: *const *const c_char,
+    library_paths_size: isize,
+    always_merkelize: bool,
+) -> Result<*mut Machine> {
+    let main_mod = {
+        let binary_path = cstr_to_string(binary_path);
+        let binary_path = Path::new(&binary_path);
+        parse_binary(binary_path)?
+    };
 
-	let mut i: isize= 0;
-	let mut libraries = Vec::new();
-	while i < library_paths_size {
-		let library_path = cstr_to_string(*(library_paths.offset(i)));
-		let library_path = Path::new(&library_path);
-		libraries.push(parse_binary(library_path)?);
-		i += 1;
-	}
-	
+    let mut i: isize = 0;
+    let mut libraries = Vec::new();
+    while i < library_paths_size {
+        let library_path = cstr_to_string(*(library_paths.offset(i)));
+        let library_path = Path::new(&library_path);
+        libraries.push(parse_binary(library_path)?);
+        i += 1;
+    }
+
     let mach = Machine::from_binary(libraries, main_mod, always_merkelize);
-	Ok(Box::into_raw(Box::new(mach)))
+    Ok(Box::into_raw(Box::new(mach)))
 }
 
 unsafe fn cstr_to_string(c_str: *const c_char) -> String {
-	CStr::from_ptr(c_str).to_string_lossy().into_owned()
+    CStr::from_ptr(c_str).to_string_lossy().into_owned()
 }
 
 #[no_mangle]
@@ -79,25 +100,27 @@ pub unsafe extern "C" fn arbitrator_clone_machine(mach: *mut Machine) -> *mut Ma
 
 #[no_mangle]
 pub unsafe extern "C" fn arbitrator_step(mach: *mut Machine, num_steps: isize) {
-	let mut i: isize = 0;
-	while i < num_steps {
-		(*mach).step();
-		i += 1;
-	}
+    let mut i: isize = 0;
+    while i < num_steps {
+        (*mach).step();
+        i += 1;
+    }
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn arbitrator_hash(mach: *mut Machine) -> utils::Bytes32 {
-	(*mach).hash()
+    (*mach).hash()
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn arbitrator_gen_proof(mach: *mut Machine) -> *mut c_char {
-	let proof = (*mach).serialize_proof();
-	CString::new(hex::encode(proof)).expect("CString new failed").into_raw()
+    let proof = (*mach).serialize_proof();
+    CString::new(hex::encode(proof))
+        .expect("CString new failed")
+        .into_raw()
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn arbitrator_free_proof(proof : *mut c_char) {
-	CString::from_raw(proof);
+pub unsafe extern "C" fn arbitrator_free_proof(proof: *mut c_char) {
+    CString::from_raw(proof);
 }
