1075cd20c83be61baa2d5d1564f6cde0cd3e68ca
Ed Felten <ed@offchainlabs.com>
2021-10-25 19:41:54 -0400

    Add chain owner set

---
+++ arbos/addressSet/addressSet.go
@@ -0,0 +1,85 @@
+//
+// Copyright 2021, Offchain Labs, Inc. All rights reserved.
+//
+
+package addressSet
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/offchainlabs/arbstate/arbos/storage"
+	"github.com/offchainlabs/arbstate/arbos/util"
+)
+
+type AddressSet struct {
+	backingStorage *storage.Storage
+	size           uint64
+	cachedMembers  map[common.Address]struct{}
+	byAddress      *storage.Storage
+}
+
+func Initialize(sto *storage.Storage) {
+	sto.SetByInt64(0, util.IntToHash(0))
+}
+
+func OpenAddressSet(sto *storage.Storage) *AddressSet {
+	return &AddressSet{
+		sto,
+		sto.GetByInt64(0).Big().Uint64(),
+		make(map[common.Address]struct{}),
+		sto.OpenSubStorage([]byte{0}),
+	}
+}
+
+func (aset *AddressSet) Size() uint64 {
+	return aset.size
+}
+
+func (aset *AddressSet) IsMember(addr common.Address) bool {
+	if _, cached := aset.cachedMembers[addr]; cached {
+		return true
+	}
+	if aset.byAddress.Get(common.BytesToHash(addr.Bytes())) != (common.Hash{}) {
+		aset.cachedMembers[addr] = struct{}{}
+		return true
+	}
+	return false
+}
+
+func (aset *AddressSet) AllMembers() []common.Address {
+	ret := make([]common.Address, aset.size)
+	for i, _ := range ret {
+		ret[i] = common.BytesToAddress(aset.backingStorage.GetByInt64(int64(i+1)).Bytes())
+	}
+	return ret
+}
+
+func (aset *AddressSet) Add(addr common.Address) {
+	if aset.IsMember(addr) {
+		return
+	}
+	slot := util.IntToHash(int64(1 + aset.size))
+	addrAsHash := common.BytesToHash(addr.Bytes())
+	aset.byAddress.Set(addrAsHash, slot)
+	aset.backingStorage.Set(slot, addrAsHash)
+	aset.size++
+	aset.backingStorage.SetByInt64(0, util.IntToHash(int64(aset.size)))
+}
+
+func (aset *AddressSet) Remove(addr common.Address) {
+	addrAsHash := common.BytesToHash(addr.Bytes())
+	slot := aset.byAddress.Get(addrAsHash).Big().Uint64()
+	if slot == 0 {
+		return
+	}
+	delete(aset.cachedMembers, addr)
+	aset.byAddress.Set(addrAsHash, common.Hash{})
+	if slot < aset.size {
+		aset.backingStorage.SetByInt64(int64(slot), aset.backingStorage.GetByInt64(int64(aset.size)))
+	}
+	aset.backingStorage.SetByInt64(int64(aset.size), common.Hash{})
+	aset.size--
+	aset.backingStorage.SetByInt64(0, util.IntToHash(int64(aset.size)))
+}
+
+
+
---
+++ arbos/addressSet/addressSet_test.go
@@ -0,0 +1,99 @@
+//
+// Copyright 2021, Offchain Labs, Inc. All rights reserved.
+//
+
+package addressSet
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/offchainlabs/arbstate/arbos/storage"
+	"testing"
+)
+
+func TestEmptyAddressSet(t *testing.T) {
+	sto := storage.NewMemoryBacked()
+	Initialize(sto)
+	aset := OpenAddressSet(sto)
+
+	if aset.Size() != 0 {
+		t.Fatal()
+	}
+	if aset.IsMember(common.Address{}) {
+		t.Fatal()
+	}
+	aset.Remove(common.Address{})
+	if aset.Size() != 0 {
+		t.Fatal()
+	}
+	if aset.IsMember(common.Address{}) {
+		t.Fatal()
+	}
+}
+
+func TestAddressSet(t *testing.T) {
+	sto := storage.NewMemoryBacked()
+	Initialize(sto)
+	aset := OpenAddressSet(sto)
+
+	addr1 := common.BytesToAddress(crypto.Keccak256([]byte{1})[:20])
+	addr2 := common.BytesToAddress(crypto.Keccak256([]byte{2})[:20])
+	addr3 := common.BytesToAddress(crypto.Keccak256([]byte{3})[:20])
+
+	aset.Add(addr1)
+	if aset.Size() != 1 {
+		t.Fatal()
+	}
+	aset.Add(addr2)
+	if aset.Size() != 2 {
+		t.Fatal()
+	}
+	aset.Add(addr1)
+	if aset.Size() != 2 {
+		t.Fatal()
+	}
+	if !aset.IsMember(addr1) {
+		t.Fatal()
+	}
+	if !aset.IsMember(addr2) {
+		t.Fatal()
+	}
+	if aset.IsMember(addr3) {
+		t.Fatal()
+	}
+
+	aset.Remove(addr1)
+	if aset.Size() != 1 {
+		t.Fatal()
+	}
+	if aset.IsMember(addr1) {
+		t.Fatal()
+	}
+	if !aset.IsMember(addr2) {
+		t.Fatal()
+	}
+
+	aset.Add(addr3)
+	if aset.Size() != 2 {
+		t.Fatal()
+	}
+	aset.Remove(addr3)
+	if aset.Size() != 1 {
+		t.Fatal()
+	}
+
+	aset.Add(addr1)
+	all := aset.AllMembers()
+	if len(all) != 2 {
+		t.Fatal()
+	}
+	if all[0] == addr1 {
+		if all[1] != addr2 {
+			t.Fatal()
+		}
+	} else {
+		if (all[0] != addr2) || (all[1] != addr1) {
+			t.Fatal()
+		}
+	}
+}
