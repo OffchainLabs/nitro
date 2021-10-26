0f6839b137ea59f599a1754254973983c675f1c9
Ed Felten <ed@offchainlabs.com>
2021-10-25 20:12:12 -0400

    Implement ArbOwner precompile

--- precompiles/ArbOwner.go
+++ precompiles/ArbOwner.go
@@ -7,40 +7,54 @@ package precompiles
 import (
 	"errors"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/params"
+	"github.com/offchainlabs/arbstate/arbos"
 )
 
 type ArbOwner struct {
 	Address addr
 }
 
+var UnauthorizedError = errors.New("unauthorized caller to access-controlled method")
+
 func (con ArbOwner) AddChainOwner(caller addr, evm mech, newOwner addr) error {
-	return errors.New("unimplemented")
+	owners := arbos.OpenArbosState(evm.StateDB).ChainOwners()
+	if !owners.IsMember(caller) {
+		return UnauthorizedError
+	}
+	owners.Add(newOwner)
+	return nil
 }
 
 func (con ArbOwner) AddChainOwnerGasCost(newOwner addr) uint64 {
-	return 0
+	return 3 * params.SstoreSetGas
 }
 
 func (con ArbOwner) GetAllChainOwners(caller addr, evm mech) ([]common.Address, error) {
-	return nil, errors.New("unimplemented")
+	return arbos.OpenArbosState(evm.StateDB).ChainOwners().AllMembers(), nil
 }
 
 func (con ArbOwner) GetAllChainOwnersGasCost() uint64 {
-	return 0
+	return 5 * params.SstoreSetGas
 }
 
 func (con ArbOwner) IsChainOwner(caller addr, evm mech, addr addr) (bool, error) {
-	return false, errors.New("unimplemented")
+	return arbos.OpenArbosState(evm.StateDB).ChainOwners().IsMember(addr), nil
 }
 
 func (con ArbOwner) IsChainOwnerGasCost(addr addr) uint64 {
-	return 0
+	return 3 * params.SstoreSetGas
 }
 
 func (con ArbOwner) RemoveChainOwner(caller addr, evm mech, addr addr) error {
-	return errors.New("unimplemented")
+	owners := arbos.OpenArbosState(evm.StateDB).ChainOwners()
+	if !owners.IsMember(caller) {
+		return UnauthorizedError
+	}
+	owners.Remove(addr)
+	return nil
 }
 
 func (con ArbOwner) RemoveChainOwnerGasCost(addr addr) uint64 {
-	return 0
+	return 3 * params.SstoreSetGas
 }
