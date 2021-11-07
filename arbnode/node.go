a9e9ec749ea9705e6ada2949b6aae831e5aad47b
Tsahi Zidenberg <tsahi@offchainlabs.com>
2021-11-07 13:05:16 +0200

    move utils to own file

--- arbnode/node.go
+++ arbnode/node.go
@@ -11,7 +11,6 @@ import (
 	"math/big"
 	"time"
 
-	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/arbitrum"
 	"github.com/ethereum/go-ethereum/cmd/utils"
@@ -26,72 +25,6 @@ import (
 	"github.com/offchainlabs/arbstate/arbstate"
 	"github.com/offchainlabs/arbstate/solgen/go/bridgegen"
 )
-
-type L1Interface interface {
-	bind.ContractBackend
-	ethereum.ChainReader
-	ethereum.TransactionReader
-	TransactionSender(ctx context.Context, tx *types.Transaction, block common.Hash, index uint) (common.Address, error)
-}
-
-// will wait untill tx is in the blockchain. attempts = 0 is infinite
-func WaitForTx(client L1Interface, txhash common.Hash, timeout time.Duration) (*types.Receipt, error) {
-	ctx := context.Background()
-	chanHead := make(chan *types.Header, 20)
-	headSubscribe, err := client.SubscribeNewHead(ctx, chanHead)
-	if err != nil {
-		return nil, err
-	}
-	defer headSubscribe.Unsubscribe()
-
-	chTimeout := time.After(timeout)
-	for {
-		reciept, err := client.TransactionReceipt(ctx, txhash)
-		if reciept != nil {
-			return reciept, err
-		}
-		select {
-		case <-chanHead:
-		case <-chTimeout:
-			return nil, errors.New("timeout waiting for transaction")
-		}
-	}
-}
-
-func EnsureTxSucceeded(client L1Interface, tx *types.Transaction) (*types.Receipt, error) {
-	txRes, err := WaitForTx(client, tx.Hash(), time.Second)
-	if err != nil {
-		return nil, err
-	}
-	if txRes == nil {
-		return nil, errors.New("expected receipt")
-	}
-	if txRes.Status != types.ReceiptStatusSuccessful {
-		// Re-execute the transaction as a call to get a better error
-		ctx := context.TODO()
-		from, err := client.TransactionSender(ctx, tx, txRes.BlockHash, txRes.TransactionIndex)
-		if err != nil {
-			return nil, err
-		}
-		callMsg := ethereum.CallMsg{
-			From:       from,
-			To:         tx.To(),
-			Gas:        tx.Gas(),
-			GasPrice:   tx.GasPrice(),
-			GasFeeCap:  tx.GasFeeCap(),
-			GasTipCap:  tx.GasTipCap(),
-			Value:      tx.Value(),
-			Data:       tx.Data(),
-			AccessList: tx.AccessList(),
-		}
-		_, err = client.CallContract(ctx, callMsg, txRes.BlockNumber)
-		if err != nil {
-			return nil, err
-		}
-		return nil, errors.New("tx failed but call succeeded")
-	}
-	return txRes, nil
-}
 
 type RollupAddresses struct {
 	Bridge         common.Address
