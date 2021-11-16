b6e55328c4f873969121e15fbfc64d0c558c904a
Tsahi Zidenberg <tsahi@offchainlabs.com>
2021-11-16 12:09:54 +0200

    move build_block into cmd/replay
    That's the only command using it

--- cmd/replay/main.go
+++ cmd/replay/main.go
@@ -9,9 +9,11 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/consensus"
+	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/rawdb"
 	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/offchainlabs/arbstate/arbos"
 	"github.com/offchainlabs/arbstate/arbstate"
@@ -68,6 +70,44 @@ func (i WavmInbox) ReadDelayedInbox(seqNum uint64) ([]byte, error) {
 	return wavmio.ReadDelayedInboxMessage(seqNum), nil
 }
 
+func BuildBlock(statedb *state.StateDB, lastBlockHeader *types.Header, chainContext core.ChainContext, inbox arbstate.InboxBackend) (*types.Block, error) {
+	var delayedMessagesRead uint64
+	if lastBlockHeader != nil {
+		delayedMessagesRead = lastBlockHeader.Nonce.Uint64()
+	}
+	inboxMultiplexer := arbstate.NewInboxMultiplexer(inbox, delayedMessagesRead)
+	blockBuilder := arbos.NewBlockBuilder(statedb, lastBlockHeader, chainContext)
+	for {
+		message, err := inboxMultiplexer.Peek()
+		if err != nil {
+			return nil, err
+		}
+		segment, err := arbos.IncomingMessageToSegment(message.Message, arbos.ChainConfig.ChainID)
+		if err != nil {
+			log.Warn("error parsing incoming message", "err", err)
+			err = inboxMultiplexer.Advance()
+			if err != nil {
+				return nil, err
+			}
+			break
+		}
+		// Always passes if the block is empty
+		if !blockBuilder.ShouldAddMessage(segment) {
+			break
+		}
+		err = inboxMultiplexer.Advance()
+		if err != nil {
+			return nil, err
+		}
+		blockBuilder.AddMessage(segment)
+		if message.MustEndBlock {
+			break
+		}
+	}
+	block, _, _ := blockBuilder.ConstructBlock(inboxMultiplexer.DelayedMessagesRead())
+	return block, nil
+}
+
 func main() {
 	raw := rawdb.NewDatabase(PreimageDb{})
 	db := state.NewDatabase(raw)
@@ -88,7 +128,7 @@ func main() {
 	}
 
 	chainContext := WavmChainContext{}
-	newBlock, err := arbstate.BuildBlock(statedb, lastBlockHeader, chainContext, WavmInbox{})
+	newBlock, err := BuildBlock(statedb, lastBlockHeader, chainContext, WavmInbox{})
 	if err != nil {
 		panic(fmt.Sprintf("Error building block: %v", err.Error()))
 	}
