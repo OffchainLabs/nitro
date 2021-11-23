a30d374db14a4975d59891dfdf5c7b72b263414e
Tsahi Zidenberg <tsahi@offchainlabs.com>
2021-11-23 22:06:06 +0200

    host-io test: restructure

--- prover/test-cases/rust/src/bin/host-io.rs
+++ prover/test-cases/rust/src/bin/host-io.rs
@@ -11,42 +11,47 @@ extern "C" {
 const BYTES32_VALS_NUM: usize = 1;
 const U64_VALS_NUM: usize = 2;
 
+#[repr(C, align(256))]
+struct Bytes32([u8; 32]);
+
 fn main() {
-    for i in 0..BYTES32_VALS_NUM {
-        let mut bytebuffer = [0x0 as u8; 32];
-        for j in 0..32 {
-            bytebuffer[j] = (j + i*0x10) as u8;
+    println!("hello!");
+    unsafe {
+        for i in 0..U64_VALS_NUM {
+            println!("set_u64 nr. {}", i);
+            wavm_set_globalstate_u64(i as u32, (i as u64) * 0x100 + 1);
         }
-        unsafe {
-            wavm_set_globalstate_bytes32(i as u32, bytebuffer.as_ptr());
+        for i in 0..BYTES32_VALS_NUM {
+            let mut bytebuffer = Bytes32([0x0; 32]);
+            for j in 0..32 {
+                bytebuffer.0[j] = (j + i*0x10) as u8;
+            }
+            println!("set_bytes32 nr. {}", i);
+            wavm_set_globalstate_bytes32(i as u32, bytebuffer.0.as_ptr());
         }
-    }
-    for i in 0..U64_VALS_NUM {
-        unsafe {
-            wavm_set_globalstate_u64(i as u32, (i as u64) * 0x100);
+        for i in 0..U64_VALS_NUM {
+            println!("get_u64 nr. {}", i);
+            let val: u64;
+            val = wavm_get_globalstate_u64(i as u32);
+            let exp = (i as u64) * 0x100 + 1;
+            if val != exp {
+                panic!("globalstate u64 val {} expected {} got {}", i, exp, val)
+            }
         }
-    }
-    for i in 0..BYTES32_VALS_NUM {
-        let mut bytebuffer = [0xff as u8; 32];
-        unsafe {
-            wavm_get_globalstate_bytes32(i as u32, bytebuffer.as_mut_ptr());
-        }
-        for j in 0..32 {
-            if bytebuffer[j] != (j + i*0x10) as u8 {
-                panic!("globalstate bytes32 idx {} byte {} expected {} got{}",
-                        i, j, j + i*0x10, bytebuffer[j])
+        for i in 0..BYTES32_VALS_NUM {
+            let mut bytebuffer = Bytes32([0xff; 32]);
+            println!("get_bytes32 nr. {}", i);
+            wavm_get_globalstate_bytes32(i as u32, bytebuffer.0.as_mut_ptr());
+            let localarray: [u8; 32] = bytebuffer.0;
+            for j in 0..32 {
+                let foundval = localarray[j];
+                println!("byte {} found {}", j, foundval);
+                if foundval != (j + i*0x10) as u8 {
+                    panic!("globalstate bytes32 idx {} byte {} expected {} got{}",
+                            i, j, j + i*0x10, foundval)
+                }
             }
         }
     }
-    for i in 0..U64_VALS_NUM {
-        let val: u64;
-        unsafe {
-            val = wavm_get_globalstate_u64(i as u32);
-        }
-        let exp = (i as u64) * 0x100;
-        if val != exp {
-            panic!("globalstate u64 val {} expected {} got {}", i, exp, val)
-        }
-    }
-
+    println!("Done!");
 }
