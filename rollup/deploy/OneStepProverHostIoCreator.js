7e466b2cb736a377306e4911e83b596a3e4d1432
Tsahi Zidenberg <tsahi@offchainlabs.com>
2021-11-28 19:58:36 +0200

    tests: deploy bridge contracts for hostio proofs

--- rollup/deploy/OneStepProverHostIoCreator.js
+++ rollup/deploy/OneStepProverHostIoCreator.js
@@ -1,11 +1,24 @@
 module.exports = async (hre) => {
-  const { deployments, getNamedAccounts } = hre;
+  const { deployments, getNamedAccounts, ethers } = hre;
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
+  await deploy("Bridge", {from: deployer, args: []})
+  await deploy("Inbox", {from: deployer, args: []})
+
+  bridge = await ethers.getContract("Bridge", deployer)
+  inbox = await ethers.getContract("Inbox", deployer)
+
+  await deploy("SequencerInbox", {from: deployer, args: [bridge.address, deployer]})
+
+  await bridge.setInbox(inbox.address, true);
+  await inbox.initialize(bridge.address);
+
+  seqInbox = await ethers.getContract("SequencerInbox", deployer)
+
   await deploy("OneStepProverHostIo", {
     from: deployer,
-    args: ["0x0000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000"],
+    args: [seqInbox.address, bridge.address],
   });
 };
 
