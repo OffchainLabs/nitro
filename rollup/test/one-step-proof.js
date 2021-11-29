fb46d441ae1357c5978fdd33111ffd99f44683b2
Tsahi Zidenberg <tsahi@offchainlabs.com>
2021-11-29 17:39:32 +0200

    one step proof test: send test messages

--- rollup/test/one-step-proof.js
+++ rollup/test/one-step-proof.js
@@ -1,13 +1,30 @@
-const { ethers, run } = require("hardhat");
+const { ethers, run, getNamedAccounts } = require("hardhat");
 const fs = require("fs");
 const assert = require("assert");
 
 const PARALLEL = 128;
 
+async function sendTestMessages(deployment) {
+  await deployment;
+  const { deployer } = await getNamedAccounts();
+  inbox = await ethers.getContract("Inbox", deployer);
+  seqInbox = await ethers.getContract("SequencerInbox", deployer);
+  const msgRoot = "../prover/test-cases/rust/messages/"
+  for (let msgNum = 0;  msgNum < 2; msgNum++) {
+    let path = msgRoot + "msg" + String(msgNum) +".bin"
+    buf = fs.readFileSync(path);
+    inbox.sendL2MessageFromOrigin(buf);
+    seqInbox.addSequencerL2BatchFromOrigin(msgNum, buf, "0", "0");
+  } 
+}
+
 describe("OneStepProof", function () {
   const deployment = run("deploy", { "tags": "OneStepProofEntry" });
   const root = "./test/proofs/";
   const dir = fs.readdirSync(root);
+
+  sendTestMessagesPromise = sendTestMessages(deployment);
+
   for (let file of dir) {
     if (!file.endsWith(".json")) continue;
     it("Should pass " + file + " proofs", async function () {
@@ -17,6 +34,8 @@ describe("OneStepProof", function () {
       let proofs = JSON.parse(fs.readFileSync(path));
 
       const osp = await ethers.getContract("OneStepProofEntry");
+
+      await sendTestMessagesPromise;
 
       const promises = [];
       for (let i = 0; i < proofs.length; i++) {
