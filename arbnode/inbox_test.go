7bd28a5a8568438365f76b4a25fb8dd51cfeee08
Rachel Franks <nfranks@protonmail.com>
2021-12-14 15:39:40 -0600

    remove empty test

--- arbnode/inbox_test.go
+++ arbnode/inbox_test.go
@@ -16,10 +16,55 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/math"
+	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/core/rawdb"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/offchainlabs/arbstate/arbos"
 	"github.com/offchainlabs/arbstate/arbstate"
 )
+
+func NewTransactionStreamerForTest(t *testing.T, ownerAddress common.Address) (*TransactionStreamer, *core.BlockChain) {
+	rewrittenOwnerAddress := util.RemapL1Address(ownerAddress)
+
+	genesisAlloc := make(map[common.Address]core.GenesisAccount)
+	genesisAlloc[rewrittenOwnerAddress] = core.GenesisAccount{
+		Balance:    big.NewInt(params.Ether),
+		Nonce:      0,
+		PrivateKey: nil,
+	}
+	genesis := &core.Genesis{
+		Config:     arbos.ChainConfig,
+		Nonce:      0,
+		Timestamp:  1633932474,
+		ExtraData:  []byte("ArbitrumTest"),
+		GasLimit:   0,
+		Difficulty: big.NewInt(1),
+		Mixhash:    common.Hash{},
+		Coinbase:   common.Address{},
+		Alloc:      genesisAlloc,
+		Number:     0,
+		GasUsed:    0,
+		ParentHash: common.Hash{},
+		BaseFee:    big.NewInt(0),
+	}
+
+	db := rawdb.NewMemoryDatabase()
+	genesis.MustCommit(db)
+	shouldPreserve := func(_ *types.Block) bool { return false }
+	bc, err := core.NewBlockChain(db, nil, arbos.ChainConfig, arbos.Engine{}, vm.Config{}, shouldPreserve, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	inbox, err := NewTransactionStreamer(db, bc)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return inbox, bc
+}
 
 type blockTestState struct {
 	balances    map[common.Address]uint64
