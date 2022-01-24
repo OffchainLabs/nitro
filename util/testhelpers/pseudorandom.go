4cda6a7e66f55f1761c3f5131bb8c6478684f6c5
Tsahi Zidenberg <tsahi@offchainlabs.com>
2022-01-24 19:16:14 +0200

    pseudorandom: use go rand

--- util/testhelpers/pseudorandom.go
+++ util/testhelpers/pseudorandom.go
@@ -5,32 +5,28 @@
 package testhelpers
 
 import (
-	"encoding/binary"
-	"math/big"
+	"math/rand"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
 )
 
 type PseudoRandomDataSource struct {
-	salt  common.Hash
-	index int64
+	rand *rand.Rand
 }
 
 // pseudorandom source that repeats on different executions
 // T param is to make sure it's only used in testing
-func NewPseudoRandomDataSource(_ *testing.T, saltParam int) *PseudoRandomDataSource {
-	salt := crypto.Keccak256Hash([]byte{'s'}, common.BigToHash(big.NewInt(int64(saltParam))).Bytes())
+func NewPseudoRandomDataSource(_ *testing.T, seed int64) *PseudoRandomDataSource {
 	return &PseudoRandomDataSource{
-		salt:  salt,
-		index: 0,
+		rand: rand.New(rand.NewSource(seed)),
 	}
 }
 
 func (r *PseudoRandomDataSource) GetHash() common.Hash {
-	r.index++
-	return crypto.Keccak256Hash(r.salt[:], common.BigToHash(big.NewInt(r.index)).Bytes())
+	var outHash common.Hash
+	r.rand.Read(outHash[:])
+	return outHash
 }
 
 func (r *PseudoRandomDataSource) GetAddress() common.Address {
@@ -38,13 +34,11 @@ func (r *PseudoRandomDataSource) GetAddress() common.Address {
 }
 
 func (r *PseudoRandomDataSource) GetUint64() uint64 {
-	return binary.BigEndian.Uint64(r.GetHash().Bytes()[:8])
+	return r.rand.Uint64()
 }
 
 func (r *PseudoRandomDataSource) GetData(size int) []byte {
-	ret := []byte{}
-	for len(ret) < size {
-		ret = append(ret, r.GetHash().Bytes()...)
-	}
-	return ret[:size]
+	outArray := make([]byte, size)
+	r.rand.Read(outArray)
+	return outArray
 }
