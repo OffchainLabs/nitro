cd1e0873be0afe5067a3d0adbee436566de8a406
Tsahi Zidenberg <tsahi@offchainlabs.com>
2022-02-15 09:32:08 +0200

    typo

--- util/keystore.go
+++ util/keystore.go
@@ -10,19 +10,19 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
-func GetTransactOptsFromKeystore(keystorePath, accoundAddress, passphrase string, chainId *big.Int) (*bind.TransactOpts, error) {
+func GetTransactOptsFromKeystore(keystorePath, accountAddress, passphrase string, chainId *big.Int) (*bind.TransactOpts, error) {
 	if keystorePath == "" {
 		return nil, errors.New("keystore path empty")
 	}
 	l1keystore := keystore.NewKeyStore(keystorePath, keystore.StandardScryptN, keystore.StandardScryptP)
 	var l1Account accounts.Account
-	if accoundAddress == "" {
+	if accountAddress == "" {
 		if len(l1keystore.Accounts()) == 0 {
 			return nil, errors.New("keystore empty")
 		}
 		l1Account = l1keystore.Accounts()[0]
 	} else {
-		address := common.HexToAddress(accoundAddress)
+		address := common.HexToAddress(accountAddress)
 		var err error
 		l1Account, err = l1keystore.Find(accounts.Account{Address: address})
 		if err != nil {
