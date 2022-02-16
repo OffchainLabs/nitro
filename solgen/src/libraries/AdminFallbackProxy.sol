e6e2a681262a39344548382e51cb03ba5455224e
fredlacs <32464905+fredlacs@users.noreply.github.com>
2022-02-16 11:51:01 +0000

    Update admin fallback proxy to use UUPS approach to upgrades

--- solgen/src/libraries/AdminFallbackProxy.sol
+++ solgen/src/libraries/AdminFallbackProxy.sol
@@ -18,39 +18,177 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Upgrade.sol";
+import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/StorageSlot.sol";
 
 
-/// @dev similar to TransparentUpgradeableProxy but allows the admin to fallback to
-/// a separate logic contract
-contract AdminFallbackProxy is TransparentUpgradeableProxy {
+/// @notice An extension to OZ's ERC1967Upgrade implementation to support two logic contracts
+abstract contract DoubleLogicERC1967Upgrade is ERC1967Upgrade {
     using Address for address;
 
-    // we hardcode the result of bytes32(uint256(keccak256("proxy.admin.fallback.logic")) - 1)
-    // since hashes aren't evaluated during compile time
-    bytes32 internal constant _ADMIN_LOGIC_SLOT = 0x0ec14cccd309f3d73c97ec9ccd142d751d5c2a665ea2437991839ee618e56b45;
+    // This is the keccak-256 hash of "eip1967.proxy.implementation.secondary" subtracted by 1
+    bytes32 internal constant _IMPLEMENTATION_SECONDARY_SLOT = 0x2b1dbce74324248c222f0ec2d5ed7bd323cfc425b336f0253c5ccfda7265546d;
 
-    function _getAdminFallbackLogic() internal view returns (address) {
-        return StorageSlot.getAddressSlot(_ADMIN_LOGIC_SLOT).value;
+    // This is the keccak-256 hash of "eip1967.proxy.rollback.secondary" subtracted by 1
+    bytes32 private constant _ROLLBACK_SECONDARY_SLOT = 0x49bd798cd84788856140a4cd5030756b4d08a9e4d55db725ec195f232d262a89;
+
+    /**
+     * @dev Emitted when the secondary implementation is upgraded.
+     */
+    event UpgradedSecondary(address indexed implementation);
+
+    /**
+     * @dev Returns the current secondary implementation address.
+     */
+    function _getSecondaryImplementation() internal view returns (address) {
+        return StorageSlot.getAddressSlot(_IMPLEMENTATION_SECONDARY_SLOT).value;
     }
 
+    /**
+     * @dev Stores a new address in the EIP1967 implementation slot.
+     */
+    function _setSecondaryImplementation(address newImplementation) private {
+        require(Address.isContract(newImplementation), "ERC1967: new secondary implementation is not a contract");
+        StorageSlot.getAddressSlot(_IMPLEMENTATION_SECONDARY_SLOT).value = newImplementation;
+    }
+
+    /**
+     * @dev Perform secondary implementation upgrade
+     *
+     * Emits an {UpgradedSecondary} event.
+     */
+    function _upgradeSecondaryTo(address newImplementation) internal {
+        _setSecondaryImplementation(newImplementation);
+        emit UpgradedSecondary(newImplementation);
+    }
+
+    /**
+     * @dev Perform secondary implementation upgrade with additional setup call.
+     *
+     * Emits an {UpgradedSecondary} event.
+     */
+    function _upgradeSecondaryToAndCall(
+        address newImplementation,
+        bytes memory data,
+        bool forceCall
+    ) internal {
+        _upgradeSecondaryTo(newImplementation);
+        if (data.length > 0 || forceCall) {
+            Address.functionDelegateCall(newImplementation, data);
+        }
+    }
+
+    /**
+     * @dev Perform secondary implementation upgrade with security checks for UUPS proxies, and additional setup call.
+     *
+     * Emits an {UpgradedSecondary} event.
+     */
+    function _upgradeSecondaryToAndCallUUPS(
+        address newImplementation,
+        bytes memory data,
+        bool forceCall
+    ) internal {
+        // Upgrades from old implementations will perform a rollback test. This test requires the new
+        // implementation to upgrade back to the old, non-ERC1822 compliant, implementation. Removing
+        // this special case will break upgrade paths from old UUPS implementation to new ones.
+        if (StorageSlot.getBooleanSlot(_ROLLBACK_SECONDARY_SLOT).value) {
+            _setSecondaryImplementation(newImplementation);
+        } else {
+            try IERC1822Proxiable(newImplementation).proxiableUUID() returns (bytes32 slot) {
+                require(slot == _IMPLEMENTATION_SECONDARY_SLOT, "ERC1967Upgrade: unsupported proxiableUUID");
+            } catch {
+                revert("ERC1967Upgrade: new secondary implementation is not UUPS");
+            }
+            _upgradeSecondaryToAndCall(newImplementation, data, forceCall);
+        }
+    }
+}
+
+/// @notice An extension to OZ's ERC1967Proxy implementation to support two logic contracts
+contract DoubleLogicERC1967Proxy is ERC1967Proxy, DoubleLogicERC1967Upgrade {
+    /**
+     * @dev Initializes the upgradeable proxy with an initial implementation specified by `_logic` and a secondary
+     * logic implementation specified by `_secondaryLogic`
+     *
+     * If `_data` is nonempty, it's used as data in a delegate call to `_logic` - the same with secondary. 
+     * This will typically be an encoded function call, and allows initializating the storage of the proxy like
+     * a Solidity constructor.
+     */
+    constructor(
+        address _logic, bytes memory _data, address _secondaryLogic, bytes memory _secondaryData
+    ) payable ERC1967Proxy(_logic, _data) {
+        assert(_IMPLEMENTATION_SECONDARY_SLOT == bytes32(uint256(keccak256("eip1967.proxy.implementation.secondary")) - 1));
+        _upgradeSecondaryToAndCall(_secondaryLogic, _secondaryData, false);
+    }
+
+    /**
+     * @dev Returns the current secondary implementation address.
+     */
+    function _secondaryImplementation() internal view returns (address impl) {
+        return DoubleLogicERC1967Upgrade._getSecondaryImplementation();
+    }
+}
+
+/// @notice An extension to OZ's UUPSUpgradeable contract to be used in secondary logic contracts from DoubleLogicERC1967Upgrade
+abstract contract SecondaryLogicUUPSUpgradeable is UUPSUpgradeable, DoubleLogicERC1967Upgrade {
+    /// @inheritdoc UUPSUpgradeable
+    function proxiableUUID() external view override notDelegated returns (bytes32) {
+        return _IMPLEMENTATION_SECONDARY_SLOT;
+    }
+
+    /**
+     * @dev Upgrade the secondary implementation of the proxy to `newImplementation`.
+     *
+     * Calls {_authorizeUpgrade}.
+     *
+     * Emits an {UpgradedSecondary} event.
+     */
+    function upgradeSecondaryTo(address newImplementation) external onlyProxy {
+        _authorizeUpgrade(newImplementation);
+        _upgradeSecondaryToAndCallUUPS(newImplementation, new bytes(0), false);
+    }
+
+    /**
+     * @dev Upgrade the secondary implementation of the proxy to `newImplementation`, and subsequently execute the function call
+     * encoded in `data`.
+     *
+     * Calls {_authorizeUpgrade}.
+     *
+     * Emits an {UpgradedSecondary} event.
+     */
+    function upgradeSecondaryToAndCall(address newImplementation, bytes memory data) external payable onlyProxy {
+        _authorizeUpgrade(newImplementation);
+        _upgradeSecondaryToAndCallUUPS(newImplementation, data, true);
+    }
+
+    function upgradeTo(address newImplementation) external override {
+        revert("NOT_PRIMARY_LOGIC");
+    }
+
+    function upgradeToAndCall(address newImplementation, bytes memory data) external payable override {
+        revert("NOT_PRIMARY_LOGIC_AND_CALL");
+    }
+}
+
+
+/// @notice similar to TransparentUpgradeableProxy but allows the admin to fallback to a separate logic contract using DoubleLogicERC1967Proxy
+/// @dev this follows the UUPS pattern for upgradeability - read more at https://github.com/OpenZeppelin/openzeppelin-contracts/tree/v4.5.0/contracts/proxy#transparent-vs-uups-proxies
+contract AdminFallbackProxy is DoubleLogicERC1967Proxy {
     constructor(
         address userLogic,
         bytes memory userData,
         address adminLogic,
         bytes memory adminData,
         address adminAddr
-    ) payable TransparentUpgradeableProxy(userLogic, adminAddr, userData) {
-        assert(_ADMIN_LOGIC_SLOT == bytes32(uint256(keccak256("proxy.admin.fallback.logic")) - 1));
-        _upgradeAdminFallbackToAndCall(adminLogic, adminData, false);
+    ) payable DoubleLogicERC1967Proxy(userLogic, userData, adminLogic, adminData) {
+         assert(_ADMIN_SLOT == bytes32(uint256(keccak256("eip1967.proxy.admin")) - 1));
+        _changeAdmin(adminAddr);
     }
 
-    /**
-     * @dev This is a virtual function that should be overriden so it returns the address to which the fallback function
-     * and {_fallback} should delegate.
-     */
+    /// @inheritdoc Proxy
     function _implementation()
         internal
         view
@@ -61,43 +199,19 @@ contract AdminFallbackProxy is TransparentUpgradeableProxy {
         address _admin = _getAdmin();
         // if there is an owner and it is the sender, delegate to admin logic
         address target = _admin != address(0) && _admin == msg.sender
-            ? _getAdminFallbackLogic()
+            ? _secondaryImplementation()
             : _implementation();
-        require(target.isContract(), "TARGET_NOT_CONTRACT");
+        // implementation setters already do an existence check
+        // require(target.isContract(), "TARGET_NOT_CONTRACT");
         return target;
     }
 
-    /// @dev this allows the admin to access the fallback function, but we direct them
-    /// to a different logic contract implementation. If the same function signature
-    /// is available in both the proxy and logic, the proxy code will execute - not the logic.
+    /**
+     * @dev unlike transparent upgradeable proxies, this does allow the admin to fallback to a logic contract
+     * the admin is expected to interact only with the secondary logic contract, which handles contract
+     * upgrades using the UUPS approach
+     */
     function _beforeFallback() internal override {
-        // we override the superclass _beforeFallback to remove 
-        // the `require(msg.sender != _getAdmin())` check
-    }
-
-    event AdminFallbackUpgraded(address indexed implementation);
-
-    function _upgradeAdminFallbackTo(address newImplementation) internal {
-        StorageSlot.getAddressSlot(_ADMIN_LOGIC_SLOT).value = newImplementation;
-        emit AdminFallbackUpgraded(newImplementation);
-    }
-
-    function _upgradeAdminFallbackToAndCall(
-        address newImplementation,
-        bytes memory data,
-        bool forceCall
-    ) internal {
-        _upgradeAdminFallbackTo(newImplementation);
-        if (data.length > 0 || forceCall) {
-            Address.functionDelegateCall(newImplementation, data);
-        }
-    }
-
-    function upgradeAdminFallbackTo(address newImplementation) external ifAdmin {
-        _upgradeAdminFallbackToAndCall(newImplementation, bytes(""), false);
-    }
-
-    function _upgradeAdminFallbackToAndCall(address newImplementation, bytes calldata data) external payable ifAdmin {
-        _upgradeAdminFallbackToAndCall(newImplementation, data, true);
+        super._beforeFallback();
     }
 }
