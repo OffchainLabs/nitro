d7e36b035e36a81ef96ad1eb7532dd5723c8a102
fredlacs <32464905+fredlacs@users.noreply.github.com>
2022-02-18 14:28:16 +0000

    Add modifier to ensure admin methods never break initialisation

--- solgen/src/rollup/RollupAdminLogic.sol
+++ solgen/src/rollup/RollupAdminLogic.sol
@@ -15,10 +15,15 @@ contract RollupAdminLogic is RollupCore, IRollupAdmin, SecondaryLogicUUPSUpgrade
         return confirmPeriodBlocks != 0;
     }
 
+    modifier isInitAfterExec {
+        _;
+        require(isInit(), "METHOD_REMOVES_INIT");
+    }
+
     function initialize(
         Config calldata config,
         ContractDependencies calldata connectedContracts
-    ) external override onlyProxy {
+    ) external override onlyProxy isInitAfterExec {
         require(!isInit(), "NOT_INIT");
 
         delayedBridge = connectedContracts.delayedBridge;
@@ -58,7 +63,6 @@ contract RollupAdminLogic is RollupCore, IRollupAdmin, SecondaryLogicUUPSUpgrade
         sequencerBridge.setMaxTimeVariation(config.sequencerInboxMaxTimeVariation);
 
         emit RollupInitialized(config.wasmModuleRoot, config.chainId);
-        require(isInit(), "INITIALIZE_NOT_INIT");
     }
 
     function createInitialNode()
@@ -94,7 +98,7 @@ contract RollupAdminLogic is RollupCore, IRollupAdmin, SecondaryLogicUUPSUpgrade
      * @notice Add a contract authorized to put messages into this rollup's inbox
      * @param _outbox Outbox contract to add
      */
-    function setOutbox(IOutbox _outbox) external override {
+    function setOutbox(IOutbox _outbox) external override isInitAfterExec {
         outbox = _outbox;
         delayedBridge.setOutbox(address(_outbox), true);
         emit OwnerFunctionCalled(0);
@@ -104,7 +108,7 @@ contract RollupAdminLogic is RollupCore, IRollupAdmin, SecondaryLogicUUPSUpgrade
      * @notice Disable an old outbox from interacting with the bridge
      * @param _outbox Outbox contract to remove
      */
-    function removeOldOutbox(address _outbox) external override {
+    function removeOldOutbox(address _outbox) external override isInitAfterExec {
         require(_outbox != address(outbox), "CUR_OUTBOX");
         delayedBridge.setOutbox(_outbox, false);
         emit OwnerFunctionCalled(1);
@@ -115,7 +119,7 @@ contract RollupAdminLogic is RollupCore, IRollupAdmin, SecondaryLogicUUPSUpgrade
      * @param _inbox Inbox contract to add or remove
      * @param _enabled New status of inbox
      */
-    function setInbox(address _inbox, bool _enabled) external override {
+    function setInbox(address _inbox, bool _enabled) external override isInitAfterExec {
         delayedBridge.setInbox(address(_inbox), _enabled);
         emit OwnerFunctionCalled(2);
     }
@@ -124,7 +128,7 @@ contract RollupAdminLogic is RollupCore, IRollupAdmin, SecondaryLogicUUPSUpgrade
      * @notice Pause interaction with the rollup contract.
      * The time spent paused is not incremented in the rollup's timing for node validation.
      */
-    function pause() external override {
+    function pause() external override isInitAfterExec {
         _pause();
         emit OwnerFunctionCalled(3);
     }
@@ -132,7 +136,7 @@ contract RollupAdminLogic is RollupCore, IRollupAdmin, SecondaryLogicUUPSUpgrade
     /**
      * @notice Resume interaction with the rollup contract
      */
-    function resume() external override {
+    function resume() external override isInitAfterExec {
         _unpause();
         emit OwnerFunctionCalled(4);
     }
@@ -154,7 +158,7 @@ contract RollupAdminLogic is RollupCore, IRollupAdmin, SecondaryLogicUUPSUpgrade
      * @param _validator addresses to set in the whitelist
      * @param _val value to set in the whitelist for corresponding address
      */
-    function setValidator(address[] calldata _validator, bool[] calldata _val) external override {
+    function setValidator(address[] calldata _validator, bool[] calldata _val) external override isInitAfterExec {
         require(_validator.length == _val.length, "WRONG_LENGTH");
 
         for (uint256 i = 0; i < _validator.length; i++) {
@@ -168,7 +172,7 @@ contract RollupAdminLogic is RollupCore, IRollupAdmin, SecondaryLogicUUPSUpgrade
      * @dev it is expected that only the rollup admin can use this facet to set a new owner
      * @param newOwner address of new rollup owner
      */
-    function setOwner(address newOwner) external override {
+    function setOwner(address newOwner) external override isInitAfterExec {
         _changeAdmin(newOwner);
         emit OwnerFunctionCalled(7);
     }
@@ -177,7 +181,7 @@ contract RollupAdminLogic is RollupCore, IRollupAdmin, SecondaryLogicUUPSUpgrade
      * @notice Set minimum assertion period for the rollup
      * @param newPeriod new minimum period for assertions
      */
-    function setMinimumAssertionPeriod(uint256 newPeriod) external override {
+    function setMinimumAssertionPeriod(uint256 newPeriod) external override isInitAfterExec {
         minimumAssertionPeriod = newPeriod;
         emit OwnerFunctionCalled(8);
     }
@@ -186,7 +190,7 @@ contract RollupAdminLogic is RollupCore, IRollupAdmin, SecondaryLogicUUPSUpgrade
      * @notice Set number of blocks until a node is considered confirmed
      * @param newConfirmPeriod new number of blocks
      */
-    function setConfirmPeriodBlocks(uint64 newConfirmPeriod) external override {
+    function setConfirmPeriodBlocks(uint64 newConfirmPeriod) external override isInitAfterExec {
         confirmPeriodBlocks = newConfirmPeriod;
         emit OwnerFunctionCalled(9);
     }
@@ -195,7 +199,7 @@ contract RollupAdminLogic is RollupCore, IRollupAdmin, SecondaryLogicUUPSUpgrade
      * @notice Set number of extra blocks after a challenge
      * @param newExtraTimeBlocks new number of blocks
      */
-    function setExtraChallengeTimeBlocks(uint64 newExtraTimeBlocks) external override {
+    function setExtraChallengeTimeBlocks(uint64 newExtraTimeBlocks) external override isInitAfterExec {
         extraChallengeTimeBlocks = newExtraTimeBlocks;
         emit OwnerFunctionCalled(10);
     }
@@ -204,7 +208,7 @@ contract RollupAdminLogic is RollupCore, IRollupAdmin, SecondaryLogicUUPSUpgrade
      * @notice Set base stake required for an assertion
      * @param newBaseStake minimum amount of stake required
      */
-    function setBaseStake(uint256 newBaseStake) external override {
+    function setBaseStake(uint256 newBaseStake) external override isInitAfterExec {
         baseStake = newBaseStake;
         emit OwnerFunctionCalled(12);
     }
@@ -215,7 +219,7 @@ contract RollupAdminLogic is RollupCore, IRollupAdmin, SecondaryLogicUUPSUpgrade
      * implementation of the Rollup User facet!
      * @param newStakeToken address of token used for staking
      */
-    function setStakeToken(address newStakeToken) external override whenPaused {
+    function setStakeToken(address newStakeToken) external override whenPaused isInitAfterExec {
         /*
          * To change the stake token without breaking consistency one would need to:
          * Pause the system, have all stakers remove their funds,
@@ -238,7 +242,11 @@ contract RollupAdminLogic is RollupCore, IRollupAdmin, SecondaryLogicUUPSUpgrade
      * @notice Set max delay for sequencer inbox
      * @param maxTimeVariation the maximum time variation parameters
      */
-    function setSequencerInboxMaxTimeVariation(ISequencerInbox.MaxTimeVariation calldata maxTimeVariation) external override {
+    function setSequencerInboxMaxTimeVariation(ISequencerInbox.MaxTimeVariation calldata maxTimeVariation)
+        external
+        override
+        isInitAfterExec
+    {
         sequencerBridge.setMaxTimeVariation(maxTimeVariation);
         emit OwnerFunctionCalled(14);
     }
@@ -250,6 +258,7 @@ contract RollupAdminLogic is RollupCore, IRollupAdmin, SecondaryLogicUUPSUpgrade
     function setChallengeExecutionBisectionDegree(uint256 newChallengeExecutionBisectionDegree)
         external
         override
+        isInitAfterExec
     {
         challengeExecutionBisectionDegree = newChallengeExecutionBisectionDegree;
         emit OwnerFunctionCalled(16);
@@ -260,7 +269,7 @@ contract RollupAdminLogic is RollupCore, IRollupAdmin, SecondaryLogicUUPSUpgrade
      * @param addr the address
      * @param isBatchPoster if the specified address should be authorized as a batch poster
      */
-    function setIsBatchPoster(address addr, bool isBatchPoster) external override {
+    function setIsBatchPoster(address addr, bool isBatchPoster) external override isInitAfterExec {
         ISequencerInbox(sequencerBridge).setIsBatchPoster(addr, isBatchPoster);
         emit OwnerFunctionCalled(19);
     }
@@ -270,7 +279,7 @@ contract RollupAdminLogic is RollupCore, IRollupAdmin, SecondaryLogicUUPSUpgrade
      * @param beacon address of beacon to be upgraded
      * @param newImplementation new address of implementation
      */
-    function upgradeBeacon(address beacon, address newImplementation) external override {
+    function upgradeBeacon(address beacon, address newImplementation) external override isInitAfterExec {
         UpgradeableBeacon(beacon).upgradeTo(newImplementation);
         emit OwnerFunctionCalled(20);
     }
@@ -279,6 +288,7 @@ contract RollupAdminLogic is RollupCore, IRollupAdmin, SecondaryLogicUUPSUpgrade
         external
         override
         whenPaused
+        isInitAfterExec
     {
         require(stakerA.length == stakerB.length, "WRONG_LENGTH");
         for (uint256 i = 0; i < stakerA.length; i++) {
@@ -293,7 +303,7 @@ contract RollupAdminLogic is RollupCore, IRollupAdmin, SecondaryLogicUUPSUpgrade
         emit OwnerFunctionCalled(21);
     }
 
-    function forceRefundStaker(address[] calldata staker) external override whenPaused {
+    function forceRefundStaker(address[] calldata staker) external override whenPaused isInitAfterExec {
         for (uint256 i = 0; i < staker.length; i++) {
             reduceStakeTo(staker[i], 0);
             turnIntoZombie(staker[i]);
@@ -306,7 +316,7 @@ contract RollupAdminLogic is RollupCore, IRollupAdmin, SecondaryLogicUUPSUpgrade
         uint256 prevNodeInboxMaxCount,
         RollupLib.Assertion calldata assertion,
         bytes32 expectedNodeHash
-    ) external override whenPaused {
+    ) external override whenPaused isInitAfterExec {
         require(prevNode == latestConfirmed(), "ONLY_LATEST_CONFIRMED");
 
         createNewNode(
@@ -323,7 +333,7 @@ contract RollupAdminLogic is RollupCore, IRollupAdmin, SecondaryLogicUUPSUpgrade
         uint64 nodeNum,
         bytes32 blockHash,
         bytes32 sendRoot
-    ) external override whenPaused {
+    ) external override whenPaused isInitAfterExec {
         // this skips deadline, staker and zombie validation
         confirmNode(
             nodeNum,
@@ -333,7 +343,7 @@ contract RollupAdminLogic is RollupCore, IRollupAdmin, SecondaryLogicUUPSUpgrade
         emit OwnerFunctionCalled(24);
     }
 
-    function setLoserStakeEscrow(address newLoserStakerEscrow) external override {
+    function setLoserStakeEscrow(address newLoserStakerEscrow) external override isInitAfterExec {
         // escrow holder can't be proxy admin, since escrow is only redeemable through
         // the primary user logic contract
         require(newLoserStakerEscrow != _getAdmin(), "INVALID_ESCROW");
@@ -345,7 +355,7 @@ contract RollupAdminLogic is RollupCore, IRollupAdmin, SecondaryLogicUUPSUpgrade
      * @notice Set the proving WASM module root
      * @param newWasmModuleRoot new module root
      */
-    function setWasmModuleRoot(bytes32 newWasmModuleRoot) external override {
+    function setWasmModuleRoot(bytes32 newWasmModuleRoot) external override isInitAfterExec {
         wasmModuleRoot = newWasmModuleRoot;
         emit OwnerFunctionCalled(26);
     }
