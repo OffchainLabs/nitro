e53824afffc7f3cc30e612ac9f921ca9b5265fc7
Lee Bousfield <ljbousfield@gmail.com>
2022-02-23 10:47:44 -0600

    Add new replay fuzz harness

--- arbstate/inbox.go
+++ arbstate/inbox.go
@@ -234,6 +234,7 @@ func (r *inboxMultiplexer) getNextMsg() (*MessageWithMetadata, error) {
 			advancing, err := rlp.NewStream(rd, 16).Uint()
 			if err != nil {
 				log.Warn("error parsing sequencer advancing segment", "err", err)
+				segmentNum++
 				continue
 			}
 			if segmentKind == BatchSegmentKindAdvanceTimestamp {
