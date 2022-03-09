97814d35ad93e1418fdb55f79a50da5b649bf41c
Tsahi Zidenberg <tsahi@offchainlabs.com>
2022-03-09 13:41:38 +0200

    cargo fmt fixes

--- arbitrator/wasm-libraries/brotli/build.rs
+++ arbitrator/wasm-libraries/brotli/build.rs
@@ -1,4 +1,3 @@
-
 fn main() {
     // Tell Cargo that if the given file changes, to rerun this build script.
     println!("cargo:rustc-link-search=../../target/lib-wasm/");
--- arbitrator/wasm-libraries/brotli/src/lib.rs
+++ arbitrator/wasm-libraries/brotli/src/lib.rs
@@ -5,20 +5,27 @@ extern "C" {
         encoded_size: usize,
         encoded_buffer: *const u8,
         decoded_size: *mut usize,
-        decoded_buffer: *mut u8) -> u32;
+        decoded_buffer: *mut u8,
+    ) -> u32;
 
     pub fn BrotliEncoderCompress(
-            quality: u32, lgwin :u32, mode: u32, input_size: usize,
-            input_buffer: *const u8,
-            encoded_size: *mut usize,
-            encoded_buffer: *mut u8) -> u32;
+        quality: u32,
+        lgwin: u32,
+        mode: u32,
+        input_size: usize,
+        input_buffer: *const u8,
+        encoded_size: *mut usize,
+        encoded_buffer: *mut u8,
+    ) -> u32;
 }
 
-const BROTLI_MODE_GENERIC:u32 = 0;
-const BROTLI_RES_SUCCESS:u32 = 1;
+const BROTLI_MODE_GENERIC: u32 = 0;
+const BROTLI_RES_SUCCESS: u32 = 1;
 
 #[no_mangle]
-pub unsafe extern "C" fn go__github_com_offchainlabs_nitro_arbcompress_brotliDecompress(sp: GoStack) {
+pub unsafe extern "C" fn go__github_com_offchainlabs_nitro_arbcompress_brotliDecompress(
+    sp: GoStack,
+) {
     //(inBuf []byte, outBuf []byte) int
     let in_buf_ptr = sp.read_u64(0);
     let in_buf_len = sp.read_u64(1);
@@ -29,7 +36,12 @@ pub unsafe extern "C" fn go__github_com_offchainlabs_nitro_arbcompress_brotliDec
     let in_slice = read_slice(in_buf_ptr, in_buf_len);
     let mut output = vec![0u8; out_buf_len as usize];
     let mut output_len = out_buf_len as usize;
-    let res = BrotliDecoderDecompress(in_buf_len as usize, in_slice.as_ptr(), &mut output_len, output.as_mut_ptr());
+    let res = BrotliDecoderDecompress(
+        in_buf_len as usize,
+        in_slice.as_ptr(),
+        &mut output_len,
+        output.as_mut_ptr(),
+    );
     if (res != BROTLI_RES_SUCCESS) || (output_len as u64 > out_buf_len) {
         sp.write_u64(OUTPUT_ARG, u64::MAX);
         return;
@@ -53,7 +65,15 @@ pub unsafe extern "C" fn go__github_com_offchainlabs_nitro_arbcompress_brotliCom
     let in_slice = read_slice(in_buf_ptr, in_buf_len);
     let mut output = vec![0u8; out_buf_len as usize];
     let mut output_len = out_buf_len as usize;
-    let res = BrotliEncoderCompress(level, windowsize, BROTLI_MODE_GENERIC, in_buf_len as usize, in_slice.as_ptr(), &mut output_len, output.as_mut_ptr());
+    let res = BrotliEncoderCompress(
+        level,
+        windowsize,
+        BROTLI_MODE_GENERIC,
+        in_buf_len as usize,
+        in_slice.as_ptr(),
+        &mut output_len,
+        output.as_mut_ptr(),
+    );
     if (res != BROTLI_RES_SUCCESS) || (output_len as u64 > out_buf_len) {
         sp.write_u64(OUTPUT_ARG, u64::MAX);
         return;
