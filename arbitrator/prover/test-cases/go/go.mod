15e1c478a428162cc27046fdced77eb6240f4b6c
Tsahi Zidenberg <tsahi@offchainlabs.com>
2022-03-09 16:43:22 +0200

    add compression tests to arbitrator

--- arbitrator/prover/test-cases/go/go.mod
+++ arbitrator/prover/test-cases/go/go.mod
@@ -2,9 +2,10 @@ module gomin
 
 go 1.16
 
+replace github.com/offchainlabs/nitro => ./../../../../
+
+require github.com/offchainlabs/nitro v0.1.0
+
 require github.com/wealdtech/go-merkletree v1.0.0
 
-require (
-	golang.org/x/crypto v0.0.0-20210915214749-c084706c2272 // indirect
-	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
-)
+require golang.org/x/crypto v0.0.0-20210915214749-c084706c2272 // indirect
--- arbitrator/prover/test-cases/go/main.go
+++ arbitrator/prover/test-cases/go/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"runtime"
 	"time"
 
 	merkletree "github.com/wealdtech/go-merkletree"
+
+	"github.com/offchainlabs/nitro/arbcompress"
 )
 
 // Example using the Merkle tree to generate and verify proofs.
@@ -33,6 +36,20 @@ func MerkleSample(data [][]byte, toproove int) (bool, error) {
 	}
 	return merkletree.VerifyProof(baz, proof, root)
 	// Verify the proof for 'Baz'
+}
+
+func testCompression(data []byte) {
+	compressed, err := arbcompress.CompressFast(data)
+	if err != nil {
+		panic(err)
+	}
+	decompressed, err := arbcompress.Decompress(compressed, len(data)*2+0x100)
+	if err != nil {
+		panic(err)
+	}
+	if !bytes.Equal(decompressed, data) {
+		panic("data differs after compression / decompression")
+	}
 }
 
 func main() {
@@ -70,4 +87,9 @@ func main() {
 	}
 
 	println("verified both proofs!\n")
+
+	testCompression([]byte{})
+	testCompression([]byte("This is a test string la la la la la la la la la la"))
+
+	println("test compression passed!\n")
 }
