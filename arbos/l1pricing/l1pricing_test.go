ce0eb05e048f9e8bf4e6e9f2cacd47c45de154c7
Lee Bousfield <ljbousfield@gmail.com>
2022-03-15 22:49:09 -0500

    Adjust TxFixedCost to 140 and add test

---
+++ arbos/l1pricing/l1pricing_test.go
@@ -0,0 +1,50 @@
+//
+// Copyright 2021-2022, Offchain Labs, Inc. All rights reserved.
+//
+
+package l1pricing
+
+import (
+	"math"
+	"math/big"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/params"
+)
+
+func TestTxFixedCost(t *testing.T) {
+	maxChainId := new(big.Int).SetUint64(math.MaxUint64)
+	maxValue := big.NewInt(1_000_000)
+	maxValue.Mul(maxValue, big.NewInt(params.Ether))
+	var address common.Address
+	for i := range address {
+		address[i] = 0xFF
+	}
+	maxSigVal := big.NewInt(2)
+	maxSigVal.Exp(maxSigVal, big.NewInt(256), nil)
+	maxSigVal.Sub(maxSigVal, common.Big1)
+	maxGasPrice := big.NewInt(1000 * params.GWei)
+	largeTx := types.NewTx(&types.DynamicFeeTx{
+		ChainID:    maxChainId,
+		Nonce:      1 << 32,
+		GasTipCap:  maxGasPrice,
+		GasFeeCap:  maxGasPrice,
+		Gas:        100_000_000,
+		To:         &address,
+		Value:      maxValue,
+		Data:       []byte{},
+		AccessList: []types.AccessTuple{},
+		V:          common.Big1,
+		R:          maxSigVal,
+		S:          maxSigVal,
+	})
+	largeTxEncoded, err := largeTx.MarshalBinary()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(largeTxEncoded) > TxFixedCost {
+		t.Fatal("large tx is", len(largeTxEncoded), "bytes but tx fixed cost is", TxFixedCost)
+	}
+}
