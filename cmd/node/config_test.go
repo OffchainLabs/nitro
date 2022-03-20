04696cfe0efc827a9830208a686aa85fc6f555d0
Harry Kalodner <harry@offchainlabs.com>
2022-03-19 21:43:53 -0400

    Add config tests matching docker compose

--- cmd/node/config_test.go
+++ cmd/node/config_test.go
@@ -3,10 +3,24 @@ package main
 import (
 	"context"
 	"github.com/offchainlabs/nitro/util/testhelpers"
+	"strings"
 	"testing"
 )
 
-func TestConfig(t *testing.T) {
-	_, _, _, err := ParseNode(context.Background(), []string{})
+func TestSeqConfig(t *testing.T) {
+	args := strings.Split("--persistent.data /data --dev-init --l1.url ws://geth:8546 --l1wallet.pathname /l1keystore --l1.deployment /deploydata/deployment.json --http.addr 0.0.0.0 --ws.addr 0.0.0.0 --node.sequencer.enable --feed.output.enabled -feed.output.port 9642", " ")
+	_, _, _, err := ParseNode(context.Background(), args)
+	testhelpers.RequireImpl(t, err)
+}
+
+func TestUnsafeStakerConfig(t *testing.T) {
+	args := strings.Split("--persistent.data /data --dev-init --l1.url ws://geth:8546 --l1wallet.pathname /l1keystore --l1.deployment /deploydata/deployment.json --http.addr 0.0.0.0 --ws.addr 0.0.0.0 --node.validator.enable -node.validator.strategy MakeNodes --node.validator.staker-interval 10s --node.forwarding-target null -node.validator.dangerous.without-block-validator", " ")
+	_, _, _, err := ParseNode(context.Background(), args)
+	testhelpers.RequireImpl(t, err)
+}
+
+func TestValidatorConfig(t *testing.T) {
+	args := strings.Split("--persistent.data /data --dev-init -l1.url ws://geth:8546 --l1wallet.pathname /l1keystore --l1.deployment /deploydata/deployment.json --http.addr 0.0.0.0 --ws.addr 0.0.0.0 -node.validator.enable --node.validator.strategy MakeNodes --node.validator.staker-interval 10s --node.forward-ingtarget null", " ")
+	_, _, _, err := ParseNode(context.Background(), args)
 	testhelpers.RequireImpl(t, err)
 }
--- cmd/relay/config_test.go
+++ cmd/relay/config_test.go
@@ -3,13 +3,12 @@ package main
 import (
 	"context"
 	"github.com/offchainlabs/nitro/util/testhelpers"
+	"strings"
 	"testing"
 )
 
 func TestRelayConfig(t *testing.T) {
-	_, err := ParseRelay(context.Background(), []string{
-		"--feed.output.port", "9652",
-		"--feed.input.url", "ws://sequencer:9642/feed",
-	})
+	args := strings.Split("--feed.output.port 9652 --feed.input.url ws://sequencer:9642/feed", " ")
+	_, err := ParseRelay(context.Background(), args)
 	testhelpers.RequireImpl(t, err)
 }
