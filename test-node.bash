e6f325fb0eaba22d2fbccc46ffafc7c8aaf53da2
Tsahi Zidenberg <tsahi@offchainlabs.com>
2022-03-24 10:40:42 +0200

    improved scripts, separate sequencer from funnel

--- test-node.bash
+++ test-node.bash
@@ -12,6 +12,12 @@ if ! which docker-compose > /dev/null; then
     exit 1
 fi
 
+if [[ $# -gt 0 ]] && [[ $1 == "script" ]]; then
+    shift
+    docker-compose run testnode-scripts index.js "$@"
+    exit $?
+fi
+
 num_volumes=`docker volume ls --filter label=com.docker.compose.project=nitro -q | wc -l`
 
 if [[ $num_volumes -eq 0 ]]; then
@@ -23,6 +29,8 @@ fi
 run=true
 force_build=false
 validate=false
+detach=false
+blockscout=true
 while [[ $# -gt 0 ]]; do
     case $1 in
         --init)
@@ -46,18 +54,30 @@ while [[ $# -gt 0 ]]; do
             validate=true
             shift
             ;;
+        --no-blockscout)
+            blockscout=false
+            shift
+            ;;
         --no-run)
             run=false
             shift
             ;;
+        --detach)
+            detach=true
+            shift
+            ;;
         *)
             echo Usage: $0 \[OPTIONS..]
+            echo        $0 script [SCRIPT-ARGS]
             echo
             echo OPTIONS:
             echo --build:           rebuild docker image
             echo --init:            remove all data, rebuild, deploy new rollup
             echo --validate:        heavy computation, validating all blocks in WASM
+            echo --detach:          detach from nodes after running them
             echo --no-run:          does not launch nodes \(usefull with build or init\)
+            echo
+            echo script rus inside a separate docker. For SCRIPT-ARGS, run $0 script --help
             exit 0
     esac
 done
@@ -66,9 +86,19 @@ if $force_init; then
     force_build=true
 fi
 
+NODES="sequencer"
+if $validate; then
+    NODES="$NODES validator"
+else
+    NODES="$NODES staker-unsafe"
+fi
+if $blockscout; then
+    NODES="$NODES blockscout"
+fi
+
 if $force_build; then
     echo == Building..
-    docker-compose build --no-rm
+    docker-compose build --no-rm $NODES testnode-scripts
 fi
 
 if $force_init; then
@@ -84,24 +114,27 @@ if $force_init; then
     docker-compose run --entrypoint sh geth -c "echo passphrase > /root/.ethereum/passphrase"
     docker-compose run geth account new --password /root/.ethereum/passphrase --keystore /keystore 
     docker-compose run geth account new --password /root/.ethereum/passphrase --keystore /keystore 
+    docker-compose run geth account new --password /root/.ethereum/passphrase --keystore /keystore 
 
-    echo == funding validator and writing configs
-    docker-compose run testnode-scripts index.js --fundvalidator --writeconfig
+    echo == funding validator and sequencer, writing configs
+    docker-compose run testnode-scripts index.js --fund --amount 1000 --l1account validator
+    docker-compose run testnode-scripts index.js --fund --amount 1000 --l1account sequencer
+    docker-compose run testnode-scripts index.js --writeconfig
 
     echo == Deploying L2
-    docker-compose run --entrypoint target/bin/deploy sequencer -l1conn ws://geth:8546 -l1keystore /l1keystore -l1deployment /config/deployment.json -authorizevalidators 10
-fi
-
-if $validate; then
-    STAKER_NODE="validator"
-else
-    STAKER_NODE="staker-unsafe"
+    validaotraddress=`docker-compose run testnode-scripts index.js --l1account sequencer --printaddress | tail -n 1 | tr -d '\r\n'`
+    docker-compose run --entrypoint target/bin/deploy sequencer -l1conn ws://geth:8546 -l1keystore /l1keystore -l1DeployAccount $validaotraddress -l1deployment /config/deployment.json -authorizevalidators 10
 fi
 
 if $run; then
+    UP_FLAG=""
+    if $detach; then
+        UP_FLAG="-d"
+    fi
+
     echo == Launching Sequencer
     echo if things go wrong - use --init to create a new chain
     echo
 
-    docker-compose up sequencer $STAKER_NODE blockscout
+    docker-compose up $UP_FLAG $NODES
 fi
--- testnode-scripts/index.ts
+++ testnode-scripts/index.ts
@@ -1,7 +1,8 @@
 import { ethers, BigNumber } from "ethers";
 import * as fs from 'fs';
+import { boolean } from "yargs";
 import { hideBin } from 'yargs/helpers';
-const yargs = require("yargs")
+import yargs from 'yargs/yargs';
 const path = require("path");
 
 const l1keystore = "/l1keystore"
@@ -24,6 +25,9 @@ async function createSendTransaction(provider: ethers.providers.Provider, from: 
     const gasEstimate = await provider.estimateGas(transactionRequest)
 
     let feeData = await provider.getFeeData();
+    if (feeData.maxPriorityFeePerGas == null || feeData.maxFeePerGas == null) {
+        throw Error("bad L1 fee data")
+    }
     transactionRequest.gasLimit = BigNumber.from(Math.ceil(gasEstimate.toNumber() * 1.2))
     transactionRequest.maxPriorityFeePerGas = BigNumber.from(Math.ceil(feeData.maxPriorityFeePerGas.toNumber() * 1.2)), // Recommended maxPriorityFeePerGas
     transactionRequest.maxFeePerGas = BigNumber.from(Math.ceil(feeData.maxFeePerGas.toNumber() * 1.2))
@@ -95,35 +99,51 @@ async function bridgeFunds(provider: ethers.providers.Provider, from: ethers.Wal
 }
 
 async function main() {
-    const argv = yargs(hideBin(process.argv)).argv
+    const argv = yargs(hideBin(process.argv)).options({
+        writeconfig: {type: 'boolean', describe: 'write config'},
+        bridgefunds: {type: 'boolean', describe: 'bridge funds'},
+        ethamount: {type: 'string', describe:'amount to transfer (in eth)', default: "10"},
+        l1account: {choices: ["funnel", "sequencer", "validator"] as const, default: "funnel"},
+        fund: {type: 'boolean', describe: 'send funds from funnel'},
+        printaddress: {type: 'boolean', describe: 'print address'}
+    }).help().parseSync()
 
     var keyFilenames = fs.readdirSync(l1keystore)
     keyFilenames.sort()
 
-    const sequencerAccount = ethers.Wallet.fromEncryptedJsonSync(fs.readFileSync(path.join(l1keystore, keyFilenames[0])).toString(), l1passphrase)
-    const validatorAccount = ethers.Wallet.fromEncryptedJsonSync(fs.readFileSync(path.join(l1keystore, keyFilenames[1])).toString(), l1passphrase)
+    let chosenAccount = 0
+    if (argv.l1account == "sequencer") {
+        chosenAccount = 1
+    }
+    if (argv.l1account == "validator") {
+        chosenAccount = 2
+    }
+
+    let accounts = keyFilenames.map((filename) => {
+        return ethers.Wallet.fromEncryptedJsonSync(fs.readFileSync(path.join(l1keystore,filename)).toString(), l1passphrase)
+    })
 
     var provider = new ethers.providers.WebSocketProvider("ws://geth:8546")
 
-    if (argv.fundvalidator) {
-        var response = await createSendTransaction(provider, sequencerAccount, validatorAccount.address, ethers.utils.parseEther("10"), new Uint8Array())
-        console.log("sent validator funding")
+    if (argv.fund) {
+        var response = await createSendTransaction(provider, accounts[0], accounts[chosenAccount].address, ethers.utils.parseEther(argv.ethamount), new Uint8Array())
+        console.log("sent " + argv.l1account + " funding")
         console.log(response)
     }
 
     if (argv.writeconfig) {
-        writeConfigs(sequencerAccount.address, validatorAccount.address)
+        writeConfigs(accounts[1].address, accounts[2].address)
         console.log("config files written")
     }
 
     if (argv.bridgefunds) {
-        var response = await bridgeFunds(provider, sequencerAccount)
+        var response = await bridgeFunds(provider, accounts[chosenAccount])
         console.log("bridged funds")
         console.log(response)
     }
 
-    if (argv.printsequenceraddress) {
-        console.log(sequencerAccount.address)
+    if (argv.printaddress) {
+        console.log(accounts[chosenAccount].address)
     }
     provider.destroy()
 }
