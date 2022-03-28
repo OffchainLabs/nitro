ddfc49c5a8dfda8ce5e8b475ca86ca33dbf2c069
Lee Bousfield <ljbousfield@gmail.com>
2022-03-27 21:42:44 -0500

    Fix typo and disable signature verification in seq coord atomic test

--- arbnode/seq_coordinator.go
+++ arbnode/seq_coordinator.go
@@ -69,7 +69,7 @@ type SeqCoordinatorConfig struct {
 }
 
 type SeqCoordinatorDangerousConfig struct {
-	DisableSignatureVerificaiton bool `koanf:"disable-signature-verification"`
+	DisableSignatureVerification bool `koanf:"disable-signature-verification"`
 }
 
 func SeqCoordinatorConfigAddOptions(prefix string, f *flag.FlagSet) {
@@ -87,7 +87,7 @@ func SeqCoordinatorConfigAddOptions(prefix string, f *flag.FlagSet) {
 }
 
 func SeqCoordinatorDangerousConfigAddOptions(prefix string, f *flag.FlagSet) {
-	f.Bool(prefix+".disable-signature-verification", DefaultSeqCoordinatorDangerousConfig.DisableSignatureVerificaiton, "disable message signature verification")
+	f.Bool(prefix+".disable-signature-verification", DefaultSeqCoordinatorDangerousConfig.DisableSignatureVerification, "disable message signature verification")
 }
 
 var DefaultSeqCoordinatorConfig = SeqCoordinatorConfig{
@@ -106,7 +106,7 @@ var DefaultSeqCoordinatorConfig = SeqCoordinatorConfig{
 }
 
 var DefaultSeqCoordinatorDangerousConfig = SeqCoordinatorDangerousConfig{
-	DisableSignatureVerificaiton: false,
+	DisableSignatureVerification: false,
 }
 
 var TestSeqCoordinatorConfig = SeqCoordinatorConfig{
@@ -122,7 +122,7 @@ var TestSeqCoordinatorConfig = SeqCoordinatorConfig{
 	MyUrl:           "",
 	SigningKey:      "b561f5d5d98debc783aa8a1472d67ec3bcd532a1c8d95e5cb23caa70c649f7c9",
 	Dangerous: SeqCoordinatorDangerousConfig{
-		DisableSignatureVerificaiton: false,
+		DisableSignatureVerification: false,
 	},
 }
 
@@ -161,7 +161,7 @@ func NewSeqCoordinator(streamer *TransactionStreamer, sequencer *Sequencer, conf
 	if err != nil {
 		return nil, err
 	}
-	if signingKey == nil && !config.Dangerous.DisableSignatureVerificaiton {
+	if signingKey == nil && !config.Dangerous.DisableSignatureVerification {
 		return nil, errors.New("signature verification is enabled but no key is present")
 	}
 	fallbackVerificationKey, err := loadSigningKey(config.FallbackVerificationKey)
@@ -261,7 +261,7 @@ func (c *SeqCoordinator) verifyMessageSignature(prefix []byte, data []byte) ([]b
 		return nil, errors.New("data is too short to contain message signature")
 	}
 	msg := data[32:]
-	if c.config.Dangerous.DisableSignatureVerificaiton {
+	if c.config.Dangerous.DisableSignatureVerification {
 		return msg, nil
 	}
 	var haveHmac common.Hash
