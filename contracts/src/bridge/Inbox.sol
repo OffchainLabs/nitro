4fb74a89a2cfec9c827bc33b06eb5462007fedc5
fredlacs <32464905+fredlacs@users.noreply.github.com>
2022-05-03 13:11:00 +0100

    Refactor flow

--- contracts/src/bridge/Inbox.sol
+++ contracts/src/bridge/Inbox.sol
@@ -387,7 +387,7 @@ contract Inbox is DelegateCallAware, PausableUpgradeable, IInbox {
     ) internal returns (uint256) {
         if (_messageData.length > MAX_DATA_SIZE)
             revert DataTooLarge(_messageData.length, MAX_DATA_SIZE);
-        if (allowRevertOnCall) revertWithInboxHandler(_sender, _messageData);
+        if (EthCallAware.isCall() && allowRevertOnCall) revert CallAwareData(0x00, abi.encodePacked(inboxSender, messageData));
         uint256 msgNum = deliverToBridge(_kind, _sender, keccak256(_messageData));
         emit InboxMessageDelivered(msgNum, _messageData);
         return msgNum;
