7d6b0ed326e9d1105ddfa0fb5c507949509d3029
amsanghi <asanghi@offchainlabs.com>
2022-05-12 02:51:23 +0530

    Sign and verify redis data

--- das/redis_das.go
+++ das/redis_das.go
@@ -6,24 +6,29 @@ package das
 import (
 	"bytes"
 	"context"
+	"crypto/subtle"
 	"encoding/base32"
 	"encoding/json"
 	"fmt"
+	"regexp"
 	"time"
 
 	"github.com/go-redis/redis/v8"
+	"github.com/pkg/errors"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 
 	"github.com/offchainlabs/nitro/arbstate"
 )
 
-const MESSAGE_KEY_PREFIX string = "redisDas.msg."
-const CERT_KEY_PREFIX string = "redisDas.cert."
+const messageKeyPrefix string = "redisDas.msg."
+const certKeyPrefix string = "redisDas.cert."
 
 type RedisConfig struct {
 	RedisUrl   string        `koanf:"redis-url"`
 	Expiration time.Duration `koanf:"redis-expiration"`
+	KeyConfig  string        `koanf:"redis-key-config"`
 }
 
 var DefaultRedisConfig = RedisConfig{
@@ -34,6 +39,7 @@ var DefaultRedisConfig = RedisConfig{
 type RedisDAS struct {
 	das         DataAvailabilityService
 	redisConfig RedisConfig
+	signingKey  common.Hash
 	client      redis.UniversalClient
 }
 
@@ -42,24 +48,66 @@ func NewRedisDataAvailabilityService(redisConfig RedisConfig, das DataAvailabili
 	if err != nil {
 		return nil, err
 	}
+	keyIsHex := keyIsHexRegex.Match([]byte(redisConfig.KeyConfig))
+	if keyIsHex {
+		return nil, errors.New("signing key file contents are not 32 bytes of hex")
+	}
+	signingKey := common.HexToHash(redisConfig.KeyConfig)
 	return &RedisDAS{
 		das:         das,
 		redisConfig: redisConfig,
+		signingKey:  signingKey,
 		client:      redis.NewClient(redisOptions),
 	}, nil
 }
+
+var keyIsHexRegex = regexp.MustCompile("^(0x)?[a-fA-F0-9]{64}$")
+
+func (r *RedisDAS) signMessage(message []byte) []byte {
+	var hmac [32]byte
+	hmac = crypto.Keccak256Hash(r.signingKey[:], message)
+	return append(hmac[:], message...)
+}
+
 func (r *RedisDAS) setMessageAndCert(ctx context.Context, message []byte, c *arbstate.DataAvailabilityCertificate, path string) error {
-	r.client.Set(ctx, MESSAGE_KEY_PREFIX+path, message, r.redisConfig.Expiration)
+	r.client.Set(ctx, messageKeyPrefix+path, r.signMessage(message), r.redisConfig.Expiration)
 	cBytes, err := json.Marshal(c)
 	if err != nil {
 		return err
 	}
-	r.client.Set(ctx, CERT_KEY_PREFIX+path, cBytes, r.redisConfig.Expiration)
+	r.client.Set(ctx, certKeyPrefix+path, r.signMessage(cBytes), r.redisConfig.Expiration)
 	return nil
 }
+
+func (r *RedisDAS) verifyMessageSignature(data []byte) ([]byte, error) {
+	if len(data) < 32 {
+		return nil, errors.New("data is too short to contain message signature")
+	}
+	message := data[32:]
+	var haveHmac common.Hash
+	copy(haveHmac[:], data[:32])
+	expectHmac := crypto.Keccak256Hash(r.signingKey[:], message)
+	if subtle.ConstantTimeCompare(expectHmac[:], haveHmac[:]) == 1 {
+		return message, nil
+	}
+	return nil, errors.New("HMAC signature doesn't match expected value(s)")
+}
+
+func (r *RedisDAS) getVerifiedData(ctx context.Context, key string) ([]byte, error) {
+	data, err := r.client.Get(ctx, key).Bytes()
+	if err != nil {
+		return nil, err
+	}
+	data, err = r.verifyMessageSignature(data)
+	if err != nil {
+		return nil, err
+	}
+	return data, err
+}
+
 func (r *RedisDAS) Store(ctx context.Context, message []byte, timeout uint64) (c *arbstate.DataAvailabilityCertificate, err error) {
 	path := base32.StdEncoding.EncodeToString(crypto.Keccak256(message))
-	cBytes, err := r.client.Get(ctx, CERT_KEY_PREFIX+path).Bytes()
+	cBytes, err := r.getVerifiedData(ctx, certKeyPrefix+path)
 	if err != nil {
 		c, err := r.das.Store(ctx, message, timeout)
 		if err != nil {
@@ -87,7 +135,7 @@ func (r *RedisDAS) Retrieve(ctx context.Context, certBytes []byte) ([]byte, erro
 
 	path := base32.StdEncoding.EncodeToString(cert.DataHash[:])
 
-	result, err := r.client.Get(ctx, MESSAGE_KEY_PREFIX+path).Bytes()
+	result, err := r.getVerifiedData(ctx, messageKeyPrefix+path)
 	if err != nil {
 		result, err = r.das.Retrieve(ctx, certBytes)
 		if err != nil {
