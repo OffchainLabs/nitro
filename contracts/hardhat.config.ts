b4cebfe12dde9da9eb1813d8b8647fc6859989be
fredlacs <32464905+fredlacs@users.noreply.github.com>
2022-07-06 17:35:15 +0100

    refactor: handle interface tester in single condition

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -6,20 +6,23 @@ import 'solidity-coverage'
 import 'hardhat-gas-reporter'
 import prodConfig from "./hardhat.prod-config"
 
-const compilers = [
-  {
-    version: "0.8.9",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 100,
+const solidity = {
+  compilers: [
+    {
+      version: "0.8.9",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 100,
+        },
       },
     },
-  },
-];
+  ],
+  overrides: {},
+};
 
-if (process.env["INTERFACE_TESTER_SOLC_VERSION"])
-  compilers.push({
+if (process.env["INTERFACE_TESTER_SOLC_VERSION"]) {
+  solidity.compilers.push({
     version: process.env["INTERFACE_TESTER_SOLC_VERSION"],
     settings: {
       optimizer: {
@@ -28,26 +31,25 @@ if (process.env["INTERFACE_TESTER_SOLC_VERSION"])
       },
     },
   });
+  solidity.overrides = {
+    "src/test-helpers/InterfaceCompatibilityTester.sol": {
+      version: process.env["INTERFACE_TESTER_SOLC_VERSION"],
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 100,
+        },
+      },
+    },
+  };
+}
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   ...prodConfig,
-  solidity: {
-    compilers,
-    overrides: {
-      "src/test-helpers/InterfaceCompatibilityTester.sol": {
-        version: process.env["INTERFACE_TESTER_SOLC_VERSION"] || compilers[0].version,
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 100,
-          },
-        },
-      },
-    },
-  },
+  solidity,
   namedAccounts: {
     deployer: {
       default: 0,
