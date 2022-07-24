aa6f87ddbb5a0ea0dec403519141217c2454c417
Lee Bousfield <ljbousfield@gmail.com>
2022-07-24 18:00:59 -0500

    Improve logging and archive extraction

--- cmd/nitro/nitro.go
+++ cmd/nitro/nitro.go
@@ -166,6 +166,7 @@ func downloadInit(ctx context.Context, initConfig *InitConfig) (string, error) {
 					fmt.Printf("\033[2K\r  attempt %d failed: %v", attempt, err)
 					break updateLoop
 				}
+				fmt.Printf("\n")
 				log.Info("Download done", "filename", resp.Filename, "duration", resp.Duration())
 				fmt.Println()
 				return resp.Filename, nil
@@ -233,7 +234,12 @@ func openInitializeChainDb(ctx context.Context, stack *node.Node, initConfig *In
 		if err != nil {
 			return nil, nil, fmt.Errorf("couln't open init '%v' archive: %w", initFile, err)
 		}
-		err = extract.Archive(context.Background(), reader, stack.InstanceDir(), nil)
+		stat, err := reader.Stat()
+		if err != nil {
+			return nil, nil, err
+		}
+		log.Info("extracting downloaded init archive", "size", fmt.Sprintf("%dMB", stat.Size()/1024/1024))
+		err = extract.Archive(ctx, reader, stack.InstanceDir(), nil)
 		if err != nil {
 			return nil, nil, fmt.Errorf("couln't extract init archive '%v' err:%w", initFile, err)
 		}
