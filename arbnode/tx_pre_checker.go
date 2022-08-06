1fe09040b5b1682d481ae7656010aba86c33e885
Lee Bousfield <ljbousfield@gmail.com>
2022-08-05 22:59:08 -0500

    Don't cache statedb (caused races)

--- arbnode/tx_pre_checker.go
+++ arbnode/tx_pre_checker.go
@@ -5,114 +5,28 @@ package arbnode
 
 import (
 	"context"
-	"sync/atomic"
 
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/event"
-	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/offchainlabs/nitro/arbos/arbosState"
 	"github.com/offchainlabs/nitro/arbos/l1pricing"
 	"github.com/offchainlabs/nitro/util/arbmath"
 )
 
-type txPreCheckerState struct {
-	header  *types.Header
-	stateDb *state.StateDB
-	arbos   *arbosState.ArbosState
-}
-
 type TxPreChecker struct {
-	publisher    TransactionPublisher
-	bc           *core.BlockChain
-	strictness   uint
-	latestState  atomic.Value // contains a txPreCheckerState
-	subscription event.Subscription
-	headChan     chan core.ChainHeadEvent
+	TransactionPublisher
+	bc         *core.BlockChain
+	strictness uint
 }
 
 func NewTxPreChecker(publisher TransactionPublisher, bc *core.BlockChain, strictness uint) *TxPreChecker {
-	headChan := make(chan core.ChainHeadEvent, 64)
-	subscription := bc.SubscribeChainHeadEvent(headChan)
-	c := &TxPreChecker{
-		publisher:    publisher,
-		bc:           bc,
-		strictness:   strictness,
-		latestState:  atomic.Value{}, // filled in in Initialize
-		subscription: subscription,
-		headChan:     headChan,
+	return &TxPreChecker{
+		TransactionPublisher: publisher,
+		bc:                   bc,
+		strictness:           strictness,
 	}
-	go func() {
-		for {
-			event, ok := <-headChan
-			if !ok {
-				return
-			}
-		BacklogLoop:
-			for {
-				// clear out any backed up events
-				select {
-				case event, ok = <-headChan:
-					if !ok {
-						return
-					}
-				default:
-					break BacklogLoop
-				}
-			}
-			err := c.updateLatestState(event.Block)
-			if err != nil {
-				log.Warn("failed to update tx pre-checker state to latest", "err", err)
-			}
-		}
-	}()
-	return c
-}
-
-func (c *TxPreChecker) updateLatestState(block *types.Block) error {
-	stateDb, err := c.bc.StateAt(block.Root())
-	if err != nil {
-		return err
-	}
-	arbos, err := arbosState.OpenSystemArbosState(stateDb, nil, true)
-	if err != nil {
-		return err
-	}
-	fullState := txPreCheckerState{
-		header:  block.Header(),
-		stateDb: stateDb,
-		arbos:   arbos,
-	}
-	c.latestState.Store(fullState)
-	return nil
-}
-
-func (c *TxPreChecker) getLatestState() txPreCheckerState {
-	state, ok := c.latestState.Load().(txPreCheckerState)
-	if !ok {
-		panic("invalid type stored in latestState")
-	}
-	return state
-}
-
-func (c *TxPreChecker) Initialize(ctx context.Context) error {
-	err := c.updateLatestState(c.bc.CurrentBlock())
-	if err != nil {
-		return err
-	}
-	return c.publisher.Initialize(ctx)
-}
-
-func (c *TxPreChecker) Start(ctx context.Context) error {
-	return c.publisher.Start(ctx)
-}
-
-func (c *TxPreChecker) StopAndWait() {
-	c.subscription.Unsubscribe()
-	close(c.headChan)
-	c.publisher.StopAndWait()
 }
 
 const TxPreCheckerStrictnessNone uint = 0
@@ -172,14 +86,18 @@ func PreCheckTx(chainConfig *params.ChainConfig, header *types.Header, statedb *
 }
 
 func (c *TxPreChecker) PublishTransaction(ctx context.Context, tx *types.Transaction) error {
-	state := c.getLatestState()
-	err := PreCheckTx(c.bc.Config(), state.header, state.stateDb, state.arbos, tx, c.strictness)
+	block := c.bc.CurrentBlock()
+	statedb, err := c.bc.StateAt(block.Root())
 	if err != nil {
 		return err
 	}
-	return c.publisher.PublishTransaction(ctx, tx)
-}
-
-func (c *TxPreChecker) CheckHealth(ctx context.Context) error {
-	return c.publisher.CheckHealth(ctx)
+	arbos, err := arbosState.OpenSystemArbosState(statedb, nil, true)
+	if err != nil {
+		return err
+	}
+	err = PreCheckTx(c.bc.Config(), block.Header(), statedb, arbos, tx, c.strictness)
+	if err != nil {
+		return err
+	}
+	return c.TransactionPublisher.PublishTransaction(ctx, tx)
 }
