c43b8099343e5ff751ac819052769389e1ffb0af
Rachel Franks <nfranks@protonmail.com>
2022-08-08 13:23:08 -0500

    fix isTopLevel

--- precompiles/ArbSys.go
+++ precompiles/ArbSys.go
@@ -204,5 +204,5 @@ func (con ArbSys) WithdrawEth(c ctx, evm mech, value huge, destination addr) (hu
 
 func (con ArbSys) isTopLevel(c ctx, evm mech) bool {
 	depth := evm.Depth()
-	return depth < 2 || evm.Origin == c.txProcessor.Callers[depth]
+	return depth < 2 || evm.Origin == c.txProcessor.Callers[depth-2]
 }
