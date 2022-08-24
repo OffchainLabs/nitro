ae6edd08331002e2655d2570c711b7c5a62fc0c9
Harry Kalodner <harry@offchainlabs.com>
2022-08-24 01:38:54 -0400

    Fix formatting in IInbox.sol

--- contracts/src/bridge/IInbox.sol
+++ contracts/src/bridge/IInbox.sol
@@ -73,7 +73,7 @@ interface IInbox is IDelayedMessageProvider {
         returns (uint256);
 
     /**
-     * @notice Deposit eth from L1 to L2 to address of the sender if sender is an EOA, and to its aliased address if the sender is a contract.  
+     * @notice Deposit eth from L1 to L2 to address of the sender if sender is an EOA, and to its aliased address if the sender is a contract
      * @dev This does not trigger the fallback function when receiving in the L2 side.
      *      Look into retryable tickets if you are interested in this functionality.
      * @dev This function should not be called inside contract constructors
