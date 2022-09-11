dce3e6f236cc396ddbf7380da97e58c821969b99
gzeon <95478735+gzeoneth@users.noreply.github.com>
2022-09-12 07:10:19 +0800

    test: dot instead of svg

--- contracts/test/storage/test.bash
+++ contracts/test/storage/test.bash
@@ -2,9 +2,9 @@
 for CONTRACTNAME in Bridge Inbox Outbox RollupCore RollupUserLogic RollupAdminLogic SequencerInbox ChallengeManager
 do
     echo "Checking storage change of $CONTRACTNAME"
-    [ -f "./test/storage/$CONTRACTNAME.svg" ] && mv "./test/storage/$CONTRACTNAME.svg" "./test/storage/$CONTRACTNAME-old.svg"
-    yarn sol2uml storage ./ -c "$CONTRACTNAME" -o "./test/storage/$CONTRACTNAME.svg"
-    diff "./test/storage/$CONTRACTNAME-old.svg" "./test/storage/$CONTRACTNAME.svg"
+    [ -f "./test/storage/$CONTRACTNAME.dot" ] && mv "./test/storage/$CONTRACTNAME.dot" "./test/storage/$CONTRACTNAME-old.dot"
+    yarn sol2uml storage ./ -c "$CONTRACTNAME" -o "./test/storage/$CONTRACTNAME.dot" -f dot
+    diff "./test/storage/$CONTRACTNAME-old.dot" "./test/storage/$CONTRACTNAME.dot"
     if [[ $? != "0" ]]
     then
         CHANGED=1
