bb112cfdd98ad59c47714de30524aa0199c3db9b
Lee Bousfield <ljbousfield@gmail.com>
2022-10-13 18:56:35 -0500

    Include cost of proxy contract in gas refunder enabled costs

--- contracts/src/libraries/IGasRefunder.sol
+++ contracts/src/libraries/IGasRefunder.sol
@@ -21,14 +21,20 @@ abstract contract GasRefundEnabled {
         uint256 startGasLeft = gasleft();
         _;
         if (address(gasRefunder) != address(0)) {
-            uint256 calldataSize = 0;
+            uint256 calldataSize;
+            assembly {
+                calldataSize := calldatasize()
+            }
+            uint256 calldataWords = (calldataSize + 31) / 32;
+            // account for the CALLDATACOPY cost of the proxy contract, including the memory expansion cost
+            startGasLeft += calldataWords * 6 + (calldataWords ** 2) / 512;
             // if triggered in a contract call, the spender may be overrefunded by appending dummy data to the call
             // so we check if it is a top level call, which would mean the sender paid calldata as part of tx.input
             // solhint-disable-next-line avoid-tx-origin
-            if (msg.sender == tx.origin) {
-                assembly {
-                    calldataSize := calldatasize()
-                }
+            if (msg.sender != tx.origin) {
+                // We can't be sure if this calldata came from the top level tx,
+                // so to be safe we tell the gas refunder there was no calldata.
+                calldataSize = 0;
             }
             gasRefunder.onGasSpent(payable(msg.sender), startGasLeft - gasleft(), calldataSize);
         }
