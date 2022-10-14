9390bdf05bd7b21b3e82b700ce0f8c4d48a3f82e
Lee Bousfield <ljbousfield@gmail.com>
2022-10-13 19:02:22 -0500

    Apply solidity auto-formatter

--- contracts/src/libraries/IGasRefunder.sol
+++ contracts/src/libraries/IGasRefunder.sol
@@ -27,7 +27,7 @@ abstract contract GasRefundEnabled {
             }
             uint256 calldataWords = (calldataSize + 31) / 32;
             // account for the CALLDATACOPY cost of the proxy contract, including the memory expansion cost
-            startGasLeft += calldataWords * 6 + (calldataWords ** 2) / 512;
+            startGasLeft += calldataWords * 6 + (calldataWords**2) / 512;
             // if triggered in a contract call, the spender may be overrefunded by appending dummy data to the call
             // so we check if it is a top level call, which would mean the sender paid calldata as part of tx.input
             // solhint-disable-next-line avoid-tx-origin
