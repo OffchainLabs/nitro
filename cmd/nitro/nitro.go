6a1fa7365a9a696321552ca7e356babb7d0ff772
Raul Jordan <raul@prysmaticlabs.com>
2022-10-17 12:18:28 -0400

    gosec

--- cmd/nitro/nitro.go
+++ cmd/nitro/nitro.go
@@ -11,7 +11,7 @@ import (
 	"math"
 	"math/big"
 	"net/http"
-	_ "net/http/pprof"
+	_ "net/http/pprof" // #nosec G108
 	"os"
 	"os/signal"
 	"reflect"
@@ -826,6 +826,7 @@ func startPprof(address string) {
 	log.Info("Starting metrics server with pprof", "addr", fmt.Sprintf("http://%s/debug/metrics", address))
 	log.Info("Pprof endpoint", "addr", fmt.Sprintf("http://%s/debug/pprof", address))
 	go func() {
+		// #nosec G114
 		if err := http.ListenAndServe(address, http.DefaultServeMux); err != nil {
 			log.Error("Failure in running pprof server", "err", err)
 		}
