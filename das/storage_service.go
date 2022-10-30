1105bd26751d198fbebc4838308eacdcf2bbc4d6
amsanghi <asanghi@offchainlabs.com>
2022-10-30 21:32:14 +0530

    Minor fix

--- das/storage_service.go
+++ das/storage_service.go
@@ -23,7 +23,6 @@ type StorageService interface {
 	Closer
 	fmt.Stringer
 	HealthCheck(ctx context.Context) error
-	// Mainly used to complete the linked list connection used for iterating data storage.
 	putKeyValue(ctx context.Context, key common.Hash, value []byte) error
 }
 
