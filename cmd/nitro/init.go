73d0665a74fc047090371aed27953e67b2ba76f2
Lee Bousfield <ljbousfield@gmail.com>
2022-12-05 09:26:21 -0700

    Fix uppercase hex in isHexHash

--- cmd/nitro/init.go
+++ cmd/nitro/init.go
@@ -190,7 +190,7 @@ func isHexHash(s string) bool {
 		return false
 	}
 	for _, c := range s {
-		if (c < '0' || c > '9') && (c < 'a' || c > 'f') && (c < 'A' && c > 'F') {
+		if (c < '0' || c > '9') && (c < 'a' || c > 'f') && (c < 'A' || c > 'F') {
 			return false
 		}
 	}
