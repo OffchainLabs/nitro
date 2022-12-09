e67944ea2119318d105cbedc45354615825c9dd3
gzeon <95478735+gzeoneth@users.noreply.github.com>
2022-12-09 16:35:49 +0800

    chore: add uni timelock addr

--- contracts/src/libraries/Constants.sol
+++ contracts/src/libraries/Constants.sol
@@ -11,3 +11,5 @@ uint64 constant NO_CHAL_INDEX = 0;
 
 // Expected seconds per block in Ethereum PoS
 uint256 constant ETH_POS_BLOCK_TIME = 12;
+
+address constant UNISWAP_L1_TIMELOCK = 0x1a9C8182C09F50C8318d769245beA52c32BE35BC;
