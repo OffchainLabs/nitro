734908db01ffdf5c0a807fe0dce2167237e00c0f
rauljordan <raul@prysmaticlabs.com>
2023-01-11 21:31:35 -0500

    builds

--- contracts/src/AssertionChain.sol
+++ contracts/src/AssertionChain.sol
@@ -42,6 +42,10 @@ interface IAssertionChain {
     function getAssertion(uint256 seqNum) external view returns (Assertion memory assertion);
     function getChallenge(bytes32 parentStateCommitHash) external view returns (Challenge memory challenge);
     function getChallengeVertex(uint256 seqNum, bytes32 parentStateCommitHash) external view returns (ChallengeVertex memory vertex);
+    function challengeWinner(Challenge memory challenge) external returns (Assertion memory assertion);
+    function challengeCompleted(Challenge memory challenge) external returns (bool);
+    function eligibleForNewSuccessor(ChallengeVertex memory vertex) external returns (bool);
+    function isPresumptiveSuccessor(ChallengeVertex memory vertex) external returns (bool);
 
     // Mutating calls.
     function createAssertion(
@@ -55,7 +59,22 @@ interface IAssertionChain {
     function confirmForPSTimer(ChallengeVertex calldata vertex) external payable;
     function confirmForChallengeDeadline(ChallengeVertex calldata vertex) external payable;
     function confirmForSubchallengeWin(ChallengeVertex calldata vertex) external payable;
+
     function createChallenge(Assertion calldata prev) external payable returns (Challenge memory challenge);
+    function addChallengeVertex(
+        Assertion calldata assertion,
+        HistoryCommitment calldata history
+    ) external payable returns (ChallengeVertex memory vertex);
+    function bisect(
+        ChallengeVertex calldata vertex,
+        HistoryCommitment calldata history,
+        bytes32[] calldata proof
+    ) external payable returns (ChallengeVertex memory bisectedVertex);
+    function merge(
+        ChallengeVertex calldata mergingFrom,
+        ChallengeVertex calldata mergingTo,
+        bytes32[] calldata proof
+    ) external payable returns (ChallengeVertex memory mergedToVertex);
 }
 
 contract AssertionChain is IAssertionChain {
@@ -77,6 +96,18 @@ contract AssertionChain is IAssertionChain {
     }
     function getChallengeVertex(uint256 seqNum, bytes32 parentStateCommitHash) external view returns (ChallengeVertex memory vertex) {
         revert("unimplemented");
+    }
+    function challengeWinner(Challenge memory challenge) external returns (Assertion memory assertion) {
+        revert("unimplemented");
+    }
+    function challengeCompleted(Challenge memory challenge) external returns (bool) {
+        return false;
+    }
+    function eligibleForNewSuccessor(ChallengeVertex memory vertex) external returns (bool) {
+        return false;
+    }
+    function isPresumptiveSuccessor(ChallengeVertex memory vertex) external returns (bool) {
+        return false;
     }
 
     // Mutating calls.
@@ -108,6 +139,26 @@ contract AssertionChain is IAssertionChain {
         revert("unimplemented");
     }
     function createChallenge(Assertion calldata prev) external payable returns (Challenge memory challenge) {
+        revert("unimplemented");
+    }
+    function addChallengeVertex(
+        Assertion calldata assertion,
+        HistoryCommitment calldata history
+    ) external payable returns (ChallengeVertex memory vertex) {
+        revert("unimplemented");
+    }
+    function bisect(
+        ChallengeVertex calldata vertex,
+        HistoryCommitment calldata history,
+        bytes32[] calldata proof
+    ) external payable returns (ChallengeVertex memory bisectedVertex) {
+        revert("unimplemented");
+    }
+    function merge(
+        ChallengeVertex calldata mergingFrom,
+        ChallengeVertex calldata mergingTo,
+        bytes32[] calldata proof
+    ) external payable returns (ChallengeVertex memory mergedToVertex) {
         revert("unimplemented");
     }
 }
