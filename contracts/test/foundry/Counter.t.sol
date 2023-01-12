2ac89af4efd2eebd1f7f89c1b68f403703454d43
Goran Vladika <goran.vladika@gmail.com>
2023-01-12 14:23:38 +0100

    Add Foundry support

---
+++ contracts/test/foundry/Counter.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.12;
+
+import "forge-std/Test.sol";
+
+contract CounterTest is Test {
+    function setUp() public {}
+
+    function testIncrement() public {
+        assertEq(true, true);
+    }
+}
