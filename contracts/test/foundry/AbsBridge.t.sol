c7036195258ea6270e70407ba39e8fe15f35c0e5
Goran Vladika <goran.vladika@gmail.com>
2023-01-19 14:14:44 +0100

    Test setDelayedInbox bridge function

--- contracts/test/foundry/AbsBridge.t.sol
+++ contracts/test/foundry/AbsBridge.t.sol
@@ -25,19 +25,115 @@ abstract contract AbsBridgeTest is Test {
     address public outbox = address(1002);
     address public seqInbox = address(1003);
 
+    /* solhint-disable func-name-mixedcase */
     function test_setSequencerInbox() public {
+        // expect event
+        vm.expectEmit(true, true, true, true);
+        emit SequencerInboxUpdated(seqInbox);
+
+        // set seqInbox
         vm.prank(address(bridge.rollup()));
         bridge.setSequencerInbox(seqInbox);
 
+        // checks
         assertEq(bridge.sequencerInbox(), seqInbox, "Invalid seqInbox");
     }
 
-    function test_setDelayedInbox() public {
-        assertEq(bridge.allowedDelayedInboxes(inbox), false, "Inbox shouldn't be allowed");
+    function test_setDelayedInbox_enableInbox() public {
+        assertEq(bridge.allowedDelayedInboxes(inbox), false, "Invalid allowedDelayedInboxes");
 
-        // allow inbox
+        // expect event
+        vm.expectEmit(true, true, true, true);
+        emit InboxToggle(inbox, true);
+
+        // enable inbox
         vm.prank(rollup);
         bridge.setDelayedInbox(inbox, true);
-        assertEq(bridge.allowedDelayedInboxes(inbox), true, "Inbox should be allowed");
+
+        // checks
+        assertEq(bridge.allowedDelayedInboxes(inbox), true, "Invalid allowedDelayedInboxes");
+        assertEq(inbox, bridge.allowedDelayedInboxList(0), "Invalid allowedDelayedInboxList");
     }
+
+    function test_setDelayedInbox_disableInbox() public {
+        // initially enable inbox
+        vm.prank(rollup);
+        bridge.setDelayedInbox(inbox, true);
+        assertEq(bridge.allowedDelayedInboxes(inbox), true, "Invalid allowedDelayedInboxes");
+
+        // expect event
+        vm.expectEmit(true, true, true, true);
+        emit InboxToggle(inbox, false);
+
+        // disable inbox
+        vm.prank(rollup);
+        bridge.setDelayedInbox(inbox, false);
+
+        // checks
+        assertEq(bridge.allowedDelayedInboxes(inbox), false, "Invalid allowedDelayedInboxes");
+        vm.expectRevert();
+        bridge.allowedDelayedInboxList(0);
+    }
+
+    function test_setDelayedInbox_ReEnableInbox() public {
+        // initially enable inbox
+        vm.prank(rollup);
+        bridge.setDelayedInbox(inbox, true);
+        assertEq(bridge.allowedDelayedInboxes(inbox), true, "Invalid allowedDelayedInboxes");
+
+        // expect event
+        vm.expectEmit(true, true, true, true);
+        emit InboxToggle(inbox, true);
+
+        // enable again inbox
+        vm.prank(rollup);
+        bridge.setDelayedInbox(inbox, true);
+
+        // checks
+        assertEq(bridge.allowedDelayedInboxes(inbox), true, "Invalid allowedDelayedInboxes");
+        assertEq(inbox, bridge.allowedDelayedInboxList(0), "Invalid allowedDelayedInboxList");
+    }
+
+    function test_setDelayedInbox_ReDisableInbox() public {
+        assertEq(bridge.allowedDelayedInboxes(inbox), false, "Invalid allowedDelayedInboxes");
+
+        // expect event
+        vm.expectEmit(true, true, true, true);
+        emit InboxToggle(inbox, false);
+
+        // disable again inbox
+        vm.prank(rollup);
+        bridge.setDelayedInbox(inbox, false);
+
+        // checks
+        assertEq(bridge.allowedDelayedInboxes(inbox), false, "Invalid allowedDelayedInboxes");
+        vm.expectRevert();
+        bridge.allowedDelayedInboxList(0);
+    }
+
+    function test_setDelayedInbox_revert_NonOwnerCall() public {
+        address mockRollupOwner = address(10000);
+        vm.mockCall(
+            rollup,
+            abi.encodeWithSelector(IOwnable.owner.selector),
+            abi.encode(mockRollupOwner)
+        );
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                NotRollupOrOwner.selector,
+                address(this),
+                rollup,
+                mockRollupOwner
+            )
+        );
+        bridge.setDelayedInbox(inbox, true);
+    }
+
+    /****
+     **** Event declarations
+     ***/
+
+    event SequencerInboxUpdated(address newSequencerInbox);
+    event InboxToggle(address indexed inbox, bool enabled);
 }
