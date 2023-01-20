b39ac23bfaa1a078bf3e8fad8ebffc68aebdcfc3
amsanghi <asanghi@offchainlabs.com>
2023-01-20 23:17:28 +0530

    Implement the Manager interface with a backend

---
+++ state-manager-backend/manager_backend.go
@@ -0,0 +1,52 @@
+package statemanagerbackend
+
+import (
+	"context"
+	"errors"
+
+	"github.com/OffchainLabs/new-rollup-exploration/util"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+// ManagerBackend defines a struct that can provide state data and historical
+// Merkle commitments to L2 state for the validator from a backend.
+type ManagerBackend interface {
+	GetMerkleRoot(ctx context.Context, start uint64, end uint64) (common.Hash, error)
+	GetStateRoot(ctx context.Context, height uint64) (common.Hash, error)
+	GetLatestStateHeight(ctx context.Context) (uint64, error)
+}
+
+// SimulatedManagerBackend defines a very naive manager backend that is initialized from a list of predetermined
+// state roots. It can produce state and merkel roots from those roots.
+type SimulatedManagerBackend struct {
+	stateRoots []common.Hash
+}
+
+func NewSimulatedManagerBackend(stateRoots []common.Hash) *SimulatedManagerBackend {
+	if len(stateRoots) == 0 {
+		panic("must have state roots")
+	}
+	return &SimulatedManagerBackend{stateRoots}
+}
+
+// GetMerkleRoot gets merkle root from start to end state passed as arguments from our local list of state roots.
+func (s *SimulatedManagerBackend) GetMerkleRoot(ctx context.Context, start uint64, end uint64) (common.Hash, error) {
+	if end >= uint64(len(s.stateRoots)) {
+		return common.Hash{}, errors.New("commitment height out of range")
+	}
+	return util.ExpansionFromLeaves(s.stateRoots[start : end+1]).Root(), nil
+}
+
+// GetStateRoot gets the state root at a specified height from our local list of state roots.
+func (s *SimulatedManagerBackend) GetStateRoot(ctx context.Context, height uint64) (common.Hash, error) {
+	if height >= uint64(len(s.stateRoots)) {
+		return common.Hash{}, errors.New("commitment height out of range")
+	}
+	return s.stateRoots[height], nil
+}
+
+// GetLatestStateHeight gets the state height corresponding to the last, our local list of state root has.
+func (s *SimulatedManagerBackend) GetLatestStateHeight(ctx context.Context) (uint64, error) {
+	return uint64(len(s.stateRoots) - 1), nil
+}
