46200a2cb69aa08ce79e69d6a7cd3df6aad948b2
Goran Vladika <goran.vladika@gmail.com>
2023-01-22 22:36:16 +0100

    Add more ERC20Inbox test cases

--- contracts/test/foundry/ERC20Inbox.t.sol
+++ contracts/test/foundry/ERC20Inbox.t.sol
@@ -35,10 +35,165 @@ contract ERC20InboxTest is Test {
     }
 
     /* solhint-disable func-name-mixedcase */
+    function test_setAllowList() public {
+        address[] memory users = new address[](2);
+        users[0] = address(300);
+        users[1] = address(301);
+
+        bool[] memory allowed = new bool[](2);
+        allowed[0] = true;
+        allowed[0] = false;
+
+        vm.expectEmit(true, true, true, true);
+        emit AllowListAddressSet(users[0], allowed[0]);
+        emit AllowListAddressSet(users[1], allowed[1]);
+
+        vm.prank(rollup);
+        inbox.setAllowList(users, allowed);
+
+        assertEq(inbox.isAllowed(users[0]), allowed[0], "Invalid isAllowed user[0]");
+        assertEq(inbox.isAllowed(users[1]), allowed[1], "Invalid isAllowed user[1]");
+    }
+
+    function test_setAllowList_revert_InvalidLength() public {
+        address[] memory users = new address[](1);
+        users[0] = address(300);
+
+        bool[] memory allowed = new bool[](2);
+        allowed[0] = true;
+        allowed[0] = false;
+
+        vm.expectRevert("INVALID_INPUT");
+        vm.prank(rollup);
+        inbox.setAllowList(users, allowed);
+    }
+
+    function test_setOutbox_revert_NonOwnerCall() public {
+        // mock the owner() call on rollup
+        address mockRollupOwner = address(10000);
+        vm.mockCall(
+            rollup,
+            abi.encodeWithSelector(IOwnable.owner.selector),
+            abi.encode(mockRollupOwner)
+        );
+
+        // setAllowList shall revert
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                NotRollupOrOwner.selector,
+                address(this),
+                rollup,
+                mockRollupOwner
+            )
+        );
+
+        address[] memory users = new address[](2);
+        users[0] = address(300);
+        bool[] memory allowed = new bool[](2);
+        allowed[0] = true;
+        inbox.setAllowList(users, allowed);
+    }
+
+    function test_setAllowListEnabled_EnableAllowList() public {
+        assertEq(inbox.allowListEnabled(), false, "Invalid initial value for allowList");
+
+        vm.expectEmit(true, true, true, true);
+        emit AllowListEnabledUpdated(true);
+
+        vm.prank(rollup);
+        inbox.setAllowListEnabled(true);
+
+        assertEq(inbox.allowListEnabled(), true, "Invalid allowList");
+    }
+
+    function test_setAllowListEnabled_DisableAllowList() public {
+        vm.prank(rollup);
+        inbox.setAllowListEnabled(true);
+        assertEq(inbox.allowListEnabled(), true, "Invalid initial value for allowList");
+
+        vm.expectEmit(true, true, true, true);
+        emit AllowListEnabledUpdated(false);
+
+        vm.prank(rollup);
+        inbox.setAllowListEnabled(false);
+
+        assertEq(inbox.allowListEnabled(), false, "Invalid allowList");
+    }
+
+    function test_setAllowListEnabled_revert_AlreadyEnabled() public {
+        vm.prank(rollup);
+        inbox.setAllowListEnabled(true);
+        assertEq(inbox.allowListEnabled(), true, "Invalid initial value for allowList");
+
+        vm.expectRevert("ALREADY_SET");
+        vm.prank(rollup);
+        inbox.setAllowListEnabled(true);
+    }
+
+    function test_setAllowListEnabled_revert_AlreadyDisabled() public {
+        vm.prank(rollup);
+        vm.expectRevert("ALREADY_SET");
+        inbox.setAllowListEnabled(false);
+    }
+
+    function test_setAllowListEnabled_revert_NonOwnerCall() public {
+        // mock the owner() call on rollup
+        address mockRollupOwner = address(10000);
+        vm.mockCall(
+            rollup,
+            abi.encodeWithSelector(IOwnable.owner.selector),
+            abi.encode(mockRollupOwner)
+        );
+
+        // setAllowListEnabled shall revert
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                NotRollupOrOwner.selector,
+                address(this),
+                rollup,
+                mockRollupOwner
+            )
+        );
+
+        inbox.setAllowListEnabled(true);
+    }
+
+    function test_pause() public {
+        assertEq(inbox.paused(), false, "Invalid initial paused state");
+
+        vm.prank(rollup);
+        inbox.pause();
+
+        assertEq(inbox.paused(), true, "Invalid paused state");
+    }
+
+    function test_unpause() public {
+        vm.prank(rollup);
+        inbox.pause();
+        assertEq(inbox.paused(), true, "Invalid initial paused state");
+
+        vm.prank(rollup);
+        inbox.unpause();
+
+        assertEq(inbox.paused(), false, "Invalid paused state");
+    }
+
     function test_initialize() public {
         assertEq(address(inbox.bridge()), address(bridge), "Invalid bridge ref");
         assertEq(address(inbox.sequencerInbox()), seqInbox, "Invalid seqInbox ref");
         assertEq(inbox.allowListEnabled(), false, "Invalid allowListEnabled");
+        assertEq(inbox.paused(), false, "Invalid paused state");
+    }
+
+    function test_initialize_revert_ReInit() public {
+        vm.expectRevert("Initializable: contract is already initialized");
+        inbox.initialize(bridge, ISequencerInbox(seqInbox));
+    }
+
+    function test_initialize_revert_NonDelegated() public {
+        ERC20Inbox inb = new ERC20Inbox();
+        vm.expectRevert("Function must be called through delegatecall");
+        inb.initialize(bridge, ISequencerInbox(seqInbox));
     }
 
     function test_depositERC20() public {
@@ -120,4 +275,11 @@ contract ERC20InboxTest is Test {
         uint256 delayedMsgCountAfter = bridge.delayedMessageCount();
         assertEq(delayedMsgCountAfter - delayedMsgCountBefore, 1, "Invalid delayed message count");
     }
+
+    /****
+     **** Event declarations
+     ***/
+
+    event AllowListAddressSet(address indexed user, bool val);
+    event AllowListEnabledUpdated(bool isEnabled);
 }
