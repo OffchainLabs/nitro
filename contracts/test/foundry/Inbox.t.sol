45d61d4c240edc5ba67fb0995da923946c11a6dc
Goran Vladika <goran.vladika@gmail.com>
2023-01-23 15:48:01 +0100

    Add EthInbox test cases

---
+++ contracts/test/foundry/Inbox.t.sol
@@ -0,0 +1,184 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import "./AbsInbox.t.sol";
+import "./util/TestUtil.sol";
+import "../../src/bridge/Inbox.sol";
+import "../../src/bridge/IEthInbox.sol";
+import "../../src/bridge/Bridge.sol";
+import "../../src/bridge/ISequencerInbox.sol";
+import "../../src/libraries/AddressAliasHelper.sol";
+
+contract InboxTest is AbsInboxTest {
+    IEthInbox public ethInbox;
+
+    function setUp() public {
+        // deploy token, bridge and inbox
+        bridge = IBridge(TestUtil.deployProxy(address(new Bridge())));
+        inbox = IInbox(TestUtil.deployProxy(address(new Inbox())));
+        ethInbox = IEthInbox(address(inbox));
+
+        // init bridge and inbox
+        IEthBridge(address(bridge)).initialize(IOwnable(rollup));
+        inbox.initialize(bridge, ISequencerInbox(seqInbox));
+        vm.prank(rollup);
+        bridge.setDelayedInbox(address(inbox), true);
+
+        // fund user account
+        vm.deal(user, 10 ether);
+    }
+
+    /* solhint-disable func-name-mixedcase */
+    function test_depositEth_FromEOA() public {
+        uint256 depositAmount = 2 ether;
+
+        uint256 bridgeEthBalanceBefore = address(bridge).balance;
+        uint256 userEthBalanceBefore = address(user).balance;
+
+        // expect event
+        vm.expectEmit(true, true, true, true);
+        emit InboxMessageDelivered(0, abi.encodePacked(user, depositAmount));
+
+        // deposit tokens -> tx.origin == msg.sender
+        vm.prank(user, user);
+        ethInbox.depositEth{value: depositAmount}();
+
+        //// checks
+
+        uint256 bridgeEthBalanceAfter = address(bridge).balance;
+        assertEq(
+            bridgeEthBalanceAfter - bridgeEthBalanceBefore,
+            depositAmount,
+            "Invalid bridge eth balance"
+        );
+
+        uint256 userEthBalanceAfter = address(user).balance;
+        assertEq(
+            userEthBalanceBefore - userEthBalanceAfter,
+            depositAmount,
+            "Invalid user eth balance"
+        );
+
+        assertEq(bridge.delayedMessageCount(), 1, "Invalid delayed message count");
+    }
+
+    function test_depositEth_FromContract() public {
+        uint256 depositAmount = 1.2 ether;
+
+        uint256 bridgeEthBalanceBefore = address(bridge).balance;
+        uint256 userEthBalanceBefore = address(user).balance;
+
+        // expect event
+        vm.expectEmit(true, true, true, true);
+        emit InboxMessageDelivered(
+            0,
+            abi.encodePacked(AddressAliasHelper.applyL1ToL2Alias(user), depositAmount)
+        );
+
+        // deposit tokens -> tx.origin != msg.sender
+        vm.prank(user);
+        ethInbox.depositEth{value: depositAmount}();
+
+        //// checks
+
+        uint256 bridgeEthBalanceAfter = address(bridge).balance;
+        assertEq(
+            bridgeEthBalanceAfter - bridgeEthBalanceBefore,
+            depositAmount,
+            "Invalid bridge eth balance"
+        );
+
+        uint256 userEthBalanceAfter = address(user).balance;
+        assertEq(
+            userEthBalanceBefore - userEthBalanceAfter,
+            depositAmount,
+            "Invalid eth token balance"
+        );
+
+        assertEq(bridge.delayedMessageCount(), 1, "Invalid delayed message count");
+    }
+
+    function test_depositERC20_revert_EthTransferFails() public {
+        uint256 bridgeEthBalanceBefore = address(bridge).balance;
+        uint256 userEthBalanceBefore = address(user).balance;
+
+        // deposit too many eth shall fail
+        vm.prank(user);
+        uint256 invalidDepositAmount = 300 ether;
+        vm.expectRevert();
+        ethInbox.depositEth{value: invalidDepositAmount}();
+
+        //// checks
+
+        uint256 bridgeEthBalanceAfter = address(bridge).balance;
+        assertEq(bridgeEthBalanceAfter, bridgeEthBalanceBefore, "Invalid bridge token balance");
+
+        uint256 userEthBalanceAfter = address(user).balance;
+        assertEq(userEthBalanceBefore, userEthBalanceAfter, "Invalid user token balance");
+
+        assertEq(bridge.delayedMessageCount(), 0, "Invalid delayed message count");
+    }
+
+    function test_createRetryableTicket() public {
+        uint256 bridgeEthBalanceBefore = address(bridge).balance;
+        uint256 userEthBalanceBefore = address(user).balance;
+
+        uint256 ethToSend = 0.3 ether;
+
+        // retyrable params
+        uint256 l2CallValue = 0.1 ether;
+        uint256 maxSubmissionCost = 0.1 ether;
+        uint256 gasLimit = 100_000;
+        uint256 maxFeePerGas = 0.000000001 ether;
+        bytes memory data = abi.encodePacked("some msg");
+
+        // expect event
+        vm.expectEmit(true, true, true, true);
+        emit InboxMessageDelivered(
+            0,
+            abi.encodePacked(
+                uint256(uint160(user)),
+                l2CallValue,
+                ethToSend,
+                maxSubmissionCost,
+                uint256(uint160(user)),
+                uint256(uint160(user)),
+                gasLimit,
+                maxFeePerGas,
+                data.length,
+                data
+            )
+        );
+
+        // create retryable -> tx.origin == msg.sender
+        vm.prank(user, user);
+        ethInbox.createRetryableTicket{value: ethToSend}({
+            to: address(user),
+            l2CallValue: l2CallValue,
+            maxSubmissionCost: maxSubmissionCost,
+            excessFeeRefundAddress: user,
+            callValueRefundAddress: user,
+            gasLimit: gasLimit,
+            maxFeePerGas: maxFeePerGas,
+            data: data
+        });
+
+        //// checks
+
+        uint256 bridgeEthBalanceAfter = address(bridge).balance;
+        assertEq(
+            bridgeEthBalanceAfter - bridgeEthBalanceBefore,
+            ethToSend,
+            "Invalid bridge token balance"
+        );
+
+        uint256 userEthBalanceAfter = address(user).balance;
+        assertEq(
+            userEthBalanceBefore - userEthBalanceAfter,
+            ethToSend,
+            "Invalid user token balance"
+        );
+
+        assertEq(bridge.delayedMessageCount(), 1, "Invalid delayed message count");
+    }
+}
