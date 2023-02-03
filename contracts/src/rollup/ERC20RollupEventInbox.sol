4c07e3e34b56c9f09fdedf18fd2106be77fba406
Goran Vladika <goran.vladika@gmail.com>
2023-02-03 14:29:36 +0100

    Add Eth/ERC20 implementation of RollupEventInbox

--- contracts/src/rollup/ERC20RollupEventInbox.sol
+++ contracts/src/rollup/ERC20RollupEventInbox.sol
@@ -5,20 +5,22 @@
 pragma solidity ^0.8.0;
 
 import "./AbsRollupEventInbox.sol";
-import "../bridge/IEthBridge.sol";
+import "../bridge/IERC20Bridge.sol";
 
 /**
  * @title The inbox for rollup protocol events
  */
-contract RollupEventInbox is AbsRollupEventInbox {
+contract ERC20RollupEventInbox is AbsRollupEventInbox {
     constructor() AbsRollupEventInbox() {}
 
     function _enqueueInitializationMsg(bytes memory initMsg) internal override returns (uint256) {
+        uint256 tokenAmount = 0;
         return
-            IEthBridge(address(bridge)).enqueueDelayedMessage(
+            IERC20Bridge(address(bridge)).enqueueDelayedMessage(
                 INITIALIZATION_MSG_TYPE,
                 address(0),
-                keccak256(initMsg)
+                keccak256(initMsg),
+                tokenAmount
             );
     }
 }
