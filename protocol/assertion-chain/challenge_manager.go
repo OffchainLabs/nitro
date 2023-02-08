98b76dc30886a027b66f5f7fb55259f059e687a3
terence tsao <terence@prysmaticlabs.com>
2023-02-08 11:28:41 -0800

    Add challenge manager test

--- protocol/assertion-chain/challenge_manager.go
+++ protocol/assertion-chain/challenge_manager.go
@@ -1,6 +1,7 @@
 package assertionchain
 
 import (
+	"bytes"
 	"strings"
 
 	"github.com/OffchainLabs/challenge-protocol-v2/solgen/go/outgen"
@@ -21,12 +22,12 @@ type ChallengeManager struct {
 	assertionChain *AssertionChain
 	caller         *outgen.ChallengeManagerCaller
 	writer         *outgen.ChallengeManagerTransactor
-	callOpts       *bind.CallOpts
 	txOpts         *bind.TransactOpts
 }
 
 // Challenge is a wrapper around solgen bindings.
 type Challenge struct {
+	inner outgen.Challenge
 }
 
 // ChallengeVertex is a wrapper around solgen bindings.
@@ -49,17 +50,23 @@ func (ac *AssertionChain) ChallengeManager() (*ChallengeManager, error) {
 		assertionChain: ac,
 		caller:         &managerBinding.ChallengeManagerCaller,
 		writer:         &managerBinding.ChallengeManagerTransactor,
-		callOpts:       ac.callOpts,
 		txOpts:         ac.txOpts,
 	}, nil
 }
 
-func (cm *ChallengeManager) GetChallenge(challengeID common.Hash) (*Challenge, error) {
-	c, err := cm.caller.GetChallenge(cm.callOpts, challengeID)
+// ChallengeByID returns a challenge by its challenge ID.
+func (cm *ChallengeManager) ChallengeByID(challengeID common.Hash) (*Challenge, error) {
+	c, err := cm.caller.GetChallenge(cm.assertionChain.callOpts, challengeID)
 	if err != nil {
 		return nil, err
 	}
 	switch {
+	case bytes.Equal(c.RootId[:], make([]byte, 32)):
+		return nil, errors.Wrapf(
+			ErrNotFound,
+			"challenge with id %#x",
+			challengeID,
+		)
 	case strings.Contains(err.Error(), "Vertex does not exist"):
 		return nil, errors.Wrapf(
 			ErrChallengeNotFound,
@@ -70,65 +77,4 @@ func (cm *ChallengeManager) GetChallenge(challengeID common.Hash) (*Challenge, e
 		return nil, err
 	}
 	return &Challenge{inner: c}, nil
-}
-
-func (cm *ChallengeManager) GetVertex(vertexID common.Hash) (*ChallengeVertex, error) {
-	v, err := cm.caller.GetVertex(cm.callOpts, vertexID)
-	switch {
-	case strings.Contains(err.Error(), "Vertex does not exist"):
-		return nil, errors.Wrapf(
-			ErrChallengeVertexNotFound,
-			"challenge vertex id %#x",
-			vertexID,
-		)
-	case err != nil:
-		return nil, err
-	}
-	return &ChallengeVertex{inner: v}, nil
-}
-
-func (cm *ChallengeManager) CreateChallenge(assertionId common.Hash) error {
-	tx, err := cm.writer.CreateChallenge(cm.txOpts, assertionId)
-	switch {
-	case strings.Contains(err.Error(), "Only assertion chain can create challenges"):
-		return errors.Wrap(ErrInvalidCaller, err.Error())
-	case strings.Contains(err.Error(), "Challenge already exists"):
-		return errors.Wrapf(ErrChallengeExists, "assertion id %#x", assertionId)
-	case err != nil:
-		return err
-	}
-	// TODO: What to do with tx here?
-	_ = tx
-	return nil
-}
-
-func (cm *ChallengeManager) Bisect(vertexId common.Hash, prefixHistoryCommitment common.Hash, prefixProof []byte) error {
-	tx, err := cm.writer.Bisect(cm.txOpts, vertexId, prefixHistoryCommitment, prefixProof)
-	if err != nil {
-		return err
-	}
-	// TODO: What to do with tx here?
-	_ = tx
-	return nil
-}
-
-func (cm *ChallengeManager) Merge(vertexId common.Hash, prefixHistoryCommitment common.Hash, prefixProof []byte) error {
-	tx, err := cm.writer.Merge(cm.txOpts, vertexId, prefixHistoryCommitment, prefixProof)
-	if err != nil {
-		return err
-	}
-	// TODO: What to do with tx here?
-	_ = tx
-	return nil
-}
-
-func (cm *ChallengeManager) AddLeaf(addLeafArg outgen.AddLeafArgs, proof1, proof2 []byte) error {
-	tx, err := cm.writer.AddLeaf(cm.txOpts, addLeafArg, proof1, proof2)
-	if err != nil {
-		return err
-	}
-	// TODO: Add better error h andling here
-	// TODO: What to do with tx here?
-	_ = tx
-	return nil
 }
