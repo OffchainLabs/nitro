f9dd92db745c409e6364df5cffe68f213af339b8
rauljordan <raul@prysmaticlabs.com>
2023-02-13 17:20:23 -0500

    test deploy full rollup stack

--- protocol/sol-implementation/assertion_chain_test.go
+++ protocol/sol-implementation/assertion_chain_test.go
@@ -2,8 +2,6 @@ package solimpl
 
 import (
 	"context"
-	"crypto/ecdsa"
-	"fmt"
 	"math/big"
 	"testing"
 	"time"
@@ -11,15 +9,9 @@ import (
 	"github.com/OffchainLabs/challenge-protocol-v2/solgen/go/challengeV2gen"
 	"github.com/OffchainLabs/challenge-protocol-v2/util"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/stretchr/testify/require"
 )
-
-func TestFullStack(t *testing.T) {
-}
 
 func TestCreateAssertion(t *testing.T) {
 	ctx := context.Background()
@@ -378,48 +370,4 @@ func setupAssertionChainWithChallengeManager(t *testing.T) (*AssertionChain, *te
 	acc.backend.Commit()
 
 	return chain, acc
-}
-
-// Represents a test EOA account in the simulated backend,
-type testAccount struct {
-	accountAddr common.Address
-	backend     *backends.SimulatedBackend
-	txOpts      *bind.TransactOpts
-}
-
-func setupAccount() (*testAccount, error) {
-	genesis := make(core.GenesisAlloc)
-	privKey, err := crypto.GenerateKey()
-	if err != nil {
-		return nil, err
-	}
-	pubKeyECDSA, ok := privKey.Public().(*ecdsa.PublicKey)
-	if !ok {
-		return nil, fmt.Errorf("error casting public key to ECDSA")
-	}
-
-	// Strip off the 0x and the first 2 characters 04 which is always the
-	// EC prefix and is not required.
-	publicKeyBytes := crypto.FromECDSAPub(pubKeyECDSA)[4:]
-	var pubKey = make([]byte, 48)
-	copy(pubKey, publicKeyBytes)
-
-	addr := crypto.PubkeyToAddress(privKey.PublicKey)
-	chainID := big.NewInt(1337)
-	txOpts, err := bind.NewKeyedTransactorWithChainID(privKey, chainID)
-	if err != nil {
-		return nil, err
-	}
-	startingBalance, _ := new(big.Int).SetString(
-		"100000000000000000000000000000000000000",
-		10,
-	)
-	genesis[addr] = core.GenesisAccount{Balance: startingBalance}
-	gasLimit := uint64(100000000)
-	backend := backends.NewSimulatedBackend(genesis, gasLimit)
-	return &testAccount{
-		accountAddr: addr,
-		backend:     backend,
-		txOpts:      txOpts,
-	}, nil
 }
