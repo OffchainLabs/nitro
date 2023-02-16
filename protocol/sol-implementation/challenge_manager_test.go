dc1f339c79b07615ff872237c0553b7678452218
rauljordan <raul@prysmaticlabs.com>
2023-02-16 13:11:05 -0500

    tests

--- protocol/sol-implementation/challenge_manager_test.go
+++ protocol/sol-implementation/challenge_manager_test.go
@@ -1,48 +1,30 @@
 package solimpl
 
 import (
+	"context"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/require"
 	"testing"
-	// "github.com/OffchainLabs/challenge-protocol-v2/util"
-	// "github.com/ethereum/go-ethereum/common"
-	// "github.com/stretchr/testify/require"
 )
 
 func TestGetChallengeByID(t *testing.T) {
-	// genesisId := common.Hash{}
-	// chain, _ := setupAssertionChainWithChallengeManager(t)
+	ctx := context.Background()
+	height1 := uint64(6)
+	height2 := uint64(7)
+	_, _, challenge, chain, _ := setupTopLevelFork(t, ctx, height1, height2)
 
-	// commit1 := util.StateCommitment{
-	// 	Height:    1,
-	// 	StateRoot: common.BytesToHash([]byte{1}),
-	// }
-	// _, err := chain.CreateAssertion(commit1, 0)
-	// require.NoError(t, err)
+	cm, err := chain.ChallengeManager()
+	require.NoError(t, err)
 
-	// commit2 := util.StateCommitment{
-	// 	Height:    1,
-	// 	StateRoot: common.BytesToHash([]byte{2}),
-	// }
+	t.Run("challenge does not exists", func(t *testing.T) {
+		_, err = cm.ChallengeByID(ctx, common.Hash{})
+		require.ErrorContains(t, err, "challenge not found")
+	})
 
-	// _, err = chain.CreateAssertion(commit2, 0)
-	// require.NoError(t, err)
-
-	// _, err = chain.CreateSuccessionChallenge(0)
-	// require.NoError(t, err)
-
-	// cm, err := chain.ChallengeManager()
-	// require.NoError(t, err)
-
-	// t.Run("challenge does not exists", func(t *testing.T) {
-	// 	_, err = cm.ChallengeByID(genesisId)
-	// 	require.ErrorContains(t, err, "challenge not found")
-	// })
-
-	// t.Run("challenge exists", func(t *testing.T) {
-	// 	cid, err := cm.CalculateChallengeId(genesisId, 0)
-	// 	require.NoError(t, err)
-	// 	challenge, err := cm.ChallengeByID(cid)
-	// 	require.NoError(t, err)
-	// 	require.Equal(t, uint8(0), challenge.inner.ChallengeType)
-	// 	require.Equal(t, [32]byte{}, challenge.inner.WinningClaim)
-	// })
+	t.Run("challenge exists", func(t *testing.T) {
+		challenge, err := cm.ChallengeByID(ctx, challenge.id)
+		require.NoError(t, err)
+		require.Equal(t, uint8(0), challenge.inner.ChallengeType)
+		require.Equal(t, [32]byte{}, challenge.inner.WinningClaim)
+	})
 }
