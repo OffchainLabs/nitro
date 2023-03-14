a436e7e98093eff9ae7d1209bcf13fefb2bae366
rauljordan <raul@prysmaticlabs.com>
2023-03-13 22:43:33 -0400

    rev

--- contracts/src/challengeV2/libraries/ChallengeVertexLib.sol
+++ contracts/src/challengeV2/libraries/ChallengeVertexLib.sol
@@ -107,9 +107,9 @@ library ChallengeVertexLib {
         uint256 initialPsTimeSec
     ) internal pure returns (ChallengeVertex memory) {
         // root is at height 0
-        require(height != 0, "Zero height"); 
         require(challengeId != 0, "Zero challenge id");
         require(historyRoot != 0, "Zero history root");
+        require(height != 0, "Zero height"); 
         require(claimId != 0, "Zero claim id");
         require(staker != address(0), "Zero staker address");
         // initialPsTimeSec can be zero
