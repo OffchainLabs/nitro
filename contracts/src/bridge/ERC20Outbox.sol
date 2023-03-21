c3067b9e4aed621779866622231bb1188889bb2e
Goran Vladika <goran.vladika@gmail.com>
2023-03-21 15:00:45 +0100

    Add 'withdrawalAmount' field to context
    It's used in ERC20 version of rollup, so that call receiver can know how much native tokens it received.
    In standard ETH based rollup this field will always be 0, as amount withdrawn can be read from msg.value

--- contracts/src/bridge/ERC20Outbox.sol
+++ contracts/src/bridge/ERC20Outbox.sol
@@ -6,12 +6,14 @@ pragma solidity ^0.8.4;
 
 import "./AbsOutbox.sol";
 
-contract Outbox is AbsOutbox {
+contract ERC20Outbox is AbsOutbox {
+    uint256 private constant AMOUNT_DEFAULT_CONTEXT = type(uint256).max;
+
     function _defaultContextAmount() internal pure override returns (uint256) {
-        return 0;
+        return AMOUNT_DEFAULT_CONTEXT;
     }
 
-    function _amountToSetInContext(uint256) internal pure override returns (uint256) {
-        return 0;
+    function _amountToSetInContext(uint256 value) internal pure override returns (uint256) {
+        return value;
     }
 }
