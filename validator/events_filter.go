a9225e11dc4f3bbe9f748e6e2318d2264fc7cc37
amsanghi <asanghi@offchainlabs.com>
2023-03-22 19:20:07 +0530

    Use polling for assertion instead of subscribing to the events

--- validator/events_filter.go
+++ validator/events_filter.go
@@ -3,37 +3,27 @@ package validator
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/OffchainLabs/challenge-protocol-v2/protocol"
 	"github.com/OffchainLabs/challenge-protocol-v2/solgen/go/challengeV2gen"
-	"github.com/OffchainLabs/challenge-protocol-v2/solgen/go/rollupgen"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 )
 
 // Subscribes to events fired by the rollup contracts in order to listen to
-// new assertion creations or challenge start events from the protocol.
+// challenge start events from the protocol.
 // TODO: Brittle - should be based on querying the chain instead.
-func (v *Validator) handleRollupEvents(ctx context.Context) {
-	assertionCreatedChan := make(chan *rollupgen.RollupCoreAssertionCreated, 1)
+func (v *Validator) handleChallengeEvents(ctx context.Context) {
 	challengeCreatedChan := make(chan *challengeV2gen.ChallengeManagerImplChallengeCreated, 1)
-	assertionSub, err := v.rollupFilterer.WatchAssertionCreated(&bind.WatchOpts{}, assertionCreatedChan, nil, nil, nil)
-	if err != nil {
-		log.Error(err)
-		return
-	}
-	defer assertionSub.Unsubscribe()
 	chalSub, err := v.chalManager.WatchChallengeCreated(&bind.WatchOpts{}, challengeCreatedChan)
 	if err != nil {
 		log.Error(err)
 		return
 	}
 	defer chalSub.Unsubscribe()
-	defer chalSub.Unsubscribe()
 
 	for {
 		select {
-		case err := <-assertionSub.Err():
-			log.Fatal(err)
 		case err := <-chalSub.Err():
 			log.Fatal(err)
 		case chalCreated := <-challengeCreatedChan:
@@ -63,31 +53,52 @@ func (v *Validator) handleRollupEvents(ctx context.Context) {
 			if err := v.onChallengeStarted(ctx, challenge); err != nil {
 				log.Error(err)
 			}
-		case assertionCreated := <-assertionCreatedChan:
-			var assertion protocol.Assertion
-			if err := v.chain.Call(func(tx protocol.ActiveTx) error {
-				retrieved, err := v.chain.AssertionBySequenceNum(ctx, tx, protocol.AssertionSequenceNumber(assertionCreated.AssertionNum))
-				if err != nil {
-					return err
-				}
-				assertion = retrieved
-				return nil
-			}); err != nil {
-				log.Error(err)
-				continue
-			}
-			assertionNum, err := v.rollup.LatestStakedAssertion(&bind.CallOpts{}, v.address)
-			if err != nil {
-				log.Error(err)
-				continue
-			}
-			// Ignore assertions from self.
-			if assertionNum == uint64(assertion.SeqNum()) {
-				continue
-			}
-			if err := v.onLeafCreated(ctx, assertion); err != nil {
-				log.Error(err)
-			}
 		}
 	}
+}
+
+func (v *Validator) handleAssertions(ctx context.Context) time.Duration {
+	var numberOfAssertions uint64
+	if err := v.chain.Call(func(tx protocol.ActiveTx) error {
+		retrieved, err := v.chain.NumAssertions(ctx, tx)
+		if err != nil {
+			return err
+		}
+		numberOfAssertions = retrieved
+		return nil
+	}); err != nil {
+		log.Error(err)
+		return v.newAssertionCheckInterval
+	}
+	v.assertionsLock.RLock()
+	currentNumberOfAssertions := uint64(len(v.assertions))
+	v.assertionsLock.RUnlock()
+
+	for i := currentNumberOfAssertions; i < numberOfAssertions; i++ {
+		var assertion protocol.Assertion
+		if err := v.chain.Call(func(tx protocol.ActiveTx) error {
+			retrieved, err := v.chain.AssertionBySequenceNum(ctx, tx, protocol.AssertionSequenceNumber(i))
+			if err != nil {
+				return err
+			}
+			assertion = retrieved
+			return nil
+		}); err != nil {
+			log.Error(err)
+			continue
+		}
+		assertionNum, err := v.rollup.LatestStakedAssertion(&bind.CallOpts{}, v.address)
+		if err != nil {
+			log.Error(err)
+			continue
+		}
+		// Ignore assertions from self.
+		if assertionNum == uint64(assertion.SeqNum()) {
+			continue
+		}
+		if err := v.onLeafCreated(ctx, assertion); err != nil {
+			log.Error(err)
+		}
+	}
+	return v.newAssertionCheckInterval
 }
