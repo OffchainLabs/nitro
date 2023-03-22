1cd8f78fe6847dbca657d7a5900597b1b3f65ab0
Goran Vladika <goran.vladika@gmail.com>
2023-03-22 09:38:14 +0100

    Test CallTargetNotAllowed

--- contracts/test/foundry/ERC20Outbox.t.sol
+++ contracts/test/foundry/ERC20Outbox.t.sol
@@ -83,4 +83,53 @@ contract ERC20OutboxTest is AbsOutboxTest {
             "Invalid user token balance"
         );
     }
+
+    function test_executeTransaction_revert_CallTargetNotAllowed() public {
+        // // fund bridge with some tokens
+        vm.startPrank(user);
+        nativeToken.approve(address(bridge), 100);
+        nativeToken.transfer(address(bridge), 100);
+        vm.stopPrank();
+
+        // store root
+        vm.prank(rollup);
+        outbox.updateSendRoot(
+            0x5b6cd410f78e45e55eeb02133b8e72e6ca122c59b667eed4f214e374d808058e,
+            0x5b6cd410f78e45e55eeb02133b8e72e6ca122c59b667eed4f214e374d808058e
+        );
+
+        //// execute transaction
+        uint256 bridgeTokenBalanceBefore = nativeToken.balanceOf(address(bridge));
+        uint256 userTokenBalanceBefore = nativeToken.balanceOf(address(user));
+
+        bytes32[] memory proof = new bytes32[](5);
+        proof[0] = bytes32(0x1216ff070e3c87b032d79b298a3e98009ddd13bf8479b843e225857ca5f950e7);
+        proof[1] = bytes32(0x2b5ee8f4bd7664ca0cf31d7ab86119b63f6ff07bb86dbd5af356d0087492f686);
+        proof[2] = bytes32(0x0aa797064e0f3768bbac0a02ce031c4f282441a9cd8c669086cf59a083add893);
+        proof[3] = bytes32(0xc7aac0aad5108a46ac9879f0b1870fd0cbc648406f733eb9d0b944a18c32f0f8);
+        proof[4] = bytes32(0x477ce2b0bc8035ae3052b7339c7496531229bd642bb1871d81618cf93a4d2d1a);
+
+        uint256 withdrawalAmount = 15;
+
+        address invalidTarget = address(nativeToken);
+
+        vm.expectRevert(abi.encodeWithSelector(CallTargetNotAllowed.selector, invalidTarget));
+        outbox.executeTransaction({
+            proof: proof,
+            index: 12,
+            l2Sender: user,
+            to: invalidTarget,
+            l2Block: 300,
+            l1Block: 20,
+            l2Timestamp: 1234,
+            value: withdrawalAmount,
+            data: ""
+        });
+
+        uint256 bridgeTokenBalanceAfter = nativeToken.balanceOf(address(bridge));
+        assertEq(bridgeTokenBalanceBefore, bridgeTokenBalanceAfter, "Invalid bridge token balance");
+
+        uint256 userTokenBalanceAfter = nativeToken.balanceOf(address(user));
+        assertEq(userTokenBalanceAfter, userTokenBalanceBefore, "Invalid user token balance");
+    }
 }
