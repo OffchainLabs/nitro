3d8d483236808afb781e53726e08bc3cdff3b044
Goran Vladika <goran.vladika@gmail.com>
2023-03-22 08:47:05 +0100

    Add executeTransaction test case for eth rollup

--- contracts/test/foundry/Outbox.t.sol
+++ contracts/test/foundry/Outbox.t.sol
@@ -23,4 +23,51 @@ contract OutboxTest is AbsOutboxTest {
         vm.prank(rollup);
         bridge.setOutbox(address(outbox), true);
     }
+
+    function test_executeTransaction() public {
+        // fund bridge with some ether
+        vm.deal(address(bridge), 100 ether);
+
+        // store root
+        vm.prank(rollup);
+        outbox.updateSendRoot(
+            0xba910cdc206c175a8b0c8945a5f4de852ecaa6454f0f95fced80f4037b69fa69,
+            0xba910cdc206c175a8b0c8945a5f4de852ecaa6454f0f95fced80f4037b69fa69
+        );
+
+        //// execute transaction
+        uint256 bridgeBalanceBefore = address(bridge).balance;
+        uint256 userBalanceBefore = address(user).balance;
+
+        bytes32[] memory proof = new bytes32[](5);
+        proof[0] = bytes32(0x1216ff070e3c87b032d79b298a3e98009ddd13bf8479b843e225857ca5f950e7);
+        proof[1] = bytes32(0x2b5ee8f4bd7664ca0cf31d7ab86119b63f6ff07bb86dbd5af356d0087492f686);
+        proof[2] = bytes32(0x0aa797064e0f3768bbac0a02ce031c4f282441a9cd8c669086cf59a083add893);
+        proof[3] = bytes32(0xc7aac0aad5108a46ac9879f0b1870fd0cbc648406f733eb9d0b944a18c32f0f8);
+        proof[4] = bytes32(0x477ce2b0bc8035ae3052b7339c7496531229bd642bb1871d81618cf93a4d2d1a);
+
+        uint256 withdrawalAmount = 15 ether;
+
+        outbox.executeTransaction({
+            proof: proof,
+            index: 12,
+            l2Sender: user,
+            to: user,
+            l2Block: 300,
+            l1Block: 20,
+            l2Timestamp: 1234,
+            value: withdrawalAmount,
+            data: ""
+        });
+
+        uint256 bridgeBalanceAfter = address(bridge).balance;
+        assertEq(
+            bridgeBalanceBefore - bridgeBalanceAfter,
+            withdrawalAmount,
+            "Invalid bridge balance"
+        );
+
+        uint256 userBalanceAfter = address(user).balance;
+        assertEq(userBalanceAfter - userBalanceBefore, withdrawalAmount, "Invalid user balance");
+    }
 }
