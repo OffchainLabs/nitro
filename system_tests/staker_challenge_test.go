6845f84957c5ebad6d648145f1d32f9fe377d1ea
amsanghi <asanghi@offchainlabs.com>
2023-03-30 23:16:37 +0530

    Skip skater test for race test

--- system_tests/staker_challenge_test.go
+++ system_tests/staker_challenge_test.go
@@ -1,6 +1,10 @@
 // Copyright 2021-2022, Offchain Labs, Inc.
 // For license information, see https://github.com/nitro/blob/master/LICENSE
 
+// race detection makes things slow and miss timeouts
+//go:build challengetest
+// +build challengetest
+
 package arbtest
 
 import "testing"
