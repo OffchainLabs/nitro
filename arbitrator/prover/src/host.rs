1baaef4f1821855940a8d0b7622b1927b0e681e5
Rachel Franks <nfranks@protonmail.com>
2023-04-04 16:25:39 -0600

    apply the latest version of clippy

--- arbitrator/prover/src/host.rs
+++ arbitrator/prover/src/host.rs
@@ -1,6 +1,8 @@
 // Copyright 2021-2022, Offchain Labs, Inc.
 // For license information, see https://github.com/nitro/blob/master/LICENSE
 
+#![allow(clippy::vec_init_then_push)]
+
 use std::collections::HashMap;
 
 use crate::{
