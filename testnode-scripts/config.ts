be564dcba5380c880bd537478197555e1661c205
Tsahi Zidenberg <tsahi@offchainlabs.com>
2023-04-12 18:08:53 -0600

    testnode config.ts updates

--- testnode-scripts/config.ts
+++ testnode-scripts/config.ts
@@ -167,9 +167,7 @@ function writeConfigs(argv: any) {
             }
         },
         "node": {
-            "archive": true,
-            "forwarding-target": "null",
-            "validator": {
+            "staker": {
                 "dangerous": {
                     "without-block-validator": false
                 },
@@ -178,7 +176,6 @@ function writeConfigs(argv: any) {
                 "staker-interval": "10s",
                 "make-assertion-interval": "10s",
                 "strategy": "MakeNodes",
-                "target-machine-count": 4,
             },
             "sequencer":  false,
             "delayed-sequencer": {
@@ -206,6 +203,8 @@ function writeConfigs(argv: any) {
             }
         },
         "execution" : {
+            "archive": true,
+            "forwarding-target": "null",
             "sequencer": {
                 "enable": false
             },
@@ -231,13 +230,13 @@ function writeConfigs(argv: any) {
 
     let validatorConfig = JSON.parse(baseConfJSON)
     validatorConfig.l1.wallet.account = namedAccount("validator").address
-    validatorConfig.node.validator.enable = true
-    validatorConfig.node.validator["use-smart-contract-wallet"] = true
+    validatorConfig.node.staker.enable = true
+    validatorConfig.node.staker["use-smart-contract-wallet"] = true
     let validconfJSON = JSON.stringify(validatorConfig)
     fs.writeFileSync(path.join(consts.configpath, "validator_config.json"), validconfJSON)
 
     let unsafeStakerConfig = JSON.parse(validconfJSON)
-    unsafeStakerConfig.node.validator.dangerous["without-block-validator"] = true
+    unsafeStakerConfig.node.staker.dangerous["without-block-validator"] = true
     fs.writeFileSync(path.join(consts.configpath, "unsafe_staker_config.json"), JSON.stringify(unsafeStakerConfig))
 
     let sequencerConfig = JSON.parse(baseConfJSON)
