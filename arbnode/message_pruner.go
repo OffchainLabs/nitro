9393ca92b4a31fc442d6c0eec7878377d99ae525
amsanghi <asanghi@offchainlabs.com>
2023-06-15 00:04:49 +0530

    Changes based on PR comments

--- arbnode/message_pruner.go
+++ arbnode/message_pruner.go
@@ -89,30 +89,31 @@ func (m *MessagePruner) prune(ctx context.Context) time.Duration {
 }
 
 func deleteOldMessageFromDB(endBatchCount uint64, endBatchMetadata BatchMetadata, inboxTrackerDb ethdb.Database, transactionStreamerDb ethdb.Database) {
-	var allPrunedKeys [][]byte
 	prunedKeys, err := deleteFromLastPrunedUptoEndKey(inboxTrackerDb, sequencerBatchMetaPrefix, endBatchCount)
 	if err != nil {
 		log.Error("error deleting batch metadata: %w", err)
 		return
 	}
-	allPrunedKeys = append(allPrunedKeys, prunedKeys...)
+	if len(prunedKeys) > 0 {
+		log.Info("Pruned batches:", "first pruned key", prunedKeys[0], "last pruned key", prunedKeys[len(prunedKeys)-1])
+	}
 
 	prunedKeys, err = deleteFromLastPrunedUptoEndKey(transactionStreamerDb, messagePrefix, uint64(endBatchMetadata.MessageCount))
 	if err != nil {
-		log.Error("error deleting batch metadata: %w", err)
+		log.Error("error deleting last batch messages: %w", err)
 		return
 	}
-	allPrunedKeys = append(allPrunedKeys, prunedKeys...)
+	if len(prunedKeys) > 0 {
+		log.Info("Pruned last batch messages:", "first pruned key", prunedKeys[0], "last pruned key", prunedKeys[len(prunedKeys)-1])
+	}
 
 	prunedKeys, err = deleteFromLastPrunedUptoEndKey(inboxTrackerDb, rlpDelayedMessagePrefix, endBatchMetadata.DelayedMessageCount)
 	if err != nil {
-		log.Error("error deleting batch metadata: %w", err)
+		log.Error("error deleting last batch delayed messages: %w", err)
 		return
 	}
-	allPrunedKeys = append(allPrunedKeys, prunedKeys...)
-
-	if len(allPrunedKeys) > 0 {
-		log.Info("Pruned keys:", allPrunedKeys)
+	if len(prunedKeys) > 0 {
+		log.Info("Pruned last batch delayed messages", "first pruned key", prunedKeys[0], "last pruned key", prunedKeys[len(prunedKeys)-1])
 	}
 }
 
