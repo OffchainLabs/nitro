0c1b5bd83ede06ed5fcc7f428eaaf33b0007db5c
Tristan Wilson <twilson@offchainlabs.com>
2023-07-06 16:13:57 -0700

    Add comments to resourcemanager

--- arbnode/resourcemanager/resource_management.go
+++ arbnode/resourcemanager/resource_management.go
@@ -25,6 +25,11 @@ var (
 	limitCheckFailureCounter    = metrics.NewRegisteredCounter("arb/rpc/limitcheck/failure", nil)
 )
 
+// Init adds the resource manager's httpServer to a custom hook in geth.
+// Geth will add it to the stack of http.Handlers so that it is run
+// prior to RPC request handling.
+//
+// Must be run before the go-ethereum stack is set up (ethereum/go-ethereum/node.New).
 func Init(conf *Config) {
 	if conf.MemoryLimitPercent > 0 {
 		node.WrapHTTPHandler = func(srv http.Handler) (http.Handler, error) {
@@ -33,18 +38,26 @@ func Init(conf *Config) {
 	}
 }
 
+// Config contains the configuration for resourcemanager functionality.
+// Currently only a memory limit is supported, other limits may be added
+// in the future.
 type Config struct {
 	MemoryLimitPercent int `koanf:"mem-limit-percent" reload:"hot"`
 }
 
+// DefaultConfig has the defaul resourcemanager configuration,
+// all limits are disabled.
 var DefaultConfig = Config{
 	MemoryLimitPercent: 0,
 }
 
+// ConfigAddOptions adds the configuration options for resourcemanager.
 func ConfigAddOptions(prefix string, f *pflag.FlagSet) {
 	f.Int(prefix+".mem-limit-percent", DefaultConfig.MemoryLimitPercent, "RPC calls are throttled if system memory utilization exceeds this percent value, zero (default) is disabled")
 }
 
+// httpServer implements http.Handler and wraps calls to inner with a resource
+// limit check.
 type httpServer struct {
 	inner http.Handler
 	c     limitChecker
@@ -54,6 +67,8 @@ func newHttpServer(inner http.Handler, c limitChecker) *httpServer {
 	return &httpServer{inner: inner, c: c}
 }
 
+// ServeHTTP passes req to inner unless any configured system resource
+// limit is exceeded, in which case it returns a HTTP 429 error.
 func (s *httpServer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	start := time.Now()
 	exceeded, err := s.c.isLimitExceeded()
@@ -75,19 +90,23 @@ type limitChecker interface {
 	String() string
 }
 
+// newLimitChecker attempts to auto-discover the mechanism by which it
+// can check system limits. Currently Cgroups V1 is supported,
+// with Cgroups V2 likely to be implmemented next. If no supported
+// mechanism is discovered, it logs an error and fails open, ie
+// it creates a trivialLimitChecker that does no checks.
 func newLimitChecker(conf *Config) limitChecker {
-	{
-		c := newCgroupsV1MemoryLimitChecker(DefaultCgroupsV1MemoryDirectory, conf.MemoryLimitPercent)
-		if isSupported(c) {
-			log.Info("Cgroups v1 detected, enabling memory limit RPC throttling")
-			return c
-		}
+	c := newCgroupsV1MemoryLimitChecker(DefaultCgroupsV1MemoryDirectory, conf.MemoryLimitPercent)
+	if isSupported(c) {
+		log.Info("Cgroups v1 detected, enabling memory limit RPC throttling")
+		return c
 	}
 
 	log.Error("No method for determining memory usage and limits was discovered, disabled memory limit RPC throttling")
 	return &trivialLimitChecker{}
 }
 
+// trivialLimitChecker checks no limits, so its limits are never exceeded.
 type trivialLimitChecker struct{}
 
 func (_ trivialLimitChecker) isLimitExceeded() (bool, error) {
@@ -120,6 +139,12 @@ func isSupported(c limitChecker) bool {
 	return err == nil
 }
 
+// isLimitExceeded checks if the system memory used exceeds the limit
+// scaled by the configured memoryLimitPercent.
+//
+// See the following page for details of calculating the memory used,
+// which is reported as container_memory_working_set_bytes in prometheus:
+// https://mihai-albert.com/2022/02/13/out-of-memory-oom-in-kubernetes-part-3-memory-metrics-sources-and-tools-to-collect-them/
 func (c *cgroupsV1MemoryLimitChecker) isLimitExceeded() (bool, error) {
 	var limit, usage, inactive int
 	var err error
