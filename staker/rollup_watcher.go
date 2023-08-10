0ec2f62fb2826d6f767e1262aff56bc406823906
ganeshvanahalli <ganeshdvanahalli@gmail.com>
2023-08-10 15:09:51 -0500

    add unSupportedL3Method check

--- staker/rollup_watcher.go
+++ staker/rollup_watcher.go
@@ -9,6 +9,8 @@ import (
 	"errors"
 	"fmt"
 	"math/big"
+	"strings"
+	"sync/atomic"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
@@ -43,10 +45,11 @@ type StakerInfo struct {
 
 type RollupWatcher struct {
 	*rollupgen.RollupUserLogic
-	address      common.Address
-	fromBlock    *big.Int
-	client       arbutil.L1Interface
-	baseCallOpts bind.CallOpts
+	address             common.Address
+	fromBlock           *big.Int
+	client              arbutil.L1Interface
+	baseCallOpts        bind.CallOpts
+	unSupportedL3Method atomic.Bool
 }
 
 func NewRollupWatcher(address common.Address, client arbutil.L1Interface, callOpts bind.CallOpts) (*RollupWatcher, error) {
@@ -71,15 +74,21 @@ func (r *RollupWatcher) getCallOpts(ctx context.Context) *bind.CallOpts {
 
 func (r *RollupWatcher) getNodeCreationBlock(ctx context.Context, nodeNum uint64) (*big.Int, error) {
 	callOpts := r.getCallOpts(ctx)
-	createdAtBlock, err := r.GetNodeCreationBlockForLogLookup(callOpts, nodeNum)
-	if err != nil {
-		log.Trace("failed to call getNodeCreationBlockForLogLookup, falling back on node CreatedAtBlock field", "err", err)
-		node, err := r.GetNode(callOpts, nodeNum)
-		if err != nil {
-			return nil, err
+	if !r.unSupportedL3Method.Load() {
+		createdAtBlock, err := r.GetNodeCreationBlockForLogLookup(callOpts, nodeNum)
+		if err == nil {
+			return createdAtBlock, nil
 		}
-		createdAtBlock = new(big.Int).SetUint64(node.CreatedAtBlock)
+		log.Trace("failed to call getNodeCreationBlockForLogLookup, falling back on node CreatedAtBlock field", "err", err)
+		if strings.Contains(err.Error(), "execution reverted") {
+			r.unSupportedL3Method.Store(true)
+		}
 	}
+	node, err := r.GetNode(callOpts, nodeNum)
+	if err != nil {
+		return nil, err
+	}
+	createdAtBlock := new(big.Int).SetUint64(node.CreatedAtBlock)
 	return createdAtBlock, nil
 }
 
