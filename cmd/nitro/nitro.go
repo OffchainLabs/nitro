5b234fc065f4e1c6adb5f78180157cbde7b2f920
Tristan Wilson <twilson@offchainlabs.com>
2023-08-14 14:11:52 -0700

    Plumb batch limit options to geth
    Upstream implemented limits on the number of requests in a batch and the
    maximum batch response size. We had already implemented the batch
    response size limit in our geth fork, but have rolled it back in favor
    of upstream. This commit adds a nitro option for the batch request limit
    and takes the existing size limit option and plumbs them through to
    geth's node.Config.
    
    The flags in Nitro are:
    --rpc.max-batch-response-size
    --rpc.batch-request-limit

--- cmd/nitro/nitro.go
+++ cmd/nitro/nitro.go
@@ -155,6 +155,7 @@ func mainImpl() int {
 	stackConf := node.DefaultConfig
 	stackConf.DataDir = nodeConfig.Persistent.Chain
 	stackConf.DBEngine = "leveldb"
+	nodeConfig.Rpc.Apply(&stackConf)
 	nodeConfig.HTTP.Apply(&stackConf)
 	nodeConfig.WS.Apply(&stackConf)
 	nodeConfig.AuthRPC.Apply(&stackConf)
@@ -735,7 +736,6 @@ func ParseNode(ctx context.Context, args []string) (*NodeConfig, *genericconf.Wa
 	if err != nil {
 		return nil, nil, nil, err
 	}
-	nodeConfig.Rpc.Apply()
 	return &nodeConfig, &l1Wallet, &l2DevWallet, nil
 }
 
