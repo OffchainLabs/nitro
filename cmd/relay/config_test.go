1c31be29371c5cc9260ed6c842165686d6ce5499
Nodar <nodar.ambroladze@gmail.com>
2023-08-16 18:15:14 +0200

    Rename url to urls in RelayConfig test

--- cmd/relay/config_test.go
+++ cmd/relay/config_test.go
@@ -13,7 +13,7 @@ import (
 )
 
 func TestRelayConfig(t *testing.T) {
-	args := strings.Split("--node.feed.output.port 9652 --node.feed.input.url ws://sequencer:9642/feed", " ")
+	args := strings.Split("--node.feed.output.port 9652 --node.feed.input.urls ws://sequencer:9642/feed", " ")
 	_, err := relay.ParseRelay(context.Background(), args)
 	testhelpers.RequireImpl(t, err)
 }
