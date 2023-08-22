d0e6c7c8dbb0e73322ce1a9e0251cf9eb884dacb
Nodar <nodar.ambroladze@gmail.com>
2023-08-22 17:38:54 +0200

    Detect pointers in selectors

--- linter/pointercheck/pointer_test.go
+++ linter/pointercheck/pointer_test.go
@@ -15,8 +15,8 @@ func TestAll(t *testing.T) {
 	}
 	testdata := filepath.Join(filepath.Dir(wd), "testdata")
 	res := analysistest.Run(t, testdata, analyzerForTests, "pointercheck")
-	if cnt := countErrors(res); cnt != 5 {
-		t.Errorf("analysistest.Run() got %v errors, expected 5", cnt)
+	if cnt := countErrors(res); cnt != 6 {
+		t.Errorf("analysistest.Run() got %v errors, expected 6", cnt)
 	}
 }
 
