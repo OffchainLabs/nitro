dcb0db479042533b0edd94a62c9efa739ff35811
Rachel Bousfield <nfranks@protonmail.com>
2023-08-24 16:26:25 -0600

    shrink erc20 binary size

--- arbitrator/stylus/tests/erc20/src/main.rs
+++ arbitrator/stylus/tests/erc20/src/main.rs
@@ -8,6 +8,9 @@
 use crate::erc20::{Erc20, Erc20Params};
 use stylus_sdk::{alloy_primitives::U256, call, msg, prelude::*};
 
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 mod erc20;
 
 struct WethParams;
