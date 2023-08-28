2b01cf8fe37f115f548ae481e2deb34d6cbecef1
Nodar <nodar.ambroladze@gmail.com>
2023-08-28 17:34:31 +0200

    Implement linter detection of koanf fields that aren't used outside flag definitions

--- linter/koanf/koanf.go
+++ linter/koanf/koanf.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"go/ast"
 	"go/token"
 	"reflect"
-	"strings"
-	"unicode"
 
-	"github.com/fatih/structtag"
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/singlechecker"
+)
+
+var (
+	errUnused   = errors.New("unused")
+	errMismatch = errors.New("mismmatched field name and tag in a struct")
+	// e.g. f.Int("max-sz", DefaultBatchPosterConfig.MaxSize, "maximum batch size")
+	errIncorrectFlag = errors.New("mismatching flag initialization")
 )
 
 func New(conf any) ([]*analysis.Analyzer, error) {
@@ -33,8 +38,9 @@ var analyzerForTests = &analysis.Analyzer{
 
 // koanfError indicates the position of an error in configuration.
 type koanfError struct {
-	Pos     token.Position
+	Pos     token.Pos
 	Message string
+	err     error
 }
 
 // Result is returned from the checkStruct function, and holds all the
@@ -44,7 +50,14 @@ type Result struct {
 }
 
 func run(dryRun bool, pass *analysis.Pass) (interface{}, error) {
-	var ret Result
+	var (
+		ret Result
+		cnt = make(map[string]int)
+		// koanfFields map contains all the struct koanfFields that have koanf tag.
+		// It identifies field as "{pkgName}.{structName}.{field_Name}".
+		// e.g. "a.BatchPosterConfig.Enable", "a.BatchPosterConfig.MaxSize"
+		koanfFields = koanfFields(pass)
+	)
 	for _, f := range pass.Files {
 		ast.Inspect(f, func(node ast.Node) bool {
 			var res Result
@@ -52,167 +65,41 @@ func run(dryRun bool, pass *analysis.Pass) (interface{}, error) {
 			case *ast.StructType:
 				res = checkStruct(pass, v)
 			case *ast.FuncDecl:
-				res = checkFlagDefs(pass, v)
+				res = checkFlagDefs(pass, v, cnt)
+			case *ast.SelectorExpr:
+				handleSelector(pass, v, 1, cnt)
+			case *ast.IfStmt:
+				if se, ok := v.Cond.(*ast.SelectorExpr); ok {
+					handleSelector(pass, se, 1, cnt)
+				}
+			case *ast.CompositeLit:
+				handleComposite(pass, v, cnt)
 			default:
 			}
-			for _, err := range res.Errors {
-				ret.Errors = append(ret.Errors, err)
-				if !dryRun {
-					pass.Report(analysis.Diagnostic{
-						Pos:      pass.Fset.File(f.Pos()).Pos(err.Pos.Offset),
-						Message:  err.Message,
-						Category: "koanf",
-					})
-				}
-			}
+			ret.Errors = append(ret.Errors, res.Errors...)
 			return true
-		},
-		)
+		})
+	}
+	for k := range koanfFields {
+		if cnt[k] == 0 {
+			ret.Errors = append(ret.Errors,
+				koanfError{
+					Pos:     koanfFields[k],
+					Message: fmt.Sprintf("field %v not used", k),
+					err:     errUnused,
+				})
+		}
+	}
+	for _, err := range ret.Errors {
+		if !dryRun {
+			pass.Report(analysis.Diagnostic{
+				Pos:      err.Pos,
+				Message:  err.Message,
+				Category: "koanf",
+			})
+		}
 	}
 	return ret, nil
-}
-
-func containsFlagSet(params []*ast.Field) bool {
-	for _, p := range params {
-		se, ok := p.Type.(*ast.StarExpr)
-		if !ok {
-			continue
-		}
-		sle, ok := se.X.(*ast.SelectorExpr)
-		if !ok {
-			continue
-		}
-		if sle.Sel.Name == "FlagSet" {
-			return true
-		}
-	}
-	return false
-}
-
-// checkFlagDefs checks flag definitions in the function.
-// Result contains list of errors where flag name doesn't match field name.
-func checkFlagDefs(pass *analysis.Pass, f *ast.FuncDecl) Result {
-	// Ignore functions that does not get flagset as parameter.
-	if !containsFlagSet(f.Type.Params.List) {
-		return Result{}
-	}
-	var res Result
-	for _, s := range f.Body.List {
-		es, ok := s.(*ast.ExprStmt)
-		if !ok {
-			continue
-		}
-		callE, ok := es.X.(*ast.CallExpr)
-		if !ok {
-			continue
-		}
-		if len(callE.Args) != 3 {
-			continue
-		}
-		sl, ok := extractStrLit(callE.Args[0])
-		if !ok {
-			continue
-		}
-		s, ok := selector(callE.Args[1])
-		if !ok {
-			continue
-		}
-		if normSL := normalize(sl); !strings.EqualFold(normSL, s) {
-			res.Errors = append(res.Errors, koanfError{
-				Pos:     pass.Fset.Position(f.Pos()),
-				Message: fmt.Sprintf("koanf tag name: %q doesn't match the field: %q", sl, s),
-			})
-		}
-
-	}
-	return res
-}
-
-func selector(e ast.Expr) (string, bool) {
-	n, ok := e.(ast.Node)
-	if !ok {
-		return "", false
-	}
-	se, ok := n.(*ast.SelectorExpr)
-	if !ok {
-		return "", false
-	}
-	return se.Sel.Name, true
-}
-
-// Extracts literal from expression that is either:
-// - string literal or
-// - sum of variable and string literal.
-// E.g.
-// strLitFromSum(`"max-size"`) = "max-size"
-// - strLitFromSum(`prefix + ".enable"“) = ".enable".
-func extractStrLit(e ast.Expr) (string, bool) {
-	if s, ok := strLit(e); ok {
-		return s, true
-	}
-	if be, ok := e.(*ast.BinaryExpr); ok {
-		if be.Op == token.ADD {
-			if s, ok := strLit(be.Y); ok {
-				// Drop the prefix dot.
-				return s[1:], true
-			}
-		}
-	}
-	return "", false
-}
-
-func strLit(e ast.Expr) (string, bool) {
-	if s, ok := e.(*ast.BasicLit); ok {
-		if s.Kind == token.STRING {
-			return strings.Trim(s.Value, "\""), true
-		}
-	}
-	return "", false
-}
-
-func checkStruct(pass *analysis.Pass, s *ast.StructType) Result {
-	var res Result
-	for _, f := range s.Fields.List {
-		if f.Tag == nil {
-			continue
-		}
-		tags, err := structtag.Parse(strings.Trim((f.Tag.Value), "`"))
-		if err != nil {
-			continue
-		}
-		tag, err := tags.Get("koanf")
-		if err != nil {
-			continue
-		}
-		tagName := normalize(tag.Name)
-		fieldName := f.Names[0].Name
-		if !strings.EqualFold(tagName, fieldName) {
-			res.Errors = append(res.Errors, koanfError{
-				Pos:     pass.Fset.Position(f.Pos()),
-				Message: fmt.Sprintf("field name: %q doesn't match tag name: %q\n", fieldName, tagName),
-			})
-		}
-	}
-	return res
-}
-
-func normalize(s string) string {
-	ans := s[:1]
-	for i := 1; i < len(s); i++ {
-		c := rune(s[i])
-		if !isAlphanumeric(c) {
-			continue
-		}
-		if !isAlphanumeric(rune(s[i-1])) && unicode.IsLower(c) {
-			c = unicode.ToUpper(c)
-		}
-		ans += string(c)
-	}
-	return ans
-}
-
-func isAlphanumeric(c rune) bool {
-	return unicode.IsLetter(c) || unicode.IsDigit(c)
 }
 
 func main() {
