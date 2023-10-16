82663481ad99b6ea6eef6440aed735e3cdedd243
ganeshvanahalli <ganeshdvanahalli@gmail.com>
2023-10-16 09:58:24 -0500

    test success

--- .github/workflows/waitForNitro.sh
+++ .github/workflows/waitForNitro.sh
@@ -1,6 +1,6 @@
 # poll the nitro endpoint until we get a 0 return code or 30mins have passed, in that case exit 1
 start_time=$(date +%s)
-timeout=20
+timeout=1800
 
 while (( $(date +%s) - start_time <= timeout )); do
   curl -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","id":45678,"method":"eth_chainId","params":[]}' 'http://localhost:8547'
