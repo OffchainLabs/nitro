5f70043183b38e315870d7ee951cc8614f9b2928
Mathis Antony <sveitser@gmail.com>
2023-10-17 18:48:01 -0700

    WIP: nix build

--- flake.nix
+++ flake.nix
@@ -38,48 +38,67 @@
         };
       in
       {
-        devShells.default = 
-        let
+        devShells.default =
+          let
             stableToolchain = pkgs.rust-bin.stable.latest.minimal.override {
               extensions = [ "rustfmt" "clippy" "llvm-tools-preview" "rust-src" ];
-              targets = ["wasm32-unknown-unknown" "wasm32-wasi"];
+              targets = [ "wasm32-unknown-unknown" "wasm32-wasi" ];
             };
-        in 
-        pkgs.mkShell{
-          packages = with pkgs; [
-            go
-            # goimports, godoc, etc.
-            gotools
-            golangci-lint
+          in
+          # pkgs.mkShell.override { stdenv = pkgs.llvmPackages_16.stdenv; } {
+          pkgs.mkShell {
 
-            # Node
-            nodejs
-            pnpm
-            yarn
+            # By default clang does not find its resource dir. See
+            # https://discourse.nixos.org/t/why-is-the-clang-resource-dir-split-in-a-separate-package/34114
+            CPATH = "${pkgs.llvmPackages_16.libclang.lib}/lib/clang/16/include";
+            packages = with pkgs; [
+              stableToolchain
 
-            rust-cbindgen
-            cmake
-            wabt
-            llvmPackages_15.clang
-            libiconv
-            cargo
-            clang
-            stableToolchain
+              llvmPackages_16.clang-unwrapped # provides clang without wrapper
+              llvmPackages_16.bintools-unwrapped # provides wasm-ld
+              llvmPackages_16.llvm
 
+              go
+              # goimports, godoc, etc.
+              gotools
+              golangci-lint
 
-            # Docker
-            docker-compose # provides the `docker-compose` command
-          ] ++ lib.optionals stdenv.isDarwin [
-            darwin.libobjc
-            darwin.IOKit
-            darwin.apple_sdk.frameworks.CoreFoundation
-          ];
-          shellHook = ''
-                # Prevent cargo aliases from using programs in `~/.cargo` to avoid conflicts
-                # with rustup installations.
-                export CARGO_HOME=$HOME/.cargo-nix
-              '';
-          RUST_SRC_PATH = "${stableToolchain}/lib/rustlib/src/rust/library";
-        };
+              # Node
+              nodejs
+              yarn
+
+              rust-cbindgen
+              # cmake
+              # wabt
+              # libiconv
+              # cargo
+
+              # Docker
+              docker-compose # provides the `docker-compose` command
+              docker-buildx
+              docker-credential-helpers # for `docker-credential-osxkeychain` command
+            ] ++ lib.optionals stdenv.isDarwin [
+              darwin.libobjc
+              darwin.IOKit
+              darwin.apple_sdk.frameworks.CoreFoundation
+              # darwin.apple_sdk.Libsystem
+            ];
+            # With the unwrapped clang first in the path we can run `make build-wasm-libs` but
+            # it breaks `cargo build --manifest-path arbitrator/Cargo.toml --release --lib -p prover`
+            # because it doesn't find the standard library when compiling with clang.
+            shellHook = ''
+              export PATH="${pkgs.llvmPackages_16.clang-unwrapped}/bin:$PATH"
+
+              # Prevent cargo aliases from using programs in `~/.cargo` to avoid conflicts
+              # with rustup installations.
+              export CARGO_HOME=$HOME/.cargo-nix
+
+              # Fix docker-buildx command on OSX. Can we do this in a cleaner way?
+              mkdir -p ~/.docker/cli-plugins
+              # Check if the file exists, otherwise symlink
+              test -f $HOME/.docker/cli-plugins/docker-buildx || ln -sn $(which docker-buildx) $HOME/.docker/cli-plugins
+            '';
+            RUST_SRC_PATH = "${stableToolchain}/lib/rustlib/src/rust/library";
+          };
       });
 }
