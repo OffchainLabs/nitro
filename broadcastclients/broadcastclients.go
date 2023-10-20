878ec4ae5df30be16422d20ce30c937c4a03488c
ganeshvanahalli <ganeshdvanahalli@gmail.com>
2023-10-20 11:26:38 -0500

    address PR comments, refactor code to use two router design

--- broadcastclients/broadcastclients.go
+++ broadcastclients/broadcastclients.go
@@ -21,6 +21,7 @@ const ROUTER_QUEUE_SIZE = 1024
 const RECENT_FEED_INITIAL_MAP_SIZE = 1024
 const RECENT_FEED_ITEM_TTL = time.Second * 10
 const MAX_FEED_INACTIVE_TIME = time.Second * 5
+const PRIMARY_FEED_UPTIME = time.Minute * 10
 
 type Router struct {
 	stopwaiter.StopWaiter
@@ -43,7 +44,8 @@ type BroadcastClients struct {
 	secondaryClients      []*broadcastclient.BroadcastClient
 	numOfStartedSecondary int
 
-	router *Router
+	primaryRouter   *Router
+	secondaryRouter *Router
 
 	// Use atomic access
 	connected int32
@@ -62,17 +64,20 @@ func NewBroadcastClients(
 	if len(config.URL) == 0 && len(config.SecondaryURL) == 0 {
 		return nil, nil
 	}
-
-	clients := BroadcastClients{
-		router: &Router{
+	newStandardRouter := func() *Router {
+		return &Router{
 			messageChan:                 make(chan broadcaster.BroadcastFeedMessage, ROUTER_QUEUE_SIZE),
 			confirmedSequenceNumberChan: make(chan arbutil.MessageIndex, ROUTER_QUEUE_SIZE),
 			forwardTxStreamer:           txStreamer,
 			forwardConfirmationChan:     confirmedSequenceNumberListener,
-		},
+		}
+	}
+	clients := BroadcastClients{
+		primaryRouter:   newStandardRouter(),
+		secondaryRouter: newStandardRouter(),
 	}
 	var lastClientErr error
-	makeFeeds := func(url []string) []*broadcastclient.BroadcastClient {
+	makeFeeds := func(url []string, router *Router) []*broadcastclient.BroadcastClient {
 		feeds := make([]*broadcastclient.BroadcastClient, 0, len(url))
 		for _, address := range url {
 			client, err := broadcastclient.NewBroadcastClient(
@@ -80,8 +85,8 @@ func NewBroadcastClients(
 				address,
 				l2ChainId,
 				currentMessageCount,
-				clients.router,
-				clients.router.confirmedSequenceNumberChan,
+				router,
+				router.confirmedSequenceNumberChan,
 				fatalErrChan,
 				addrVerifier,
 				func(delta int32) { clients.adjustCount(delta) },
@@ -96,8 +101,8 @@ func NewBroadcastClients(
 		return feeds
 	}
 
-	clients.primaryClients = makeFeeds(config.URL)
-	clients.secondaryClients = makeFeeds(config.SecondaryURL)
+	clients.primaryClients = makeFeeds(config.URL, clients.primaryRouter)
+	clients.secondaryClients = makeFeeds(config.SecondaryURL, clients.secondaryRouter)
 
 	if len(clients.primaryClients) == 0 && len(clients.secondaryClients) == 0 {
 		log.Error("no connected feed on startup, last error: %w", lastClientErr)
@@ -121,7 +126,8 @@ func (bcs *BroadcastClients) adjustCount(delta int32) {
 }
 
 func (bcs *BroadcastClients) Start(ctx context.Context) {
-	bcs.router.StopWaiter.Start(ctx, bcs.router)
+	bcs.primaryRouter.StopWaiter.Start(ctx, bcs.primaryRouter)
+	bcs.secondaryRouter.StopWaiter.Start(ctx, bcs.secondaryRouter)
 
 	for _, client := range bcs.primaryClients {
 		client.Start(ctx)
@@ -130,17 +136,24 @@ func (bcs *BroadcastClients) Start(ctx context.Context) {
 	var lastConfirmed arbutil.MessageIndex
 	recentFeedItemsNew := make(map[arbutil.MessageIndex]time.Time, RECENT_FEED_INITIAL_MAP_SIZE)
 	recentFeedItemsOld := make(map[arbutil.MessageIndex]time.Time, RECENT_FEED_INITIAL_MAP_SIZE)
-	bcs.router.LaunchThread(func(ctx context.Context) {
+	bcs.primaryRouter.LaunchThread(func(ctx context.Context) {
 		recentFeedItemsCleanup := time.NewTicker(RECENT_FEED_ITEM_TTL)
-		startNewFeedTimer := time.NewTicker(MAX_FEED_INACTIVE_TIME)
+		startSecondaryFeedTimer := time.NewTicker(MAX_FEED_INACTIVE_TIME)
+		stopSecondaryFeedTimer := time.NewTicker(PRIMARY_FEED_UPTIME)
+		primaryFeedIsDownTimer := time.NewTicker(MAX_FEED_INACTIVE_TIME)
 		defer recentFeedItemsCleanup.Stop()
-		defer startNewFeedTimer.Stop()
+		defer startSecondaryFeedTimer.Stop()
+		defer stopSecondaryFeedTimer.Stop()
+		defer primaryFeedIsDownTimer.Stop()
 		for {
 			select {
 			case <-ctx.Done():
 				return
-			case msg := <-bcs.router.messageChan:
-				startNewFeedTimer.Reset(MAX_FEED_INACTIVE_TIME)
+
+			// Primary feeds
+			case msg := <-bcs.primaryRouter.messageChan:
+				startSecondaryFeedTimer.Reset(MAX_FEED_INACTIVE_TIME)
+				primaryFeedIsDownTimer.Reset(MAX_FEED_INACTIVE_TIME)
 				if _, ok := recentFeedItemsNew[msg.SequenceNumber]; ok {
 					continue
 				}
@@ -148,29 +161,55 @@ func (bcs *BroadcastClients) Start(ctx context.Context) {
 					continue
 				}
 				recentFeedItemsNew[msg.SequenceNumber] = time.Now()
-				// need to stop the timer because forwardTxStreamer might be blocked when traffic is high
-				// and that shouldn't create race condition between channels timer.C and messageChan
-				startNewFeedTimer.Stop()
-				if err := bcs.router.forwardTxStreamer.AddBroadcastMessages([]*broadcaster.BroadcastFeedMessage{&msg}); err != nil {
-					log.Error("Error routing message from Sequencer Feed", "err", err)
+				if err := bcs.primaryRouter.forwardTxStreamer.AddBroadcastMessages([]*broadcaster.BroadcastFeedMessage{&msg}); err != nil {
+					log.Error("Error routing message from Primary Sequencer Feeds", "err", err)
 				}
-				startNewFeedTimer.Reset(MAX_FEED_INACTIVE_TIME)
-			case cs := <-bcs.router.confirmedSequenceNumberChan:
-				startNewFeedTimer.Reset(MAX_FEED_INACTIVE_TIME)
+			case cs := <-bcs.primaryRouter.confirmedSequenceNumberChan:
+				startSecondaryFeedTimer.Reset(MAX_FEED_INACTIVE_TIME)
+				primaryFeedIsDownTimer.Reset(MAX_FEED_INACTIVE_TIME)
 				if cs == lastConfirmed {
 					continue
 				}
 				lastConfirmed = cs
-				startNewFeedTimer.Stop()
-				bcs.router.forwardConfirmationChan <- cs
-				startNewFeedTimer.Reset(MAX_FEED_INACTIVE_TIME)
+				bcs.primaryRouter.forwardConfirmationChan <- cs
+
+			// Secondary Feeds
+			case msg := <-bcs.secondaryRouter.messageChan:
+				startSecondaryFeedTimer.Reset(MAX_FEED_INACTIVE_TIME)
+				if _, ok := recentFeedItemsNew[msg.SequenceNumber]; ok {
+					continue
+				}
+				if _, ok := recentFeedItemsOld[msg.SequenceNumber]; ok {
+					continue
+				}
+				recentFeedItemsNew[msg.SequenceNumber] = time.Now()
+				if err := bcs.secondaryRouter.forwardTxStreamer.AddBroadcastMessages([]*broadcaster.BroadcastFeedMessage{&msg}); err != nil {
+					log.Error("Error routing message from Secondary Sequencer Feeds", "err", err)
+				}
+			case cs := <-bcs.secondaryRouter.confirmedSequenceNumberChan:
+				startSecondaryFeedTimer.Reset(MAX_FEED_INACTIVE_TIME)
+				if cs == lastConfirmed {
+					continue
+				}
+				lastConfirmed = cs
+				bcs.secondaryRouter.forwardConfirmationChan <- cs
+
+			// Cycle buckets to get rid of old entries
 			case <-recentFeedItemsCleanup.C:
-				// Cycle buckets to get rid of old entries
 				recentFeedItemsOld = recentFeedItemsNew
 				recentFeedItemsNew = make(map[arbutil.MessageIndex]time.Time, RECENT_FEED_INITIAL_MAP_SIZE)
-			case <-startNewFeedTimer.C:
-				// failed to get messages from primary feed for ~5 seconds, start a new feed
+
+			// failed to get messages from both primary and secondary feeds for ~5 seconds, start a new secondary feed
+			case <-startSecondaryFeedTimer.C:
 				bcs.StartSecondaryFeed(ctx)
+
+			// failed to get messages from primary feed for ~5 seconds, reset the timer responsible for stopping a secondary
+			case <-primaryFeedIsDownTimer.C:
+				stopSecondaryFeedTimer.Reset(PRIMARY_FEED_UPTIME)
+
+			// primary feeds have been up and running for PRIMARY_FEED_UPTIME=10 mins without a failure, stop the recently started secondary feed
+			case <-stopSecondaryFeedTimer.C:
+				bcs.StopSecondaryFeed(ctx)
 			}
 		}
 	})
@@ -183,6 +222,13 @@ func (bcs *BroadcastClients) StartSecondaryFeed(ctx context.Context) {
 		client.Start(ctx)
 	} else {
 		log.Warn("failed to start a new secondary feed all available secondary feeds were started")
+	}
+}
+func (bcs *BroadcastClients) StopSecondaryFeed(ctx context.Context) {
+	if bcs.numOfStartedSecondary > 0 {
+		bcs.numOfStartedSecondary -= 1
+		client := bcs.secondaryClients[bcs.numOfStartedSecondary]
+		client.StopAndWait()
 	}
 }
 
