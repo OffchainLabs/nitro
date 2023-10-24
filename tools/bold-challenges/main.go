fa70db37b09a016c764500079919060d6c507a8f
Raul Jordan <raul@prysmaticlabs.com>
2023-10-24 15:50:36 -0400

    add tooling

--- tools/bold-challenges/main.go
+++ tools/bold-challenges/main.go
@@ -1,39 +1,75 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"math/big"
 
 	solimpl "github.com/OffchainLabs/bold/chain-abstraction/sol-implementation"
+	"github.com/OffchainLabs/bold/solgen/go/mocksgen"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rpc"
+)
+
+var (
+	valPrivKey        = flag.String("validator-priv-key", "", "validator private key")
+	l1ChainIdStr      = flag.String("l1-chain-id", "", "l1 chain id")
+	l1EndpointUrl     = flag.String("l1-endpoint", "", "l1 endpoint")
+	rollupAddrStr     = flag.String("rollup-address", "", "rollup address")
+	stakeTokenAddrStr = flag.String("stake-token-address", "", "rollup address")
+	tokensToDeposit   = flag.String("tokens-to-deposit", "5", "tokens to deposit")
 )
 
 func main() {
-	validatorPrivateKey, err := crypto.HexToECDSA("182fecf15bdf909556a0f617a63e05ab22f1493d25a9f1e27c228266c772a890")
+	ctx := context.Background()
+	// validatorPrivateKey, err := crypto.HexToECDSA("182fecf15bdf909556a0f617a63e05ab22f1493d25a9f1e27c228266c772a890")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	endpoint, err := rpc.Dial(*l1EndpointUrl)
 	if err != nil {
 		panic(err)
 	}
-	validatorTxOpts, err := bind.NewKeyedTransactorWithChainID(validatorPrivateKey, l1ChainId)
+	client := ethclient.NewClient(endpoint)
+	l1ChainId, ok := new(big.Int).SetString(*l1ChainIdStr, 10)
+	if !ok {
+		panic("not big int")
+	}
+	validatorPrivateKey, err := crypto.HexToECDSA(*valPrivKey)
 	if err != nil {
 		panic(err)
 	}
-	mintTokens, ok := new(big.Int).SetString("10000", 10)
+	txOpts, err := bind.NewKeyedTransactorWithChainID(validatorPrivateKey, l1ChainId)
+	if err != nil {
+		panic(err)
+	}
+	stakeTokenAddr := common.HexToAddress(*stakeTokenAddrStr)
+	tokenBindings, err := mocksgen.NewTestWETH9(stakeTokenAddr, client)
+	if err != nil {
+		panic(err)
+	}
+	depositAmount, ok := new(big.Int).SetString(*tokensToDeposit, 10)
 	if !ok {
 		panic("could not set stake token value")
 	}
-	l1TransactionOpts.Value = mintTokens
-	tx, err = tokenBindings.Deposit(l1TransactionOpts)
+	txOpts.Value = depositAmount
+	tx, err := tokenBindings.Deposit(txOpts)
 	if err != nil {
 		panic(err)
 	}
+	_ = tx
 
+	rollupAddr := common.HexToAddress(*rollupAddrStr)
 	// We then have the validator itself authorize the rollup and challenge manager
 	// contracts to spend its stake tokens.
 	chain, err := solimpl.NewAssertionChain(
 		ctx,
-		deployedAddresses.Rollup,
-		validatorTxOpts,
-		l1Reader.Client(),
+		rollupAddr,
+		txOpts,
+		client,
 	)
 	if err != nil {
 		panic(err)
@@ -42,19 +78,14 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	amountToApproveSpend, ok := new(big.Int).SetString("10000", 10)
-	if !ok {
-		panic("not ok")
-	}
-	tx, err = tokenBindings.TestWETH9Transactor.Approve(validatorTxOpts, deployedAddresses.Rollup, amountToApproveSpend)
+	amountToApproveSpend := depositAmount
+	tx, err = tokenBindings.TestWETH9Transactor.Approve(txOpts, rollupAddr, amountToApproveSpend)
 	if err != nil {
 		panic(err)
 	}
-	ensureTxSucceeds(tx)
-	tx, err = tokenBindings.TestWETH9Transactor.Approve(validatorTxOpts, chalManager.Address(), amountToApproveSpend)
+	tx, err = tokenBindings.TestWETH9Transactor.Approve(txOpts, chalManager.Address(), amountToApproveSpend)
 	if err != nil {
 		panic(err)
 	}
-	ensureTxSucceeds(tx)
-
+	_ = tx
 }
