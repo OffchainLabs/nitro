25e42335f2ca90e61ed263ea9b1a2e31d1a8c759
Goran Vladika <goran.vladika@gmail.com>
2023-10-25 12:42:29 +0200

    Perform admin actions through upg executor in system tests

--- arbnode/node.go
+++ arbnode/node.go
@@ -72,7 +72,6 @@ func deployBridgeCreator(ctx context.Context, l1Reader *headerreader.HeaderReade
 		return common.Address{}, fmt.Errorf("bridge deploy error: %w", err)
 	}
 
-	// maxDataSize := big.NewInt(117964)
 	seqInboxTemplate, tx, _, err := bridgegen.DeploySequencerInbox(auth, client, maxDataSize)
 	err = andTxSucceeded(ctx, l1Reader, tx, err)
 	if err != nil {
