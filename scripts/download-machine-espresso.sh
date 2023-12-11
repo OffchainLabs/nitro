a2b875e0bd26b18b061803e4de23662b6bbe4cfe
Mathis <sveitser@gmail.com>
2023-12-11 21:37:02 +0100

    Download wasm binary artifact (#28)
    This now downloads the wasm machine from a previous tagged build step.
    
    I think the reason for this complicated sequence of release steps is to be able
    to include multiple wasm machines in the image so the wasm machine can be
    updated and still replay old history.

--- scripts/download-machine-espresso.sh
+++ scripts/download-machine-espresso.sh
@@ -15,7 +15,7 @@ mkdir "$2"
 ln -sfT "$2" latest
 cd "$2"
 echo "$2" > module-root.txt
-url_base="https://github.com/EspressoSystems/nitro-espresso-integrations/releases/download/$1"
+url_base="https://github.com/EspressoSystems/nitro-espresso-integration/releases/download/$1"
 wget "$url_base/machine.wavm.br"
 
 status_code="$(curl -LI "$url_base/replay.wasm" -so /dev/null -w '%{http_code}')"
