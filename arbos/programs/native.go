b75753f31776121541b6c19e691986f8d373ebc3
Rachel Bousfield <nfranks@protonmail.com>
2024-01-09 15:56:30 -0700

    update copyright years

--- arbos/programs/native.go
+++ arbos/programs/native.go
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, Offchain Labs, Inc.
+// Copyright 2022-2024, Offchain Labs, Inc.
 // For license information, see https://github.com/nitro/blob/master/LICENSE
 
 //go:build !js
