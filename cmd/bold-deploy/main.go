4fccdf3233e0d7904641ec75590a3fd35a493b92
Raul Jordan <raul@prysmaticlabs.com>
2024-01-23 16:23:28 -0600

    cap

--- cmd/bold-deploy/main.go
+++ cmd/bold-deploy/main.go
@@ -114,7 +114,7 @@ func main() {
 			panic(err)
 		}
 	}
-	l1TransactionOpts.GasFeeCap = big.NewInt(2)
+	l1TransactionOpts.GasFeeCap = big.NewInt(params.Ether * 2)
 
 	l1client, err := ethclient.Dial(*l1conn)
 	if err != nil {
