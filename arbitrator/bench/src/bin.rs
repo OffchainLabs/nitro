597243a1a5e84f1d71b722401af3abe51310005b
Raul Jordan <raul@prysmaticlabs.com>
2024-01-30 11:05:57 -0600

    edit bin

--- arbitrator/bench/src/bin.rs
+++ arbitrator/bench/src/bin.rs
@@ -20,31 +20,47 @@ struct Args {
 fn main() -> eyre::Result<()> {
     let args = Args::parse();
     let mut machine = prepare_machine(args.preimages_path, args.machine_path)?;
-    let start = std::time::Instant::now();
     let machine_hash = machine.hash();
     println!(
-        "Took {:?}, Machine hash at position {} => {:?}",
-        start.elapsed(),
+        "Starting to execute machine at position {} => {:?}",
         machine.get_steps(),
         hex::encode(&machine_hash)
     );
 
-    println!("Stepping in increments of 1024 steps at a time, 256 iterations");
-    let step_size = 16_384;
+    println!("Stepping...");
+    let step_size = 1 << 29;
     let num_iters = 100;
     for i in 0..num_iters {
         let start = std::time::Instant::now();
         machine.step_n(step_size)?;
+        let step_end_time = start.elapsed();
         match machine.get_status() {
-            MachineStatus::Errored => bail!("Machine errored => position {}", machine.get_steps()),
+            MachineStatus::Errored => {
+                let start = std::time::Instant::now();
+                let machine_hash = machine.hash();
+                let hash_end_time = start.elapsed();
+                println!(
+                    "hash time {:?}, step time {:?}, step size {}, num_iters {} machine hash at position {} => {:?}",
+                    hash_end_time,
+                    step_end_time,
+                    step_size,
+                    i,
+                    machine.get_steps(),
+                    hex::encode(&machine_hash)
+                );
+                bail!("Machine errored => position {}", machine.get_steps())
+            }
             MachineStatus::TooFar => bail!("Machine too far => position {}", machine.get_steps()),
             MachineStatus::Running => {}
             MachineStatus::Finished => return Ok(()),
         }
+        let start = std::time::Instant::now();
         let machine_hash = machine.hash();
+        let hash_end_time = start.elapsed();
         println!(
-            "Took {:?}, step size {}, num_iters {} machine hash at position {} => {:?}",
-            start.elapsed(),
+            "hash time {:?}, step time {:?}, step size {}, num_iters {} machine hash at position {} => {:?}",
+            hash_end_time,
+            step_end_time,
             step_size,
             i,
             machine.get_steps(),
