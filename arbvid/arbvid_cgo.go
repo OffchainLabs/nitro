17d778f419f46a7bdc7cd9b6bef51a98812b317c
nomaxg <noahgolub2@gmail.com>
2024-01-31 12:23:11 -0500

    dont compile arbvid_cgo to wasm

--- arbvid/arbvid_cgo.go
+++ arbvid/arbvid_cgo.go
@@ -1,11 +1,7 @@
-package arbvid
+//go:build !js
+// +build !js
 
-/*
-#cgo CFLAGS: -g -Wall -I${SRCDIR}/../target/include/
-#cgo LDFLAGS: ${SRCDIR}/../target/lib/libbrotlidec-static.a ${SRCDIR}/../target/lib/libbrotlienc-static.a ${SRCDIR}/../target/lib/libbrotlicommon-static.a -lm
-#include "brotli/encode.h"
-#include "brotli/decode.h"
-*/
+package arbvid
 
 // This is where we would use cgo to call Rust code to verify a namespace using the C FFI
 // TODO stretch goal: https://github.com/EspressoSystems/nitro-espresso-integration/issues/71
