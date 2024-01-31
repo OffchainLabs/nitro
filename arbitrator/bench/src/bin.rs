1f2b20c0f7a497671c79577b4c80e849d9ddd613
Raul Jordan <raul@prysmaticlabs.com>
2024-01-31 10:36:25 -0600

    more granularity

--- arbitrator/bench/src/bin.rs
+++ arbitrator/bench/src/bin.rs
@@ -19,27 +19,28 @@ struct Args {
 
 fn main() -> eyre::Result<()> {
     let args = Args::parse();
-    let mut machine = prepare_machine(args.preimages_path, args.machine_path)?;
-    let machine_hash = machine.hash();
-    println!(
-        "Starting to execute machine at position {} => {:?}",
-        machine.get_steps(),
-        hex::encode(&machine_hash)
-    );
+    let step_sizes = [1 << 24, 1 << 26];
+    for step_size in step_sizes {
+        let mut machine = prepare_machine(args.preimages_path.clone(), args.machine_path.clone())?;
+        let machine_hash = machine.hash();
+        println!(
+            "Starting to execute machine at position {} => {:?}",
+            machine.get_steps(),
+            hex::encode(&machine_hash)
+        );
 
-    println!("Stepping...");
-    let step_size = 1 << 29;
-    let num_iters = 100;
-    for i in 0..num_iters {
-        let start = std::time::Instant::now();
-        machine.step_n(step_size)?;
-        let step_end_time = start.elapsed();
-        match machine.get_status() {
-            MachineStatus::Errored => {
-                let start = std::time::Instant::now();
-                let machine_hash = machine.hash();
-                let hash_end_time = start.elapsed();
-                println!(
+        println!("Stepping...");
+        let num_iters = 10;
+        for i in 0..num_iters {
+            let start = std::time::Instant::now();
+            machine.step_n(step_size)?;
+            let step_end_time = start.elapsed();
+            match machine.get_status() {
+                MachineStatus::Errored => {
+                    let start = std::time::Instant::now();
+                    let machine_hash = machine.hash();
+                    let hash_end_time = start.elapsed();
+                    println!(
                     "hash time {:?}, step time {:?}, step size {}, num_iters {} machine hash at position {} => {:?}",
                     hash_end_time,
                     step_end_time,
@@ -48,24 +49,28 @@ fn main() -> eyre::Result<()> {
                     machine.get_steps(),
                     hex::encode(&machine_hash)
                 );
-                bail!("Machine errored => position {}", machine.get_steps())
+                    bail!("Machine errored => position {}", machine.get_steps())
+                }
+                MachineStatus::TooFar => {
+                    bail!("Machine too far => position {}", machine.get_steps())
+                }
+                MachineStatus::Running => {}
+                MachineStatus::Finished => return Ok(()),
             }
-            MachineStatus::TooFar => bail!("Machine too far => position {}", machine.get_steps()),
-            MachineStatus::Running => {}
-            MachineStatus::Finished => return Ok(()),
+            let start = std::time::Instant::now();
+            let machine_hash = machine.hash();
+            let hash_end_time = start.elapsed();
+            println!(
+                "hash time {:?}, step time {:?}, step size {}, num_iters {} machine hash at position {} => {:?}",
+                hash_end_time,
+                step_end_time,
+                step_size,
+                i,
+                machine.get_steps(),
+                hex::encode(&machine_hash)
+            );
         }
-        let start = std::time::Instant::now();
-        let machine_hash = machine.hash();
-        let hash_end_time = start.elapsed();
-        println!(
-            "hash time {:?}, step time {:?}, step size {}, num_iters {} machine hash at position {} => {:?}",
-            hash_end_time,
-            step_end_time,
-            step_size,
-            i,
-            machine.get_steps(),
-            hex::encode(&machine_hash)
-        );
+        println!("=================");
     }
     Ok(())
 }
