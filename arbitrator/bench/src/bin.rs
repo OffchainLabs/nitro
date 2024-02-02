1a5e79f292afab77346df6fa9eaef624e5496760
Raul Jordan <raul@prysmaticlabs.com>
2024-02-01 20:50:45 -0600

    pass in the bin

--- arbitrator/bench/src/bin.rs
+++ arbitrator/bench/src/bin.rs
@@ -19,7 +19,7 @@ struct Args {
 
 fn main() -> eyre::Result<()> {
     let args = Args::parse();
-    let step_sizes = [1];
+    let step_sizes = [1 << 15];
     for step_size in step_sizes {
         let mut machine = prepare_machine(args.preimages_path.clone(), args.machine_path.clone())?;
         let machine_hash = machine.hash();
@@ -32,15 +32,17 @@ fn main() -> eyre::Result<()> {
         println!("Stepping...");
         let mut hash_times = vec![];
         let mut step_times = vec![];
-        let num_iters = 1000;
-        for _ in 0..num_iters {
+        let mut num_iters = 0;
+        loop {
             let start = std::time::Instant::now();
             machine.step_n(step_size)?;
             let step_end_time = start.elapsed();
             step_times.push(step_end_time);
             match machine.get_status() {
                 MachineStatus::Errored => {
-                    bail!("Machine errored => position {}", machine.get_steps())
+                    println!("Errored");
+                    break;
+                    // bail!("Machine errored => position {}", machine.get_steps())
                 }
                 MachineStatus::TooFar => {
                     bail!("Machine too far => position {}", machine.get_steps())
@@ -52,6 +54,8 @@ fn main() -> eyre::Result<()> {
             let _ = machine.hash();
             let hash_end_time = start.elapsed();
             hash_times.push(hash_end_time);
+            num_iters += 1;
+            println!("Num iters {}", num_iters)
         }
         println!(
             "avg hash time {:?}, avg step time {:?}, step size {}, num_iters {} machine hash at position {} => {:?}",
