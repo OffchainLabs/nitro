bfe11b6e00c09714117aabd6d507473f3b82fa9b
Raul Jordan <raul@prysmaticlabs.com>
2024-02-02 06:58:34 -0600

    cond break

--- arbitrator/bench/src/bin.rs
+++ arbitrator/bench/src/bin.rs
@@ -19,7 +19,7 @@ struct Args {
 
 fn main() -> eyre::Result<()> {
     let args = Args::parse();
-    let step_sizes = [1 << 15];
+    let step_sizes = [1];
     for step_size in step_sizes {
         let mut machine = prepare_machine(args.preimages_path.clone(), args.machine_path.clone())?;
         let machine_hash = machine.hash();
@@ -55,6 +55,9 @@ fn main() -> eyre::Result<()> {
             let hash_end_time = start.elapsed();
             hash_times.push(hash_end_time);
             num_iters += 1;
+            if num_iters == 10 {
+                break;
+            }
             println!("Num iters {}", num_iters)
         }
         println!(
