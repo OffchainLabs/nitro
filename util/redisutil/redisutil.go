68d046f020e754c161531d159adf0fb544de3707
Nodar Ambroladze <nodar.ambroladze@gmail.com>
2024-03-15 15:03:06 +0100

    Return concrete type instead of interface

--- util/redisutil/redisutil.go
+++ util/redisutil/redisutil.go
@@ -2,7 +2,7 @@ package redisutil
 
 import "github.com/go-redis/redis/v8"
 
-func RedisClientFromURL(url string) (redis.UniversalClient, error) {
+func RedisClientFromURL(url string) (*redis.Client, error) {
 	if url == "" {
 		return nil, nil
 	}
