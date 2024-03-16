70aebed7fdc5dbcde2edc878b7269f277e5779cb
Rachel Bousfield <nfranks@protonmail.com>
2024-03-15 22:32:43 -0600

    streaming decompression api with dictionary

---
+++ arbitrator/caller-env/src/brotli/types.rs
@@ -0,0 +1,52 @@
+// Copyright 2021-2024, Offchain Labs, Inc.
+// For license information, see https://github.com/OffchainLabs/nitro/blob/master/LICENSE
+
+#![allow(dead_code, clippy::len_without_is_empty)]
+
+use core::ptr;
+use num_enum::{IntoPrimitive, TryFromPrimitive};
+
+#[derive(PartialEq, IntoPrimitive, TryFromPrimitive)]
+#[repr(u32)]
+pub enum BrotliStatus {
+    Failure,
+    Success,
+}
+
+#[derive(PartialEq)]
+#[repr(usize)]
+pub(super) enum BrotliBool {
+    True,
+    False,
+}
+
+#[repr(C)]
+pub(super) enum BrotliSharedDictionaryType {
+    /// LZ77 prefix dictionary
+    Raw,
+    /// Serialized dictionary
+    Serialized,
+}
+
+#[derive(PartialEq)]
+#[repr(u32)]
+pub enum Dictionary {
+    None,
+    StylusProgram,
+}
+
+impl Dictionary {
+    pub fn len(&self) -> usize {
+        match self {
+            Self::None => 0,
+            Self::StylusProgram => todo!(),
+        }
+    }
+
+    pub fn data(&self) -> *const u8 {
+        match self {
+            Self::None => ptr::null(),
+            Self::StylusProgram => todo!(),
+        }
+    }
+}
