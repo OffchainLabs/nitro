b49b660ec0002d2a00a5d1111c889b35eb90c8ae
Tsahi Zidenberg <tsahi@offchainlabs.com>
2024-03-20 16:07:26 -0600

    Revert "remove seqinbox_test from race testing"
    This reverts commit 7e82acf04ce19977667aab3fb7d49c39e00f7d1a.

--- system_tests/seqinbox_test.go
+++ system_tests/seqinbox_test.go
@@ -1,10 +1,6 @@
 // Copyright 2021-2022, Offchain Labs, Inc.
 // For license information, see https://github.com/nitro/blob/master/LICENSE
 
-// race detection makes things slow and miss timeouts
-//go:build !race
-// +build !race
-
 package arbtest
 
 import (
