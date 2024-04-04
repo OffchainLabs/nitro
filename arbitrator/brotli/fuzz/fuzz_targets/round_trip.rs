cc51a988436fa3ecc59893543ef69814796d4c7a
Rachel Bousfield <nfranks@protonmail.com>
2024-04-04 00:17:34 -0600

    clippy

--- arbitrator/brotli/fuzz/fuzz_targets/round_trip.rs
+++ arbitrator/brotli/fuzz/fuzz_targets/round_trip.rs
@@ -9,13 +9,13 @@ use libfuzzer_sys::fuzz_target;
 fuzz_target!(|data: &[u8]| {
     let dict = Dictionary::Empty;
     let split = data
-        .get(0)
+        .first()
         .map(|x| *x as usize)
         .unwrap_or_default()
         .min(data.len());
 
     let (header, data) = data.split_at(split);
-    let image = brotli::compress_into(&data, header.to_owned(), 0, 22, dict).unwrap();
+    let image = brotli::compress_into(data, header.to_owned(), 0, 22, dict).unwrap();
     let prior = brotli::decompress(&image[split..], dict).unwrap();
 
     assert_eq!(&image[..split], header);
