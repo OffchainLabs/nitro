d81ea15ca766a4c796c793c6ec51b4abe42b727c
Diego Ximenes <dxmendes1@gmail.com>
2024-05-20 14:38:45 -0300

    general improvements when testing state-scheme args

--- cmd/nitro/nitro.go
+++ cmd/nitro/nitro.go
@@ -823,7 +823,7 @@ func (c *NodeConfig) CanReload(new *NodeConfig) error {
 }
 
 var (
-	invalidCachingStateSchemeForValidator = errors.New("PathScheme cannot be used as Caching.StateScheme when validator is required")
+	invalidCachingStateSchemeForValidator = errors.New("path cannot be used as execution.caching.state-scheme when validator is required")
 )
 
 func (c *NodeConfig) Validate() error {
