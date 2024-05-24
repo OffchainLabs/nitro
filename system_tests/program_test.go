c1507b05410efc0a9309000a1facb977e225ebb5
Diego Ximenes <dxmendes1@gmail.com>
2024-05-24 20:40:12 -0300

    Test for openInitializeChainDb

--- system_tests/program_test.go
+++ system_tests/program_test.go
@@ -1481,7 +1481,7 @@ func TestWasmRecreate(t *testing.T) {
 	Require(t, err)
 
 	testDir := t.TempDir()
-	nodeBStack := createStackConfigForTest(testDir)
+	nodeBStack := testhelpers.CreateStackConfigForTest(testDir)
 	nodeB, cleanupB := builder.Build2ndNode(t, &SecondNodeParams{stackConfig: nodeBStack})
 
 	_, err = EnsureTxSucceeded(ctx, nodeB.Client, storeTx)
