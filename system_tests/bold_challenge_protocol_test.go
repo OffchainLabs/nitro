9d720a89332501bdbb687f73c5ae41b76c8b659b
Aman Sanghi <asanghi@offchainlabs.com>
2024-05-31 16:55:56 +0530

    clean up bold_challenge_protocol_test.go

--- system_tests/bold_challenge_protocol_test.go
+++ system_tests/bold_challenge_protocol_test.go
@@ -1,8 +1,6 @@
 // Copyright 2023, Offchain Labs, Inc.
 // For license information, see https://github.com/nitro/blob/master/LICENSE
 
-//asdgo:build challengetest && !race
-
 package arbtest
 
 import (
