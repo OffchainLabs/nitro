f5962139e907a4f16bd8e7807341752b06ec80cc
Raul Jordan <raul@prysmaticlabs.com>
2024-06-13 15:50:07 -0500

    add bold deps

---
+++ staker/challenge-cache/cache.go
@@ -0,0 +1,272 @@
+// Copyright 2023, Offchain Labs, Inc.
+// For license information, see https://github.com/offchainlabs/bold/blob/main/LICENSE
+/*
+* Package challengecache stores hashes required for making history commitments in Arbitrum BOLD.
+When a challenge begins, validators need to post Merkle commitments to a series of block hashes to
+narrow down their disagreement to a single block. Once a disagreement is reached, another BOLD challenge begins
+to narrow down within the execution of a block. This requires using the Arbitrator emulator to compute
+the intermediate hashes of executing the block as WASM opcodes. These hashes are expensive to compute, so we
+store them in a filesystem cache to avoid recomputing them and for hierarchical access.
+Each file contains a list of 32 byte hashes, concatenated together as bytes.
+Using this structure, we can namespace hashes by message number and by challenge level.
+
+Once a validator receives a full list of computed machine hashes for the first time from a validatio node,
+it will write the roots to this filesystem hierarchy for fast access next time these roots are needed.
+
+Example:
+- Compute all the hashes for the execution of message num 70 with the required step size for the big step challenge level.
+- Compute all the hashes for the execution of individual steps for a small step challenge level from big step 100 to 101
+
+	  wavm-module-root-0xab/
+		message-num-70/
+			hashes.bin
+			subchallenge-level-1-big-step-100/
+				hashes.bin
+
+We namespace top-level block challenges by wavm module root. Then, we can retrieve
+the state roots for any data within a challenge or associated subchallenge based on the hierarchy above.
+*/
+
+package challengecache
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/log"
+)
+
+var (
+	ErrNotFoundInCache   = errors.New("no found in challenge cache")
+	ErrFileAlreadyExists = errors.New("file already exists")
+	ErrNoStateRoots      = errors.New("no state roots being written")
+	stateRootsFileName   = "hashes.bin"
+	wavmModuleRootPrefix = "wavm-module-root"
+	messageNumberPrefix  = "message-num"
+	bigStepPrefix        = "big-step"
+	challengeLevelPrefix = "subchallenge-level"
+)
+
+// HistoryCommitmentCacher can retrieve history commitment state roots given lookup keys.
+type HistoryCommitmentCacher interface {
+	Get(lookup *Key, numToRead uint64) ([]common.Hash, error)
+	Put(lookup *Key, stateRoots []common.Hash) error
+}
+
+// Cache for history commitments on disk.
+type Cache struct {
+	baseDir string
+}
+
+// New cache from a base directory path.
+func New(baseDir string) *Cache {
+	return &Cache{
+		baseDir: baseDir,
+	}
+}
+
+// Key for cache lookups includes the wavm module root of a challenge, as well
+// as the heights for messages and big steps as needed.
+type Key struct {
+	WavmModuleRoot common.Hash
+	MessageHeight  uint64
+	StepHeights    []uint64
+}
+
+// Get a list of state roots from the cache up to a certain index. State roots are saved as files in the directory
+// hierarchy for the cache. If a file is not present, ErrNotFoundInCache
+// is returned.
+func (c *Cache) Get(
+	lookup *Key,
+	numToRead uint64,
+) ([]common.Hash, error) {
+	fName, err := determineFilePath(c.baseDir, lookup)
+	if err != nil {
+		return nil, err
+	}
+	if _, err := os.Stat(fName); err != nil {
+		log.Warn("Cache miss", "fileName", fName)
+		return nil, ErrNotFoundInCache
+	}
+	log.Debug("Cache hit", "fileName", fName)
+	f, err := os.Open(fName)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Error("Could not close file after reading", "err", err, "file", fName)
+		}
+	}()
+	return readStateRoots(f, numToRead)
+}
+
+// Put a list of state roots into the cache.
+// State roots are saved as files in a directory hierarchy for the cache.
+// This function first creates a temporary file, writes the state roots to it, and then renames the file
+// to the final directory to ensure atomic writes.
+func (c *Cache) Put(lookup *Key, stateRoots []common.Hash) error {
+	// We should error if trying to put 0 state roots to disk.
+	if len(stateRoots) == 0 {
+		return ErrNoStateRoots
+	}
+	fName, err := determineFilePath(c.baseDir, lookup)
+	if err != nil {
+		return err
+	}
+	// We create a tmp file to write our state roots to first. If writing fails,
+	// we don't want to leave a half-written file in our cache directory.
+	// Once writing succeeds, we rename in an atomic operation to the correct file name
+	// in the cache directory hierarchy.
+	tmp := os.TempDir()
+	tmpFName := filepath.Join(tmp, fName)
+	dir := filepath.Dir(tmpFName)
+	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+		return fmt.Errorf("could not make tmp directory %s: %w", dir, err)
+	}
+	f, err := os.Create(tmpFName)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Error("Could not close file after writing", "err", err, "file", fName)
+		}
+	}()
+	if err := writeStateRoots(f, stateRoots); err != nil {
+		return err
+	}
+	if err := os.MkdirAll(filepath.Dir(fName), os.ModePerm); err != nil {
+		return fmt.Errorf("could not make file directory %s: %w", fName, err)
+	}
+	// If the file writing was successful, we rename the file from the tmp directory
+	// into our cache directory. This is an atomic operation.
+	// For more information on this atomic write pattern, see:
+	// https://stackoverflow.com/questions/2333872/how-to-make-file-creation-an-atomic-operation
+	return Move(tmpFName /* old */, fName /* new */)
+}
+
+// Reads 32 bytes at a time from a reader up to a specified height. If none, then read all.
+func readStateRoots(r io.Reader, numToRead uint64) ([]common.Hash, error) {
+	br := bufio.NewReader(r)
+	stateRoots := make([]common.Hash, 0)
+	buf := make([]byte, 0, 32)
+	for totalRead := uint64(0); totalRead < numToRead; totalRead++ {
+		n, err := br.Read(buf[:cap(buf)])
+		if err != nil {
+			// If we try to read but reach EOF, we break out of the loop.
+			if err == io.EOF {
+				break
+			}
+			return nil, err
+		}
+		buf = buf[:n]
+		if n != 32 {
+			return nil, fmt.Errorf("expected to read 32 bytes, got %d bytes", n)
+		}
+		stateRoots = append(stateRoots, common.BytesToHash(buf))
+	}
+	if numToRead > uint64(len(stateRoots)) {
+		return nil, fmt.Errorf(
+			"wanted to read %d roots, but only read %d state roots",
+			numToRead,
+			len(stateRoots),
+		)
+	}
+	return stateRoots, nil
+}
+
+func writeStateRoots(w io.Writer, stateRoots []common.Hash) error {
+	for i, rt := range stateRoots {
+		n, err := w.Write(rt[:])
+		if err != nil {
+			return err
+		}
+		if n != len(rt) {
+			return fmt.Errorf(
+				"for state root %d, wrote %d bytes, expected to write %d bytes",
+				i,
+				n,
+				len(rt),
+			)
+		}
+	}
+	return nil
+}
+
+/*
+*
+When provided with a cache lookup struct, this function determines the file path
+for the data requested within the cache directory hierarchy. The folder structure
+for a given filesystem challenge cache will look as follows:
+
+	  wavm-module-root-0xab/
+		message-num-70/
+			hashes.bin
+			subchallenge-level-1-big-step-100/
+				hashes.bin
+*/
+func determineFilePath(baseDir string, lookup *Key) (string, error) {
+	key := make([]string, 0)
+	key = append(key, fmt.Sprintf("%s-%s", wavmModuleRootPrefix, lookup.WavmModuleRoot.Hex()))
+	key = append(key, fmt.Sprintf("%s-%d", messageNumberPrefix, lookup.MessageHeight))
+	for challengeLevel, height := range lookup.StepHeights {
+		key = append(key, fmt.Sprintf(
+			"%s-%d-%s-%d",
+			challengeLevelPrefix,
+			challengeLevel+1, // subchallenges start at 1, as level 0 is the block challenge level.
+			bigStepPrefix,
+			height,
+		),
+		)
+
+	}
+	key = append(key, stateRootsFileName)
+	return filepath.Join(baseDir, filepath.Join(key...)), nil
+}
+
+// Move function that is robust against cross-device link errors. Credits to:
+// https://gist.github.com/var23rav/23ae5d0d4d830aff886c3c970b8f6c6b
+func Move(source, destination string) error {
+	err := os.Rename(source, destination)
+	if err != nil && strings.Contains(err.Error(), "cross-device link") {
+		return moveCrossDevice(source, destination)
+	}
+	return err
+}
+
+func moveCrossDevice(source, destination string) error {
+	src, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+	dst, err := os.Create(destination)
+	if err != nil {
+		src.Close()
+		return err
+	}
+	_, err = io.Copy(dst, src)
+	src.Close()
+	dst.Close()
+	if err != nil {
+		return err
+	}
+	fi, err := os.Stat(source)
+	if err != nil {
+		os.Remove(destination)
+		return err
+	}
+	err = os.Chmod(destination, fi.Mode())
+	if err != nil {
+		os.Remove(destination)
+		return err
+	}
+	os.Remove(source)
+	return nil
+}
---
+++ staker/challenge-cache/cache_test.go
@@ -0,0 +1,318 @@
+// Copyright 2023, Offchain Labs, Inc.
+// For license information, see https://github.com/offchainlabs/bold/blob/main/LICENSE
+package challengecache
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"testing"
+
+	l2stateprovider "github.com/OffchainLabs/bold/layer2-state-provider"
+	"github.com/ethereum/go-ethereum/common"
+)
+
+var _ HistoryCommitmentCacher = (*Cache)(nil)
+
+func TestCache(t *testing.T) {
+	basePath := t.TempDir()
+	if err := os.MkdirAll(basePath, os.ModePerm); err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() {
+		if err := os.RemoveAll(basePath); err != nil {
+			t.Fatal(err)
+		}
+	})
+	cache := New(basePath)
+	key := &Key{
+		WavmModuleRoot: common.BytesToHash([]byte("foo")),
+		MessageHeight:  0,
+		StepHeights:    []l2stateprovider.Height{l2stateprovider.Height(0)},
+	}
+	t.Run("Not found", func(t *testing.T) {
+		_, err := cache.Get(key, 0)
+		if !errors.Is(err, ErrNotFoundInCache) {
+			t.Fatal(err)
+		}
+	})
+	t.Run("Putting empty root fails", func(t *testing.T) {
+		if err := cache.Put(key, []common.Hash{}); !errors.Is(err, ErrNoStateRoots) {
+			t.Fatalf("Unexpected error: %v", err)
+		}
+	})
+	want := []common.Hash{
+		common.BytesToHash([]byte("foo")),
+		common.BytesToHash([]byte("bar")),
+		common.BytesToHash([]byte("baz")),
+	}
+	err := cache.Put(key, want)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got, err := cache.Get(key, 3)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(got) != len(want) {
+		t.Fatalf("Wrong number of roots. Expected %d, got %d", len(want), len(got))
+	}
+	for i, rt := range got {
+		if rt != want[i] {
+			t.Fatalf("Wrong root. Expected %#x, got %#x", want[i], rt)
+		}
+	}
+}
+
+func TestReadWriteStateRoots(t *testing.T) {
+	t.Run("read up to, but had empty reader", func(t *testing.T) {
+		b := bytes.NewBuffer([]byte{})
+		_, err := readStateRoots(b, 100)
+		if err == nil {
+			t.Fatal("Wanted error")
+		}
+		if !strings.Contains(err.Error(), "only read 0 state roots") {
+			t.Fatal("Unexpected error")
+		}
+	})
+	t.Run("read single root", func(t *testing.T) {
+		b := bytes.NewBuffer([]byte{})
+		want := common.BytesToHash([]byte("foo"))
+		b.Write(want.Bytes())
+		roots, err := readStateRoots(b, 1)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(roots) == 0 {
+			t.Fatal("Got no roots")
+		}
+		if roots[0] != want {
+			t.Fatalf("Wrong root. Expected %#x, got %#x", want, roots[0])
+		}
+	})
+	t.Run("Three roots exist, want to read only two", func(t *testing.T) {
+		b := bytes.NewBuffer([]byte{})
+		foo := common.BytesToHash([]byte("foo"))
+		bar := common.BytesToHash([]byte("bar"))
+		baz := common.BytesToHash([]byte("baz"))
+		b.Write(foo.Bytes())
+		b.Write(bar.Bytes())
+		b.Write(baz.Bytes())
+		roots, err := readStateRoots(b, 2)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(roots) != 2 {
+			t.Fatalf("Expected two roots, got %d", len(roots))
+		}
+		if roots[0] != foo {
+			t.Fatalf("Wrong root. Expected %#x, got %#x", foo, roots[0])
+		}
+		if roots[1] != bar {
+			t.Fatalf("Wrong root. Expected %#x, got %#x", bar, roots[1])
+		}
+	})
+	t.Run("Fails to write enough data to writer", func(t *testing.T) {
+		m := &mockWriter{wantErr: true}
+		err := writeStateRoots(m, []common.Hash{common.BytesToHash([]byte("foo"))})
+		if err == nil {
+			t.Fatal("Wanted error")
+		}
+		m = &mockWriter{wantErr: false, numWritten: 16}
+		err = writeStateRoots(m, []common.Hash{common.BytesToHash([]byte("foo"))})
+		if err == nil {
+			t.Fatal("Wanted error")
+		}
+		if !strings.Contains(err.Error(), "expected to write 32 bytes") {
+			t.Fatalf("Got wrong error kind: %v", err)
+		}
+	})
+}
+
+type mockWriter struct {
+	wantErr    bool
+	numWritten int
+}
+
+func (m *mockWriter) Write(_ []byte) (n int, err error) {
+	if m.wantErr {
+		return 0, errors.New("something went wrong")
+	}
+	return m.numWritten, nil
+}
+
+type mockReader struct {
+	wantErr   bool
+	err       error
+	roots     []common.Hash
+	readIdx   int
+	bytesRead int
+}
+
+func (m *mockReader) Read(out []byte) (n int, err error) {
+	if m.wantErr {
+		return 0, m.err
+	}
+	if m.readIdx == len(m.roots) {
+		return 0, io.EOF
+	}
+	copy(out, m.roots[m.readIdx].Bytes())
+	m.readIdx++
+	return m.bytesRead, nil
+}
+
+func Test_readStateRoots(t *testing.T) {
+	t.Run("Unexpected error", func(t *testing.T) {
+		want := []common.Hash{
+			common.BytesToHash([]byte("foo")),
+			common.BytesToHash([]byte("bar")),
+			common.BytesToHash([]byte("baz")),
+		}
+		m := &mockReader{wantErr: true, roots: want, err: errors.New("foo")}
+		_, err := readStateRoots(m, 1)
+		if err == nil {
+			t.Fatal(err)
+		}
+		if !strings.Contains(err.Error(), "foo") {
+			t.Fatalf("Unexpected error: %v", err)
+		}
+	})
+	t.Run("EOF, but did not read as much as was expected", func(t *testing.T) {
+		want := []common.Hash{
+			common.BytesToHash([]byte("foo")),
+			common.BytesToHash([]byte("bar")),
+			common.BytesToHash([]byte("baz")),
+		}
+		m := &mockReader{wantErr: true, roots: want, err: io.EOF}
+		_, err := readStateRoots(m, 100)
+		if err == nil {
+			t.Fatal(err)
+		}
+		if !strings.Contains(err.Error(), "wanted to read 100") {
+			t.Fatalf("Unexpected error: %v", err)
+		}
+	})
+	t.Run("Reads wrong number of bytes", func(t *testing.T) {
+		want := []common.Hash{
+			common.BytesToHash([]byte("foo")),
+			common.BytesToHash([]byte("bar")),
+			common.BytesToHash([]byte("baz")),
+		}
+		m := &mockReader{wantErr: false, roots: want, bytesRead: 16}
+		_, err := readStateRoots(m, 2)
+		if err == nil {
+			t.Fatal(err)
+		}
+		if !strings.Contains(err.Error(), "expected to read 32 bytes, got 16") {
+			t.Fatalf("Unexpected error: %v", err)
+		}
+	})
+	t.Run("Reads all until EOF", func(t *testing.T) {
+		want := []common.Hash{
+			common.BytesToHash([]byte("foo")),
+			common.BytesToHash([]byte("bar")),
+			common.BytesToHash([]byte("baz")),
+		}
+		m := &mockReader{wantErr: false, roots: want, bytesRead: 32}
+		got, err := readStateRoots(m, 3)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(want) != len(got) {
+			t.Fatal("Wrong number of roots")
+		}
+		for i, rt := range got {
+			if rt != want[i] {
+				t.Fatal("Wrong root")
+			}
+		}
+	})
+}
+
+func Test_determineFilePath(t *testing.T) {
+	type args struct {
+		baseDir string
+		key     *Key
+	}
+	tests := []struct {
+		name        string
+		args        args
+		want        string
+		wantErr     bool
+		errContains string
+	}{
+		{
+			name: "OK",
+			args: args{
+				baseDir: "",
+				key: &Key{
+					MessageHeight: 100,
+					StepHeights:   []l2stateprovider.Height{l2stateprovider.Height(50)},
+				},
+			},
+			want:    "wavm-module-root-0x0000000000000000000000000000000000000000000000000000000000000000/message-num-100/subchallenge-level-1-big-step-50/hashes.bin",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := determineFilePath(tt.args.baseDir, tt.args.key)
+			if (err != nil) != tt.wantErr {
+				t.Logf("got: %v, and key %+v, got %s", err, tt.args.key, got)
+				if !strings.Contains(err.Error(), tt.errContains) {
+					t.Fatalf("Expected %s, got %s", tt.errContains, err.Error())
+				}
+				t.Errorf("determineFilePath() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf(
+					"determineFilePath() = %v, want %v",
+					got,
+					tt.want,
+				)
+			}
+		})
+	}
+}
+
+func BenchmarkCache_Read_32Mb(b *testing.B) {
+	b.StopTimer()
+	basePath := os.TempDir()
+	if err := os.MkdirAll(basePath, os.ModePerm); err != nil {
+		b.Fatal(err)
+	}
+	b.Cleanup(func() {
+		if err := os.RemoveAll(basePath); err != nil {
+			b.Fatal(err)
+		}
+	})
+	cache := New(basePath)
+	key := &Key{
+		WavmModuleRoot: common.BytesToHash([]byte("foo")),
+		MessageHeight:  0,
+		StepHeights:    []l2stateprovider.Height{l2stateprovider.Height(0)},
+	}
+	numRoots := 1 << 20
+	roots := make([]common.Hash, numRoots)
+	for i := range roots {
+		roots[i] = common.BytesToHash([]byte(fmt.Sprintf("%d", i)))
+	}
+	if err := cache.Put(key, roots); err != nil {
+		b.Fatal(err)
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		readUpTo := uint64(1 << 20)
+		roots, err := cache.Get(key, readUpTo)
+		if err != nil {
+			b.Fatal(err)
+		}
+		if len(roots) != numRoots {
+			b.Fatalf("Wrong number of roots. Expected %d, got %d", numRoots, len(roots))
+		}
+	}
+}
