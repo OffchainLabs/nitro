e5e4ea17f9aa04bcae477c255957abfbaa1914b1
Raul Jordan <raul@prysmaticlabs.com>
2024-06-13 11:10:44 -0500

    gotags

--- system_tests/bold_challenge_protocol_test.go
+++ system_tests/bold_challenge_protocol_test.go
@@ -1,7 +1,7 @@
 // Copyright 2023, Offchain Labs, Inc.
 // For license information, see https://github.com/nitro/blob/master/LICENSE
 
-//gasdo:build challengetest && !raced
+//go:build challengetest && !raced
 
 package arbtest
 
