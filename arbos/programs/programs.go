ab185a98356c1e2eaec2b47d5d1bb21d1d34fe19
Lee Bousfield <ljbousfield@gmail.com>
2024-06-21 15:27:29 -0600

    Specify address when caching Stylus program

--- arbos/programs/programs.go
+++ arbos/programs/programs.go
@@ -342,10 +342,12 @@ func (p Programs) ProgramCached(codeHash common.Hash) (bool, error) {
 }
 
 // Sets whether a program is cached. Errors if trying to cache an expired program.
+// `address` must be present if setting cache to true, and must have the specified codeHash.
 func (p Programs) SetProgramCached(
 	emitEvent func() error,
 	db vm.StateDB,
 	codeHash common.Hash,
+	address common.Address,
 	cache bool,
 	time uint64,
 	params *StylusParams,
--- precompiles/ArbWasmCache.go
+++ precompiles/ArbWasmCache.go
@@ -3,6 +3,8 @@
 
 package precompiles
 
+import "github.com/ethereum/go-ethereum/common"
+
 type ArbWasmCache struct {
 	Address addr // 0x72
 
@@ -20,14 +22,18 @@ func (con ArbWasmCache) AllCacheManagers(c ctx, _ mech) ([]addr, error) {
 	return c.State.Programs().CacheManagers().AllMembers(65536)
 }
 
-// Caches all programs with the given codehash. Caller must be a cache manager or chain owner.
-func (con ArbWasmCache) CacheCodehash(c ctx, evm mech, codehash hash) error {
-	return con.setProgramCached(c, evm, codehash, true)
+// Caches all programs with a codehash equal to the given address. Caller must be a cache manager or chain owner.
+func (con ArbWasmCache) CacheProgram(c ctx, evm mech, address addr) error {
+	codehash, err := c.GetCodeHash(address)
+	if err != nil {
+		return err
+	}
+	return con.setProgramCached(c, evm, address, codehash, true)
 }
 
 // Evicts all programs with the given codehash. Caller must be a cache manager or chain owner.
 func (con ArbWasmCache) EvictCodehash(c ctx, evm mech, codehash hash) error {
-	return con.setProgramCached(c, evm, codehash, false)
+	return con.setProgramCached(c, evm, common.Address{}, codehash, false)
 }
 
 // Gets whether a program is cached. Note that the program may be expired.
@@ -36,7 +42,7 @@ func (con ArbWasmCache) CodehashIsCached(c ctx, evm mech, codehash hash) (bool, 
 }
 
 // Caches all programs with the given codehash.
-func (con ArbWasmCache) setProgramCached(c ctx, evm mech, codehash hash, cached bool) error {
+func (con ArbWasmCache) setProgramCached(c ctx, evm mech, address addr, codehash hash, cached bool) error {
 	if !con.hasAccess(c) {
 		return c.BurnOut()
 	}
@@ -51,7 +57,7 @@ func (con ArbWasmCache) setProgramCached(c ctx, evm mech, codehash hash, cached 
 		return con.UpdateProgramCache(c, evm, c.caller, codehash, cached)
 	}
 	return programs.SetProgramCached(
-		emitEvent, evm.StateDB, codehash, cached, evm.Context.Time, params, txRunMode, debugMode,
+		emitEvent, evm.StateDB, codehash, address, cached, evm.Context.Time, params, txRunMode, debugMode,
 	)
 }
 
