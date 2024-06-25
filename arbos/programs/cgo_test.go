07aebbfd216e353686ab8e673bc62b0d38b74393
Tsahi Zidenberg <tsahi@offchainlabs.com>
2024-06-25 17:49:30 -0600

    testCompileArch: add load/store

--- arbos/programs/cgo_test.go
+++ arbos/programs/cgo_test.go
@@ -1,9 +1,17 @@
 // Copyright 2024, Offchain Labs, Inc.
 // For license information, see https://github.com/OffchainLabs/nitro/blob/master/LICENSE
 
+//go:build !wasm
+// +build !wasm
+
 package programs
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+)
 
 func TestConstants(t *testing.T) {
 	err := testConstants()
@@ -13,8 +21,19 @@ func TestConstants(t *testing.T) {
 }
 
 func TestCompileArch(t *testing.T) {
-	err := testCompileArch()
+	compile_env := os.Getenv("TEST_COMPILE")
+	if compile_env == "" {
+		fmt.Print("use TEST_COMPILE=[STORE|LOAD] to allow store/load in compile test")
+	}
+	store := strings.Contains(compile_env, "STORE")
+	err := testCompileArch(store)
 	if err != nil {
 		t.Fatal(err)
+	}
+	if store || strings.Contains(compile_env, "LOAD") {
+		err = testCompileLoad()
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
 }
--- arbos/programs/testcompile.go
+++ arbos/programs/testcompile.go
@@ -11,6 +11,13 @@ package programs
 /*
 #cgo CFLAGS: -g -Wall -I../../target/include/
 #include "arbitrator.h"
+
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef size_t usize;
+
+void handleReqWrap(usize api, u32 req_type, RustSlice *data, u64 *out_cost, GoSliceData *out_result, GoSliceData *out_raw_data);
 */
 import "C"
 import (
@@ -21,10 +28,18 @@ import (
 	"github.com/wasmerio/wasmer-go/wasmer"
 )
 
-func testCompileArch() error {
+func isNativeArm() bool {
+	return runtime.GOOS == "linux" && runtime.GOARCH == "arm64"
+}
 
-	nativeArm64 := false
-	nativeAmd64 := false
+func isNativeX86() bool {
+	return runtime.GOOS == "linux" && runtime.GOARCH == "amd64"
+}
+
+func testCompileArch(store bool) error {
+
+	nativeArm64 := isNativeArm()
+	nativeAmd64 := isNativeX86()
 
 	arm64CompileName := []byte("arm64")
 	amd64CompileName := []byte("amd64")
@@ -32,16 +47,12 @@ func testCompileArch() error {
 	arm64TargetString := []byte("arm64-linux-unknown+neon")
 	amd64TargetString := []byte("x86_64-linux-unknown+sse4.2")
 
-	if runtime.GOOS == "linux" {
-		if runtime.GOARCH == "amd64" {
-			nativeAmd64 = true
-		}
-		if runtime.GOARCH == "arm64" {
-			nativeArm64 = true
-		}
-	}
-
 	output := &rustBytes{}
+
+	_, err := fmt.Print("starting test.. native arm? ", nativeArm64, " amd? ", nativeAmd64, " GOARCH/GOOS: ", runtime.GOARCH+"/"+runtime.GOOS, "\n")
+	if err != nil {
+		return err
+	}
 
 	status := C.stylus_target_set(goSlice(arm64CompileName),
 		goSlice(arm64TargetString),
@@ -61,7 +72,7 @@ func testCompileArch() error {
 		return fmt.Errorf("failed setting compilation target amd: %v", string(output.intoBytes()))
 	}
 
-	source, err := os.ReadFile("../../arbitrator/stylus/tests/memory.wat")
+	source, err := os.ReadFile("../../arbitrator/stylus/tests/add.wat")
 	if err != nil {
 		return fmt.Errorf("failed reading stylus contract: %w", err)
 	}
@@ -69,6 +80,17 @@ func testCompileArch() error {
 	wasm, err := wasmer.Wat2Wasm(string(source))
 	if err != nil {
 		return err
+	}
+
+	if store {
+		_, err := fmt.Print("storing compiled files to ../../target/testdata/\n")
+		if err != nil {
+			return err
+		}
+		err = os.MkdirAll("../../target/testdata", 0644)
+		if err != nil {
+			return err
+		}
 	}
 
 	status = C.stylus_compile(
@@ -92,6 +114,17 @@ func testCompileArch() error {
 	if status != 0 {
 		return fmt.Errorf("failed compiling native: %v", string(output.intoBytes()))
 	}
+	if store && !nativeAmd64 && !nativeArm64 {
+		_, err := fmt.Printf("writing host file\n")
+		if err != nil {
+			return err
+		}
+
+		err = os.WriteFile("../../target/testdata/host.bin", output.intoBytes(), 0644)
+		if err != nil {
+			return err
+		}
+	}
 
 	status = C.stylus_compile(
 		goSlice(wasm),
@@ -102,6 +135,17 @@ func testCompileArch() error {
 	)
 	if status != 0 {
 		return fmt.Errorf("failed compiling arm: %v", string(output.intoBytes()))
+	}
+	if store {
+		_, err := fmt.Printf("writing arm file\n")
+		if err != nil {
+			return err
+		}
+
+		err = os.WriteFile("../../target/testdata/arm64.bin", output.intoBytes(), 0644)
+		if err != nil {
+			return err
+		}
 	}
 
 	status = C.stylus_compile(
@@ -114,6 +158,83 @@ func testCompileArch() error {
 	if status != 0 {
 		return fmt.Errorf("failed compiling amd: %v", string(output.intoBytes()))
 	}
+	if store {
+		_, err := fmt.Printf("writing amd64 file\n")
+		if err != nil {
+			return err
+		}
+
+		err = os.WriteFile("../../target/testdata/amd64.bin", output.intoBytes(), 0644)
+		if err != nil {
+			return err
+		}
+	}
 
 	return nil
+}
+
+func testCompileLoad() error {
+	filePath := "../../target/testdata/host.bin"
+	if isNativeArm() {
+		filePath = "../../target/testdata/arm64.bin"
+	}
+	if isNativeX86() {
+		filePath = "../../target/testdata/amd64.bin"
+	}
+
+	_, err := fmt.Print("starting load test. FilePath: ", filePath, " GOARCH/GOOS: ", runtime.GOARCH+"/"+runtime.GOOS, "\n")
+	if err != nil {
+		return err
+	}
+
+	localAsm, err := os.ReadFile(filePath)
+	if err != nil {
+		return err
+	}
+
+	calldata := []byte{}
+
+	evmData := EvmData{}
+	progParams := ProgParams{
+		MaxDepth:  10000,
+		InkPrice:  1,
+		DebugMode: true,
+	}
+	reqHandler := C.NativeRequestHandler{
+		handle_request_fptr: (*[0]byte)(C.handleReqWrap),
+		id:                  0,
+	}
+
+	inifiniteGas := u64(0xfffffffffffffff)
+
+	output := &rustBytes{}
+
+	_, err = fmt.Print("launching program..\n")
+	if err != nil {
+		return err
+	}
+
+	status := userStatus(C.stylus_call(
+		goSlice(localAsm),
+		goSlice(calldata),
+		progParams.encode(),
+		reqHandler,
+		evmData.encode(),
+		cbool(true),
+		output,
+		&inifiniteGas,
+		u32(0),
+	))
+
+	_, err = fmt.Print("returned: ", status, "\n")
+	if err != nil {
+		return err
+	}
+
+	_, msg, err := status.toResult(output.intoBytes(), true)
+	if status == userFailure {
+		err = fmt.Errorf("%w: %v", err, msg)
+	}
+
+	return err
 }
