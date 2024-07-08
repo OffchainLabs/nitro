80cf3d424f1cb567d96a7cf6fb866b4917e7fe4c
Ganesh Vanahalli <gvanahalli@offchainlabs.com>
2024-07-08 10:52:29 -0500

    add descriptive comments to stack args

--- arbos/util/tracing.go
+++ arbos/util/tracing.go
@@ -51,10 +51,10 @@ func NewTracingInfo(evm *vm.EVM, from, to common.Address, scenario TracingScenar
 func (info *TracingInfo) RecordEmitLog(topics []common.Hash, data []byte) {
 	size := uint64(len(data))
 	var args []uint256.Int
-	args = append(args, *uint256.NewInt(0))
-	args = append(args, *uint256.NewInt(size))
+	args = append(args, *uint256.NewInt(0))    // offset: byte offset in the memory in bytes
+	args = append(args, *uint256.NewInt(size)) // size: byte size to copy (length of data)
 	for _, topic := range topics {
-		args = append(args, HashToUint256(topic))
+		args = append(args, HashToUint256(topic)) // topic: 32-byte value. Max topics count is 4
 	}
 	memory := vm.NewMemory()
 	memory.Resize(size)
