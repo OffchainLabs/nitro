e880cbca1c97fbf46454e47871196b7eeab4fbb3
Raul Jordan <raul@prysmaticlabs.com>
2024-07-08 08:32:13 -0500

    reset

--- scripts/download-machine.sh
+++ scripts/download-machine.sh
@@ -2,7 +2,7 @@
 set -e
 
 mkdir "$2"
-ln -sf "$2" latest
+ln -sfT "$2" latest
 cd "$2"
 echo "$2" > module-root.txt
 url_base="https://github.com/OffchainLabs/nitro/releases/download/$1"
@@ -10,5 +10,5 @@ wget "$url_base/machine.wavm.br"
 
 status_code="$(curl -LI "$url_base/replay.wasm" -so /dev/null -w '%{http_code}')"
 if [ "$status_code" -ne 404 ]; then
-    wget "$url_base/replay.wasm"
+	wget "$url_base/replay.wasm"
 fi
