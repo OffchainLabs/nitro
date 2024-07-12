eba8d2af56c7cc9d9852dd7a04cceac09a0bd4b0
Diego Ximenes <dxmendes1@gmail.com>
2024-07-12 13:48:36 -0300

    Removes unused SignedBlockNumberToMessageCount

--- arbutil/block_message_relation.go
+++ arbutil/block_message_relation.go
@@ -13,12 +13,6 @@ func BlockNumberToMessageCount(blockNumber uint64, genesisBlockNumber uint64) Me
 	return MessageCount(blockNumber + 1 - genesisBlockNumber)
 }
 
-// Block number must correspond to a message count, meaning it may not be less than -1
-func SignedBlockNumberToMessageCount(blockNumber int64, genesisBlockNumber uint64) MessageCount {
-	// #nosec G115
-	return MessageCount(uint64(blockNumber+1) - genesisBlockNumber)
-}
-
 func MessageCountToBlockNumber(messageCount MessageCount, genesisBlockNumber uint64) int64 {
 	// #nosec G115
 	return int64(uint64(messageCount)+genesisBlockNumber) - 1
