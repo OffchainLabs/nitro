e3015c892370bb37b67d2b3ad2ac6d4992482ff9
Diego Ximenes <dxmendes1@gmail.com>
2024-08-08 18:54:37 -0300

    Renames firstMsdIdxReorg to firstMsdIdxReorged

--- arbnode/transaction_streamer.go
+++ arbnode/transaction_streamer.go
@@ -210,14 +210,14 @@ func (s *TransactionStreamer) cleanupInconsistentState() error {
 	return nil
 }
 
-func (s *TransactionStreamer) ReorgAt(firstMsgIdxReorg arbutil.MessageIndex) error {
-	return s.ReorgAtAndEndBatch(s.db.NewBatch(), firstMsgIdxReorg)
+func (s *TransactionStreamer) ReorgAt(firstMsgIdxReorged arbutil.MessageIndex) error {
+	return s.ReorgAtAndEndBatch(s.db.NewBatch(), firstMsgIdxReorged)
 }
 
-func (s *TransactionStreamer) ReorgAtAndEndBatch(batch ethdb.Batch, firstMsgIdxReorg arbutil.MessageIndex) error {
+func (s *TransactionStreamer) ReorgAtAndEndBatch(batch ethdb.Batch, firstMsgIdxReorged arbutil.MessageIndex) error {
 	s.insertionMutex.Lock()
 	defer s.insertionMutex.Unlock()
-	err := s.addMessagesAndReorg(batch, firstMsgIdxReorg, nil)
+	err := s.addMessagesAndReorg(batch, firstMsgIdxReorged, nil)
 	if err != nil {
 		return err
 	}
