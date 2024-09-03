5c668c0107b58a00068244cd3bda3e0a59b27933
Aman Sanghi <asanghi@offchainlabs.com>
2024-09-03 19:25:00 +0530

    Replace kilic/bls12-381 with gnark-crypto/ecc/bls12-381

--- blsSignatures/blsSignatures.go
+++ blsSignatures/blsSignatures.go
@@ -4,25 +4,27 @@
 package blsSignatures
 
 import (
-	cryptorand "crypto/rand"
 	"encoding/base64"
 	"errors"
+	bls12381 "github.com/consensys/gnark-crypto/ecc/bls12-381"
+	"github.com/consensys/gnark-crypto/ecc/bls12-381/fp"
+	"github.com/consensys/gnark-crypto/ecc/bls12-381/fr"
 	"github.com/ethereum/go-ethereum/crypto"
-	bls12381 "github.com/kilic/bls12-381"
+	"math/big"
 )
 
 type PublicKey struct {
-	key           *bls12381.PointG2
-	validityProof *bls12381.PointG1 // if this is nil, key came from a trusted source
+	key           *bls12381.G2Affine
+	validityProof *bls12381.G1Affine // if this is nil, key came from a trusted source
 }
 
-type PrivateKey *bls12381.Fr
+type PrivateKey *fr.Element
 
-type Signature *bls12381.PointG1
+type Signature *bls12381.G1Affine
 
 func GeneratePrivKeyString() (string, error) {
-	fr := bls12381.NewFr()
-	privKey, err := fr.Rand(cryptorand.Reader)
+	fr := new(fr.Element)
+	privKey, err := fr.SetRandom()
 	if err != nil {
 		return "", err
 	}
@@ -33,8 +35,8 @@ func GeneratePrivKeyString() (string, error) {
 }
 
 func GenerateKeys() (PublicKey, PrivateKey, error) {
-	fr := bls12381.NewFr()
-	privateKey, err := fr.Rand(cryptorand.Reader)
+	fr := new(fr.Element)
+	privateKey, err := fr.SetRandom()
 	if err != nil {
 		return PublicKey{}, nil, err
 	}
@@ -43,9 +45,13 @@ func GenerateKeys() (PublicKey, PrivateKey, error) {
 }
 
 func PublicKeyFromPrivateKey(privateKey PrivateKey) (PublicKey, error) {
-	pubKey := &bls12381.PointG2{}
-	g2 := bls12381.NewG2()
-	g2.MulScalar(pubKey, g2.One(), privateKey)
+	pubKey := new(bls12381.G2Affine)
+	g2 := new(bls12381.G2Affine)
+	g2.X.SetOne()
+	g2.Y.SetOne()
+	fr := new(fr.Element)
+	fr.Set(privateKey)
+	pubKey.ScalarMultiplication(g2, fr.BigInt(new(big.Int)))
 	proof, err := KeyValidityProof(pubKey, privateKey)
 	if err != nil {
 		return PublicKey{}, err
@@ -64,15 +70,15 @@ func PublicKeyFromPrivateKey(privateKey PrivateKey) (PublicKey, error) {
 //
 // For a proof that this is sufficient, see Theorem 1 in
 // Ristenpart & Yilek, "The Power of Proofs-of-Possession: ..." from EUROCRYPT 2007.
-func KeyValidityProof(pubKey *bls12381.PointG2, privateKey PrivateKey) (Signature, error) {
-	g2 := bls12381.NewG2()
-	return signMessage2(privateKey, g2.ToBytes(pubKey), true)
+func KeyValidityProof(pubKey *bls12381.G2Affine, privateKey PrivateKey) (Signature, error) {
+	message := pubKey.Bytes()
+	return signMessage2(privateKey, message[:], true)
 }
 
-func NewPublicKey(pubKey *bls12381.PointG2, validityProof *bls12381.PointG1) (PublicKey, error) {
-	g2 := bls12381.NewG2()
+func NewPublicKey(pubKey *bls12381.G2Affine, validityProof *bls12381.G1Affine) (PublicKey, error) {
+	message := pubKey.Bytes()
 	unverifiedPublicKey := PublicKey{pubKey, validityProof}
-	verified, err := verifySignature2(validityProof, g2.ToBytes(pubKey), unverifiedPublicKey, true)
+	verified, err := verifySignature2(validityProof, message[:], unverifiedPublicKey, true)
 	if err != nil {
 		return PublicKey{}, err
 	}
@@ -82,7 +88,7 @@ func NewPublicKey(pubKey *bls12381.PointG2, validityProof *bls12381.PointG1) (Pu
 	return unverifiedPublicKey, nil
 }
 
-func NewTrustedPublicKey(pubKey *bls12381.PointG2) PublicKey {
+func NewTrustedPublicKey(pubKey *bls12381.G2Affine) PublicKey {
 	return PublicKey{pubKey, nil}
 }
 
@@ -102,9 +108,10 @@ func signMessage2(priv PrivateKey, message []byte, keyValidationMode bool) (Sign
 	if err != nil {
 		return nil, err
 	}
-	g1 := bls12381.NewG1()
-	result := &bls12381.PointG1{}
-	g1.MulScalar(result, pointOnCurve, priv)
+	result := new(bls12381.G1Affine)
+	fr := new(fr.Element)
+	fr.Set(priv)
+	result.ScalarMultiplication(pointOnCurve, fr.BigInt(new(big.Int)))
 	return result, nil
 }
 
@@ -118,31 +125,35 @@ func verifySignature2(sig Signature, message []byte, publicKey PublicKey, keyVal
 		return false, err
 	}
 
-	engine := bls12381.NewEngine()
-	engine.Reset()
-	engine.AddPair(pointOnCurve, publicKey.key)
-	leftSide := engine.Result()
-	engine.AddPair(sig, engine.G2.One())
-	rightSide := engine.Result()
-	return leftSide.Equal(rightSide), nil
+	leftSide, err := bls12381.Pair([]bls12381.G1Affine{*pointOnCurve}, []bls12381.G2Affine{*publicKey.key})
+	if err != nil {
+		return false, err
+	}
+	g2 := new(bls12381.G2Affine)
+	g2.X.SetOne()
+	g2.Y.SetOne()
+	rightSide, err := bls12381.Pair([]bls12381.G1Affine{*sig}, []bls12381.G2Affine{*g2})
+	return leftSide.Equal(&rightSide), nil
 }
 
 func AggregatePublicKeys(pubKeys []PublicKey) PublicKey {
-	g2 := bls12381.NewG2()
-	ret := g2.Zero()
+	g2 := new(bls12381.G2Affine)
+	g2.X.SetZero()
+	g2.Y.SetZero()
 	for _, pk := range pubKeys {
-		g2.Add(ret, ret, pk.key)
+		g2.Add(g2, pk.key)
 	}
-	return NewTrustedPublicKey(ret)
+	return NewTrustedPublicKey(g2)
 }
 
 func AggregateSignatures(sigs []Signature) Signature {
-	g1 := bls12381.NewG1()
-	ret := g1.Zero()
+	g1 := new(bls12381.G1Affine)
+	g1.X.SetZero()
+	g1.Y.SetZero()
 	for _, s := range sigs {
-		g1.Add(ret, ret, s)
+		g1.Add(g1, s)
 	}
-	return ret
+	return g1
 }
 
 func VerifyAggregatedSignatureSameMessage(sig Signature, message []byte, pubKeys []PublicKey) (bool, error) {
@@ -154,21 +165,28 @@ func VerifyAggregatedSignatureDifferentMessages(sig Signature, messages [][]byte
 	if len(messages) != len(pubKeys) {
 		return false, errors.New("len(messages) does not match (len(pub keys) in verification")
 	}
-	engine := bls12381.NewEngine()
-	engine.Reset()
+	var p []bls12381.G1Affine
+	var q []bls12381.G2Affine
 	for i, msg := range messages {
 		pointOnCurve, err := hashToG1Curve(msg, false)
 		if err != nil {
 			return false, err
 		}
-		engine.AddPair(pointOnCurve, pubKeys[i].key)
+		p = append(p, *pointOnCurve)
+		q = append(q, *pubKeys[i].key)
 	}
-	leftSide := engine.Result()
-
-	engine.Reset()
-	engine.AddPair(sig, engine.G2.One())
-	rightSide := engine.Result()
-	return leftSide.Equal(rightSide), nil
+	leftSide, err := bls12381.Pair(p, q)
+	if err != nil {
+		return false, err
+	}
+	g2 := new(bls12381.G2Affine)
+	g2.X.SetOne()
+	g2.Y.SetOne()
+	rightSide, err := bls12381.Pair([]bls12381.G1Affine{*sig}, []bls12381.G2Affine{*g2})
+	if err != nil {
+		return false, err
+	}
+	return leftSide.Equal(&rightSide), nil
 }
 
 // This hashes a message to a [32]byte, then maps the result to the G1 curve using
@@ -177,41 +195,42 @@ func VerifyAggregatedSignatureDifferentMessages(sig Signature, messages [][]byte
 //
 // If keyValidationMode is true, this uses a tweaked version of the padding,
 // so that the result will not collide with a result generated in an ordinary signature.
-func hashToG1Curve(message []byte, keyValidationMode bool) (*bls12381.PointG1, error) {
+func hashToG1Curve(message []byte, keyValidationMode bool) (*bls12381.G1Affine, error) {
 	var padding [16]byte
 	h := crypto.Keccak256(message)
 	if keyValidationMode {
 		// modify padding, for domain separation
 		padding[0] = 1
 	}
-	g1 := bls12381.NewG1()
-	return g1.MapToCurve(append(padding[:], h...))
+	fp := new(fp.Element)
+	fp.Unmarshal(append(padding[:], h...))
+	res := bls12381.MapToG1(*fp)
+	return &res, nil
 }
 
 func PublicKeyToBytes(pub PublicKey) []byte {
-	g2 := bls12381.NewG2()
+	keyBytes := pub.key.Bytes()
 	if pub.validityProof == nil {
-		return append([]byte{0}, g2.ToBytes(pub.key)...)
+		return append([]byte{0}, keyBytes[:]...)
 	}
-	keyBytes := g2.ToBytes(pub.key)
 	sigBytes := SignatureToBytes(pub.validityProof)
 	if len(sigBytes) > 255 {
 		panic("validity proof too large to serialize")
 	}
-	return append(append([]byte{byte(len(sigBytes))}, sigBytes...), keyBytes...)
+	return append(append([]byte{byte(len(sigBytes))}, sigBytes...), keyBytes[:]...)
 }
 
 func PublicKeyFromBytes(in []byte, trustedSource bool) (PublicKey, error) {
 	if len(in) == 0 {
 		return PublicKey{}, errors.New("tried to deserialize empty public key")
 	}
-	g2 := bls12381.NewG2()
+	key := new(bls12381.G2Affine)
 	proofLen := int(in[0])
 	if proofLen == 0 {
 		if !trustedSource {
 			return PublicKey{}, errors.New("tried to deserialize unvalidated public key from untrusted source")
 		}
-		key, err := g2.FromBytes(in[1:])
+		err := key.Unmarshal(in[1:])
 		if err != nil {
 			return PublicKey{}, err
 		}
@@ -220,14 +239,14 @@ func PublicKeyFromBytes(in []byte, trustedSource bool) (PublicKey, error) {
 		if len(in) < 1+proofLen {
 			return PublicKey{}, errors.New("invalid serialized public key")
 		}
-		g1 := bls12381.NewG1()
+		validityProof := new(bls12381.G1Affine)
 		proofBytes := in[1 : 1+proofLen]
-		validityProof, err := g1.FromBytes(proofBytes)
+		err := validityProof.Unmarshal(proofBytes)
 		if err != nil {
 			return PublicKey{}, err
 		}
 		keyBytes := in[1+proofLen:]
-		key, err := g2.FromBytes(keyBytes)
+		err = key.Unmarshal(keyBytes)
 		if err != nil {
 			return PublicKey{}, err
 		}
@@ -240,19 +259,26 @@ func PublicKeyFromBytes(in []byte, trustedSource bool) (PublicKey, error) {
 }
 
 func PrivateKeyToBytes(priv PrivateKey) []byte {
-	return bls12381.NewFr().Set(priv).ToBytes()
+	bytes := new(fr.Element).Set(priv).Bytes()
+	return bytes[:]
 }
 
 func PrivateKeyFromBytes(in []byte) (PrivateKey, error) {
-	return bls12381.NewFr().FromBytes(in), nil
+	return new(fr.Element).SetBytes(in), nil
 }
 
 func SignatureToBytes(sig Signature) []byte {
-	g1 := bls12381.NewG1()
-	return g1.ToBytes(sig)
+	g1 := new(bls12381.G1Affine)
+	g1.Set(sig)
+	bytes := g1.Bytes()
+	return bytes[:]
 }
 
 func SignatureFromBytes(in []byte) (Signature, error) {
-	g1 := bls12381.NewG1()
-	return g1.FromBytes(in)
+	g1 := new(bls12381.G1Affine)
+	err := g1.Unmarshal(in)
+	if err != nil {
+		return nil, err
+	}
+	return g1, nil
 }
