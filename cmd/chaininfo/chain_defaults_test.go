015285614f2df69b6b342c183199405d1be38cff
Ganesh Vanahalli <gvanahalli@offchainlabs.com>
2024-09-11 21:40:48 +0530

    modify test

--- cmd/chaininfo/chain_defaults_test.go
+++ cmd/chaininfo/chain_defaults_test.go
@@ -9,19 +9,9 @@ import (
 )
 
 func TestDefaultChainConfigsCopyCorrectly(t *testing.T) {
-	if !reflect.DeepEqual(DefaultChainConfigs["arb1"], ArbitrumOneChainConfig()) {
-		t.Fatal("copy of arb1 default chain config mismatch")
-	}
-	if !reflect.DeepEqual(DefaultChainConfigs["nova"], ArbitrumNovaChainConfig()) {
-		t.Fatal("copy of nova default chain config mismatch")
-	}
-	if !reflect.DeepEqual(DefaultChainConfigs["goerli-rollup"], ArbitrumRollupGoerliTestnetChainConfig()) {
-		t.Fatal("copy of goerli-rollup default chain config mismatch")
-	}
-	if !reflect.DeepEqual(DefaultChainConfigs["arb-dev-test"], ArbitrumDevTestChainConfig()) {
-		t.Fatal("copy of arb-dev-test default chain config mismatch")
-	}
-	if !reflect.DeepEqual(DefaultChainConfigs["anytrust-dev-test"], ArbitrumDevTestDASChainConfig()) {
-		t.Fatal("copy of anytrust-dev-test default chain config mismatch")
+	for _, chainName := range []string{"arb1", "nova", "goerli-rollup", "arb-dev-test", "anytrust-dev-test"} {
+		if !reflect.DeepEqual(DefaultChainConfigs[chainName], fetchChainConfig(chainName)) {
+			t.Fatalf("copy of %s default chain config mismatch", chainName)
+		}
 	}
 }
