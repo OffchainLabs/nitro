9968d4c9a7c620ac156516e2fd3ae0aed1738cbd
Ganesh Vanahalli <gvanahalli@offchainlabs.com>
2024-10-01 16:50:55 +0530

    [config change] Improve BlocksReExecutor implementation

--- blocks_reexecutor/blocks_reexecutor.go
+++ blocks_reexecutor/blocks_reexecutor.go
@@ -9,22 +9,27 @@ import (
 	"strings"
 
 	"github.com/ethereum/go-ethereum/arbitrum"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/core/rawdb"
 	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/triedb"
+	"github.com/ethereum/go-ethereum/triedb/hashdb"
 	"github.com/offchainlabs/nitro/util/arbmath"
 	"github.com/offchainlabs/nitro/util/stopwaiter"
 	flag "github.com/spf13/pflag"
 )
 
 type Config struct {
-	Enable          bool   `koanf:"enable"`
-	Mode            string `koanf:"mode"`
-	StartBlock      uint64 `koanf:"start-block"`
-	EndBlock        uint64 `koanf:"end-block"`
-	Room            int    `koanf:"room"`
-	BlocksPerThread uint64 `koanf:"blocks-per-thread"`
+	Enable             bool   `koanf:"enable"`
+	Mode               string `koanf:"mode"`
+	StartBlock         uint64 `koanf:"start-block"`
+	EndBlock           uint64 `koanf:"end-block"`
+	Room               int    `koanf:"room"`
+	MinBlocksPerThread uint64 `koanf:"min-blocks-per-thread"`
 }
 
 func (c *Config) Validate() error {
@@ -48,10 +53,10 @@ var DefaultConfig = Config{
 }
 
 var TestConfig = Config{
-	Enable:          true,
-	Mode:            "full",
-	Room:            runtime.NumCPU(),
-	BlocksPerThread: 10,
+	Enable:             true,
+	Mode:               "full",
+	Room:               runtime.NumCPU(),
+	MinBlocksPerThread: 10,
 }
 
 func ConfigAddOptions(prefix string, f *flag.FlagSet) {
@@ -60,22 +65,26 @@ func ConfigAddOptions(prefix string, f *flag.FlagSet) {
 	f.Uint64(prefix+".start-block", DefaultConfig.StartBlock, "first block number of the block range for re-execution")
 	f.Uint64(prefix+".end-block", DefaultConfig.EndBlock, "last block number of the block range for re-execution")
 	f.Int(prefix+".room", DefaultConfig.Room, "number of threads to parallelize blocks re-execution")
-	f.Uint64(prefix+".blocks-per-thread", DefaultConfig.BlocksPerThread, "minimum number of blocks to execute per thread. When mode is random this acts as the size of random block range sample")
+	f.Uint64(prefix+".min-blocks-per-thread", DefaultConfig.MinBlocksPerThread, "minimum number of blocks to execute per thread. When mode is random this acts as the size of random block range sample")
 }
 
 type BlocksReExecutor struct {
 	stopwaiter.StopWaiter
-	config          *Config
-	blockchain      *core.BlockChain
-	stateFor        arbitrum.StateForHeaderFunction
-	done            chan struct{}
-	fatalErrChan    chan error
-	startBlock      uint64
-	currentBlock    uint64
-	blocksPerThread uint64
+	config             *Config
+	db                 state.Database
+	blockchain         *core.BlockChain
+	stateFor           arbitrum.StateForHeaderFunction
+	done               chan struct{}
+	fatalErrChan       chan error
+	startBlock         uint64
+	currentBlock       uint64
+	minBlocksPerThread uint64
 }
 
-func New(c *Config, blockchain *core.BlockChain, fatalErrChan chan error) *BlocksReExecutor {
+func New(c *Config, blockchain *core.BlockChain, ethDb ethdb.Database, fatalErrChan chan error) (*BlocksReExecutor, error) {
+	if blockchain.TrieDB().Scheme() == rawdb.PathScheme {
+		return nil, errors.New("blocksReExecutor not supported on pathdb")
+	}
 	start := c.StartBlock
 	end := c.EndBlock
 	chainStart := blockchain.Config().ArbitrumChainParams.GenesisBlockNum
@@ -92,13 +101,13 @@ func New(c *Config, blockchain *core.BlockChain, fatalErrChan chan error) *Block
 		log.Warn("invalid state reexecutor's end block number, resetting to latest", "end", end, "latest", chainEnd)
 		end = chainEnd
 	}
-	blocksPerThread := uint64(10000)
-	if c.BlocksPerThread != 0 {
-		blocksPerThread = c.BlocksPerThread
+	minBlocksPerThread := uint64(10000)
+	if c.MinBlocksPerThread != 0 {
+		minBlocksPerThread = c.MinBlocksPerThread
 	}
 	if c.Mode == "random" && end != start {
-		// Reexecute a range of 10000 or (non-zero) c.BlocksPerThread number of blocks between start to end picked randomly
-		rng := blocksPerThread
+		// Reexecute a range of 10000 or (non-zero) c.MinBlocksPerThread number of blocks between start to end picked randomly
+		rng := minBlocksPerThread
 		if rng > end-start {
 			rng = end - start
 		}
@@ -111,32 +120,41 @@ func New(c *Config, blockchain *core.BlockChain, fatalErrChan chan error) *Block
 	if start > 0 && start != chainStart {
 		start--
 	}
-	// Divide work equally among available threads when BlocksPerThread is zero
-	if c.BlocksPerThread == 0 {
+	// Divide work equally among available threads when MinBlocksPerThread is zero
+	if c.MinBlocksPerThread == 0 {
 		// #nosec G115
 		work := (end - start) / uint64(c.Room)
 		if work > 0 {
-			blocksPerThread = work
+			minBlocksPerThread = work
 		}
 	}
-	return &BlocksReExecutor{
-		config:          c,
-		blockchain:      blockchain,
-		currentBlock:    end,
-		startBlock:      start,
-		blocksPerThread: blocksPerThread,
-		done:            make(chan struct{}, c.Room),
-		fatalErrChan:    fatalErrChan,
-		stateFor: func(header *types.Header) (*state.StateDB, arbitrum.StateReleaseFunc, error) {
-			state, err := blockchain.StateAt(header.Root)
-			return state, arbitrum.NoopStateRelease, err
-		},
+	trieConfig := triedb.Config{
+		Preimages: false,
+		HashDB:    hashdb.Defaults,
 	}
+	blocksReExecutor := &BlocksReExecutor{
+		config:             c,
+		db:                 state.NewDatabaseWithConfig(ethDb, &trieConfig),
+		blockchain:         blockchain,
+		currentBlock:       end,
+		startBlock:         start,
+		minBlocksPerThread: minBlocksPerThread,
+		done:               make(chan struct{}, c.Room),
+		fatalErrChan:       fatalErrChan,
+	}
+	blocksReExecutor.stateFor = func(header *types.Header) (*state.StateDB, arbitrum.StateReleaseFunc, error) {
+		sdb, err := state.NewDeterministic(header.Root, blocksReExecutor.db)
+		if err == nil {
+			_ = blocksReExecutor.db.TrieDB().Reference(header.Root, common.Hash{}) // Will be dereferenced later in advanceStateUpToBlock
+		}
+		return sdb, arbitrum.NoopStateRelease, err
+	}
+	return blocksReExecutor, nil
 }
 
-// LaunchBlocksReExecution launches the thread to apply blocks of range [currentBlock-s.config.BlocksPerThread, currentBlock] to the last available valid state
+// LaunchBlocksReExecution launches the thread to apply blocks of range [currentBlock-s.config.MinBlocksPerThread, currentBlock] to the last available valid state
 func (s *BlocksReExecutor) LaunchBlocksReExecution(ctx context.Context, currentBlock uint64) uint64 {
-	start := arbmath.SaturatingUSub(currentBlock, s.blocksPerThread)
+	start := arbmath.SaturatingUSub(currentBlock, s.minBlocksPerThread)
 	if start < s.startBlock {
 		start = s.startBlock
 	}
@@ -149,8 +167,8 @@ func (s *BlocksReExecutor) LaunchBlocksReExecution(ctx context.Context, currentB
 	defer release()
 	start = startHeader.Number.Uint64()
 	s.LaunchThread(func(ctx context.Context) {
-		_, err := arbitrum.AdvanceStateUpToBlock(ctx, s.blockchain, startState, s.blockchain.GetHeaderByNumber(currentBlock), startHeader, nil)
-		if err != nil {
+		log.Info("Starting reexecution of blocks against historic state", "stateAt", start, "startBlock", start+1, "endBlock", currentBlock)
+		if err := s.advanceStateUpToBlock(ctx, startState, s.blockchain.GetHeaderByNumber(currentBlock), startHeader); err != nil {
 			s.fatalErrChan <- fmt.Errorf("blocksReExecutor errored advancing state from block %d to block %d, err: %w", start, currentBlock, err)
 		} else {
 			log.Info("Successfully reexecuted blocks against historic state", "stateAt", start, "startBlock", start+1, "endBlock", currentBlock)
@@ -198,4 +216,51 @@ func (s *BlocksReExecutor) Start(ctx context.Context, done chan struct{}) {
 
 func (s *BlocksReExecutor) StopAndWait() {
 	s.StopWaiter.StopAndWait()
+}
+
+func (s *BlocksReExecutor) commitStateAndVerify(statedb *state.StateDB, expected common.Hash, blockNumber uint64) (*state.StateDB, error) {
+	result, err := statedb.Commit(blockNumber, true)
+	if err != nil {
+		return nil, err
+	}
+	if result != expected {
+		return nil, fmt.Errorf("bad root hash expected: %v got: %v", expected, result)
+	}
+	_ = s.db.TrieDB().Reference(result, common.Hash{})
+	return state.New(result, statedb.Database(), nil)
+}
+
+func (s *BlocksReExecutor) advanceStateUpToBlock(ctx context.Context, state *state.StateDB, targetHeader *types.Header, lastAvailableHeader *types.Header) error {
+	targetBlockNumber := targetHeader.Number.Uint64()
+	blockToRecreate := lastAvailableHeader.Number.Uint64() + 1
+	prevHash := lastAvailableHeader.Hash()
+	lastRoot := lastAvailableHeader.Root
+	defer func() {
+		if (lastRoot != common.Hash{}) {
+			_ = s.db.TrieDB().Dereference(lastRoot)
+		}
+	}()
+	var block *types.Block
+	var err error
+	for ctx.Err() == nil {
+		state, block, err = arbitrum.AdvanceStateByBlock(ctx, s.blockchain, state, targetHeader, blockToRecreate, prevHash, nil)
+		if err != nil {
+			return err
+		}
+		prevHash = block.Hash()
+		state, err = s.commitStateAndVerify(state, block.Root(), block.NumberU64())
+		if err != nil {
+			return fmt.Errorf("failed committing state for block %d : %w", blockToRecreate, err)
+		}
+		_ = s.db.TrieDB().Dereference(lastRoot)
+		lastRoot = block.Root()
+		if blockToRecreate >= targetBlockNumber {
+			if block.Hash() != targetHeader.Hash() {
+				return fmt.Errorf("blockHash doesn't match when recreating number: %d expected: %v got: %v", blockToRecreate, targetHeader.Hash(), block.Hash())
+			}
+			return nil
+		}
+		blockToRecreate++
+	}
+	return ctx.Err()
 }
