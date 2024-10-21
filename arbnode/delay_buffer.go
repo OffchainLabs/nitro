a05ee98f41f7ed5b111c946033ae145ac0f13219
Gabriel de Quadros Ligneul <gligneul@offchainlabs.com>
2024-10-21 11:30:30 -0300

    Add delay proof when posting a batch

--- arbnode/delay_buffer.go
+++ arbnode/delay_buffer.go
@@ -9,8 +9,12 @@ package arbnode
 import (
 	"context"
 	"fmt"
+	"math/big"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/offchainlabs/nitro/arbos/arbostypes"
 	"github.com/offchainlabs/nitro/solgen/go/bridgegen"
 	"github.com/offchainlabs/nitro/util/headerreader"
 )
@@ -26,9 +30,7 @@ type DelayBufferConfig struct {
 func GetDelayBufferConfig(ctx context.Context, sequencerInbox *bridgegen.SequencerInbox) (
 	*DelayBufferConfig, error) {
 
-	callOpts := bind.CallOpts{
-		Context: ctx,
-	}
+	callOpts := bind.CallOpts{Context: ctx}
 	enabled, err := sequencerInbox.IsDelayBufferable(&callOpts)
 	if err != nil {
 		if headerreader.ExecutionRevertedRegexp.MatchString(err.Error()) {
@@ -48,4 +50,34 @@ func GetDelayBufferConfig(ctx context.Context, sequencerInbox *bridgegen.Sequenc
 		Threshold: bufferData.Threshold,
 	}
 	return config, nil
+}
+
+// GenDelayProof generates the delay proof based on batch's first delayed message and the delayed
+// accumulater from the inbox.
+func GenDelayProof(ctx context.Context, message *arbostypes.MessageWithMetadata, inbox *InboxTracker) (
+	*bridgegen.DelayProof, error) {
+
+	seqNum := message.DelayedMessagesRead
+	var beforeDelayedAcc common.Hash
+	if seqNum > 0 {
+		var err error
+		beforeDelayedAcc, err = inbox.GetDelayedAcc(seqNum)
+		if err != nil {
+			return nil, err
+		}
+	}
+	delayedMessage := bridgegen.MessagesMessage{
+		Kind:            message.Message.Header.Kind,
+		Sender:          message.Message.Header.Poster,
+		BlockNumber:     message.Message.Header.BlockNumber,
+		Timestamp:       message.Message.Header.Timestamp,
+		InboxSeqNum:     new(big.Int).SetUint64(seqNum),
+		BaseFeeL1:       message.Message.Header.L1BaseFee,
+		MessageDataHash: crypto.Keccak256Hash(message.Message.L2msg),
+	}
+	delayProof := &bridgegen.DelayProof{
+		BeforeDelayedAcc: beforeDelayedAcc,
+		DelayedMessage:   delayedMessage,
+	}
+	return delayProof, nil
 }
