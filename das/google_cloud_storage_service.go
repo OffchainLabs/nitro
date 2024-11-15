e6571ca397c43642dda0dc6da31140c16a3a5013
Antonio Nunez <antonio@altresear.ch>
2024-11-15 17:55:28 +0800

    feat: update google-cloud-storage parameters and object retention setting

--- das/google_cloud_storage_service.go
+++ das/google_cloud_storage_service.go
@@ -21,151 +21,88 @@ import (
 	"github.com/offchainlabs/nitro/util/pretty"
 )
 
-type GoogleCloudStorageOperator interface {
-	Bucket(name string) *googlestorage.BucketHandle
-	Upload(ctx context.Context, bucket, objectPrefix string, value []byte) error
-	Download(ctx context.Context, bucket, objectPrefix string, key common.Hash) ([]byte, error)
-	Close(ctx context.Context) error
-}
-
-type GoogleCloudStorageClient struct {
-	client *googlestorage.Client
-}
-
-func (g *GoogleCloudStorageClient) Bucket(name string) *googlestorage.BucketHandle {
-	return g.client.Bucket(name)
-}
-
-func (g *GoogleCloudStorageClient) Upload(ctx context.Context, bucket, objectPrefix string, value []byte) error {
-	obj := g.client.Bucket(bucket).Object(objectPrefix + EncodeStorageServiceKey(dastree.Hash(value)))
-	w := obj.NewWriter(ctx)
-
-	if _, err := fmt.Fprintln(w, value); err != nil {
-		return err
-	}
-	return w.Close()
-
-}
-
-func (g *GoogleCloudStorageClient) Download(ctx context.Context, bucket, objectPrefix string, key common.Hash) ([]byte, error) {
-	obj := g.client.Bucket(bucket).Object(objectPrefix + EncodeStorageServiceKey(key))
-	reader, err := obj.NewReader(ctx)
-	if err != nil {
-		return nil, err
-	}
-	return io.ReadAll(reader)
-}
-
-func (g *GoogleCloudStorageClient) Close(ctx context.Context) error {
-	return g.client.Close()
-}
-
 type GoogleCloudStorageServiceConfig struct {
-	Enable       bool          `koanf:"enable"`
-	AccessToken  string        `koanf:"access-token"`
-	Bucket       string        `koanf:"bucket"`
-	ObjectPrefix string        `koanf:"object-prefix"`
-	EnableExpiry bool          `koanf:"enable-expiry"`
-	MaxRetention time.Duration `koanf:"max-retention"`
+	Enable              bool   `koanf:"enable"`
+	AccessTokenFile     string `koanf:"access-token-file"`
+	Bucket              string `koanf:"bucket"`
+	ObjectPrefix        string `koanf:"object-prefix"`
+	DiscardAfterTimeout bool   `koanf:"discard-after-timeout"`
 }
 
 var DefaultGoogleCloudStorageServiceConfig = GoogleCloudStorageServiceConfig{}
 
 func GoogleCloudConfigAddOptions(prefix string, f *flag.FlagSet) {
 	f.Bool(prefix+".enable", DefaultGoogleCloudStorageServiceConfig.Enable, "EXPERIMENTAL/unsupported - enable storage/retrieval of sequencer batch data from an Google Cloud Storage bucket")
-	f.String(prefix+".access-token", DefaultGoogleCloudStorageServiceConfig.AccessToken, "Google Cloud Storage access token")
+	f.String(prefix+".access-token-file", DefaultGoogleCloudStorageServiceConfig.AccessTokenFile, "Google Cloud Storage access token")
 	f.String(prefix+".bucket", DefaultGoogleCloudStorageServiceConfig.Bucket, "Google Cloud Storage bucket")
 	f.String(prefix+".object-prefix", DefaultGoogleCloudStorageServiceConfig.ObjectPrefix, "prefix to add to Google Cloud Storage objects")
-	f.Bool(prefix+".enable-expiry", DefaultLocalFileStorageConfig.EnableExpiry, "enable expiry of batches")
-	f.Duration(prefix+".max-retention", DefaultLocalFileStorageConfig.MaxRetention, "store requests with expiry times farther in the future than max-retention will be rejected")
+	f.Bool(prefix+".discard-after-timeout", DefaultGoogleCloudStorageServiceConfig.DiscardAfterTimeout, "discard data after its expiry timeout")
 
 }
 
 type GoogleCloudStorageService struct {
-	operator     GoogleCloudStorageOperator
-	bucket       string
-	objectPrefix string
-	enableExpiry bool
-	maxRetention time.Duration
+	client              *googlestorage.Client
+	bucket              string
+	objectPrefix        string
+	discardAfterTimeout bool
 }
 
 func NewGoogleCloudStorageService(config GoogleCloudStorageServiceConfig) (StorageService, error) {
-	var client *googlestorage.Client
-	var err error
-	// Note that if the credentials are not specified, the client library will find credentials using ADC(Application Default Credentials)
-	// https://cloud.google.com/docs/authentication/provide-credentials-adc.
-	if config.AccessToken == "" {
-		client, err = googlestorage.NewClient(context.Background())
-	} else {
-		client, err = googlestorage.NewClient(context.Background(), option.WithCredentialsJSON([]byte(config.AccessToken)))
-	}
+	client, err := buildGoogleCloudStorageClient(config.AccessTokenFile)
 	if err != nil {
 		return nil, fmt.Errorf("error creating Google Cloud Storage client: %w", err)
 	}
-	service := &GoogleCloudStorageService{
-		operator:     &GoogleCloudStorageClient{client: client},
-		bucket:       config.Bucket,
-		objectPrefix: config.ObjectPrefix,
-		enableExpiry: config.EnableExpiry,
-		maxRetention: config.MaxRetention,
-	}
-	if config.EnableExpiry {
-		lifecycleRule := googlestorage.LifecycleRule{
-			Action:    googlestorage.LifecycleAction{Type: "Delete"},
-			Condition: googlestorage.LifecycleCondition{AgeInDays: int64(config.MaxRetention.Hours() / 24)}, // Objects older than 30 days
-		}
-		ctx := context.Background()
-		bucket := service.operator.Bucket(service.bucket)
-		// check if bucket exists (and others), and update expiration policy if enabled
-		attrs, err := bucket.Attrs(ctx)
-		if err != nil {
-			return nil, fmt.Errorf("error getting bucket attributes: %w", err)
-		}
-		attrs.Lifecycle.Rules = append(attrs.Lifecycle.Rules, lifecycleRule)
-
-		bucketAttrsToUpdate := googlestorage.BucketAttrsToUpdate{
-			Lifecycle: &attrs.Lifecycle,
-		}
-		if _, err := bucket.Update(ctx, bucketAttrsToUpdate); err != nil {
-			return nil, fmt.Errorf("failed to update bucket lifecycle: %w", err)
-		}
-	}
-	return service, nil
+	return &GoogleCloudStorageService{
+		client:              client,
+		bucket:              config.Bucket,
+		objectPrefix:        config.ObjectPrefix,
+		discardAfterTimeout: config.DiscardAfterTimeout,
+	}, nil
 }
 
-func (gcs *GoogleCloudStorageService) Put(ctx context.Context, data []byte, expiry uint64) error {
-	logPut("das.GoogleCloudStorageService.Store", data, expiry, gcs)
-	if expiry > math.MaxInt64 {
-		return fmt.Errorf("request expiry time (%v) exceeds max int64", expiry)
+func buildGoogleCloudStorageClient(accessTokenFile string) (*googlestorage.Client, error) {
+	// Note that if the credentials are not specified, the client library will find credentials using ADC(Application Default Credentials)
+	// https://cloud.google.com/docs/authentication/provide-credentials-adc.
+	if accessTokenFile == "" {
+		return googlestorage.NewClient(context.Background())
 	}
-	// #nosec G115
-	expiryTime := time.Unix(int64(expiry), 0)
-	currentTimePlusRetention := time.Now().Add(gcs.maxRetention)
-	if expiryTime.After(currentTimePlusRetention) {
-		return fmt.Errorf("requested expiry time (%v) exceeds current time plus maximum allowed retention period(%v)", expiryTime, currentTimePlusRetention)
-	}
-	if err := gcs.operator.Upload(ctx, gcs.bucket, gcs.objectPrefix, data); err != nil {
-		log.Error("das.GoogleCloudStorageService.Store", "err", err)
-		return err
-	}
-	return nil
+	return googlestorage.NewClient(context.Background(), option.WithCredentialsFile(accessTokenFile))
 }
 
 func (gcs *GoogleCloudStorageService) GetByHash(ctx context.Context, key common.Hash) ([]byte, error) {
 	log.Trace("das.GoogleCloudStorageService.GetByHash", "key", pretty.PrettyHash(key), "this", gcs)
-	buf, err := gcs.operator.Download(ctx, gcs.bucket, gcs.objectPrefix, key)
+	obj := gcs.client.Bucket(gcs.bucket).Object(gcs.objectPrefix + EncodeStorageServiceKey(key))
+	reader, err := obj.NewReader(ctx)
 	if err != nil {
 		log.Error("das.GoogleCloudStorageService.GetByHash", "err", err)
 		return nil, err
 	}
+	buf, err := io.ReadAll(reader)
+	if err != nil {
+		log.Error("das.GoogleCloudStorageService.GetByHash", "err", err)
+	}
 	return buf, nil
 }
 
-func (gcs *GoogleCloudStorageService) ExpirationPolicy(ctx context.Context) (daprovider.ExpirationPolicy, error) {
-	if gcs.enableExpiry {
-		return daprovider.KeepForever, nil
+func (gcs *GoogleCloudStorageService) Put(ctx context.Context, value []byte, timeout uint64) error {
+	logPut("das.GoogleCloudStorageService.Store", value, timeout, gcs)
+	obj := gcs.client.Bucket(gcs.bucket).Object(gcs.objectPrefix + EncodeStorageServiceKey(dastree.Hash(value)))
+	w := obj.NewWriter(ctx)
+	if gcs.discardAfterTimeout && timeout <= math.MaxInt64 {
+		w.Retention = &googlestorage.ObjectRetention{
+			Mode:        "Unlocked",
+			RetainUntil: time.Unix(int64(timeout), 0),
+		}
 	}
-	return daprovider.DiscardAfterDataTimeout, nil
+	if _, err := fmt.Fprintln(w, value); err != nil {
+		log.Error("das.GoogleCloudStorageService.Store", "err", err)
+		return err
+	}
+	err := w.Close()
+	if err != nil {
+		log.Error("das.GoogleCloudStorageService.Store", "err", err)
+	}
+	return err
 }
 
 func (gcs *GoogleCloudStorageService) Sync(ctx context.Context) error {
@@ -173,7 +110,14 @@ func (gcs *GoogleCloudStorageService) Sync(ctx context.Context) error {
 }
 
 func (gcs *GoogleCloudStorageService) Close(ctx context.Context) error {
-	return gcs.operator.Close(ctx)
+	return gcs.client.Close()
+}
+
+func (gcs *GoogleCloudStorageService) ExpirationPolicy(ctx context.Context) (daprovider.ExpirationPolicy, error) {
+	if gcs.discardAfterTimeout {
+		return daprovider.DiscardAfterDataTimeout, nil
+	}
+	return daprovider.KeepForever, nil
 }
 
 func (gcs *GoogleCloudStorageService) String() string {
@@ -181,11 +125,9 @@ func (gcs *GoogleCloudStorageService) String() string {
 }
 
 func (gcs *GoogleCloudStorageService) HealthCheck(ctx context.Context) error {
-	bucket := gcs.operator.Bucket(gcs.bucket)
+	bucket := gcs.client.Bucket(gcs.bucket)
 	// check if we have bucket permissions
 	permissions := []string{
-		"storage.buckets.get",
-		"storage.buckets.list",
 		"storage.objects.create",
 		"storage.objects.delete",
 		"storage.objects.list",
@@ -200,6 +142,5 @@ func (gcs *GoogleCloudStorageService) HealthCheck(ctx context.Context) error {
 	if !cmp.Equal(perms, permissions) {
 		return fmt.Errorf("permissions mismatch (-want +got):\n%s", cmp.Diff(permissions, perms))
 	}
-
 	return nil
 }
