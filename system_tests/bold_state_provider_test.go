1f259a987445a63021b4edbc3780e400a5024656
Raul Jordan <raul@prysmaticlabs.com>
2024-11-25 13:41:19 -0600

    add back tag

--- system_tests/bold_state_provider_test.go
+++ system_tests/bold_state_provider_test.go
@@ -1,6 +1,8 @@
 // Copyright 2023, Offchain Labs, Inc.
 // For license information, see https://github.com/offchainlabs/bold/blob/main/LICENSE
 
+//go:build challengetest && !race
+
 package arbtest
 
 import (
