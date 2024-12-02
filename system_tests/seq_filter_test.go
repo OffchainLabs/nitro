c25a7c6bcf27eb937b8e565cb62645e157be00a5
Ganesh Vanahalli <gvanahalli@offchainlabs.com>
2024-12-02 15:37:44 -0600

    address PR comments

--- system_tests/seq_filter_test.go
+++ system_tests/seq_filter_test.go
@@ -21,53 +21,12 @@ import (
 	"github.com/offchainlabs/nitro/util/arbmath"
 )
 
-func TestSequencerFilter(t *testing.T) {
+func TestSequencerTxFilter(t *testing.T) {
 	t.Parallel()
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
 
-	builder := NewNodeBuilder(ctx).DefaultConfig(t, false)
-	builder.isSequencer = true
-	cleanup := builder.Build(t)
+	builder, header, txes, hooks, cleanup := setupSequencerFilterTest(t, false)
 	defer cleanup()
 
-	builder.L2Info.GenerateAccount("User")
-	var latestL2 uint64
-	var err error
-	for i := 0; latestL2 < 3; i++ {
-		_, _ = builder.L2.TransferBalance(t, "Owner", "User", big.NewInt(1e18), builder.L2Info)
-		latestL2, err = builder.L2.Client.BlockNumber(ctx)
-		Require(t, err)
-	}
-
-	preTxFilter := func(withBlock bool) func(_ *params.ChainConfig, _ *types.Header, statedb *state.StateDB, _ *arbosState.ArbosState, tx *types.Transaction, _ *arbitrum_types.ConditionalOptions, _ common.Address, _ *arbos.L1Info) error {
-		return func(_ *params.ChainConfig, _ *types.Header, statedb *state.StateDB, _ *arbosState.ArbosState, tx *types.Transaction, _ *arbitrum_types.ConditionalOptions, _ common.Address, _ *arbos.L1Info) error {
-			if _, ok := tx.GetInner().(*types.DynamicFeeTx); ok {
-				statedb.FilterTx(withBlock)
-			}
-			return nil
-		}
-	}
-	postTxFilter := func(_ *types.Header, statedb *state.StateDB, _ *arbosState.ArbosState, _ *types.Transaction, _ common.Address, _ uint64, _ *core.ExecutionResult) error {
-		if statedb.IsTxInvalid() {
-			return errors.New("internal error")
-		}
-		return nil
-	}
-
-	header := &arbostypes.L1IncomingMessageHeader{
-		Kind:        arbostypes.L1MessageType_L2Message,
-		Poster:      l1pricing.BatchPosterAddress,
-		BlockNumber: 1,
-		Timestamp:   arbmath.SaturatingUCast[uint64](time.Now().Unix()),
-		RequestId:   nil,
-		L1BaseFee:   nil,
-	}
-	var txes types.Transactions
-	txes = append(txes, builder.L2Info.PrepareTx("Owner", "User", builder.L2Info.TransferGas, big.NewInt(1e12), nil))
-	txes = append(txes, builder.L2Info.PrepareTx("User", "Owner", builder.L2Info.TransferGas, big.NewInt(1e12), nil))
-
-	hooks := &arbos.SequencingHooks{TxErrors: []error{}, DiscardInvalidTxsEarly: false, PreTxFilter: preTxFilter(false), PostTxFilter: postTxFilter, ConditionalOptionsForTx: nil}
 	block, err := builder.L2.ExecNode.ExecEngine.SequenceTransactions(header, txes, hooks)
 	if block != nil {
 		t.Fatal("block shouldn't be generated when all txes have failed")
@@ -81,10 +40,15 @@ func TestSequencerFilter(t *testing.T) {
 			t.Fatalf("expected ErrArbTxFilter, found: %s", err.Error())
 		}
 	}
+}
 
-	hooks.TxErrors = []error{}
-	hooks.PreTxFilter = preTxFilter(true)
-	block, err = builder.L2.ExecNode.ExecEngine.SequenceTransactions(header, txes, hooks)
+func TestSequencerBlockFilter(t *testing.T) {
+	t.Parallel()
+
+	builder, header, txes, hooks, cleanup := setupSequencerFilterTest(t, true)
+	defer cleanup()
+
+	block, err := builder.L2.ExecNode.ExecEngine.SequenceTransactions(header, txes, hooks)
 	if block != nil {
 		t.Fatal("block shouldn't be generated when all txes have failed")
 	}
@@ -94,4 +58,55 @@ func TestSequencerFilter(t *testing.T) {
 	if err.Error() != state.ErrArbTxFilter.Error() {
 		t.Fatalf("expected ErrArbTxFilter, found: %s", err.Error())
 	}
+}
+
+func setupSequencerFilterTest(t *testing.T, withBlock bool) (*NodeBuilder, *arbostypes.L1IncomingMessageHeader, types.Transactions, *arbos.SequencingHooks, func()) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	builder := NewNodeBuilder(ctx).DefaultConfig(t, false)
+	builder.isSequencer = true
+	builderCleanup := builder.Build(t)
+
+	builder.L2Info.GenerateAccount("User")
+	var latestL2 uint64
+	var err error
+	for i := 0; latestL2 < 3; i++ {
+		_, _ = builder.L2.TransferBalance(t, "Owner", "User", big.NewInt(1e18), builder.L2Info)
+		latestL2, err = builder.L2.Client.BlockNumber(ctx)
+		Require(t, err)
+	}
+
+	header := &arbostypes.L1IncomingMessageHeader{
+		Kind:        arbostypes.L1MessageType_L2Message,
+		Poster:      l1pricing.BatchPosterAddress,
+		BlockNumber: 1,
+		Timestamp:   arbmath.SaturatingUCast[uint64](time.Now().Unix()),
+		RequestId:   nil,
+		L1BaseFee:   nil,
+	}
+
+	var txes types.Transactions
+	txes = append(txes, builder.L2Info.PrepareTx("Owner", "User", builder.L2Info.TransferGas, big.NewInt(1e12), nil))
+	txes = append(txes, builder.L2Info.PrepareTx("User", "Owner", builder.L2Info.TransferGas, big.NewInt(1e12), nil))
+
+	preTxFilter := func(_ *params.ChainConfig, _ *types.Header, statedb *state.StateDB, _ *arbosState.ArbosState, tx *types.Transaction, _ *arbitrum_types.ConditionalOptions, _ common.Address, _ *arbos.L1Info) error {
+		if _, ok := tx.GetInner().(*types.DynamicFeeTx); ok {
+			statedb.FilterTx(withBlock)
+		}
+		return nil
+	}
+	postTxFilter := func(_ *types.Header, statedb *state.StateDB, _ *arbosState.ArbosState, _ *types.Transaction, _ common.Address, _ uint64, _ *core.ExecutionResult) error {
+		if statedb.IsTxInvalid() {
+			return errors.New("internal error")
+		}
+		return nil
+	}
+	hooks := &arbos.SequencingHooks{TxErrors: []error{}, DiscardInvalidTxsEarly: false, PreTxFilter: preTxFilter, PostTxFilter: postTxFilter, ConditionalOptionsForTx: nil}
+
+	cleanup := func() {
+		builderCleanup()
+		cancel()
+	}
+
+	return builder, header, txes, hooks, cleanup
 }
