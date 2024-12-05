d7060599736fced40b845f7120aaa1b8d59f2304
Raul Jordan <raul@prysmaticlabs.com>
2024-12-05 13:45:38 -0600

    build

--- system_tests/validation_mock_test.go
+++ system_tests/validation_mock_test.go
@@ -84,7 +84,7 @@ func (s *mockSpawner) Stop()        {}
 func (s *mockSpawner) Name() string { return "mock" }
 func (s *mockSpawner) Room() int    { return 4 }
 
-func (s *mockSpawner) CreateExecutionRun(wasmModuleRoot common.Hash, input *validator.ValidationInput, useBoldMachine *bool) containers.PromiseInterface[validator.ExecutionRun] {
+func (s *mockSpawner) CreateExecutionRun(wasmModuleRoot common.Hash, input *validator.ValidationInput, useBoldMachine bool) containers.PromiseInterface[validator.ExecutionRun] {
 	s.ExecSpawned = append(s.ExecSpawned, input.Id)
 	return containers.NewReadyPromise[validator.ExecutionRun](&mockExecRun{
 		startState: input.StartState,
@@ -261,7 +261,7 @@ func TestValidationServerAPI(t *testing.T) {
 		t.Error("unexpected mock validation run")
 	}
 	useBoldMachine := false
-	execRun, err := client.CreateExecutionRun(wasmRoot, &valInput, &useBoldMachine).Await(ctx)
+	execRun, err := client.CreateExecutionRun(wasmRoot, &valInput, useBoldMachine).Await(ctx)
 	Require(t, err)
 	step0 := execRun.GetStepAt(0)
 	step0Res, err := step0.Await(ctx)
@@ -387,9 +387,9 @@ func TestExecutionKeepAlive(t *testing.T) {
 
 	valInput := validator.ValidationInput{}
 	useBoldMachine := false
-	runDefault, err := clientDefault.CreateExecutionRun(wasmRoot, &valInput, &useBoldMachine).Await(ctx)
+	runDefault, err := clientDefault.CreateExecutionRun(wasmRoot, &valInput, useBoldMachine).Await(ctx)
 	Require(t, err)
-	runShortTO, err := clientShortTO.CreateExecutionRun(wasmRoot, &valInput, &useBoldMachine).Await(ctx)
+	runShortTO, err := clientShortTO.CreateExecutionRun(wasmRoot, &valInput, useBoldMachine).Await(ctx)
 	Require(t, err)
 	<-time.After(time.Second * 10)
 	stepDefault := runDefault.GetStepAt(0)
