babadc9b77d68e824959af8b1473a036f5226ce1
Diego Ximenes <dxmendes1@gmail.com>
2024-12-19 15:58:09 -0300

    Uses loop in benchmark programs

--- arbitrator/tools/stylus_benchmark/src/scenario.rs
+++ arbitrator/tools/stylus_benchmark/src/scenario.rs
@@ -13,29 +13,50 @@ pub enum Scenario {
 }
 
 fn generate_add_i32_wat() -> Vec<u8> {
-    let number_of_ops = 20_000;
+    let number_of_loop_iterations = 10_000;
+    let number_of_ops_per_loop_iteration = 2000;
+    let number_of_ops = number_of_loop_iterations * number_of_ops_per_loop_iteration;
 
     let mut wat = Vec::new();
 
+    // preamble
     wat.write_all(b"(module\n").unwrap();
     wat.write_all(b"    (import \"debug\" \"toggle_benchmark\" (func $toggle_benchmark))\n")
         .unwrap();
     wat.write_all(b"    (memory (export \"memory\") 0 0)\n")
+        .unwrap();
+    wat.write_all(b"    (global $ops_counter (mut i32) (i32.const 0))\n")
         .unwrap();
     wat.write_all(b"    (func (export \"user_entrypoint\") (param i32) (result i32)\n")
         .unwrap();
 
     wat.write_all(b"        call $toggle_benchmark\n").unwrap();
 
-    wat.write_all(b"        i32.const 1\n").unwrap();
-    for _ in 0..number_of_ops {
-        wat.write_all(b"        i32.const 1\n").unwrap();
-        wat.write_all(b"        i32.add\n").unwrap();
+    // ops to be benchmarked
+    wat.write_all(b"        (loop $loop\n").unwrap();
+    wat.write_all(b"            i32.const 0\n").unwrap();
+    for _ in 0..number_of_ops_per_loop_iteration {
+        wat.write_all(b"            i32.const 1\n").unwrap();
+        wat.write_all(b"            i32.add\n").unwrap();
     }
+    wat.write_all(b"            drop\n").unwrap();
+
+    // update ops_counter
+    wat.write_all(b"            global.get $ops_counter\n").unwrap();
+    wat.write_all(format!("            i32.const {}\n", number_of_ops_per_loop_iteration).as_bytes())
+        .unwrap();
+    wat.write_all(b"            i32.add\n").unwrap();
+    wat.write_all(b"            global.set $ops_counter\n").unwrap();
+
+    // check if we need to continue looping
+    wat.write_all(b"            global.get $ops_counter\n").unwrap();
+    wat.write_all(format!("            i32.const {}\n", number_of_ops).as_bytes())
+        .unwrap();
+    wat.write_all(b"            i32.lt_s\n").unwrap();
+    wat.write_all(b"            br_if $loop)\n").unwrap();
 
     wat.write_all(b"        call $toggle_benchmark\n").unwrap();
 
-    wat.write_all(b"        drop\n").unwrap();
     wat.write_all(b"        i32.const 0)\n").unwrap();
     wat.write_all(b")").unwrap();
 
