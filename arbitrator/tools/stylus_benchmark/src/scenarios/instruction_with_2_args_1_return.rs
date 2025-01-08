134c64acec261b22912c19d0ffd842a9ec5efb3a
Diego Ximenes <dxmendes1@gmail.com>
2025-01-08 17:19:35 -0300

    Adds DataType to intruction_with_2_args_1_return

--- arbitrator/tools/stylus_benchmark/src/scenarios/instruction_with_2_args_1_return.rs
+++ arbitrator/tools/stylus_benchmark/src/scenarios/instruction_with_2_args_1_return.rs
@@ -2,12 +2,23 @@
 // For license information, see https://github.com/OffchainLabs/nitro/blob/master/LICENSE
 
 use std::io::Write;
+use crate::scenarios::data_type::DataType;
 
-pub fn write_wat_ops(wat: &mut Vec<u8>, number_of_ops_per_loop_iteration: usize, instruction: &str, first_arg: usize, second_arg: usize) {
+pub fn write_wat_ops(
+    wat: &mut Vec<u8>,
+    number_of_ops_per_loop_iteration: usize,
+    data_type: DataType,
+    instruction: &str,
+    first_arg: usize,
+    second_arg: usize,
+) {
     for _ in 0..number_of_ops_per_loop_iteration {
-        wat.write_all(format!("            i32.const {}\n", first_arg).as_bytes()).unwrap();
-        wat.write_all(format!("            i32.const {}\n", second_arg).as_bytes()).unwrap();
-        wat.write_all(format!("            {}\n", instruction).as_bytes()).unwrap();
+        wat.write_all(format!("            {}.const {}\n", data_type, first_arg).as_bytes())
+            .unwrap();
+        wat.write_all(format!("            {}.const {}\n", data_type, second_arg).as_bytes())
+            .unwrap();
+        wat.write_all(format!("            {}.{}\n", data_type, instruction).as_bytes())
+            .unwrap();
         wat.write_all(b"            drop\n").unwrap();
     }
 }
