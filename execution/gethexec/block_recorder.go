5ed263abb2639cf41788a1d5c81f9db6bfd0a89c
Dalibor <dalibor@tenderly.co>
2025-01-25 01:09:48 +0100

    cleanup

--- execution/gethexec/block_recorder.go
+++ execution/gethexec/block_recorder.go
@@ -102,6 +102,7 @@ func (r *BlockRecorder) RecordBlockCreation(
 	pos arbutil.MessageIndex,
 	msg *arbostypes.MessageWithMetadata,
 ) (*execution.RecordResult, error) {
+
 	blockNum := r.execEngine.MessageIndexToBlockNumber(pos)
 
 	var prevHeader *types.Header
