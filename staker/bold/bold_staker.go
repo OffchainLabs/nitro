c6acd576ebeb6a4600d3816d8b6d12661af998a1
Aman Sanghi <asanghi@offchainlabs.com>
2025-02-20 20:11:52 +0530

    Handle getting creationAtBlock for l3 bold

--- staker/bold/bold_staker.go
+++ staker/bold/bold_staker.go
@@ -566,7 +566,7 @@ func newBOLDChallengeManager(
 func readBoldAssertionCreationInfo(
 	ctx context.Context,
 	rollup *boldrollup.RollupUserLogic,
-	client bind.ContractFilterer,
+	client protocol.ChainBackend,
 	rollupAddress common.Address,
 	assertionHash common.Hash,
 ) (*protocol.AssertionCreatedInfo, error) {
@@ -616,6 +616,10 @@ func readBoldAssertionCreationInfo(
 		return nil, err
 	}
 	afterState := parsedLog.Assertion.AfterState
+	creationL1Block, err := util.CorrespondingL1BlockNumber(ctx, client, ethLog.BlockNumber)
+	if err != nil {
+		return nil, err
+	}
 	return &protocol.AssertionCreatedInfo{
 		ConfirmPeriodBlocks: parsedLog.ConfirmPeriodBlocks,
 		RequiredStake:       parsedLog.RequiredStake,
@@ -629,5 +633,6 @@ func readBoldAssertionCreationInfo(
 		ChallengeManager:    parsedLog.ChallengeManager,
 		TransactionHash:     ethLog.TxHash,
 		CreationBlock:       ethLog.BlockNumber,
+		CreationL1Block:     creationL1Block,
 	}, nil
 }
