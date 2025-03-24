d7db18493c8c3b0bfa90a4a10b75314e9fec654d
Pepper Lebeck-Jobe <eljobe@gmail.com>
2025-03-24 11:06:27 +0100

    Actually set the upper-bound back to 44.
    The target diff is 40, but the tolerance is bounded on the upper-side at 44.

--- arbnode/batch_poster.go
+++ arbnode/batch_poster.go
@@ -615,7 +615,7 @@ func (b *BatchPoster) ParentChainIsUsingEIP7623(ctx context.Context, latestHeade
 	if diffIsClose(gas1, gas2, 14, 18) {
 		// targetDiff is 16
 		parentChainIsUsingEIP7623 = false
-	} else if diffIsClose(gas1, gas2, 36, 40) {
+	} else if diffIsClose(gas1, gas2, 36, 44) {
 		// targetDiff is 40
 		parentChainIsUsingEIP7623 = true
 	} else {
