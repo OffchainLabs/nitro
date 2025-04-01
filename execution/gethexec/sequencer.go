1af08a3f67f90ddf9c0e6bdec30237e8fba3c705
Diego Ximenes <dxmendes1@gmail.com>
2025-04-01 10:13:20 -0300

    Doesn't block sequencer if there is no transaction to process

--- execution/gethexec/sequencer.go
+++ execution/gethexec/sequencer.go
@@ -1270,6 +1270,9 @@ func (s *Sequencer) createBlock(ctx context.Context) (returnValue bool) {
 					continue
 				case <-ctx.Done():
 					return false
+				default:
+					// Doesn't block if there is no transaction to process
+					return true
 				}
 			}
 		} else {
