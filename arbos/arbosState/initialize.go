e5b4f63b5f5fd96019d20143d9956bd179a8d728
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-04-02 19:33:07 +0300

    Update initialize.go

--- arbos/arbosState/initialize.go
+++ arbos/arbosState/initialize.go
@@ -139,7 +139,7 @@ func InitializeArbosInDatabase(db ethdb.Database, cacheConfig *core.CacheConfig,
 		return common.Hash{}, err
 	}
 
-	log.Info("addresss table import complete")
+	log.Info("address table import complete")
 
 	retryableReader, err := initData.GetRetryableDataReader()
 	if err != nil {
