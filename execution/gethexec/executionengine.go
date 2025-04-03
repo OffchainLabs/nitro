37964f24dbc74fca4f6a1a7bffff37dc0d2443b6
Diego Ximenes <dxmendes1@gmail.com>
2025-04-03 12:18:18 -0300

    Skip resequencing reorged message if block creation is stopped

--- execution/gethexec/executionengine.go
+++ execution/gethexec/executionengine.go
@@ -419,6 +419,10 @@ func MessageFromTxes(header *arbostypes.L1IncomingMessageHeader, hooks *arbos.Se
 }
 
 func (s *ExecutionEngine) ResequenceReorgedMessage(msg *arbostypes.MessageWithMetadata) (*execution.SequencedMsg, bool) {
+	if s.syncTillBlock > 0 && s.latestBlock.NumberU64() >= s.syncTillBlock {
+		log.Debug("skipping resequencing reorged message since block creation is stopped")
+		return nil, false
+	}
 	s.createBlocksMutex.Lock()
 	defer s.createBlocksMutex.Unlock()
 
