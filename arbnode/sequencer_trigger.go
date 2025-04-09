266eab137b6d2ab6356b98807483f48e467a1629
Diego Ximenes <dxmendes1@gmail.com>
2025-04-09 16:03:34 -0300

    Expects to be the chosen sequencer before triggering sequencing

--- arbnode/sequencer_trigger.go
+++ arbnode/sequencer_trigger.go
@@ -5,6 +5,7 @@ package arbnode
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	"github.com/ethereum/go-ethereum/log"
@@ -38,6 +39,15 @@ func (s *SequencerTrigger) Start(ctx_in context.Context) {
 func (s *SequencerTrigger) triggerSequencing(ctx context.Context) time.Duration {
 	s.txStreamer.insertionMutex.Lock()
 	defer s.txStreamer.insertionMutex.Unlock()
+
+	if err := s.txStreamer.ExpectChosenSequencer(); err != nil {
+		if errors.Is(err, execution.ErrRetrySequencer) {
+			log.Debug("Not active sequencer, retrying", "err", err)
+		} else {
+			log.Error("Error expecting chosen sequencer", "err", err)
+		}
+		return 100 * time.Millisecond
+	}
 
 	sequencedMsg, nextSequenceCall := s.execSequencer.Sequence(ctx)
 	if sequencedMsg != nil {
