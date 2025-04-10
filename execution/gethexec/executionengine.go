1b42736418257e73eb85ea7e6c876e18b0d13786
Diego Ximenes <dxmendes1@gmail.com>
2025-04-10 16:12:26 -0300

    MsgResult as pointer in SequencedMsg

--- execution/gethexec/executionengine.go
+++ execution/gethexec/executionengine.go
@@ -680,7 +680,7 @@ func (s *ExecutionEngine) sequenceTransactionsWithBlockMutex(header *arbostypes.
 	sequencedMsg := &execution.SequencedMsg{
 		MsgIdx:        msgIdx,
 		MsgWithMeta:   msgWithMeta,
-		MsgResult:     *msgResult,
+		MsgResult:     msgResult,
 		BlockMetadata: blockMetadata,
 	}
 
@@ -761,7 +761,7 @@ func (s *ExecutionEngine) sequenceDelayedMessageWithBlockMutex(message *arbostyp
 	sequencedMsg := &execution.SequencedMsg{
 		MsgIdx:        msgIdx,
 		MsgWithMeta:   messageWithMeta,
-		MsgResult:     *msgResult,
+		MsgResult:     msgResult,
 		BlockMetadata: s.blockMetadataFromBlock(block, nil),
 	}
 
