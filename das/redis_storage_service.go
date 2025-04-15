58d2be26932e30440f43b7f366b5c6fa5310f11d
Tomás Andróil <tomasandroil@gmail.com>
2025-04-15 13:19:36 +0200

    Update redis_storage_service.go

--- das/redis_storage_service.go
+++ das/redis_storage_service.go
@@ -85,7 +85,7 @@ func (rs *RedisStorageService) verifyMessageSignature(data []byte) ([]byte, erro
 func (rs *RedisStorageService) getVerifiedData(ctx context.Context, key common.Hash) ([]byte, error) {
 	data, err := rs.client.Get(ctx, string(key.Bytes())).Bytes()
 	if err != nil {
-		if err != redis.Nil {
+		if !errors.Is(err, redis.Nil) {
 		    log.Error("das.RedisStorageService.getVerifiedData", "err", err)
 		}
 		return nil, err
