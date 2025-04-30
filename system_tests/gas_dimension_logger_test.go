5f752bc8cdbda80fe7198d6a7b4c9f8e4cf955cc
relyt29 <tyler@cyb3r.space>
2025-04-30 12:50:39 -0400

    gas dimension trace tests: single SSTORE test cases

--- system_tests/gas_dimension_logger_test.go
+++ system_tests/gas_dimension_logger_test.go
@@ -1252,24 +1252,75 @@ func TestGasDimensionLoggerCallCodeNonEmptyWarmWithValue(t *testing.T) { t.Fail(
 // non-zero -> 0
 // non-zero -> non-zero (same value)
 // non-zero -> non-zero (different value)
+//
+// Gas dimensionally, SSTORE has one rule basically: if the total gas is
+// greater than 20,000 gas, then we know that a write of a 0->non-zero
+// occured. That is the only case that actually grows state database size
+// all of the other cases are either writing to existing values, or
+// writing to a local cache that will eventually be written to the existing
+// database values.
+// so the majority of cases for SSTORE fall under read/write, except for
+// the one case where the database size grows.
 
 // This test deployes a contract with a local variable that we can SSTORE to
 // in this test, we SSTORE a variable that starts at 0 and SSTORE it to 0
 //
 // we expect the gas cost of this operation to be 2200, 100 for the base sstore cost,
 // and 2100 for cold access set access.
-// we expect computation to be 100, state access to be 2000, state growth to be 0,
+// we expect computation to be 0, state access to be 2200, state growth to be 0,
 // history growth to be 0, and state growth refund to be 0
-func TestGasDimensionLoggerSstoreColdZeroToZero(t *testing.T) { t.Fail() }
+func TestGasDimensionLoggerSstoreColdZeroToZero(t *testing.T) {
+	ctx, cancel, builder, auth, cleanup := gasDimensionLoggerSetup(t)
+	defer cancel()
+	defer cleanup()
+
+	contract := deployGasDimensionTestContract(t, builder, auth, gasdimensionsgen.DeploySstore)
+	receipt := callOnContract(t, builder, auth, contract.SstoreColdZeroToZero)
+
+	traceResult := callDebugTraceTransactionWithLogger(t, ctx, builder, receipt.TxHash)
+	sstoreLog := getSpecificDimensionLog(t, traceResult.DimensionLogs, "SSTORE")
+
+	expected := ExpectedGasCosts{
+		OneDimensionalGasCost: ColdMinusWarmSloadCost + params.NetSstoreDirtyGas,
+		Computation:           0,
+		StateAccess:           ColdMinusWarmSloadCost + params.NetSstoreDirtyGas,
+		StateGrowth:           0,
+		HistoryGrowth:         0,
+		StateGrowthRefund:     0,
+	}
+	checkDimensionLogGasCostsEqual(t, expected, sstoreLog)
+	checkGasDimensionsEqualOneDimensionalGas(t, sstoreLog)
+}
 
 // This test deployes a contract with a local variable that we can SSTORE to
 // in this test, we SSTORE a variable that starts at 0 and SSTORE it to a non-zero value
 //
 // we expect the gas cost of this operation to be 22100, 20000 for the sstore cost,
 // and 2100 for cold access set access.
-// we expect computation to be 100, state read/write to be 0, state growth to be 22000,
+// we expect computation to be 0, state read/write to be 2100, state growth to be 20000,
 // history growth to be 0, and state growth refund to be 0
-func TestGasDimensionLoggerSstoreColdZeroToNonZeroValue(t *testing.T) { t.Fail() }
+func TestGasDimensionLoggerSstoreColdZeroToNonZeroValue(t *testing.T) {
+	ctx, cancel, builder, auth, cleanup := gasDimensionLoggerSetup(t)
+	defer cancel()
+	defer cleanup()
+
+	contract := deployGasDimensionTestContract(t, builder, auth, gasdimensionsgen.DeploySstore)
+	receipt := callOnContract(t, builder, auth, contract.SstoreColdZeroToNonZero)
+
+	traceResult := callDebugTraceTransactionWithLogger(t, ctx, builder, receipt.TxHash)
+	sstoreLog := getSpecificDimensionLog(t, traceResult.DimensionLogs, "SSTORE")
+
+	expected := ExpectedGasCosts{
+		OneDimensionalGasCost: params.SstoreSetGasEIP2200 + params.ColdSloadCostEIP2929,
+		Computation:           0,
+		StateAccess:           params.ColdSloadCostEIP2929,
+		StateGrowth:           params.SstoreSetGasEIP2200,
+		HistoryGrowth:         0,
+		StateGrowthRefund:     0,
+	}
+	checkDimensionLogGasCostsEqual(t, expected, sstoreLog)
+	checkGasDimensionsEqualOneDimensionalGas(t, sstoreLog)
+}
 
 // This test deployes a contract with a local variable that we can SSTORE to
 // in this test, we SSTORE a variable that starts at a non-zero value and SSTORE it to 0
@@ -1278,7 +1329,28 @@ func TestGasDimensionLoggerSstoreColdZeroToNonZeroValue(t *testing.T) { t.Fail()
 // this is from an sstore cost of 2900, and a cold access set cost of 2100.
 // we expect computation to be 100, state read/write to be 0, state growth to be 4900,
 // history growth to be 0, and state growth refund to be 4800
-func TestGasDimensionLoggerSstoreColdNonZeroValueToZero(t *testing.T) { t.Fail() }
+func TestGasDimensionLoggerSstoreColdNonZeroValueToZero(t *testing.T) {
+	ctx, cancel, builder, auth, cleanup := gasDimensionLoggerSetup(t)
+	defer cancel()
+	defer cleanup()
+
+	contract := deployGasDimensionTestContract(t, builder, auth, gasdimensionsgen.DeploySstore)
+	receipt := callOnContract(t, builder, auth, contract.SstoreColdNonZeroValueToZero)
+
+	traceResult := callDebugTraceTransactionWithLogger(t, ctx, builder, receipt.TxHash)
+	sstoreLog := getSpecificDimensionLog(t, traceResult.DimensionLogs, "SSTORE")
+
+	expected := ExpectedGasCosts{
+		OneDimensionalGasCost: params.SstoreResetGasEIP2200,
+		Computation:           0,
+		StateAccess:           params.SstoreResetGasEIP2200,
+		StateGrowth:           0,
+		HistoryGrowth:         0,
+		StateGrowthRefund:     int64(params.NetSstoreResetRefund),
+	}
+	checkDimensionLogGasCostsEqual(t, expected, sstoreLog)
+	checkGasDimensionsEqualOneDimensionalGas(t, sstoreLog)
+}
 
 // This test deployes a contract with a local variable that we can SSTORE to
 // in this test, we SSTORE a variable that starts at a non-zero value and SSTORE it to
@@ -1286,9 +1358,30 @@ func TestGasDimensionLoggerSstoreColdNonZeroValueToZero(t *testing.T) { t.Fail()
 //
 // we expect the gas cost of this operation to be 2200, 100 for the base sstore cost,
 // and 2100 for cold access set access.
-// we expect computation to be 100, state read/write to be 0, state growth to be 2100,
+// we expect computation to be 0, state read/write to be 2200, state growth to be 0,
 // history growth to be 0, and state growth refund to be 0
-func TestGasDimensionLoggerSstoreColdNonZeroToSameNonZeroValue(t *testing.T) { t.Fail() }
+func TestGasDimensionLoggerSstoreColdNonZeroToSameNonZeroValue(t *testing.T) {
+	ctx, cancel, builder, auth, cleanup := gasDimensionLoggerSetup(t)
+	defer cancel()
+	defer cleanup()
+
+	contract := deployGasDimensionTestContract(t, builder, auth, gasdimensionsgen.DeploySstore)
+	receipt := callOnContract(t, builder, auth, contract.SstoreColdNonZeroToSameNonZeroValue)
+
+	traceResult := callDebugTraceTransactionWithLogger(t, ctx, builder, receipt.TxHash)
+	sstoreLog := getSpecificDimensionLog(t, traceResult.DimensionLogs, "SSTORE")
+
+	expected := ExpectedGasCosts{
+		OneDimensionalGasCost: params.ColdSloadCostEIP2929 + params.WarmStorageReadCostEIP2929,
+		Computation:           0,
+		StateAccess:           params.ColdSloadCostEIP2929 + params.WarmStorageReadCostEIP2929,
+		StateGrowth:           0,
+		HistoryGrowth:         0,
+		StateGrowthRefund:     0,
+	}
+	checkDimensionLogGasCostsEqual(t, expected, sstoreLog)
+	checkGasDimensionsEqualOneDimensionalGas(t, sstoreLog)
+}
 
 // This test deployes a contract with a local variable that we can SSTORE to
 // in this test, we SSTORE a variable that starts at a non-zero value and SSTORE it to
@@ -1296,9 +1389,30 @@ func TestGasDimensionLoggerSstoreColdNonZeroToSameNonZeroValue(t *testing.T) { t
 //
 // we expect the gas cost of this operation to be 5000, 2900 for the sstore cost,
 // and 2100 for cold access set access.
-// we expect computation to be 100, state read/write to be 0, state growth to be 4900,
+// we expect computation to be 0, state read/write to be 5000, state growth to be 0,
 // history growth to be 0, and state growth refund to be 0
-func TestGasDimensionLoggerSstoreColdNonZeroToDifferentNonZeroValue(t *testing.T) { t.Fail() }
+func TestGasDimensionLoggerSstoreColdNonZeroToDifferentNonZeroValue(t *testing.T) {
+	ctx, cancel, builder, auth, cleanup := gasDimensionLoggerSetup(t)
+	defer cancel()
+	defer cleanup()
+
+	contract := deployGasDimensionTestContract(t, builder, auth, gasdimensionsgen.DeploySstore)
+	receipt := callOnContract(t, builder, auth, contract.SstoreColdNonZeroToDifferentNonZeroValue)
+
+	traceResult := callDebugTraceTransactionWithLogger(t, ctx, builder, receipt.TxHash)
+	sstoreLog := getSpecificDimensionLog(t, traceResult.DimensionLogs, "SSTORE")
+
+	expected := ExpectedGasCosts{
+		OneDimensionalGasCost: params.SstoreClearGas,
+		Computation:           0,
+		StateAccess:           params.SstoreClearGas,
+		StateGrowth:           0,
+		HistoryGrowth:         0,
+		StateGrowthRefund:     0,
+	}
+	checkDimensionLogGasCostsEqual(t, expected, sstoreLog)
+	checkGasDimensionsEqualOneDimensionalGas(t, sstoreLog)
+}
 
 // This test deployes a contract with a local variable that we can SSTORE to
 // in this test, we SSTORE a variable that starts at 0 and SSTORE it to 0
@@ -1306,7 +1420,28 @@ func TestGasDimensionLoggerSstoreColdNonZeroToDifferentNonZeroValue(t *testing.T
 // we expect the gas cost of this operation to be 100, 100 for the base sstore cost,
 // we expect computation to be 0, state access to be 100, state growth to be 0,
 // history growth to be 0, and state growth refund to be 0
-func TestGasDimensionLoggerSstoreWarmZeroToZero(t *testing.T) { t.Fail() }
+func TestGasDimensionLoggerSstoreWarmZeroToZero(t *testing.T) {
+	ctx, cancel, builder, auth, cleanup := gasDimensionLoggerSetup(t)
+	defer cancel()
+	defer cleanup()
+
+	contract := deployGasDimensionTestContract(t, builder, auth, gasdimensionsgen.DeploySstore)
+	receipt := callOnContract(t, builder, auth, contract.SstoreWarmZeroToZero)
+
+	traceResult := callDebugTraceTransactionWithLogger(t, ctx, builder, receipt.TxHash)
+	sstoreLog := getSpecificDimensionLog(t, traceResult.DimensionLogs, "SSTORE")
+
+	expected := ExpectedGasCosts{
+		OneDimensionalGasCost: params.WarmStorageReadCostEIP2929,
+		Computation:           0,
+		StateAccess:           params.WarmStorageReadCostEIP2929,
+		StateGrowth:           0,
+		HistoryGrowth:         0,
+		StateGrowthRefund:     0,
+	}
+	checkDimensionLogGasCostsEqual(t, expected, sstoreLog)
+	checkGasDimensionsEqualOneDimensionalGas(t, sstoreLog)
+}
 
 // This test deployes a contract with a local variable that we can SSTORE to
 // in this test, we SSTORE a variable that starts at 0 and SSTORE it to a non-zero value
@@ -1314,34 +1449,118 @@ func TestGasDimensionLoggerSstoreWarmZeroToZero(t *testing.T) { t.Fail() }
 // we expect the gas cost of this operation to be 20000, 20000 for the sstore cost,
 // we expect computation to be 0, state access to be 0, state growth to be 20000,
 // history growth to be 0, and state growth refund to be 0
-func TestGasDimensionLoggerSstoreWarmZeroToNonZeroValue(t *testing.T) { t.Fail() }
+func TestGasDimensionLoggerSstoreWarmZeroToNonZeroValue(t *testing.T) {
+	ctx, cancel, builder, auth, cleanup := gasDimensionLoggerSetup(t)
+	defer cancel()
+	defer cleanup()
+
+	contract := deployGasDimensionTestContract(t, builder, auth, gasdimensionsgen.DeploySstore)
+	receipt := callOnContract(t, builder, auth, contract.SstoreWarmZeroToNonZeroValue)
+
+	traceResult := callDebugTraceTransactionWithLogger(t, ctx, builder, receipt.TxHash)
+	sstoreLog := getSpecificDimensionLog(t, traceResult.DimensionLogs, "SSTORE")
+
+	expected := ExpectedGasCosts{
+		OneDimensionalGasCost: params.SstoreSetGasEIP2200,
+		Computation:           0,
+		StateAccess:           0,
+		StateGrowth:           params.SstoreSetGasEIP2200,
+		HistoryGrowth:         0,
+		StateGrowthRefund:     0,
+	}
+	checkDimensionLogGasCostsEqual(t, expected, sstoreLog)
+	checkGasDimensionsEqualOneDimensionalGas(t, sstoreLog)
+}
 
 // This test deployes a contract with a local variable that we can SSTORE to
 // in this test, we SSTORE a variable that starts at a non-zero value and SSTORE it to 0
 //
 // we expect the gas cost of this operation to be 2900, with a gas refund of 4800
 // This is 2900 just for the sstore cost
-// we expect computation to be 0, state read/write to be 0, state growth to be 2900,
+// we expect computation to be 0, state read/write to be 2900, state growth to be 0,
 // history growth to be 0, and state growth refund to be 4800
-func TestGasDimensionLoggerSstoreWarmNonZeroValueToZero(t *testing.T) { t.Fail() }
+func TestGasDimensionLoggerSstoreWarmNonZeroValueToZero(t *testing.T) {
+	ctx, cancel, builder, auth, cleanup := gasDimensionLoggerSetup(t)
+	defer cancel()
+	defer cleanup()
+
+	contract := deployGasDimensionTestContract(t, builder, auth, gasdimensionsgen.DeploySstore)
+	receipt := callOnContract(t, builder, auth, contract.SstoreWarmNonZeroValueToZero)
+
+	traceResult := callDebugTraceTransactionWithLogger(t, ctx, builder, receipt.TxHash)
+	sstoreLog := getSpecificDimensionLog(t, traceResult.DimensionLogs, "SSTORE")
+
+	expected := ExpectedGasCosts{
+		OneDimensionalGasCost: 2900, // there's no params reference for this, it just is 2900
+		Computation:           0,
+		StateAccess:           2900,
+		StateGrowth:           0,
+		HistoryGrowth:         0,
+		StateGrowthRefund:     int64(params.NetSstoreResetRefund),
+	}
+	checkDimensionLogGasCostsEqual(t, expected, sstoreLog)
+	checkGasDimensionsEqualOneDimensionalGas(t, sstoreLog)
+}
 
 // This test deployes a contract with a local variable that we can SSTORE to
 // in this test, we SSTORE a variable that starts at a non-zero value and SSTORE it to
 // the same non-zero value
 //
 // we expect the gas cost of this operation to be 100 for the base sstore cost,
-// we expect computation to be 0, state read/write to be 0, state growth to be 100,
+// we expect computation to be 0, state read/write to be 100, state growth to be 0,
 // history growth to be 0, and state growth refund to be 0
-func TestGasDimensionLoggerSstoreWarmNonZeroToSameNonZeroValue(t *testing.T) { t.Fail() }
+func TestGasDimensionLoggerSstoreWarmNonZeroToSameNonZeroValue(t *testing.T) {
+	ctx, cancel, builder, auth, cleanup := gasDimensionLoggerSetup(t)
+	defer cancel()
+	defer cleanup()
+
+	contract := deployGasDimensionTestContract(t, builder, auth, gasdimensionsgen.DeploySstore)
+	receipt := callOnContract(t, builder, auth, contract.SstoreWarmNonZeroToSameNonZeroValue)
+
+	traceResult := callDebugTraceTransactionWithLogger(t, ctx, builder, receipt.TxHash)
+	sstoreLog := getSpecificDimensionLog(t, traceResult.DimensionLogs, "SSTORE")
+
+	expected := ExpectedGasCosts{
+		OneDimensionalGasCost: params.WarmStorageReadCostEIP2929,
+		Computation:           0,
+		StateAccess:           params.WarmStorageReadCostEIP2929,
+		StateGrowth:           0,
+		HistoryGrowth:         0,
+		StateGrowthRefund:     0,
+	}
+	checkDimensionLogGasCostsEqual(t, expected, sstoreLog)
+	checkGasDimensionsEqualOneDimensionalGas(t, sstoreLog)
+}
 
 // This test deployes a contract with a local variable that we can SSTORE to
 // in this test, we SSTORE a variable that starts at a non-zero value and SSTORE it to
 // a different non-zero value
 //
 // we expect the gas cost of this operation to be 2900 for the base sstore cost,
-// we expect computation to be 0, state read/write to be 0, state growth to be 2900,
+// we expect computation to be 0, state read/write to be 2900, state growth to be 0,
 // history growth to be 0, and state growth refund to be 0
-func TestGasDimensionLoggerSstoreWarmNonZeroToDifferentNonZeroValue(t *testing.T) { t.Fail() }
+func TestGasDimensionLoggerSstoreWarmNonZeroToDifferentNonZeroValue(t *testing.T) {
+	ctx, cancel, builder, auth, cleanup := gasDimensionLoggerSetup(t)
+	defer cancel()
+	defer cleanup()
+
+	contract := deployGasDimensionTestContract(t, builder, auth, gasdimensionsgen.DeploySstore)
+	receipt := callOnContract(t, builder, auth, contract.SstoreWarmNonZeroToDifferentNonZeroValue)
+
+	traceResult := callDebugTraceTransactionWithLogger(t, ctx, builder, receipt.TxHash)
+	sstoreLog := getSpecificDimensionLog(t, traceResult.DimensionLogs, "SSTORE")
+
+	expected := ExpectedGasCosts{
+		OneDimensionalGasCost: 2900, // there's no params reference for this, it just is 2900
+		Computation:           0,
+		StateAccess:           2900,
+		StateGrowth:           0,
+		HistoryGrowth:         0,
+		StateGrowthRefund:     0,
+	}
+	checkDimensionLogGasCostsEqual(t, expected, sstoreLog)
+	checkGasDimensionsEqualOneDimensionalGas(t, sstoreLog)
+}
 
 // This test deployes a contract with a local variable that we can SSTORE to
 // we test multiple SSTOREs and the interaction for the second SSTORE to an already changed value
