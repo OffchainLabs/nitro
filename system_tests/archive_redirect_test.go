918d7867c403d2af64b53030a8aa6a3bdbf86879
Ganesh Vanahalli <gvanahalli@offchainlabs.com>
2025-05-09 11:53:26 +0530

    update geth pin and modify test

--- system_tests/archive_redirect_test.go
+++ system_tests/archive_redirect_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"math/big"
-	"strings"
 	"testing"
 	"time"
 
@@ -44,7 +43,7 @@ func TestRequestForwardingToArchiveNodes(t *testing.T) {
 	// Start a second node so that it already has blocks and then check that archive requests are routed correctly
 	archiveNodeNames := []string{"nodeA", "nodeB", "nodeC", "nodeD"}
 	lastBlocks := []uint64{2, 6, 10, 15}
-	var archiveConfigs []arbitrum.ArchiveRedirectConfig
+	var archiveConfigs []arbitrum.BlockRedirectConfig
 	for i, node := range archiveNodeNames {
 		ipcPath := tmpPath(t, node+".ipc")
 		var apis []rpc.API
@@ -58,39 +57,39 @@ func TestRequestForwardingToArchiveNodes(t *testing.T) {
 		Require(t, err)
 		defer srv.Stop()
 		defer listener.Close()
-		archiveConfigs = append(archiveConfigs, arbitrum.ArchiveRedirectConfig{
+		archiveConfigs = append(archiveConfigs, arbitrum.BlockRedirectConfig{
 			URL:       ipcPath,
 			Timeout:   time.Second,
 			LastBlock: lastBlocks[i],
 		})
 	}
 	execConfig := builder.execConfig
-	execConfig.RPC.ArchiveRedirects = archiveConfigs
+	execConfig.RPC.BlockRedirects = archiveConfigs
 	testClientB, cleanupB := builder.Build2ndNode(t, &SecondNodeParams{execConfig: execConfig})
 	defer cleanupB()
 
-	// All nodes should be able to receive this- so one of them will be randomly picked, so we'll check against general target string
+	// Only nodeA should receive this request
 	_, err = testClientB.Client.BalanceAt(ctx, builder.L2Info.GetAddress("Owner"), big.NewInt(2))
 	t.Logf("error from archive redirect: %s", err.Error())
-	if err == nil || !strings.Contains(err.Error(), "error from") {
-		t.Fatal("incorrect form of error received")
+	if err.Error() != "error from nodeA" {
+		t.Fatalf("unexpected error from archive redirect. Got: %s, Want: error from nodeA", err.Error())
 	}
 
-	// nodeA should not be considered in the random pick so we check againt that not being selected
+	// Only nodeB should receive this request
 	_, err = testClientB.Client.BalanceAt(ctx, builder.L2Info.GetAddress("Owner"), big.NewInt(5))
 	t.Logf("error from archive redirect: %s", err.Error())
-	if err == nil || !strings.Contains(err.Error(), "error from") || err.Error() == "error from nodeA" {
-		t.Fatal("incorrect form of error received")
+	if err.Error() != "error from nodeB" {
+		t.Fatalf("unexpected error from archive redirect. Got: %s, Want: error from nodeB", err.Error())
 	}
 
-	// nodeA and nodeB should not be considered in the random pick so we check againt that not being selected
+	// Only nodeC should receive this request
 	_, err = testClientB.Client.BalanceAt(ctx, builder.L2Info.GetAddress("Owner"), big.NewInt(10))
 	t.Logf("error from archive redirect: %s", err.Error())
-	if err.Error() != "error from nodeC" && err.Error() != "error from nodeD" {
-		t.Fatal("incorrect form of error received")
+	if err.Error() != "error from nodeC" {
+		t.Fatalf("unexpected error from archive redirect. Got: %s, Want: error from nodeC", err.Error())
 	}
 
-	// nodeD should be the only one to be considered in the random pick so we check againt that being selected
+	// Only nodeD should receive this request
 	_, err = testClientB.Client.BalanceAt(ctx, builder.L2Info.GetAddress("Owner"), big.NewInt(15))
 	if err.Error() != "error from nodeD" {
 		t.Fatalf("unexpected error from archive redirect. Got: %s, Want: error from nodeD", err.Error())
