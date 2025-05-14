4c6519a394971a67b5b63b96c2e3d30c1fafce96
Tristan Wilson <twilson@offchainlabs.com>
2025-05-14 11:26:14 -0700

    Validator interface

---
+++ daprovider/validator.go
@@ -0,0 +1,32 @@
+// Copyright 2021-2025, Offchain Labs, Inc.
+// For license information, see https://github.com/nitro/blob/master/LICENSE
+
+package daprovider
+
+import (
+	"context"
+
+	"github.com/ethereum/go-ethereum/common"
+
+	"github.com/offchainlabs/nitro/arbutil"
+)
+
+// PreimageWithType represents a preimage along with its type
+type PreimageWithType struct {
+	Hash         common.Hash
+	Data         []byte
+	PreimageType arbutil.PreimageType
+}
+
+// Validator defines the interface for custom data availability systems
+// This interface is used to validate and generate proofs for CustomDA preimages
+type Validator interface {
+	// RecordPreimages extracts and records preimages from a batch
+	// Returns a list of preimages with their types that were extracted
+	RecordPreimages(ctx context.Context, batch []byte) ([]PreimageWithType, error)
+
+	// GenerateProof generates a proof for a specific preimage at a given offset
+	// The proof format depends on the implementation and must be compatible with
+	// the Solidity ICustomDAValidator contract
+	GenerateProof(ctx context.Context, preimageType arbutil.PreimageType, hash common.Hash, offset uint64) ([]byte, error)
+}
