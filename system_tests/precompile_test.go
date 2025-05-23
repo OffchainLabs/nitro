8011f92a0c53bd6dbf93cf0810760d200c35f0ad
Pepper Lebeck-Jobe <eljobe@gmail.com>
2025-05-23 05:06:33 +0200

    Fix import order

--- system_tests/precompile_test.go
+++ system_tests/precompile_test.go
@@ -12,12 +12,13 @@ import (
 	"testing"
 	"time"
 
+	"github.com/google/go-cmp/cmp"
+
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/params"
-	"github.com/google/go-cmp/cmp"
 
 	"github.com/offchainlabs/nitro/arbos"
 	"github.com/offchainlabs/nitro/arbos/l1pricing"
