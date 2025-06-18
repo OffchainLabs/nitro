f66aa3d8fe357991c2c19361d26c68a88d1b3131
Aman Sanghi <asanghi@offchainlabs.com>
2025-06-18 17:28:02 +0530

    fix build

--- arbnode/message-extraction/extraction-function/delayed_message_lookup.go
+++ arbnode/message-extraction/extraction-function/delayed_message_lookup.go
@@ -12,6 +12,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
+
 	"github.com/offchainlabs/nitro/arbnode"
 	meltypes "github.com/offchainlabs/nitro/arbnode/message-extraction/types"
 	"github.com/offchainlabs/nitro/arbos/arbostypes"
