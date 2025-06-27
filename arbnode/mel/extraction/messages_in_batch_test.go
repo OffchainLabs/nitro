0205d10a1c0a4329c959097104cb7740b603af2e
Raul Jordan <raul@prysmaticlabs.com>
2025-06-27 11:39:40 -0500

    table driven

--- arbnode/mel/extraction/messages_in_batch_test.go
+++ arbnode/mel/extraction/messages_in_batch_test.go
@@ -56,195 +56,244 @@ func Test_extractMessagesInBatch_delayedMessages(t *testing.T) {
 
 func Test_extractArbosMessage(t *testing.T) {
 	ctx := context.Background()
-	t.Run("segment advances timestamp", func(t *testing.T) {
-		compressedData, err := arbcompress.CompressWell([]byte("foobar"))
-		require.NoError(t, err)
-		encodedTimestampAdvance, err := rlp.EncodeToBytes(uint64(50))
-		require.NoError(t, err)
-		melState := &mel.State{}
-		seqMsg := &arbstate.SequencerMessage{
-			Segments: [][]byte{
-				append([]byte{arbstate.BatchSegmentKindAdvanceTimestamp}, encodedTimestampAdvance...),
-				append([]byte{arbstate.BatchSegmentKindL2MessageBrotli}, compressedData...),
+
+	tests := []struct {
+		name            string
+		setupSegments   func() [][]byte
+		setupMelState   func() *mel.State
+		setupSeqMsg     func(segments [][]byte) *arbstate.SequencerMessage
+		setupMockDB     func() *mockDelayedMessageDB
+		wantErr         bool
+		wantErrContains string
+		validateResult  func(t *testing.T, msg *arbostypes.MessageWithMetadata)
+	}{
+		{
+			name: "segment advances timestamp",
+			setupSegments: func() [][]byte {
+				compressedData, _ := arbcompress.CompressWell([]byte("foobar"))
+				encodedTimestampAdvance, _ := rlp.EncodeToBytes(uint64(50))
+				return [][]byte{
+					append([]byte{arbstate.BatchSegmentKindAdvanceTimestamp}, encodedTimestampAdvance...),
+					append([]byte{arbstate.BatchSegmentKindL2MessageBrotli}, compressedData...),
+				}
 			},
-			MinTimestamp: 0,
-			MaxTimestamp: 1_000_000,
-		}
-		params := &arbosExtractionParams{
-			melState:         melState,
-			seqMsg:           seqMsg,
-			delayedMsgDB:     nil,
-			targetSubMessage: 0,
-			segmentNum:       0,
-			blockNumber:      0,
-			timestamp:        0,
-		}
-		msg, _, err := extractArbosMessage(ctx, params)
-		require.NoError(t, err)
-		require.Equal(t, msg.Message.L2msg, []byte("foobar"))
-		require.Equal(t, msg.Message.Header.Timestamp, uint64(50))
-	})
-	t.Run("segment advances blocknum", func(t *testing.T) {
-		compressedData, err := arbcompress.CompressWell([]byte("foobar"))
-		require.NoError(t, err)
-		encodedBlockNum, err := rlp.EncodeToBytes(uint64(20))
-		require.NoError(t, err)
-		melState := &mel.State{}
-		seqMsg := &arbstate.SequencerMessage{
-			Segments: [][]byte{
-				append([]byte{arbstate.BatchSegmentKindAdvanceL1BlockNumber}, encodedBlockNum...),
-				append([]byte{arbstate.BatchSegmentKindL2MessageBrotli}, compressedData...),
+			setupMelState: func() *mel.State {
+				return &mel.State{}
 			},
-			MinTimestamp: 0,
-			MaxTimestamp: 1_000_000,
-			MaxL1Block:   1_000_000,
-		}
-		params := &arbosExtractionParams{
-			melState:         melState,
-			seqMsg:           seqMsg,
-			delayedMsgDB:     nil,
-			targetSubMessage: 0,
-			segmentNum:       0,
-			blockNumber:      0,
-			timestamp:        0,
-		}
-		msg, _, err := extractArbosMessage(ctx, params)
-		require.NoError(t, err)
-		require.Equal(t, msg.Message.L2msg, []byte("foobar"))
-		require.Equal(t, msg.Message.Header.BlockNumber, uint64(20))
-	})
-	t.Run("brotli compressed message", func(t *testing.T) {
-		compressedData, err := arbcompress.CompressWell([]byte("foobar"))
-		require.NoError(t, err)
-		encodedTimestampAdvance := make([]byte, 8)
-		binary.BigEndian.PutUint64(encodedTimestampAdvance, 50)
-		melState := &mel.State{}
-		seqMsg := &arbstate.SequencerMessage{
-			Segments: [][]byte{
-				append([]byte{arbstate.BatchSegmentKindAdvanceTimestamp}, encodedTimestampAdvance...),
-				append([]byte{arbstate.BatchSegmentKindL2MessageBrotli}, compressedData...),
+			setupSeqMsg: func(segments [][]byte) *arbstate.SequencerMessage {
+				return &arbstate.SequencerMessage{
+					Segments:     segments,
+					MinTimestamp: 0,
+					MaxTimestamp: 1_000_000,
+				}
 			},
-			MinTimestamp: 0,
-			MaxTimestamp: 1_000_000,
-		}
-		params := &arbosExtractionParams{
-			melState:         melState,
-			seqMsg:           seqMsg,
-			delayedMsgDB:     nil,
-			targetSubMessage: 0,
-			segmentNum:       0,
-			blockNumber:      0,
-			timestamp:        0,
-		}
-		msg, _, err := extractArbosMessage(ctx, params)
-		require.NoError(t, err)
-		require.Equal(t, msg.Message.L2msg, []byte("foobar"))
-	})
-	t.Run("delayed message segment greater than what has been read", func(t *testing.T) {
-		melState := &mel.State{
-			DelayedMessagesRead: 1,
-		}
-		seqMsg := &arbstate.SequencerMessage{
-			AfterDelayedMessages: 1,
-			Segments: [][]byte{
-				{},
+			setupMockDB: func() *mockDelayedMessageDB {
+				return nil
 			},
-		}
-		params := &arbosExtractionParams{
-			melState:         melState,
-			seqMsg:           seqMsg,
-			delayedMsgDB:     nil,
-			targetSubMessage: 0,
-			segmentNum:       0,
-			blockNumber:      0,
-			timestamp:        0,
-		}
-		msg, _, err := extractArbosMessage(ctx, params)
-		require.NoError(t, err)
-		require.Equal(t, arbostypes.InvalidL1Message, msg.Message)
-	})
-	t.Run("gets error fetching delayed message from database", func(t *testing.T) {
-		melState := &mel.State{
-			DelayedMessagesRead: 0,
-		}
-		seqMsg := &arbstate.SequencerMessage{
-			AfterDelayedMessages: 1,
-			Segments: [][]byte{
-				{},
+			wantErr: false,
+			validateResult: func(t *testing.T, msg *arbostypes.MessageWithMetadata) {
+				require.Equal(t, []byte("foobar"), msg.Message.L2msg)
+				require.Equal(t, uint64(50), msg.Message.Header.Timestamp)
 			},
-		}
-		mockDB := &mockDelayedMessageDB{
-			err: errors.New("oops"),
-		}
-		params := &arbosExtractionParams{
-			melState:         melState,
-			seqMsg:           seqMsg,
-			delayedMsgDB:     mockDB,
-			targetSubMessage: 0,
-			segmentNum:       0,
-			blockNumber:      0,
-			timestamp:        0,
-		}
-		_, _, err := extractArbosMessage(ctx, params)
-		require.ErrorContains(t, err, "oops")
-	})
-	t.Run("gets nil delayed message from database", func(t *testing.T) {
-		melState := &mel.State{
-			DelayedMessagesRead: 0,
-		}
-		seqMsg := &arbstate.SequencerMessage{
-			AfterDelayedMessages: 1,
-			Segments: [][]byte{
-				{},
+		},
+		{
+			name: "segment advances blocknum",
+			setupSegments: func() [][]byte {
+				compressedData, _ := arbcompress.CompressWell([]byte("foobar"))
+				encodedBlockNum, _ := rlp.EncodeToBytes(uint64(20))
+				return [][]byte{
+					append([]byte{arbstate.BatchSegmentKindAdvanceL1BlockNumber}, encodedBlockNum...),
+					append([]byte{arbstate.BatchSegmentKindL2MessageBrotli}, compressedData...),
+				}
 			},
-		}
-		mockDB := &mockDelayedMessageDB{
-			DelayedMessages: map[uint64]*arbnode.DelayedInboxMessage{},
-		}
-		params := &arbosExtractionParams{
-			melState:         melState,
-			seqMsg:           seqMsg,
-			delayedMsgDB:     mockDB,
-			targetSubMessage: 0,
-			segmentNum:       0,
-			blockNumber:      0,
-			timestamp:        0,
-		}
-		_, _, err := extractArbosMessage(ctx, params)
-		require.ErrorContains(t, err, "no more delayed messages in db")
-	})
-	t.Run("reading delayed message OK", func(t *testing.T) {
-		melState := &mel.State{
-			DelayedMessagesRead: 0,
-		}
-		seqMsg := &arbstate.SequencerMessage{
-			AfterDelayedMessages: 1,
-			Segments: [][]byte{
-				{},
+			setupMelState: func() *mel.State {
+				return &mel.State{}
 			},
-		}
-		mockDB := &mockDelayedMessageDB{
-			DelayedMessages: map[uint64]*arbnode.DelayedInboxMessage{
-				0: {
-					Message: &arbostypes.L1IncomingMessage{
-						L2msg: []byte("foobar"),
+			setupSeqMsg: func(segments [][]byte) *arbstate.SequencerMessage {
+				return &arbstate.SequencerMessage{
+					Segments:     segments,
+					MinTimestamp: 0,
+					MaxTimestamp: 1_000_000,
+					MaxL1Block:   1_000_000,
+				}
+			},
+			setupMockDB: func() *mockDelayedMessageDB {
+				return nil
+			},
+			wantErr: false,
+			validateResult: func(t *testing.T, msg *arbostypes.MessageWithMetadata) {
+				require.Equal(t, []byte("foobar"), msg.Message.L2msg)
+				require.Equal(t, uint64(20), msg.Message.Header.BlockNumber)
+			},
+		},
+		{
+			name: "brotli compressed message",
+			setupSegments: func() [][]byte {
+				compressedData, _ := arbcompress.CompressWell([]byte("foobar"))
+				encodedTimestampAdvance := make([]byte, 8)
+				binary.BigEndian.PutUint64(encodedTimestampAdvance, 50)
+				return [][]byte{
+					append([]byte{arbstate.BatchSegmentKindAdvanceTimestamp}, encodedTimestampAdvance...),
+					append([]byte{arbstate.BatchSegmentKindL2MessageBrotli}, compressedData...),
+				}
+			},
+			setupMelState: func() *mel.State {
+				return &mel.State{}
+			},
+			setupSeqMsg: func(segments [][]byte) *arbstate.SequencerMessage {
+				return &arbstate.SequencerMessage{
+					Segments:     segments,
+					MinTimestamp: 0,
+					MaxTimestamp: 1_000_000,
+				}
+			},
+			setupMockDB: func() *mockDelayedMessageDB {
+				return nil
+			},
+			wantErr: false,
+			validateResult: func(t *testing.T, msg *arbostypes.MessageWithMetadata) {
+				require.Equal(t, []byte("foobar"), msg.Message.L2msg)
+			},
+		},
+		{
+			name: "delayed message segment greater than what has been read",
+			setupSegments: func() [][]byte {
+				return [][]byte{{}}
+			},
+			setupMelState: func() *mel.State {
+				return &mel.State{
+					DelayedMessagesRead: 1,
+				}
+			},
+			setupSeqMsg: func(segments [][]byte) *arbstate.SequencerMessage {
+				return &arbstate.SequencerMessage{
+					AfterDelayedMessages: 1,
+					Segments:             segments,
+				}
+			},
+			setupMockDB: func() *mockDelayedMessageDB {
+				return nil
+			},
+			wantErr: false,
+			validateResult: func(t *testing.T, msg *arbostypes.MessageWithMetadata) {
+				require.Equal(t, arbostypes.InvalidL1Message, msg.Message)
+			},
+		},
+		{
+			name: "gets error fetching delayed message from database",
+			setupSegments: func() [][]byte {
+				return [][]byte{{}}
+			},
+			setupMelState: func() *mel.State {
+				return &mel.State{
+					DelayedMessagesRead: 0,
+				}
+			},
+			setupSeqMsg: func(segments [][]byte) *arbstate.SequencerMessage {
+				return &arbstate.SequencerMessage{
+					AfterDelayedMessages: 1,
+					Segments:             segments,
+				}
+			},
+			setupMockDB: func() *mockDelayedMessageDB {
+				return &mockDelayedMessageDB{
+					err: errors.New("oops"),
+				}
+			},
+			wantErr:         true,
+			wantErrContains: "oops",
+		},
+		{
+			name: "gets nil delayed message from database",
+			setupSegments: func() [][]byte {
+				return [][]byte{{}}
+			},
+			setupMelState: func() *mel.State {
+				return &mel.State{
+					DelayedMessagesRead: 0,
+				}
+			},
+			setupSeqMsg: func(segments [][]byte) *arbstate.SequencerMessage {
+				return &arbstate.SequencerMessage{
+					AfterDelayedMessages: 1,
+					Segments:             segments,
+				}
+			},
+			setupMockDB: func() *mockDelayedMessageDB {
+				return &mockDelayedMessageDB{
+					DelayedMessages: map[uint64]*arbnode.DelayedInboxMessage{},
+				}
+			},
+			wantErr:         true,
+			wantErrContains: "no more delayed messages in db",
+		},
+		{
+			name: "reading delayed message OK",
+			setupSegments: func() [][]byte {
+				return [][]byte{{}}
+			},
+			setupMelState: func() *mel.State {
+				return &mel.State{
+					DelayedMessagesRead: 0,
+				}
+			},
+			setupSeqMsg: func(segments [][]byte) *arbstate.SequencerMessage {
+				return &arbstate.SequencerMessage{
+					AfterDelayedMessages: 1,
+					Segments:             segments,
+				}
+			},
+			setupMockDB: func() *mockDelayedMessageDB {
+				return &mockDelayedMessageDB{
+					DelayedMessages: map[uint64]*arbnode.DelayedInboxMessage{
+						0: {
+							Message: &arbostypes.L1IncomingMessage{
+								L2msg: []byte("foobar"),
+							},
+						},
 					},
-				},
+				}
 			},
-		}
-		params := &arbosExtractionParams{
-			melState:         melState,
-			seqMsg:           seqMsg,
-			delayedMsgDB:     mockDB,
-			targetSubMessage: 0,
-			segmentNum:       0,
-			blockNumber:      0,
-			timestamp:        0,
-		}
-		msg, _, err := extractArbosMessage(ctx, params)
-		require.NoError(t, err)
-		require.Equal(t, []byte("foobar"), msg.Message.L2msg)
-	})
+			wantErr: false,
+			validateResult: func(t *testing.T, msg *arbostypes.MessageWithMetadata) {
+				require.Equal(t, []byte("foobar"), msg.Message.L2msg)
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			segments := tt.setupSegments()
+			melState := tt.setupMelState()
+			seqMsg := tt.setupSeqMsg(segments)
+			mockDB := tt.setupMockDB()
+
+			params := &arbosExtractionParams{
+				melState:         melState,
+				seqMsg:           seqMsg,
+				delayedMsgDB:     mockDB,
+				targetSubMessage: 0,
+				segmentNum:       0,
+				blockNumber:      0,
+				timestamp:        0,
+			}
+
+			msg, _, err := extractArbosMessage(ctx, params)
+
+			if tt.wantErr {
+				require.Error(t, err)
+				if tt.wantErrContains != "" {
+					require.ErrorContains(t, err, tt.wantErrContains)
+				}
+				return
+			}
+
+			require.NoError(t, err)
+			if tt.validateResult != nil {
+				tt.validateResult(t, msg)
+			}
+		})
+	}
 }
 
 func Test_isLastSegment(t *testing.T) {
