68d6bcd6e8e1a42799a789d25ea1ed8ffa0c8aa4
Theodore Schnepper <tschnepper@espressosys.com>
2025-07-17 09:14:40 -0600

    Add MockEspressoChain
    When testing our implementation that performs finalization checks against Espresso, it
    is beneficial to operate on a controllable idealized version the Espresso Chain that
    operates and documents our initial assumptions of how the chain should progress.
    Additionally, it is beneficial to have direct control over this chain so that we are able
    to control exactly when and how it advances, and the rate at which it produces blocks.
    
    This simplified idealized representation of the `EspressoChain`, implements only the
    methods needed for operation and utilization within the `TransactionStreamer`
    interface.

---
+++ system_tests/espresso/chain/mock_espresso_chain.go
@@ -0,0 +1,352 @@
+package chain
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"sync"
+	"time"
+
+	espresso_client "github.com/EspressoSystems/espresso-network/sdks/go/client"
+	tagged_base64 "github.com/EspressoSystems/espresso-network/sdks/go/tagged-base64"
+	espresso_types "github.com/EspressoSystems/espresso-network/sdks/go/types"
+	espresso_common "github.com/EspressoSystems/espresso-network/sdks/go/types/common"
+)
+
+// MockEspressoChain is a mock implementation of an Espresso Blockchain for
+// testing purposes.
+// It's meant to simulate a basic, likely incorrect and incomplete,
+// implementation of an Espresso chain.
+//
+// This implementation is not concerned with specifics or details about the
+// Espresso chain, and is not meant to be utilized as a sufficient substitute
+// for a real Espresso chain.
+//
+// What matters most about this implementation is that it allows the user to
+// control when new blocks are produced, and allows the user to submit, and
+// check for the existence of, transactions within the chain.
+type MockEspressoChain struct {
+	UnimplementedEspressoClient
+	Lock sync.RWMutex
+
+	Height     uint64
+	PendingTxs []espresso_common.Transaction
+
+	BlockHeightStore map[uint64]BlockDetail
+	BlockHashStore   map[string]BlockDetail // This is not used in the mock, but kept for interface compatibility
+	TxnHashStore     map[string]TransactionDetail
+}
+
+// Ensure MockEspressoChain implements the EspressoClient interface.
+var _ espresso_client.EspressoClient = &MockEspressoChain{}
+
+// NewMockEspressoChain creates a new instance of MockEspressoChain with
+// initialized stores for blocks and transactions.
+func NewMockEspressoChain() *MockEspressoChain {
+	return &MockEspressoChain{
+		BlockHeightStore: make(map[uint64]BlockDetail),
+		BlockHashStore:   make(map[string]BlockDetail),
+		TxnHashStore:     make(map[string]TransactionDetail),
+	}
+}
+
+// ErrorBlockNotFoundForHeight is an error type that indicates a block was not
+// found at a given height in the mock Espresso chain.
+type ErrorBlockNotFoundForHeight struct {
+	Height uint64
+}
+
+// Error implements error
+func (e ErrorBlockNotFoundForHeight) Error() string {
+	return fmt.Sprintf("block not found at height: %d", e.Height)
+}
+
+// ErrorFailedToComputeBlockHash is an error type that indicates a failure to
+// compute the hash of a block in the mock Espresso chain.
+type ErrorFailedToComputeBlockHash struct {
+	Cause error
+}
+
+// Error implements error
+func (e ErrorFailedToComputeBlockHash) Error() string {
+	return fmt.Sprintf("failed to compute block hash: %v", e.Cause)
+}
+
+// Advance simulates the advancement of the Espresso chain by creating a new
+// block with the list of pending transactions, advancing the height, and
+// storing the block and transaction details in their respective stores.
+func (m *MockEspressoChain) Advance() {
+	m.Lock.Lock()
+	defer m.Lock.Unlock()
+
+	// Let's "build" a new block
+	height := m.Height
+	m.Height++
+
+	block := BlockDetail{
+		Height:       height,
+		NumTxns:      uint64(len(m.PendingTxs)),
+		Transactions: m.PendingTxs,
+	}
+
+	m.PendingTxs = nil // Clear pending transactions after committing them to the block
+
+	blockTag, err := block.TaggedBase64()
+	if err != nil {
+		panic(fmt.Sprintf("failed to create tagged base64 for block: %v", err))
+	}
+
+	m.BlockHeightStore[height] = block
+	m.BlockHashStore[blockTag.String()] = block
+
+	for i, tx := range block.Transactions {
+		txTag, err := TransactionTaggedBase64(tx)
+		if err != nil {
+			panic(fmt.Sprintf("failed to create tagged base64 for transaction: %v", err))
+		}
+
+		txDetail := TransactionDetail{
+			Block:       height,
+			Index:       uint64(i),    // Index is not used in this mock
+			Namespace:   tx.Namespace, // Namespace is not used in this mock
+			Size:        uint64(len(tx.Payload)),
+			Transaction: tx,
+		}
+
+		m.TxnHashStore[txTag.String()] = txDetail
+	}
+}
+
+// FetchTransactionsInBlock retrieves the transactions in a block at a given
+// height and namespace. It returns an error if the block is not found.
+func (m *MockEspressoChain) FetchTransactionsInBlock(ctx context.Context, blockHeight uint64, namespace uint64) (espresso_client.TransactionsInBlock, error) {
+	m.Lock.RLock()
+	block, blockOk := m.BlockHeightStore[blockHeight]
+	m.Lock.RUnlock()
+	if !blockOk {
+		return espresso_client.TransactionsInBlock{}, ErrorBlockNotFoundForHeight{Height: blockHeight}
+	}
+
+	txns := make([]espresso_types.Bytes, 0, len(block.Transactions))
+	for _, tx := range block.Transactions {
+		txns = append(txns, espresso_types.Bytes(tx.Payload))
+	}
+	return espresso_client.TransactionsInBlock{
+		Transactions: txns,
+	}, nil
+}
+
+// ErrorInvalidHash is an error type that indicates an invalid hash was provided
+// when trying to fetch a transaction by its hash in the mock Espresso chain.
+type ErrorInvalidHash struct {
+	Hash *espresso_common.TaggedBase64
+}
+
+// Error implements error
+func (e ErrorInvalidHash) Error() string {
+	if e.Hash == nil {
+		return "invalid hash: nil"
+	}
+
+	return fmt.Sprintf("invalid hash: %s", e.Hash.String())
+}
+
+// ErrorTransactionNotFoundForHash is an error type that indicates a transaction
+// was not found for a given hash in the mock Espresso chain.
+type ErrorTransactionNotFoundForHash struct {
+	Hash espresso_common.TaggedBase64
+}
+
+// Error implements error
+func (e ErrorTransactionNotFoundForHash) Error() string {
+	return fmt.Sprintf("transaction not found for hash: %s", e.Hash.String())
+}
+
+// FetchTransactionByHash retrieves a transaction by its hash. It returns an
+// error if the hash is nil or if the transaction is not found in the mock
+// Espresso chain.
+func (m *MockEspressoChain) FetchTransactionByHash(ctx context.Context, hash *espresso_common.TaggedBase64) (espresso_types.TransactionQueryData, error) {
+	if hash == nil {
+		return espresso_types.TransactionQueryData{}, ErrorInvalidHash{
+			Hash: hash,
+		}
+	}
+
+	key := hash.String()
+	m.Lock.RLock()
+	txDetail, txDetailOk := m.TxnHashStore[key]
+	m.Lock.RUnlock()
+	if !txDetailOk {
+		return espresso_types.TransactionQueryData{}, ErrorTransactionNotFoundForHash{
+			Hash: *hash,
+		}
+	}
+
+	blockDetail, blockDetailOk := m.BlockHeightStore[txDetail.Block]
+	if !blockDetailOk {
+		return espresso_types.TransactionQueryData{}, ErrorBlockNotFoundForHeight{Height: txDetail.Block}
+	}
+
+	blockHash, err := blockDetail.TaggedBase64()
+	if err != nil {
+		return espresso_types.TransactionQueryData{}, ErrorFailedToComputeBlockHash{Cause: err}
+	}
+
+	return espresso_types.TransactionQueryData{
+		Transaction: txDetail.Transaction,
+		Hash:        hash,
+		Index:       txDetail.Index,
+		Proof:       json.RawMessage(`[]`), // Mocking proof as empty JSON array
+		BlockHash:   blockHash,
+		BlockHeight: blockDetail.Height,
+	}, nil
+}
+
+// FetchExplorerTransactionByHash retrieves transaction details for a given
+// hash from the mock Espresso chain.
+func (m *MockEspressoChain) FetchExplorerTransactionByHash(ctx context.Context, hash *espresso_types.TaggedBase64) (espresso_types.ExplorerTransactionQueryData, error) {
+	if hash == nil {
+		return espresso_types.ExplorerTransactionQueryData{}, ErrorInvalidHash{
+			Hash: hash,
+		}
+	}
+
+	key := hash.String()
+	m.Lock.RLock()
+	txDetail, txDetailOk := m.TxnHashStore[key]
+	m.Lock.RUnlock()
+	if !txDetailOk {
+		return espresso_types.ExplorerTransactionQueryData{}, ErrorTransactionNotFoundForHash{
+			Hash: *hash,
+		}
+	}
+
+	return espresso_types.ExplorerTransactionQueryData{
+		TransactionsDetails: espresso_common.ExplorerTransactionsDetails{
+			ExplorerDetails: espresso_common.ExplorerDetails{
+				BlockHeight: txDetail.Block,
+				Hash:        *hash,
+			},
+		},
+	}, nil
+}
+
+// ErrorSubmitTransaction is an error type that indicates a failure to submit
+// a transaction to the mock Espresso chain.
+type ErrorSubmitTransaction struct {
+	Cause error
+}
+
+// Error implements error
+func (e ErrorSubmitTransaction) Error() string {
+	return fmt.Sprintf("failed to submit transaction: %v", e.Cause)
+}
+
+// SubmitTransaction simulates the submission of a transaction to the mock
+// Espresso chain. It generates a unique hash for the transaction, appends it
+// to the list of pending transactions, and returns the generated hash.
+func (m *MockEspressoChain) SubmitTransaction(ctx context.Context, tx espresso_common.Transaction) (*espresso_common.TaggedBase64, error) {
+	// Need to generate a unique hash for the transaction
+	tag, err := TransactionTaggedBase64(tx)
+	if err != nil {
+		return nil, ErrorSubmitTransaction{
+			Cause: err,
+		}
+	}
+
+	m.Lock.Lock()
+	m.PendingTxs = append(m.PendingTxs, tx)
+	m.Lock.Unlock()
+
+	return tag, nil
+}
+
+// FetchLatestBlockHeight retrieves the latest block height from the mock
+// Espresso chain. It returns the current height and does not produce an error.
+// This method is used to simulate the behavior of fetching the latest block
+// height in a real Espresso chain.
+//
+// NOTE: This method does not acquire a lock, as it only reads the current
+// height, which should be safe to do.
+func (m *MockEspressoChain) FetchLatestBlockHeight(ctx context.Context) (uint64, error) {
+	return m.Height, nil
+}
+
+// ProduceEspressoBlocksAtInterval is a convenience function that calls
+// Advance on the provided MockEspressoChain at a specified interval.
+//
+// It is meant to be spawned in a separate goroutine to simulate the
+// production of blocks in the Espresso chain at regular intervals.
+//
+// NOTE: This can be used to simulate a live Espresso chain that produces blocks
+// at a given interval, which can be useful for testing purposes.
+func ProduceEspressoBlocksAtInterval(ctx context.Context, chain *MockEspressoChain, interval time.Duration) {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	ticker := time.NewTicker(interval)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+			chain.Advance()
+		}
+	}
+}
+
+// BlockDetail represents the minimal amount of information needed to represent
+// a Block in the Espresso chain for testing purposes.
+type BlockDetail struct {
+	Height       uint64
+	NumTxns      uint64
+	Transactions []espresso_common.Transaction
+}
+
+// Commit returns a commitment for the BlockDetail which is comprised of its
+// contents.
+func (b BlockDetail) Commit() espresso_common.Commitment {
+	builder := espresso_common.NewRawCommitmentBuilder("BlockDetail").
+		Uint64Field("height", b.Height)
+
+	for _, tx := range b.Transactions {
+		tCommit := tx.Commit()
+		builder.VarSizeBytes(espresso_common.Bytes(tCommit[:]))
+	}
+
+	return builder.
+		Finalize()
+}
+
+// TaggedBase64 is a convenience method to create a TaggedBase64
+func (b BlockDetail) TaggedBase64() (*espresso_common.TaggedBase64, error) {
+	commitment := b.Commit()
+	tag, err := tagged_base64.New("MOCK-BLOCK", commitment[:])
+	if err != nil {
+		return nil, fmt.Errorf("failed to create tagged base64 for block: %w", err)
+	}
+	return tag, nil
+}
+
+// TransactionDetail represents the minimal amount of information needed to
+// represent a Transaction in the Espresso chain for testing purposes.
+type TransactionDetail struct {
+	Block       uint64
+	Index       uint64
+	Namespace   uint64
+	Size        uint64
+	Transaction espresso_common.Transaction
+}
+
+// TransactionTaggedBase64 is a convenience method to create a TaggedBase64
+// for a TransactionDetail.
+func TransactionTaggedBase64(tx espresso_common.Transaction) (*espresso_common.TaggedBase64, error) {
+	commitment := tx.Commit()
+	tag, err := tagged_base64.New("MOCK-TXN", commitment[:])
+	if err != nil {
+		return nil, fmt.Errorf("failed to create tagged base64 for transaction: %w", err)
+	}
+
+	return tag, nil
+}
---
+++ system_tests/espresso/chain/mock_espresso_chain_test.go
@@ -0,0 +1,239 @@
+package chain_test
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	tagged_base64 "github.com/EspressoSystems/espresso-network/sdks/go/tagged-base64"
+	espresso_common "github.com/EspressoSystems/espresso-network/sdks/go/types/common"
+	"github.com/offchainlabs/nitro/system_tests/espresso/chain"
+)
+
+// ExampleNewMockEspressoChain demonstrates how to create and utilize the
+// MockEspressoChain.
+func ExampleNewMockEspressoChain() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Create a new mock Espresso chain
+	mockChain := chain.NewMockEspressoChain()
+
+	// The Mock Chain is simplified representation of an Espresso Chain
+	// that allows the consumer control over when the chain advances and
+	// produces the next block.
+	//
+	// This approach allows for the flexibility of controlling specific
+	// progression patterns that might result in edge cases that can
+	// be reproduced.
+
+	{
+		fmt.Println()
+		// The chain starts at height 0, and will only Advance, when
+		// the Advance method is called.
+
+		height, err := mockChain.FetchLatestBlockHeight(ctx)
+		if err != nil {
+			panic(fmt.Errorf("error fetching latest block height: %w\n", err))
+		}
+
+		fmt.Printf("Starting Block Height: %d\n", height)
+
+		// Advance the chain to the next block.
+		mockChain.Advance()
+
+		height, err = mockChain.FetchLatestBlockHeight(ctx)
+		if err != nil {
+			panic(fmt.Errorf("error fetching latest block height after advance: %w\n", err))
+		}
+		fmt.Printf("After Advance Block Height: %d\n", height)
+	}
+
+	{
+		fmt.Println()
+		// If we submit a transaction, it will not immediately result in a block
+		// but instead will be pending until the next Advance call.
+		// Once the next Advance call occurs, the pending transactions will
+		// be included in the next block.
+
+		txHash, err := mockChain.SubmitTransaction(ctx, espresso_common.Transaction{
+			Namespace: 1234,
+			Payload:   []byte("example transaction payload"),
+		})
+
+		if err != nil {
+			panic(fmt.Errorf("error submitting transaction: %w\n", err))
+		}
+
+		fmt.Printf("Submitted transaction with hash: %s\n", txHash)
+
+		// If we try to grab the block for the transaction now, it will not be
+		// found
+
+		_, err = mockChain.FetchTransactionByHash(ctx, txHash)
+		if err == nil {
+			// This is an error
+			panic(fmt.Errorf("expected error fetching transaction by hash, but received no error"))
+		}
+
+		fmt.Printf("Lookup for transaction by hash returned error: %s\n", err)
+
+		// Advance the chain to produce the next block
+		mockChain.Advance()
+
+		// Now we can fetch the transaction by hash, and it should be found
+		txData, err := mockChain.FetchTransactionByHash(ctx, txHash)
+		if err != nil {
+			panic(fmt.Errorf("error fetching transaction by hash after advance: %w\n", err))
+		}
+
+		fmt.Printf("Fetched transaction by hash: %s\n", txData.Hash)
+
+		// This will also allow us to fetch all of the transactions within the
+		// block
+
+		txsInBlock, err := mockChain.FetchTransactionsInBlock(ctx, 1, 1234)
+		if err != nil {
+			panic(fmt.Errorf("error fetching transactions in block: %w\n", err))
+		}
+
+		for i, tx := range txsInBlock.Transactions {
+			fmt.Printf("Transaction in block [%d]: \"%s\"\n", i, tx)
+		}
+
+		// As an alternative, we can also fetch the explorer block information.
+		explorerBlock, err := mockChain.FetchExplorerTransactionByHash(ctx, txHash)
+		if err != nil {
+			panic(fmt.Errorf("error fetching explorer transaction by hash: %w\n", err))
+		}
+
+		fmt.Printf("Explorer Block Height: %d\n", explorerBlock.TransactionsDetails.ExplorerDetails.BlockHeight)
+		fmt.Printf("Explorer Block Hash: %s\n", explorerBlock.TransactionsDetails.ExplorerDetails.Hash.String())
+	}
+
+	// Output:
+	// Starting Block Height: 0
+	// After Advance Block Height: 1
+	//
+	// Submitted transaction with hash: MOCK-TXN~Q8U3UD4JrPhZfR24S5u7ulmWIj48P6AlHxxeQF-xUmfI
+	// Lookup for transaction by hash returned error: transaction not found for hash: MOCK-TXN~Q8U3UD4JrPhZfR24S5u7ulmWIj48P6AlHxxeQF-xUmfI
+	// Fetched transaction by hash: MOCK-TXN~Q8U3UD4JrPhZfR24S5u7ulmWIj48P6AlHxxeQF-xUmfI
+	// Transaction in block [0]: "example transaction payload"
+	// Explorer Block Height: 1
+	// Explorer Block Hash: MOCK-TXN~Q8U3UD4JrPhZfR24S5u7ulmWIj48P6AlHxxeQF-xUmfI
+}
+
+// ExampleProduceEspressoBlocksAtInterval demonstrates how to produce blocks
+// at a regular interval using the MockEspressoChain, to more accurately
+// reflect the real Espresso Chain.
+func ExampleProduceEspressoBlocksAtInterval() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Create a new mock Espresso chain
+	mockChain := chain.NewMockEspressoChain()
+
+	{
+		produceCtx, produceCancel := context.WithCancel(ctx)
+
+		// We tell the mock chain to produce blocks at a 2 second interval,
+		// to simulate a real Espresso chain that produces blocks at a regular
+		// interval.
+		go chain.ProduceEspressoBlocksAtInterval(produceCtx, mockChain, 2*time.Second)
+
+		// Allow some time for blocks to be produced
+		time.Sleep(5 * time.Second)
+
+		// Stop the production of blocks
+		produceCancel()
+	}
+
+	// Fetch the latest block height to verify that blocks were produced
+	height, err := mockChain.FetchLatestBlockHeight(ctx)
+	if err != nil {
+		panic(fmt.Errorf("error fetching latest block height: %w\n", err))
+	}
+
+	fmt.Printf("Latest Block Height after producing blocks: %d\n", height)
+
+	// Output:
+	// Latest Block Height after producing blocks: 2
+}
+
+// ExampleErrorBlockNotFoundForHeight demonstrates how to create an error
+// for a block not found at a specific height, which can occur if the block
+// has not been produced yet or has been pruned.
+func ExampleErrorBlockNotFoundForHeight() {
+	err := chain.ErrorBlockNotFoundForHeight{
+		Height: 1,
+	}
+	fmt.Printf("Error: %s\n", err.Error())
+
+	// Output: Error: block not found at height: 1
+}
+
+// ExampleErrorFailedToComputeBlockHash demonstrates how to create an error
+// for a failure in computing the block hash, which can occur if the block
+// data is invalid or corrupted.
+func ExampleErrorFailedToComputeBlockHash() {
+	err := chain.ErrorFailedToComputeBlockHash{
+		Cause: errors.New("example error"),
+	}
+	fmt.Printf("Error: %s\n", err.Error())
+
+	// Output: Error: failed to compute block hash: example error
+}
+
+// ExampleErrorInvalidHash demonstrates how to create an error
+// for an invalid hash, which can be nil or malformed.
+func ExampleErrorInvalidHash() {
+	{
+		err := chain.ErrorInvalidHash{}
+		fmt.Printf("Error nil hash: %s\n", err.Error())
+	}
+
+	{
+		tag, err := tagged_base64.New("EXAMPLE", nil)
+		if err != nil {
+			panic(fmt.Sprintf("failed to create tagged base64: %v\n", err))
+		}
+
+		err = chain.ErrorInvalidHash{
+			Hash: tag,
+		}
+
+		fmt.Printf("Error with hash: %s\n", err.Error())
+	}
+
+	// Output: Error nil hash: invalid hash: nil
+	// Error with hash: invalid hash: EXAMPLE~2A
+}
+
+// ExampleErrorTransactionNotFoundForHash demonstrates how to create an error
+// for a transaction not found by its hash.
+func ExampleErrorTransactionNotFoundForHash() {
+	tag, err := tagged_base64.New("EXAMPLE", nil)
+	if err != nil {
+		panic(fmt.Sprintf("failed to create tagged base64: %v\n", err))
+	}
+
+	err = chain.ErrorTransactionNotFoundForHash{
+		Hash: *tag,
+	}
+
+	fmt.Printf("Error: %s\n", err.Error())
+
+	// Output: Error: transaction not found for hash: EXAMPLE~2A
+}
+
+// ExampleErrorSubmitTransaction demonstrates how to create an error
+// for a failed transaction submission.
+func ExampleErrorSubmitTransaction() {
+	err := chain.ErrorSubmitTransaction{
+		Cause: errors.New("failed to submit transaction"),
+	}
+	fmt.Printf("Error: %s\n", err.Error())
+
+	// Output: Error: failed to submit transaction: failed to submit transaction
+}
