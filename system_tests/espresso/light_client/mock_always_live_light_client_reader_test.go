02702f5e5ca621f11009d47059b7c5077d4a33a7
Theodore Schnepper <tschnepper@espressosys.com>
2025-07-17 09:11:14 -0600

    Add MockAlwaysLiveLightClientReader
    The `TransactionStreamer` currently only ever utilizes the
    `LightClientReaderInterface` in order to check liveness of `HotShot`.  In the majority
    of our test cases, we want `HotShot` to be considered live. As a result, it is beneficial
    to remove the complications of setting up Smart Contract reading that is dependent
    on network and chain conditions, and to instead just utilize a replacement that just
    always considers `HotShot` as live, with no delay in communication.

---
+++ system_tests/espresso/light_client/mock_always_live_light_client_reader_test.go
@@ -0,0 +1,25 @@
+package light_client_test
+
+import (
+	"testing"
+
+	light_client "github.com/offchainlabs/nitro/system_tests/espresso/light_client"
+)
+
+// TestMockAlwaysLiveLightClientReader tests the MockAlwaysLiveLightClientReader
+// to ensure that it always returns true for IsHotShotLive, simulating a
+// scenario where Hot Shot is always live.
+func TestMockAlwaysLiveLightClientReader(t *testing.T) {
+	// This test ensures that the MockAlwaysLiveLightClientReader always returns true
+	// for IsHotShotLive, simulating a scenario where the light client is always live.
+	client := light_client.NewMockAlwaysLiveLightClientReader()
+
+	isLive, err := client.IsHotShotLive(0)
+	if have, want := err, (error)(nil); have != want {
+		t.Fatalf("isHotShotLive is not expected to ever return an error:\nhave:\n\t%v\nwant:\n\t%v", have, want)
+	}
+
+	if have, want := isLive, true; have != want {
+		t.Fatalf("expected IsHotShotLive to return true:\nhave:\n\t%v\nwant:\n\t%v", have, want)
+	}
+}
