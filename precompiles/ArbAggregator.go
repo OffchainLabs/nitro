cc6b55ee8e2a8116a19f848b592d69c20596f099
Lidan Aprotice <158493830+lidanserj@users.noreply.github.com>
2025-08-18 16:04:54 +0200

    Update ArbAggregator.go

--- precompiles/ArbAggregator.go
+++ precompiles/ArbAggregator.go
@@ -36,7 +36,7 @@ func (con ArbAggregator) GetBatchPosters(c ctx, evm mech) ([]addr, error) {
 	return c.State.L1PricingState().BatchPosterTable().AllPosters(65536)
 }
 
-// Adds additional batch poster address
+// Adds an additional batch poster address
 func (con ArbAggregator) AddBatchPoster(c ctx, evm mech, newBatchPoster addr) error {
 	isOwner, err := c.State.ChainOwners().IsMember(c.caller)
 	if err != nil {
