162cdca14be692cd8811da6adaf536c5a19b590f
Alvarez <140459501+prestoalvarez@users.noreply.github.com>
2025-08-19 19:04:30 +0200

    Update bold_new_challenge_test.go

--- system_tests/bold_new_challenge_test.go
+++ system_tests/bold_new_challenge_test.go
@@ -116,7 +116,7 @@ func (s *incorrectBlockStateProvider) CollectMachineHashes(
 			honestHashes = append(honestHashes, s.evilMachineHash)
 		}
 	} else if uint64(cfg.BlockChallengeHeight) >= s.wrongAtBlockHeight {
-		panic(fmt.Sprintf("challenge occured at block height %v at or after wrongAtBlockHeight %v", cfg.BlockChallengeHeight, s.wrongAtBlockHeight))
+		panic(fmt.Sprintf("challenge occurred at block height %v at or after wrongAtBlockHeight %v", cfg.BlockChallengeHeight, s.wrongAtBlockHeight))
 	}
 	return honestHashes, nil
 }
