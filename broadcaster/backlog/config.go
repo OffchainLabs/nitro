4a198d3ffa58c97f990037ab6beda99860cb37f5
Tristan Wilson <twilson@offchainlabs.com>
2025-08-19 14:41:46 +0200

    Add debug flag to enable deep copy of backlog msgs

--- broadcaster/backlog/config.go
+++ broadcaster/backlog/config.go
@@ -7,18 +7,22 @@ import (
 type ConfigFetcher func() *Config
 
 type Config struct {
-	SegmentLimit int `koanf:"segment-limit" reload:"hot"`
+	SegmentLimit          int  `koanf:"segment-limit" reload:"hot"`
+	EnableBacklogDeepCopy bool `koanf:"enable-backlog-deep-copy" reload:"hot"`
 }
 
 func AddOptions(prefix string, f *flag.FlagSet) {
 	f.Int(prefix+".segment-limit", DefaultConfig.SegmentLimit, "the maximum number of messages each segment within the backlog can contain")
+	f.Bool(prefix+".enable-backlog-deep-copy", DefaultConfig.EnableBacklogDeepCopy, "enable deep copying of L2 messages for memory profiling (debug only)")
 }
 
 var (
 	DefaultConfig = Config{
-		SegmentLimit: 240,
+		SegmentLimit:          240,
+		EnableBacklogDeepCopy: false,
 	}
 	DefaultTestConfig = Config{
-		SegmentLimit: 3,
+		SegmentLimit:          3,
+		EnableBacklogDeepCopy: false,
 	}
 )
