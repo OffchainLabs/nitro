ab7b2abe474c3b5734fecdfebcc848a24afef519
Raul Jordan <raul@prysmaticlabs.com>
2025-08-26 11:10:37 -0500

    Return False When Checking if Deposit Tx Type Has Poster Costs (#3536)
    * return false for deposit tx type
    * test

--- arbos/l1pricing/l1pricing_test.go
+++ arbos/l1pricing/l1pricing_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/params"
 
 	"github.com/offchainlabs/nitro/arbos/burn"
@@ -30,6 +31,32 @@ func TestL1PriceUpdate(t *testing.T) {
 	priceEstimate, err := ps.PricePerUnit()
 	Require(t, err)
 	if priceEstimate.Cmp(initialPriceEstimate) != 0 {
+		Fail(t)
+	}
+}
+
+func Test_getPosterUnitsWithoutCache(t *testing.T) {
+	depositorAddr := common.HexToAddress("0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF")
+	txData := &types.ArbitrumDepositTx{
+		From:  depositorAddr,
+		To:    common.Address{},
+		Value: big.NewInt(1),
+	}
+	tx := types.NewTx(types.TxData(txData))
+	pricingState := &L1PricingState{}
+	posterAddr := common.Address{}
+	// Only txs that come from the batch poster address will be checked for a poster cost.
+	units := pricingState.getPosterUnitsWithoutCache(tx, posterAddr, 11)
+	if units != 0 {
+		Fail(t)
+	}
+
+	// This can never happen in prod, but even if the batch poster sends a
+	// deposit tx, the poster costs should still be 0.
+	posterAddr = BatchPosterAddress
+	// Only txs that come from the batch poster address will be checked for a poster cost.
+	units = pricingState.getPosterUnitsWithoutCache(tx, posterAddr, 11)
+	if units != 0 {
 		Fail(t)
 	}
 }
