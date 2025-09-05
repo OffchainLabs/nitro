8c8dca8ad71aa500a1ebe3827fbd2dc0bcc1cca4
Piotr Mikołajczyk <piomiko41@gmail.com>
2025-09-05 10:06:49 +0200

    Turn on linter for all structs (no opt-in)

--- linters/structinit/structinit.go
+++ linters/structinit/structinit.go
@@ -91,14 +91,14 @@ func markedStructs(pass *analysis.Pass) map[string]int {
 				}
 			case *ast.TypeSpec:
 				if st, ok := n.Type.(*ast.StructType); ok {
-					p := pass.Fset.Position(st.Struct)
-					if tips[position{p.Filename, p.Line}] {
-						fieldsCnt := 0
-						for _, field := range st.Fields.List {
-							fieldsCnt += len(field.Names)
-						}
-						res[pass.Pkg.Path()+"."+n.Name.Name] = fieldsCnt
+					//p := pass.Fset.Position(st.Struct)
+					//if tips[position{p.Filename, p.Line}] {
+					fieldsCnt := 0
+					for _, field := range st.Fields.List {
+						fieldsCnt += len(field.Names)
 					}
+					res[pass.Pkg.Path()+"."+n.Name.Name] = fieldsCnt
+					//}
 				}
 			}
 			return true
