63dad8b9706858bbde850b4af1c9752a6078bed1
Piotr Mikołajczyk <piomiko41@gmail.com>
2025-09-09 16:26:35 +0200

    Start streaming protocol

--- daprovider/das/data_streaming.go
+++ daprovider/das/data_streaming.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/rpc"
 	"github.com/offchainlabs/nitro/util/signature"
 )
@@ -52,20 +53,48 @@ func calculateEffectiveChunkSize(maxStoreChunkBodySize int) (uint64, error) {
 }
 
 func (ds *DataStreamer) StreamData(ctx context.Context, data []byte, timeout uint64) error {
-	//params := newStreamParams(uint64(len(data)), ds.chunkSize)
-	return errors.New("")
+	params := newStreamParams(uint64(len(data)), ds.chunkSize, timeout)
+
+	startReqSig, err := ds.generateStartReqSignature(params)
+	if err != nil {
+		return err
+	}
+
+	batchId, err := ds.startStream(ctx, startReqSig, params)
+	if err != nil {
+		return err
+	}
+
+	if err := ds.doStream(ctx, batchId, params); err != nil {
+		return err
+	}
 }
 
-func (ds *DataStreamer) generateStartReqSignature(params streamParams, timeout uint64) ([]byte, error) {
-	return applyDasSigner(ds.dataSigner, []byte{}, params.timestamp, params.nChunks, ds.chunkSize, params.dataLen, timeout)
+func (ds *DataStreamer) generateStartReqSignature(params streamParams) ([]byte, error) {
+	return applyDasSigner(ds.dataSigner, []byte{}, params.timestamp, params.nChunks, ds.chunkSize, params.dataLen, params.timeout)
+}
+
+func (ds *DataStreamer) startStream(ctx context.Context, startReqSig []byte, params streamParams) (hexutil.Uint64, error) {
+	var startChunkedStoreResult StartChunkedStoreResult
+	err := ds.rpcClient.CallContext(
+		ctx,
+		&startChunkedStoreResult,
+		"das_startChunkedStore",
+		hexutil.Uint64(params.timestamp),
+		hexutil.Uint64(params.nChunks),
+		hexutil.Uint64(ds.chunkSize),
+		hexutil.Uint64(params.dataLen),
+		hexutil.Uint64(params.timeout),
+		hexutil.Bytes(startReqSig))
+	return startChunkedStoreResult.BatchId, err
 }
 
 type streamParams struct {
-	timestamp, nChunks, lastChunkSize, dataLen uint64
+	timestamp, nChunks, lastChunkSize, dataLen, timeout uint64
 }
 
 // todo chunksize and datalen must be > 0
-func newStreamParams(dataLen, chunkSize uint64) streamParams {
+func newStreamParams(dataLen, chunkSize, timeout uint64) streamParams {
 	nChunks := (dataLen + chunkSize - 1) / chunkSize
 	lastChunkSize := (dataLen-1)%chunkSize + 1
 
@@ -74,5 +103,6 @@ func newStreamParams(dataLen, chunkSize uint64) streamParams {
 		nChunks:       nChunks,
 		lastChunkSize: lastChunkSize,
 		dataLen:       dataLen,
+		timeout:       timeout,
 	}
 }
