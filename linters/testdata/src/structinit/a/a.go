e9a3935ff3b825aaa8dd9ba29d4fc9aec4db491a
Piotr Mikołajczyk <piomiko41@gmail.com>
2025-09-10 11:37:47 +0200

    Fix comment

--- linters/testdata/src/structinit/a/a.go
+++ linters/testdata/src/structinit/a/a.go
@@ -2,12 +2,13 @@
    For license information, see https://github.com/OffchainLabs/nitro/blob/master/LICENSE.md
 
 // want package:"{package a .*structinit/a.* .*}"
-   The comment above ensures that during tests, the `structinit` analyzer
-   will produce a `Fact` about the `structinit/a` package (with some prefix
-   and suffix in its path). Since the fact will be of type `*accumulatedFieldCounts`,
-   we just match arbitrary pattern (.*) - it will be just some address.
-   For a reference, see: https://pkg.go.dev/golang.org/x/tools/go/analysis/analysistest#Run
 */
+
+// The comment above ensures that during tests, the `structinit` analyzer
+// will produce a `Fact` about the `structinit/a` package (with some prefix
+// and suffix in its path). Since the fact will be of type `*accumulatedFieldCounts`,
+// we just match arbitrary pattern (.*) - it will be just some address.
+// For a reference, see: https://pkg.go.dev/golang.org/x/tools/go/analysis/analysistest#Run
 
 package a
 
