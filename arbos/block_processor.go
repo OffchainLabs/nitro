310c8bf70105c061b39c4744055697074b1a65c0
Mikhail Rogachev <mrogachev@offchainlabs.com>
2025-09-10 02:58:29 +0200

    Multigas: Add multi gas to transaction receipt and remove collector (#3594)
    * Replace multigas collector with receipts in system tests
    
    * Delete multigas collector (use receipts instead)
    
    * Fixup TODO and bump go-ethereum version
    
    * Simplify TestMultigasDataFromReceipts

--- arbos/block_processor.go
+++ arbos/block_processor.go
@@ -23,7 +23,6 @@ import (
 	"github.com/offchainlabs/nitro/arbos/arbosState"
 	"github.com/offchainlabs/nitro/arbos/arbostypes"
 	"github.com/offchainlabs/nitro/arbos/l2pricing"
-	"github.com/offchainlabs/nitro/arbos/multigascollector"
 	"github.com/offchainlabs/nitro/arbos/util"
 	"github.com/offchainlabs/nitro/util/arbmath"
 )
@@ -194,7 +193,7 @@ func ProduceBlock(
 	hooks := NoopSequencingHooks(txes)
 
 	return ProduceBlockAdvanced(
-		message.Header, delayedMessagesRead, lastBlockHeader, statedb, chainContext, hooks, isMsgForPrefetch, runCtx, nil,
+		message.Header, delayedMessagesRead, lastBlockHeader, statedb, chainContext, hooks, isMsgForPrefetch, runCtx,
 	)
 }
 
@@ -208,7 +207,6 @@ func ProduceBlockAdvanced(
 	sequencingHooks *SequencingHooks,
 	isMsgForPrefetch bool,
 	runCtx *core.MessageRunContext,
-	mgcCollector multigascollector.Collector,
 ) (*types.Block, types.Receipts, error) {
 
 	arbState, err := arbosState.OpenSystemArbosState(statedb, nil, true)
@@ -506,16 +504,6 @@ func ProduceBlockAdvanced(
 
 		if isUserTx {
 			userTxsProcessed++
-		}
-
-		// Submit multigas transaction message, if the multi gas collector is set
-		if mgcCollector != nil {
-			mgcCollector.CollectTransactionMultiGas(multigascollector.TransactionMultiGas{
-				TxHash:    tx.Hash().Bytes(),
-				TxIndex:   uint32(receipt.TransactionIndex), // #nosec G115 -- block tx count << MaxUint32; safe cast
-				MultiGas:  result.UsedMultiGas,
-				SingleGas: result.UsedGas,
-			})
 		}
 	}
 
