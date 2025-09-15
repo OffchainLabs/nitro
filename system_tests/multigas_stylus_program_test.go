df3f7efdc21200d6f222dec52451ff8b9b16b6b9
Mikhail Rogachev <mrogachev@offchainlabs.com>
2025-09-15 18:18:00 +0200

    Instrument multi-gas in account HostIOs

--- system_tests/multigas_stylus_program_test.go
+++ system_tests/multigas_stylus_program_test.go
@@ -10,6 +10,8 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/ethereum/go-ethereum/arbitrum/multigas"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/params"
 
 	"github.com/offchainlabs/nitro/util/testhelpers"
@@ -50,4 +52,42 @@ func TestMultigasStylus_GetBytes32(t *testing.T) {
 	// TODO(NIT-3793, NIT-3793, NIT-3795): Once all WASM operations are instrumented, WasmComputation
 	// should be derived as the residual from SingleGas instead of asserted directly.
 	require.Greater(t, receipt.MultiGasUsed.Get(multigas.ResourceKindWasmComputation), uint64(0))
+}
+
+func TestMultigasStylus_AccountBalance(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	builder := NewNodeBuilder(ctx).DefaultConfig(t, false)
+	builder.execConfig.ExposeMultiGas = true
+	cleanup := builder.Build(t)
+	defer cleanup()
+
+	l2info := builder.L2Info
+	l2client := builder.L2.Client
+
+	// Deploy programs
+	owner := l2info.GetDefaultTransactOpts("Owner", ctx)
+	hostio := deployWasm(t, ctx, owner, l2client, rustFile("hostio-test"))
+
+	// Send tx with account read
+	target := common.HexToAddress("0xbeefdead00000000000000000000000000000000")
+	selector := crypto.Keccak256([]byte("accountBalance(address)"))[:4]
+	callData := append([]byte{}, selector...)
+	callData = append(callData, common.LeftPadBytes(target.Bytes(), 32)...)
+
+	tx := l2info.PrepareTxTo("Owner", &hostio, l2info.TransferGas, nil, callData)
+	require.NoError(t, l2client.SendTransaction(ctx, tx))
+
+	receipt, err := EnsureTxSucceeded(ctx, l2client, tx)
+	require.NoError(t, err)
+
+	require.Equal(t,
+		params.ColdAccountAccessCostEIP2929-params.WarmStorageReadCostEIP2929,
+		receipt.MultiGasUsed.Get(multigas.ResourceKindStorageAccess),
+	)
+
+	require.Equal(t, params.WarmStorageReadCostEIP2929,
+		receipt.MultiGasUsed.Get(multigas.ResourceKindComputation),
+	)
 }
