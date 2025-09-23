7b94e835d82b8c98298c7432f852a969ae48d405
Daniel <DanielGuupta@gmail.com>
2025-09-24 04:04:20 +0800

    test timeout stuff

--- cmd/genericconf/server_test.go
+++ cmd/genericconf/server_test.go
@@ -1,9 +1,19 @@
 package genericconf
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestHTTPConfigDefault(t *testing.T) {
 	if HTTPConfigDefault.Port != 8547 {
 		t.Error("wrong port")
+	}
+}
+
+func TestTimeoutConfig(t *testing.T) {
+	config := HTTPServerTimeoutConfigDefault
+	if config.ReadTimeout == 0 {
+		t.Error("ReadTimeout should not be zero")
 	}
 }
