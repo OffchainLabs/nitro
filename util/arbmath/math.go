08b5b234499a62f858dc49b749611f958a20871a
oooLowNeoNooo <ooolowneonooo@gmail.com>
2025-10-01 12:01:24 +0200

    Update math.go

--- util/arbmath/math.go
+++ util/arbmath/math.go
@@ -71,6 +71,14 @@ func Bisect(pre, post uint64) (uint64, error) {
 	return (post - 1) & mask, nil
 }
 
+type Signed interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
+}
+
+type Unsigned interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+}
+
 type Integer interface {
 	Signed | Unsigned
 }
