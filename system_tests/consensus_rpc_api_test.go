bfa1bec04998b7863ea173d47e3051bd65ee51f4
Diego Ximenes <dxmendes1@gmail.com>
2025-11-10 15:49:45 -0300

    Fixes lint issue

--- system_tests/consensus_rpc_api_test.go
+++ system_tests/consensus_rpc_api_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
+
 	"github.com/offchainlabs/nitro/solgen/go/node_interfacegen"
 )
 
