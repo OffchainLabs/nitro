27daac1dcf79b491453ec20f7f992039820aacc2
Piotr Mikołajczyk <piomiko41@gmail.com>
2025-11-11 12:50:47 +0100

    Add streaming utils to Rust

--- arbitrator/brotli/src/cgo.rs
+++ arbitrator/brotli/src/cgo.rs
@@ -1,8 +1,14 @@
-// Copyright 2021-2024, Offchain Labs, Inc.
+// Copyright 2021-2025, Offchain Labs, Inc.
 // For license information, see https://github.com/OffchainLabs/nitro/blob/master/LICENSE.md
 
-use crate::{BrotliStatus, Dictionary, DEFAULT_WINDOW_SIZE};
+use crate::types::{BrotliEncoderOperation, BrotliEncoderParameter};
+use crate::{
+    BrotliEncoderCompressStream, BrotliEncoderCreateInstance, BrotliEncoderDestroyInstance,
+    BrotliEncoderSetParameter, BrotliStatus, Dictionary, EncoderState, DEFAULT_WINDOW_SIZE,
+};
 use core::{mem::MaybeUninit, slice};
+use std::ptr;
+use BrotliEncoderParameter::*;
 
 /// Mechanism for passing data between Go and Rust where Rust can specify the initialized length.
 #[derive(Clone, Copy)]
@@ -49,6 +55,111 @@ pub extern "C" fn brotli_compress(
         Err(status) => return status,
     }
     BrotliStatus::Success
+}
+
+#[no_mangle]
+pub extern "C" fn brotli_create_compressing_writer(level: u32) -> *mut EncoderState {
+    unsafe {
+        let state = BrotliEncoderCreateInstance(None, None, ptr::null_mut());
+
+        if BrotliEncoderSetParameter(state, Quality, level).is_err() {
+            panic!("Aaaaaa qualityyyy")
+        }
+        if BrotliEncoderSetParameter(state, WindowSize, DEFAULT_WINDOW_SIZE).is_err() {
+            panic!("Aaaaaa windowwwww")
+        }
+
+        state
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn brotli_write_to_stream(
+    state: *mut EncoderState,
+    input: BrotliBuffer,
+    mut output: BrotliBuffer,
+) -> usize {
+    unsafe {
+        let buffer = output.as_uninit();
+        let input = input.as_slice();
+
+        let mut in_len = input.len();
+        let mut in_ptr = input.as_ptr();
+        let mut out_left = buffer.len();
+        let mut out_ptr = buffer.as_mut_ptr() as *mut u8;
+        let mut out_len = out_left;
+
+        let status = BrotliEncoderCompressStream(
+            state,
+            BrotliEncoderOperation::Process,
+            &mut in_len as _,
+            &mut in_ptr as _,
+            &mut out_left as _,
+            &mut out_ptr as _,
+            &mut out_len as _,
+        );
+
+        if status.is_err() {
+            panic!("Error compressing stream");
+        }
+        out_len
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn brotli_flush_stream(state: *mut EncoderState, mut output: BrotliBuffer) {
+    unsafe {
+        let buffer = output.as_uninit();
+        let input = &[];
+
+        let mut in_len = input.len();
+        let mut in_ptr = input.as_ptr();
+        let mut out_left = buffer.len();
+        let mut out_ptr = buffer.as_mut_ptr() as *mut u8;
+        let mut out_len = out_left;
+
+        let status = BrotliEncoderCompressStream(
+            state,
+            BrotliEncoderOperation::Flush,
+            &mut in_len as _,
+            &mut in_ptr as _,
+            &mut out_left as _,
+            &mut out_ptr as _,
+            &mut out_len as _,
+        );
+
+        if status.is_err() {
+            panic!("Error compressing stream");
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn brotli_close_stream(state: *mut EncoderState, mut output: BrotliBuffer) {
+    unsafe {
+        let buffer = output.as_uninit();
+        let input = &[];
+
+        let mut in_len = input.len();
+        let mut in_ptr = input.as_ptr();
+        let mut out_left = buffer.len();
+        let mut out_ptr = buffer.as_mut_ptr() as *mut u8;
+        let mut out_len = out_left;
+
+        let status = BrotliEncoderCompressStream(
+            state,
+            BrotliEncoderOperation::Finish,
+            &mut in_len as _,
+            &mut in_ptr as _,
+            &mut out_left as _,
+            &mut out_ptr as _,
+            &mut out_len as _,
+        );
+
+        if status.is_err() {
+            panic!("Error compressing stream");
+        }
+    }
 }
 
 /// Brotli decompresses the given Go data into a buffer of limited capacity.
