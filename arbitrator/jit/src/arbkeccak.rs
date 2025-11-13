d3b64726534fc4de4ddf230d059569ea35cab458
Piotr Mikołajczyk <piomiko41@gmail.com>
2025-11-13 13:44:42 +0100

    Keep keccak implementation in jit

--- arbitrator/jit/src/arbkeccak.rs
+++ arbitrator/jit/src/arbkeccak.rs
@@ -1,6 +1,8 @@
-use caller_env::GuestPtr;
-use crate::caller_env::{JitEnv, JitExecEnv};
+use crate::caller_env::JitEnv;
 use crate::machine::{MaybeEscape, WasmEnvMut};
+use caller_env::{GuestPtr, MemAccess};
+use core::mem::MaybeUninit;
+use tiny_keccak::{Hasher, Keccak};
 
 #[allow(clippy::too_many_arguments)]
 pub fn keccak256(
@@ -10,12 +12,16 @@ pub fn keccak256(
     out_buf_ptr: GuestPtr,
 ) -> MaybeEscape {
     let (mut mem, wenv) = src.jit_env();
+    let input = mem.read_slice(in_buf_ptr, in_buf_len as usize);
 
-    Ok(caller_env::arbkeccak::keccak256(
-        &mut mem,
-        &mut JitExecEnv { wenv },
-        in_buf_ptr,
-        in_buf_len,
-        out_buf_ptr,
-    ))
+    let mut output = MaybeUninit::<[u8; 32]>::uninit();
+    let mut hasher = Keccak::v256();
+    hasher.update(input.as_ref());
+
+    // SAFETY: finalize() writes 32 bytes
+    unsafe {
+        hasher.finalize(&mut *output.as_mut_ptr());
+        mem.write_slice(out_buf_ptr, output.assume_init().as_slice());
+    }
+    Ok(())
 }
