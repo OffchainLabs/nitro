f4871f4dbaccdbcafa77c3839778571156796a59
Piotr Mikołajczyk <piomiko41@gmail.com>
2025-11-13 13:53:30 +0100

    Add new wasm library (wip)

---
+++ arbitrator/wasm-libraries/arbkeccak/src/lib.rs
@@ -0,0 +1,16 @@
+// Copyright 2025, Offchain Labs, Inc.
+// For license information, see https://github.com/OffchainLabs/nitro/blob/master/LICENSE.md
+
+#![allow(clippy::missing_safety_doc)]
+
+use caller_env::{self, GuestPtr};
+
+#[no_mangle]
+pub unsafe extern "C" fn arbkeccak__keccak256(in_buf_ptr: GuestPtr, in_buf_len: u32, out_buf_ptr: GuestPtr)
+{
+    caller_env::brotli::brotli_compress(
+        &mut caller_env::static_caller::STATIC_MEM,
+        &mut caller_env::static_caller::STATIC_ENV,
+        in_buf_ptr, in_buf_len, out_buf_ptr, out_len_ptr, level, window_size,
+    )
+}
