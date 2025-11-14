9c79893bf4a1505992b819ebccb157a0faba09fb
Mikhail Rogachev <mrogachev@offchainlabs.com>
2025-11-14 15:00:53 +0100

    Correct AddToGasPool signature to support multi-gas

--- arbos/l2pricing/model.go
+++ arbos/l2pricing/model.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"math/big"
 
+	"github.com/ethereum/go-ethereum/arbitrum/multigas"
 	"github.com/ethereum/go-ethereum/params"
 
 	"github.com/offchainlabs/nitro/util/arbmath"
@@ -56,33 +57,42 @@ func (ps *L2PricingState) GasModelToUse(arbosVersion uint64) (GasModel, error) {
 	return GasModelLegacy, nil
 }
 
-func (ps *L2PricingState) AddToGasPool(gas int64, arbosVersion uint64) error {
+// AddToGasPool updates the backlog(s) for the active gas pricing model.
+//
+//	growBacklog = true  -> backlog increases (gas was consumed)
+//	growBacklog = false -> backlog decreases (unused capacity pays down backlog)
+//
+// Model behavior:
+//   - Legacy and single-constraint models use only `usedGas`.
+//   - Multi-dimensional model uses `usedMultiGas` to update each constraint,
+//     but `usedGas` must still match `usedMultiGas.SingleGas()`.
+func (ps *L2PricingState) AddToGasPool(growBacklog bool, usedGas uint64, usedMultiGas multigas.MultiGas, arbosVersion uint64) error {
 	gasModel, err := ps.GasModelToUse(arbosVersion)
 	if err != nil {
 		return err
 	}
 	switch gasModel {
 	case GasModelLegacy:
-		return ps.addToGasPoolLegacy(gas)
+		return ps.addToGasPoolLegacy(growBacklog, usedGas)
 	case GasModelSingleGasConstraints:
-		return ps.addToGasPoolWithSingleGasConstraints(gas)
+		return ps.addToGasPoolWithSingleGasConstraints(growBacklog, usedGas)
 	case GasModelMultiGasConstraints:
-		return ps.addToGasPoolWithMultiGasConstraints(gas)
+		return ps.addToGasPoolWithMultiGasConstraints(growBacklog, usedMultiGas)
 	default:
 		return fmt.Errorf("can not determine gas model")
 	}
 }
 
-func (ps *L2PricingState) addToGasPoolLegacy(gas int64) error {
+func (ps *L2PricingState) addToGasPoolLegacy(growBacklog bool, usedGas uint64) error {
 	backlog, err := ps.GasBacklog()
 	if err != nil {
 		return err
 	}
-	backlog = applyGasDelta(backlog, gas)
+	backlog = applyGasDelta(backlog, growBacklog, usedGas)
 	return ps.SetGasBacklog(backlog)
 }
 
-func (ps *L2PricingState) addToGasPoolWithSingleGasConstraints(gas int64) error {
+func (ps *L2PricingState) addToGasPoolWithSingleGasConstraints(growBacklog bool, usedGas uint64) error {
 	constraintsLength, err := ps.gasConstraints.Length()
 	if err != nil {
 		return fmt.Errorf("failed to get number of constraints: %w", err)
@@ -93,7 +103,7 @@ func (ps *L2PricingState) addToGasPoolWithSingleGasConstraints(gas int64) error 
 		if err != nil {
 			return fmt.Errorf("failed to get backlog of constraint %v: %w", i, err)
 		}
-		err = constraint.SetBacklog(applyGasDelta(backlog, gas))
+		err = constraint.SetBacklog(applyGasDelta(backlog, growBacklog, usedGas))
 		if err != nil {
 			return fmt.Errorf("failed to set backlog of constraint %v: %w", i, err)
 		}
@@ -101,7 +111,7 @@ func (ps *L2PricingState) addToGasPoolWithSingleGasConstraints(gas int64) error 
 	return nil
 }
 
-func (ps *L2PricingState) addToGasPoolWithMultiGasConstraints(_gas int64) error {
+func (ps *L2PricingState) addToGasPoolWithMultiGasConstraints(_growBacklog bool, _gas multigas.MultiGas) error {
 	return fmt.Errorf("addToGasPoolWithMultiGasConstraints not implemented")
 }
 
@@ -119,17 +129,17 @@ func (ps *L2PricingState) UpdatePricingModel(timePassed uint64, arbosVersion uin
 }
 
 // applyGasDelta grows the backlog if the gas is negative and pays off  if the gas is positive.
-func applyGasDelta(backlog uint64, gas int64) uint64 {
-	if gas > 0 {
-		return arbmath.SaturatingUSub(backlog, uint64(gas))
+func applyGasDelta(backlog uint64, growBacklog bool, delta uint64) uint64 {
+	if growBacklog {
+		return arbmath.SaturatingUAdd(backlog, delta)
 	} else {
-		return arbmath.SaturatingUAdd(backlog, uint64(-gas))
+		return arbmath.SaturatingUSub(backlog, delta)
 	}
 }
 
 func (ps *L2PricingState) updatePricingModelLegacy(timePassed uint64) {
 	speedLimit, _ := ps.SpeedLimitPerSecond()
-	_ = ps.addToGasPoolLegacy(arbmath.SaturatingCast[int64](arbmath.SaturatingUMul(timePassed, speedLimit)))
+	_ = ps.addToGasPoolLegacy(false, arbmath.SaturatingUMul(timePassed, speedLimit))
 	inertia, _ := ps.PricingInertia()
 	tolerance, _ := ps.BacklogTolerance()
 	backlog, _ := ps.GasBacklog()
@@ -153,8 +163,8 @@ func (ps *L2PricingState) updatePricingModelGasConstraints(timePassed uint64) {
 
 		// Pay off backlog
 		backlog, _ := constraint.Backlog()
-		gas := arbmath.SaturatingCast[int64](arbmath.SaturatingUMul(timePassed, target))
-		backlog = applyGasDelta(backlog, gas)
+		gas := arbmath.SaturatingUMul(timePassed, target)
+		backlog = applyGasDelta(backlog, false, gas)
 		_ = constraint.SetBacklog(backlog)
 
 		// Calculate exponent with the formula backlog/divisor
