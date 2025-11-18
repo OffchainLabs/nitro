e4bb0a24bcf7b639e3ed7816e88584217241c83e
Diego Ximenes <dxmendes1@gmail.com>
2025-11-18 13:04:03 -0300

    Fixes bold tests

--- system_tests/bold_challenge_protocol_test.go
+++ system_tests/bold_challenge_protocol_test.go
@@ -695,7 +695,7 @@ func createL2NodeForBoldProtocol(
 	)
 	Require(t, err)
 
-	l2client = ClientForStack(t, l2stack)
+	l2client = ClientForStack(t, l2stack, false)
 
 	StartWatchChanErr(t, ctx, fatalErrChan, currentNode)
 
@@ -986,7 +986,7 @@ func create2ndNodeWithConfigForBoldProtocol(
 	l2node, err := arbnode.CreateNodeFullExecutionClient(ctx, l2stack, execNode, execNode, execNode, execNode, l2arbDb, NewCommonConfigFetcher(nodeConfig), l2blockchain.Config(), l1client, addresses, &txOpts, &txOpts, dataSigner, fatalErrChan, l1ChainId, nil /* blob reader */, locator.LatestWasmModuleRoot())
 	Require(t, err)
 
-	l2client := ClientForStack(t, l2stack)
+	l2client := ClientForStack(t, l2stack, clientForStackUseHTTP(stackConfig))
 
 	StartWatchChanErr(t, ctx, fatalErrChan, l2node)
 
