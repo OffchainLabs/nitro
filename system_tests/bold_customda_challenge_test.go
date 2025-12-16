f64a91f02584d2082c20d1be3c8fd6b17a65d72d
ganeshvanahalli <ganeshdvanahalli@gmail.com>
2025-12-16 08:53:39 +0530

    fix testChallengeProtocolBOLDCustomDA

--- system_tests/bold_customda_challenge_test.go
+++ system_tests/bold_customda_challenge_test.go
@@ -43,6 +43,7 @@ import (
 	"github.com/offchainlabs/nitro/daprovider/referenceda"
 	dapserver "github.com/offchainlabs/nitro/daprovider/server"
 	"github.com/offchainlabs/nitro/execution/gethexec"
+	"github.com/offchainlabs/nitro/execution_consensus"
 	"github.com/offchainlabs/nitro/solgen/go/bridgegen"
 	"github.com/offchainlabs/nitro/solgen/go/challengeV2gen"
 	"github.com/offchainlabs/nitro/solgen/go/mocksgen"
@@ -167,7 +168,7 @@ func createNodeBWithSharedContracts(
 	stakeTokenAddr common.Address,
 	l1client *ethclient.Client,
 	assertionChain *solimpl.AssertionChain,
-) (*ethclient.Client, *arbnode.Node) {
+) (*ethclient.Client, *arbnode.Node, *gethexec.ExecutionNode, *node.Node) {
 	fatalErrChan := make(chan error, 10)
 
 	firstExec, ok := first.ExecutionClient.(*gethexec.ExecutionNode)
@@ -225,7 +226,7 @@ func createNodeBWithSharedContracts(
 
 	StartWatchChanErr(t, ctx, fatalErrChan, l2node)
 
-	return l2client, l2node
+	return l2client, l2node, execNode, l2stack
 }
 
 func testChallengeProtocolBOLDCustomDA(t *testing.T, evilStrategy EvilStrategy, spawnerOpts ...server_arb.SpawnerOption) {
@@ -321,7 +322,7 @@ func testChallengeProtocolBOLDCustomDA(t *testing.T, evilStrategy EvilStrategy, 
 	nodeConfigA.DA.ExternalProvider.RPC.URL = providerURLNodeA
 
 	// Create L2 node A
-	l2info, l2nodeA, _, _, _, assertionChain := createL2NodeForBoldProtocol(
+	l2info, l2nodeA, l2execNodeA, _, l2stackA, assertionChain := createL2NodeForBoldProtocol(
 		t, ctx, true, nodeConfigA, l2chainConfig, l2info,
 		l1info, l1backend, l1client, l1stack, addresses, stakeTokenAddr,
 		false, asserterOpts, signerCfg, // useExternalSigner=false
@@ -340,7 +341,7 @@ func testChallengeProtocolBOLDCustomDA(t *testing.T, evilStrategy EvilStrategy, 
 	l2nodeConfig.DA.ExternalProvider.RPC.URL = providerURLNodeB
 
 	// Create node B using the same contracts as node A
-	l2clientB, l2nodeB := createNodeBWithSharedContracts(
+	l2clientB, l2nodeB, l2execNodeB, l2stackB := createNodeBWithSharedContracts(
 		t,
 		ctx,
 		l2nodeA,
@@ -502,8 +503,10 @@ func testChallengeProtocolBOLDCustomDA(t *testing.T, evilStrategy EvilStrategy, 
 	)
 	Require(t, err)
 
-	Require(t, l2nodeA.Start(ctx))
-	Require(t, l2nodeB.Start(ctx))
+	_, err = execution_consensus.InitAndStartExecutionAndConsensusNodes(ctx, l2stackA, l2execNodeA, l2nodeA)
+	Require(t, err)
+	_, err = execution_consensus.InitAndStartExecutionAndConsensusNodes(ctx, l2stackB, l2execNodeB, l2nodeB)
+	Require(t, err)
 
 	chalManagerAddr := assertionChain.SpecChallengeManager()
 	evilOpts := l1info.GetDefaultTransactOpts("EvilAsserter", ctx)
