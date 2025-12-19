2578cf9e7b6033432dbd1be0106a9155f6d41ac9
Igor Braga <5835477+bragaigor@users.noreply.github.com>
2025-12-19 09:07:21 -0300

    remove melrunner from import
    Signed-off-by: Igor Braga <5835477+bragaigor@users.noreply.github.com>

--- cmd/mel-replay/delayed_message_db_test.go
+++ cmd/mel-replay/delayed_message_db_test.go
@@ -13,7 +13,7 @@ import (
 	"github.com/ethereum/go-ethereum/rlp"
 
 	"github.com/offchainlabs/nitro/arbnode/mel"
-	melrunner "github.com/offchainlabs/nitro/arbnode/mel/runner"
+	"github.com/offchainlabs/nitro/arbnode/mel/runner"
 	"github.com/offchainlabs/nitro/arbos/arbostypes"
 	"github.com/offchainlabs/nitro/arbutil"
 )
