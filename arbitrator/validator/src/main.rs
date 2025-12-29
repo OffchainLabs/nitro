ee283f8c1d05125d3ac4d16cbf4aaf108391d49c
Piotr Mikołajczyk <piomiko41@gmail.com>
2025-12-29 11:40:45 +0100

    Move logging initialization to separate module

--- arbitrator/validator/src/main.rs
+++ arbitrator/validator/src/main.rs
@@ -1,16 +1,14 @@
-use crate::config::LoggingFormat;
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use axum::{
     routing::{get, post},
     Router,
 };
 use clap::Parser;
-use std::{env, io};
 use tokio::net::TcpListener;
 use tracing::info;
-use tracing_subscriber::EnvFilter;
 
 mod config;
+mod logging;
 mod spawner_endpoints;
 
 const BASE_NAMESPACE: &str = "/validation";
@@ -18,7 +16,7 @@ const BASE_NAMESPACE: &str = "/validation";
 #[tokio::main]
 async fn main() -> Result<()> {
     let config = config::ServerConfig::parse();
-    init_logging(config.logging_format)?;
+    logging::init(config.logging_format)?;
     info!("Starting validator server with config: {:#?}", config);
 
     let app = Router::new()
@@ -45,26 +43,4 @@ async fn main() -> Result<()> {
 
     let listener = TcpListener::bind(format!("{}:{}", config.host, config.port)).await?;
     axum::serve(listener, app).await.map_err(Into::into)
-}
-
-/// Initialize `tracing` logging based on the specified format. By default, the logging level is set
-/// to "info" unless overridden by the `RUST_LOG` environment variable.
-fn init_logging(format: LoggingFormat) -> Result<()> {
-    const LOG_CONFIGURATION_ENVVAR: &str = "RUST_LOG";
-
-    let filter = EnvFilter::new(
-        env::var(LOG_CONFIGURATION_ENVVAR)
-            .as_deref()
-            .unwrap_or("info"),
-    );
-
-    let subscriber = tracing_subscriber::fmt()
-        .with_writer(io::stdout)
-        .with_env_filter(filter);
-
-    match format {
-        LoggingFormat::Json => subscriber.json().try_init(),
-        LoggingFormat::Text => subscriber.try_init(),
-    }
-    .map_err(|err| anyhow!(err))
 }
