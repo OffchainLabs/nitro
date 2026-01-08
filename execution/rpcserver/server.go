d3527f0e3ff3d9ae7b071d9d0b4bbce9d358bb46
Piotr Mikołajczyk <piomiko41@gmail.com>
2026-01-08 12:16:39 +0100

    Rename execution packages (#4206)
    * Rename executionrpcclient to rpcclient
    
    * Rename executionrpcserver to rpcserver
    
    * Rename ExecutionRPCClient to Client
    
    * Rename ExecutionRPCServer to Server
    
    * Do not use capitalize letter nor dashes in package name, nor files
    
    * Changelog
    
    ---------
    
    Co-authored-by: Pepper Lebeck-Jobe <pepper@offchainlabs.com>

---
+++ execution/rpcserver/server.go
@@ -0,0 +1,65 @@
+package rpcserver
+
+import (
+	"context"
+
+	"github.com/offchainlabs/nitro/arbos/arbostypes"
+	"github.com/offchainlabs/nitro/arbutil"
+	"github.com/offchainlabs/nitro/execution"
+)
+
+type Server struct {
+	executionClient execution.ExecutionClient
+}
+
+func NewServer(executionClient execution.ExecutionClient) *Server {
+	return &Server{executionClient}
+}
+
+func (c *Server) DigestMessage(ctx context.Context, msgIdx arbutil.MessageIndex, msg *arbostypes.MessageWithMetadata, msgForPrefetch *arbostypes.MessageWithMetadata) (*execution.MessageResult, error) {
+	return c.executionClient.DigestMessage(msgIdx, msg, msgForPrefetch).Await(ctx)
+}
+
+func (c *Server) Reorg(ctx context.Context, msgIdxOfFirstMsgToAdd arbutil.MessageIndex, newMessages []arbostypes.MessageWithMetadataAndBlockInfo, oldMessages []*arbostypes.MessageWithMetadata) ([]*execution.MessageResult, error) {
+	return c.executionClient.Reorg(msgIdxOfFirstMsgToAdd, newMessages, oldMessages).Await(ctx)
+}
+
+func (c *Server) HeadMessageIndex(ctx context.Context) (arbutil.MessageIndex, error) {
+	return c.executionClient.HeadMessageIndex().Await(ctx)
+}
+
+func (c *Server) ResultAtMessageIndex(ctx context.Context, msgIdx arbutil.MessageIndex) (*execution.MessageResult, error) {
+	return c.executionClient.ResultAtMessageIndex(msgIdx).Await(ctx)
+}
+
+func (c *Server) SetFinalityData(ctx context.Context, safeFinalityData *arbutil.FinalityData, finalizedFinalityData *arbutil.FinalityData, validatedFinalityData *arbutil.FinalityData) error {
+	_, err := c.executionClient.SetFinalityData(safeFinalityData, finalizedFinalityData, validatedFinalityData).Await(ctx)
+	return err
+}
+
+func (c *Server) SetConsensusSyncData(ctx context.Context, syncData *execution.ConsensusSyncData) error {
+	_, err := c.executionClient.SetConsensusSyncData(syncData).Await(ctx)
+	return err
+}
+
+func (c *Server) MarkFeedStart(ctx context.Context, to arbutil.MessageIndex) error {
+	_, err := c.executionClient.MarkFeedStart(to).Await(ctx)
+	return err
+}
+
+func (c *Server) TriggerMaintenance(ctx context.Context) error {
+	_, err := c.executionClient.TriggerMaintenance().Await(ctx)
+	return err
+}
+
+func (c *Server) ShouldTriggerMaintenance(ctx context.Context) (bool, error) {
+	return c.executionClient.ShouldTriggerMaintenance().Await(ctx)
+}
+
+func (c *Server) MaintenanceStatus(ctx context.Context) (*execution.MaintenanceStatus, error) {
+	return c.executionClient.MaintenanceStatus().Await(ctx)
+}
+
+func (c *Server) ArbOSVersionForMessageIndex(ctx context.Context, msgIdx arbutil.MessageIndex) (uint64, error) {
+	return c.executionClient.ArbOSVersionForMessageIndex(msgIdx).Await(ctx)
+}
