d7fc642088fc04b8f95c70906a7e6f4997e688cd
Piotr Mikołajczyk <piomiko41@gmail.com>
2026-01-12 22:00:49 +0100

    Make the fields pub (they are simple data structures)

--- arbitrator/jit/src/lib.rs
+++ arbitrator/jit/src/lib.rs
@@ -59,27 +59,27 @@ pub enum InputMode {
 #[derive(Clone, Debug, Args)]
 pub struct LocalInput {
     #[clap(flatten)]
-    old_state: GlobalState,
+    pub old_state: GlobalState,
     #[clap(long, default_value = "0")]
-    delayed_inbox_position: u64,
+    pub delayed_inbox_position: u64,
     #[clap(long)]
-    inbox: Vec<PathBuf>,
+    pub inbox: Vec<PathBuf>,
     #[clap(long)]
-    delayed_inbox: Vec<PathBuf>,
+    pub delayed_inbox: Vec<PathBuf>,
     #[clap(long)]
-    preimages: Option<PathBuf>,
+    pub preimages: Option<PathBuf>,
 }
 
 #[derive(Clone, Debug, Args)]
 pub struct GlobalState {
     #[clap(long, value_parser = cli_parsing::parse_hex)]
-    last_block_hash: Bytes32,
+    pub last_block_hash: Bytes32,
     #[clap(long, value_parser = cli_parsing::parse_hex)]
-    last_send_root: Bytes32,
+    pub last_send_root: Bytes32,
     #[clap(long, default_value = "0")]
-    inbox_position: u64,
+    pub inbox_position: u64,
     #[clap(long, default_value = "0")]
-    position_within_message: u64,
+    pub position_within_message: u64,
 }
 
 mod cli_parsing {
