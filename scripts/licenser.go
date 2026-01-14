c554b2766f8e20140078f480349a481d93258692
Piotr Mikołajczyk <piomiko41@gmail.com>
2026-01-14 10:48:43 +0100

    Find files

--- scripts/licenser.go
+++ scripts/licenser.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+var supportedExtensions = []string{".go", ".rs"}
 
 func main() {
-	fmt.Println("Hello world")
+	flag.Parse()
+
+	files, err := getFiles()
+	if err != nil {
+		fmt.Printf("Fatal: could not list files: %v\n", err)
+		return
+	}
+	fmt.Printf("Found %d files\n", len(files))
+}
+
+func getFiles() ([]string, error) {
+	out, err := exec.Command("git", "ls-files").Output()
+	if err != nil {
+		return nil, err
+	}
+	var filtered []string
+	for _, line := range strings.Split(string(out), "\n") {
+		for _, extension := range supportedExtensions {
+			if strings.HasSuffix(line, extension) {
+				filtered = append(filtered, line)
+			}
+		}
+	}
+	return filtered, nil
 }
