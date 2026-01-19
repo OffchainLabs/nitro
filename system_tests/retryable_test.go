fd142c16a6130a234d848ccbde9d4c3564123335
Mikhail Rogachev <mrogachev@offchainlabs.com>
2026-01-19 13:06:23 +0100

    Update geth submodule and minor fix

--- system_tests/retryable_test.go
+++ system_tests/retryable_test.go
@@ -1296,8 +1296,6 @@ func TestL1FundedUnsignedTransaction(t *testing.T) {
 }
 
 func TestRetryableSubmissionAndRedeemFees(t *testing.T) {
-	t.Helper()
-
 	builder, delayedInbox, lookupL2Tx, ctx, teardown := retryableSetup(t, func(b *NodeBuilder) {
 		b.WithDatabase(rawdb.DBPebble)
 	})
