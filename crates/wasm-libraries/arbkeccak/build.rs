68fc9dc121c5cf683add4ffa3c7b0fc100fbf9a5
Piotr Mikołajczyk <piomiko41@gmail.com>
2026-01-20 14:30:34 +0100

    Update paths in build.rs

--- crates/wasm-libraries/arbkeccak/build.rs
+++ crates/wasm-libraries/arbkeccak/build.rs
@@ -3,6 +3,6 @@
 
 fn main() {
     // Tell Cargo that if the given file changes, to rerun this build script.
-    println!("cargo:rustc-link-search=../../target/lib-wasm/");
-    println!("cargo:rustc-link-search=../target/lib/");
+    println!("cargo:rustc-link-search=../../../target/lib-wasm/");
+    println!("cargo:rustc-link-search=../../../target/lib/");
 }
