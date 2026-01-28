f4dcfb488985f4674945f2a5e9258654b2b9d0f3
Piotr Mikołajczyk <piomiko41@gmail.com>
2026-01-28 15:39:54 +0100

    Markers

--- crates/validation/src/transfer/mod.rs
+++ crates/validation/src/transfer/mod.rs
@@ -5,14 +5,9 @@ mod receiver;
 mod sender;
 #[cfg(test)]
 mod tests;
+mod markers;
 
 pub use receiver::*;
 pub use sender::*;
 
 pub type IOResult<T> = Result<T, io::Error>;
-
-const SUCCESS: u8 = 0x0;
-const FAILURE: u8 = 0x1;
-// const PREIMAGE: u8 = 0x2; // legacy, not used
-const ANOTHER: u8 = 0x3;
-const READY: u8 = 0x4;
