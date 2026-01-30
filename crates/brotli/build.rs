c639cf762121f102c95d29948b4e0bcb0c4dbdf8
Piotr Mikołajczyk <piomiko41@gmail.com>
2026-01-30 16:55:42 +0100

    Remove old paths for linking

--- crates/brotli/build.rs
+++ crates/brotli/build.rs
@@ -9,10 +9,8 @@ fn main() {
 
     if target_arch.contains("wasm32") {
         println!("cargo:rustc-link-search=target/lib-wasm/");
-        println!("cargo:rustc-link-search=../../target/lib-wasm/");
     } else {
         println!("cargo:rustc-link-search=target/lib/");
-        println!("cargo:rustc-link-search=../../target/lib/");
     }
     println!("cargo:rustc-link-lib=static=brotlienc-static");
     println!("cargo:rustc-link-lib=static=brotlidec-static");
