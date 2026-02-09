a6a1a56ea4e8711c6716eb43496f9f65e01737ad
Diego Ximenes <dxmendes1@gmail.com>
2026-02-09 14:59:36 -0300

    Warns in case of error when unfiltering transactions

--- arbnode/delayed_sequencer.go
+++ arbnode/delayed_sequencer.go
@@ -291,8 +291,10 @@ func (d *DelayedSequencer) sequenceWithoutLockout(ctx context.Context, lastBlock
 					"txHashes", d.waitingForFilteredTx.TxHashes,
 					"delayedMsgIdx", d.waitingForFilteredTx.DelayedMsgIdx,
 					"waitedFor", time.Since(d.waitingForFilteredTx.FirstSeen))
-				// Ignore error since filterer RPC client could be missing
-				_ = d.exec.UnfilterTxHashes(d.waitingForFilteredTx.TxHashes)
+				err = d.exec.UnfilterTxHashes(d.waitingForFilteredTx.TxHashes)
+				if err != nil {
+					log.Warn("error unfiltering tx hashes", "err", err, "txHashes", d.waitingForFilteredTx.TxHashes)
+				}
 				d.waitingForFilteredTx = nil
 				delayedSequencerFilteredTxWaitSeconds.Update(0)
 			}
