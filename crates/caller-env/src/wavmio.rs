33f94b20a8cb12d6076306c21e8c68ff4b17aef5
Piotr Mikołajczyk <piomiko41@gmail.com>
2026-03-02 16:55:04 +0100

    Add get_global_state_bytes32 generic implementation

--- crates/caller-env/src/wavmio.rs
+++ crates/caller-env/src/wavmio.rs
@@ -1,6 +1,8 @@
 // Copyright 2026, Offchain Labs, Inc.
 // For license information, see https://github.com/OffchainLabs/nitro/blob/master/LICENSE.md
 
+use crate::{GuestPtr, MemAccess};
+
 /// Trait for accessing wavmio host state (globals, inbox, preimages).
 pub trait WavmState {
     fn get_u64_global(&self, idx: usize) -> Option<u64>;
@@ -10,4 +12,18 @@ pub trait WavmState {
     fn get_sequencer_message(&self, num: u64) -> Option<&[u8]>;
     fn get_delayed_message(&self, num: u64) -> Option<&[u8]>;
     fn get_preimage(&self, preimage_type: u8, hash: &[u8; 32]) -> Option<&[u8]>;
+}
+
+/// Reads 32-bytes of global state and writes to guest memory.
+pub fn get_global_state_bytes32<M: MemAccess, S: WavmState>(
+    mem: &mut M,
+    state: &S,
+    idx: u32,
+    out_ptr: GuestPtr,
+) -> Result<(), String> {
+    let Some(global) = state.get_bytes32_global(idx as usize) else {
+        return Err("global read out of bounds in wavmio.getGlobalStateBytes32".into());
+    };
+    mem.write_slice(out_ptr, &global[..]);
+    Ok(())
 }
