c9608004a0395a601ffcdb5eaecfa954623a895e
Piotr Mikołajczyk <piomiko41@gmail.com>
2026-03-03 18:37:31 +0100

    Update retry wrapper

--- validator/retry_wrapper/retry_wrapper.go
+++ validator/retry_wrapper/retry_wrapper.go
@@ -1,11 +1,13 @@
 // Copyright 2025-2026, Offchain Labs, Inc.
 // For license information, see https://github.com/OffchainLabs/nitro/blob/master/LICENSE.md
+
 package retry_wrapper
 
 import (
 	"context"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/log"
 
 	"github.com/offchainlabs/nitro/util/stopwaiter"
 	"github.com/offchainlabs/nitro/validator"
@@ -23,24 +25,36 @@ func NewValidationSpawnerRetryWrapper(spawner validator.ValidationSpawner) *Vali
 	}
 }
 
-// LaunchWithNAllowedAttempts launches the validation with a specified number of allowed attempts to retry in case of failure.
+// LaunchWithNAllowedAttempts launches the validation with a specified number of
+// allowed attempts to retry in case of failure. Timeout errors are retried
+// indefinitely and do not count against the retry limit, since they represent
+// transient conditions (e.g., CPU starvation, slow validation) rather than
+// genuine validation failures.
 func (v *ValidationSpawnerRetryWrapper) LaunchWithNAllowedAttempts(entry *validator.ValidationInput, moduleRoot common.Hash, allowedAttempts uint64) validator.ValidationRun {
 	promise := stopwaiter.LaunchPromiseThread(v, func(ctx context.Context) (validator.GoGlobalState, error) {
-		totalAttempts := uint64(0)
+		nonTimeoutAttempts := uint64(0)
 		for {
 			res, err := v.ValidationSpawner.Launch(entry, moduleRoot).Await(ctx)
-			totalAttempts++
 			// If the attempt is successful, return immediately
 			if err == nil {
 				return res, nil
 			}
-			// If we have reached the retry limit, return the error
-			if totalAttempts >= allowedAttempts {
-				return validator.GoGlobalState{}, err
-			}
 			// If the context is done, return the error
 			if ctx.Err() != nil {
 				return validator.GoGlobalState{}, ctx.Err()
+			}
+			// Timeout errors are retried indefinitely without counting against the retry limit.
+			if validator.IsTimeoutError(err) {
+				log.Warn("validation attempt timed out, retrying",
+					"err", err,
+					"moduleRoot", moduleRoot,
+				)
+				continue
+			}
+			// Non-timeout error: count against retry limit
+			nonTimeoutAttempts++
+			if nonTimeoutAttempts >= allowedAttempts {
+				return validator.GoGlobalState{}, err
 			}
 		}
 	})
