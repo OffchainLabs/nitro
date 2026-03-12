e638907afb9817f88b37cf538bf2ac71ed4b9f99
Piotr Mikołajczyk <piomiko41@gmail.com>
2026-03-12 16:09:12 +0100

    Chain config shouldn't be nil

--- cmd/nitro/init/init.go
+++ cmd/nitro/init/init.go
@@ -1108,7 +1108,7 @@ func GetExecutionParsedInitMsg(genesis *core.Genesis, genesisOverride *conf.Gene
 }
 
 func validateChainConfigCompatibility(parsedInitMessage *arbostypes.ParsedInitMessage, chainConfig *params.ChainConfig) error {
-	if parsedInitMessage.ChainConfig != nil && chainConfig != nil {
+	if parsedInitMessage.ChainConfig != nil {
 		if err := parsedInitMessage.ChainConfig.CheckCompatible(chainConfig, chainConfig.ArbitrumChainParams.GenesisBlockNum, 0); err != nil {
 			return fmt.Errorf("local chain config is incompatible with the chain config from the init message: %w", err)
 		}
