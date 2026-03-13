1f1cbfa7b57bb4633051dbfcdd6a19f07ac8bec0
Ganesh Vanahalli <gvanahalli@offchainlabs.com>
2026-03-13 16:53:08 -0500

    Apply suggestion from @ganeshvanahalli

--- arbnode/mel/runner/mel.go
+++ arbnode/mel/runner/mel.go
@@ -493,17 +493,12 @@ func (m *MessageExtractor) GetBatchCount() (uint64, error) {
 }
 
 func (m *MessageExtractor) GetDelayedAcc(seqNum uint64) (common.Hash, error) {
-	headState, err := m.melDB.GetHeadMelState()
+	delayedMsg, err := m.GetDelayedMessage(seqNum)
 	if err != nil {
 		return common.Hash{}, err
 	}
-	delayed, err := m.melDB.ReadDelayedMessage(headState, seqNum)
-	if err != nil {
-		return common.Hash{}, err
-	}
-	return delayed.AfterInboxAcc(), nil
+	return delayedMsg.AfterInboxAcc(), nil
 }
-
 func (m *MessageExtractor) CaughtUp() chan struct{} {
 	return m.caughtUpChan
 }
