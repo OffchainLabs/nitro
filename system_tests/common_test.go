97d7f8890e0c7eb988cada74f16dd6a1ae7557a2
Piotr Mikołajczyk <piomiko41@gmail.com>
2026-03-18 09:52:51 +0100

    Try adding tip limit 1 inside BuildL2*

--- system_tests/common_test.go
+++ system_tests/common_test.go
@@ -953,6 +953,15 @@ func (b *NodeBuilder) BuildL2OnL1(t *testing.T) func() {
 			_, err = EnsureTxSucceeded(b.ctx, b.L2.Client, tx)
 			Require(t, err)
 		}
+
+		if b.chainConfig.ArbitrumChainParams.InitialArbOSVersion >= params.ArbosVersion_60 {
+			arbowner, err := precompilesgen.NewArbOwner(common.HexToAddress("70"), b.L2.Client)
+			Require(t, err)
+			tx, err = arbowner.SetTipCapFloor(&debugAuth, big.NewInt(1))
+			Require(t, err)
+			_, err = EnsureTxSucceeded(b.ctx, b.L2.Client, tx)
+			Require(t, err)
+		}
 	}
 
 	return func() {
@@ -1032,6 +1041,15 @@ func (b *NodeBuilder) BuildL2(t *testing.T) func() {
 
 		_, err = EnsureTxSucceeded(b.ctx, b.L2.Client, tx)
 		Require(t, err)
+
+		if b.chainConfig.ArbitrumChainParams.InitialArbOSVersion >= params.ArbosVersion_60 {
+			arbowner, err := precompilesgen.NewArbOwner(common.HexToAddress("70"), b.L2.Client)
+			Require(t, err)
+			tx, err = arbowner.SetTipCapFloor(&debugAuth, big.NewInt(1))
+			Require(t, err)
+			_, err = EnsureTxSucceeded(b.ctx, b.L2.Client, tx)
+			Require(t, err)
+		}
 	}
 
 	StartWatchChanErr(t, b.ctx, fatalErrChan, b.L2.ConsensusNode)
