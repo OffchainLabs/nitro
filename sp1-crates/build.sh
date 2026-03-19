c93242d141217c7815c203da660448b05438ae0e
Igor Braga <5835477+bragaigor@users.noreply.github.com>
2026-03-19 14:34:49 -0400

    readd brotli and update upstream wasmer
    Signed-off-by: Igor Braga <5835477+bragaigor@users.noreply.github.com>

--- sp1-crates/build.sh
+++ sp1-crates/build.sh
@@ -18,24 +18,24 @@ if [ ! -d "$HOME/.sp1/riscv" ]; then
 fi
 
 # Build brotli for SP1
-# cp sp1-crates/brotli_cmake_patch.txt brotli/CMakeLists.txt
-# rm -rf target/build-sp1/brotli target/lib-sp1
-# mkdir -p target/build-sp1/brotli
-# cd target/build-sp1/brotli
-# cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
-#   -DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY \
-#   -DCMAKE_SYSTEM_NAME=Generic \
-#   -DCMAKE_C_COMPILER="$HOME"/.sp1/riscv/bin/riscv64-unknown-elf-gcc \
-#   -DCMAKE_C_FLAGS="-march=rv64im -mabi=lp64 -DBROTLI_BUILD_PORTABLE -mcmodel=medany -ffunction-sections -fdata-sections -fPIC" \
-#   -DCMAKE_AR="$HOME"/.sp1/riscv/bin/riscv64-unknown-elf-ar \
-#   -DCMAKE_RANLIB="$HOME"/.sp1/riscv/bin/riscv64-unknown-elf-ranlib \
-#   -DCMAKE_BUILD_TYPE=Release \
-#   -DCMAKE_INSTALL_PREFIX="$TOP"/target/lib-sp1 \
-#   -DBROTLI_DISABLE_TESTS=ON \
-#   "$TOP"/brotli
-# make
-# make install
-# cd "$TOP"
+cp sp1-crates/brotli_cmake_patch.txt brotli/CMakeLists.txt
+rm -rf target/build-sp1/brotli target/lib-sp1
+mkdir -p target/build-sp1/brotli
+cd target/build-sp1/brotli
+cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+  -DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY \
+  -DCMAKE_SYSTEM_NAME=Generic \
+  -DCMAKE_C_COMPILER="$HOME"/.sp1/riscv/bin/riscv64-unknown-elf-gcc \
+  -DCMAKE_C_FLAGS="-march=rv64im -mabi=lp64 -DBROTLI_BUILD_PORTABLE -mcmodel=medany -ffunction-sections -fdata-sections -fPIC" \
+  -DCMAKE_AR="$HOME"/.sp1/riscv/bin/riscv64-unknown-elf-ar \
+  -DCMAKE_RANLIB="$HOME"/.sp1/riscv/bin/riscv64-unknown-elf-ranlib \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX="$TOP"/target/lib-sp1 \
+  -DBROTLI_DISABLE_TESTS=ON \
+  "$TOP"/brotli
+make
+make install
+cd "$TOP"
 
 # Build nitro dependencies
 make build-replay-env test-go-deps
