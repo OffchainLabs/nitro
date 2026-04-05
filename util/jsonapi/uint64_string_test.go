3f5b37615c0ec22538be95a632c5535244c9a355
White-soldier-GOAT <raised_snide6i@icloud.com>
2026-04-05 17:15:02 +0300

    add basic tests for Uint64String marshaling
    noticed this type doesn't have any tests, starting with basic
    marshal/unmarshal to cover the common cases

---
+++ util/jsonapi/uint64_string_test.go
@@ -0,0 +1,92 @@
+// Copyright 2024-2026, Offchain Labs, Inc.
+// For license information, see https://github.com/OffchainLabs/nitro/blob/master/LICENSE.md
+
+package jsonapi
+
+import (
+	"testing"
+)
+
+func TestUint64String_MarshalJSON(t *testing.T) {
+	tests := []struct {
+		name     string
+		value    Uint64String
+		expected string
+	}{
+		{
+			name:     "zero value",
+			value:    Uint64String(0),
+			expected: `"0"`,
+		},
+		{
+			name:     "small positive value",
+			value:    Uint64String(123),
+			expected: `"123"`,
+		},
+		{
+			name:     "large value",
+			value:    Uint64String(9876543210),
+			expected: `"9876543210"`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result, err := tt.value.MarshalJSON()
+			if err != nil {
+				t.Fatalf("MarshalJSON() unexpected error: %v", err)
+			}
+			if string(result) != tt.expected {
+				t.Errorf("MarshalJSON() = %s, want %s", string(result), tt.expected)
+			}
+		})
+	}
+}
+
+func TestUint64String_UnmarshalJSON(t *testing.T) {
+	tests := []struct {
+		name      string
+		input     string
+		expected  Uint64String
+		shouldErr bool
+	}{
+		{
+			name:      "valid zero",
+			input:     `"0"`,
+			expected:  Uint64String(0),
+			shouldErr: false,
+		},
+		{
+			name:      "valid small number",
+			input:     `"456"`,
+			expected:  Uint64String(456),
+			shouldErr: false,
+		},
+		{
+			name:      "valid large number",
+			input:     `"9876543210"`,
+			expected:  Uint64String(9876543210),
+			shouldErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var result Uint64String
+			err := result.UnmarshalJSON([]byte(tt.input))
+
+			if tt.shouldErr {
+				if err == nil {
+					t.Errorf("UnmarshalJSON() expected error but got nil")
+				}
+			} else {
+				if err != nil {
+					t.Errorf("UnmarshalJSON() unexpected error: %v", err)
+				}
+				if result != tt.expected {
+					t.Errorf("UnmarshalJSON() = %d, want %d", result, tt.expected)
+				}
+			}
+		})
+	}
+}
