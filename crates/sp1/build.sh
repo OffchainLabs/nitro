2d1fdc2e14cc09f586e6ab32bda81db40e62543d
Piotr Mikołajczyk <piomiko41@gmail.com>
2026-04-23 13:37:06 +0200

    Move brotli buidling to Makefile

--- crates/sp1/build.sh
+++ crates/sp1/build.sh
@@ -28,25 +28,7 @@ if [ ! -d "$HOME/.sp1/riscv" ]; then
     "$HOME"/.sp1/riscv/bin/riscv64-unknown-elf-gcc --version
 fi
 
-# Build brotli for SP1
-cp crates/sp1/brotli_cmake_patch.txt brotli/CMakeLists.txt
-rm -rf target/build-sp1/brotli target/lib-sp1
-mkdir -p target/build-sp1/brotli
-cd target/build-sp1/brotli
-cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
-  -DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY \
-  -DCMAKE_SYSTEM_NAME=Generic \
-  -DCMAKE_C_COMPILER="$HOME"/.sp1/riscv/bin/riscv64-unknown-elf-gcc \
-  -DCMAKE_C_FLAGS="-march=rv64im -mabi=lp64 -DBROTLI_BUILD_PORTABLE -mcmodel=medany -ffunction-sections -fdata-sections -fPIC" \
-  -DCMAKE_AR="$HOME"/.sp1/riscv/bin/riscv64-unknown-elf-ar \
-  -DCMAKE_RANLIB="$HOME"/.sp1/riscv/bin/riscv64-unknown-elf-ranlib \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_INSTALL_PREFIX="$TOP"/target/lib-sp1 \
-  -DBROTLI_DISABLE_TESTS=ON \
-  "$TOP"/brotli
-make
-make install
-cd "$TOP"
+make -C "$SCRIPT_DIR" brotli
 
 # Build nitro dependencies
 make build-replay-env test-go-deps
