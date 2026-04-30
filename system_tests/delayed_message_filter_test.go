15629b5502fd89b4365b3c728efb0dc750959cf6
Diego Ximenes <dxmendes1@gmail.com>
2026-04-30 15:36:41 -0300

    Refactor CheckReportBlockNumberAndParentBlockHash to CheckCommonReportFields. Refactor SetupFilteringReport.

--- system_tests/delayed_message_filter_test.go
+++ system_tests/delayed_message_filter_test.go
@@ -15,6 +15,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/node"
 	"github.com/ethereum/go-ethereum/params"
 
 	"github.com/offchainlabs/nitro/arbnode"
@@ -36,13 +37,25 @@ import (
 	"github.com/offchainlabs/nitro/util/sqsclient"
 )
 
-func CheckReportBlockNumberAndParentBlockHash(t *testing.T, ctx context.Context, builder *NodeBuilder, report *addressfilter.FilteredTxReport) {
+// CheckCommonReportFields asserts FilteredTxReport fields common to every reporter (prechecker, delayed sequencer, regular sequencer).
+func CheckCommonReportFields(t *testing.T, ctx context.Context, builder *NodeBuilder, report *addressfilter.FilteredTxReport, tx *types.Transaction) {
 	t.Helper()
+	require.Equal(t, tx.Hash(), report.TxHash, "txHash")
+	require.NotEmpty(t, report.ID, "report ID must be set")
+	require.NotEmpty(t, report.TxRLP, "txRLP must be set")
+	require.NotEmpty(t, report.FilteredAddresses, "report must contain at least one filtered address")
+	require.Equal(t, builder.chainConfig.ChainID.Uint64(), report.ChainID, "chainID")
 	require.NotZero(t, report.BlockNumber, "block number shouldn't be genesis")
+	require.False(t, report.FilteredAt.IsZero(), "filteredAt must be populated")
+	require.WithinDuration(t, time.Now().UTC(), report.FilteredAt, 5*time.Minute, "filteredAt must be recent")
+
+	var decoded types.Transaction
+	require.NoError(t, decoded.UnmarshalBinary(report.TxRLP), "txRLP should decode")
+	require.Equal(t, tx.Hash(), decoded.Hash(), "decoded txRLP hash should match")
+
 	parentBlock, err := builder.L2.Client.BlockByNumber(ctx, big.NewInt(int64(report.BlockNumber-1))) // #nosec G115
 	require.NoError(t, err)
-	require.Equal(t, parentBlock.Hash(), report.ParentBlockHash,
-		"parent block hash should match hash of block N-1")
+	require.Equal(t, parentBlock.Hash(), report.ParentBlockHash, "parent block hash should match hash of block N-1")
 }
 
 // sendDelayedTx sends a transaction via L1 delayed inbox.
@@ -166,20 +179,19 @@ func createTransactionFiltererService(t *testing.T, ctx context.Context, builder
 	return transactionFiltererAPI
 }
 
-func SetupFilteringReport(t *testing.T, builder *NodeBuilder) *forwarder.MockExternalEndpoint {
+func SetupFilteringReport(t *testing.T) (*node.Node, *forwarder.MockExternalEndpoint) {
 	t.Helper()
 
 	queueClient := &sqsclient.MockQueueClient{}
 	externalEndpoint := forwarder.NewMockExternalEndpoint(t)
 
 	stack := filteringreportapi.NewTestStack(t, queueClient)
-	builder.execConfig.TransactionFiltering.FilteringReportRPCClient.URL = stack.HTTPEndpoint()
 
 	fwd := forwarder.NewTestForwarder(t, queueClient, externalEndpoint.URL())
 	fwd.Start(t.Context())
 	t.Cleanup(func() { fwd.StopAndWait() })
 
-	return externalEndpoint
+	return stack, externalEndpoint
 }
 
 // addTxHashToOnChainFilter adds a tx hash to the onchain filter via the precompile.
