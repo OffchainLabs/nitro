bfed2fddc4b983b75e5b05e30f6bafe78ef59bdd
ayaanoncrypto <1.06945712e+08+ayaanoncrypto@users.noreply.github.com>
2026-08-27 00:23:41 +0000

    linter: require exhaustive initialization for creatorResult

--- bold/testing/setup/rollup_stack.go
+++ bold/testing/setup/rollup_stack.go
@@ -1038,6 +1038,7 @@ func deployRollupCreator(
 		return nil, common.Address{}, common.Address{}, common.Address{}, common.Address{}, err
 	}
 
+	// lint:require-exhaustive-initialization
 	type creatorResult struct {
 		rollupCreatorAddress common.Address
 		rollupCreator        *rollupgen.RollupCreator
